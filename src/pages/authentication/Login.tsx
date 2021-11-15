9e7651dca990b02354b2121eb25966407c9d96fa
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-16 00:54:09 +0100

    Update login page

--- src/pages/authentication/Login.tsx
+++ src/pages/authentication/Login.tsx
@@ -9,6 +9,11 @@ import {
 import { endpoint } from "../../common/helpers";
 import { authenticate } from "../../common/stores/slices/user";
 import axios, { AxiosError, AxiosResponse } from "axios";
+import { Link } from "react-router-dom";
+import { LinkStyled } from "../../components/forms/Link";
+import { Input } from "../../components/forms/Input";
+import { Checkbox } from "../../components/forms/Checkbox";
+import { Button } from "../../components/forms/Button";
 
 export function Login() {
   const dispatch = useDispatch();
@@ -51,23 +56,52 @@ export function Login() {
 
   return (
     <>
-      {message}
-      <form onSubmit={form.handleSubmit}>
-        <input
-          type="email"
-          id="email"
-          placeholder="E-mail address"
-          onChange={form.handleChange}
-        />
-        <input
-          type="password"
-          id="password"
-          placeholder="Password"
-          onChange={form.handleChange}
-        />
-        <br />
-        <button type="submit">Log in with e-mail</button>
-      </form>
+      <div className="h-screen bg-gray-100 min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <Link to="/">
+            <img
+              className="mx-auto h-12 w-auto"
+              src="https://invoiceninja.github.io/assets/images/logo-rounded.png"
+              alt="Invoice Ninja"
+            />
+          </Link>
+        </div>
+
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <form className="space-y-6" onSubmit={form.handleSubmit}>
+              <Input
+                label="E-mail address"
+                type="email"
+                id="email"
+                placeholder="you@example.com"
+                onChange={form.handleChange}
+              />
+
+              <Input
+                label="Password"
+                type="password"
+                id="password"
+                onChange={form.handleChange}
+              />
+
+              <div className="flex items-center justify-between">
+                <Checkbox label="Keep me signed in" />
+
+                <div className="text-sm">
+                  <LinkStyled to="/forgot-password">
+                    Forgot your password?
+                  </LinkStyled>
+                </div>
+              </div>
+
+              <Button block>Sign in</Button>
+
+              {message}
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
