f97975aecb8ccec4b927b63590f0e2ef8722dc51
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-16 00:53:51 +0100

    Checkbox component

---
+++ src/components/forms/Checkbox.tsx
@@ -0,0 +1,31 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+interface Props {
+  label: string;
+  id?: string;
+  name?: string;
+}
+
+export function Checkbox(props: Props) {
+  return (
+    <div className="flex items-center">
+      <input
+        id={props.id}
+        name={props.name}
+        type="checkbox"
+        className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+      />
+      <label htmlFor={props.id} className="ml-2 block text-sm text-gray-900">
+        {props.label}
+      </label>
+    </div>
+  );
+}
