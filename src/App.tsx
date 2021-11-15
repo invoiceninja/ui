73f2551875aad5ca627461e96254fd95a59bf72d
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-15 12:41:31 +0100

    Update routes file path

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { routes } from "./routes";
+import { routes } from "./common/routes";
 
 export function App() {
   return <div className="App">{routes}</div>;
--- src/common/routes.tsx
+++ src/common/routes.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router";
-import { Login } from "./pages/authentication/Login";
-import { Index } from "./pages/Index";
+import { Login } from "../pages/authentication/Login";
+import { Index } from "../pages/Index";
 
 export const routes = (
   <Routes>
