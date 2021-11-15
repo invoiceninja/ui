4121d53b3c16e9136c5a0cbe44d2f082eb9ecdb9
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-16 00:53:46 +0100

    Button component

---
+++ src/components/forms/Button.tsx
@@ -0,0 +1,33 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import classNames from "classnames";
+
+interface Props {
+  block?: boolean;
+  children?: any;
+  type?: string | "button";
+}
+
+export function Button(props: any) {
+  return (
+    <button
+      type={props.type}
+      className={
+        "flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 " +
+        classNames({
+          "w-full": props.block,
+        })
+      }
+    >
+      {props.children}
+    </button>
+  );
+}
