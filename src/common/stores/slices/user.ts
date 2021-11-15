3da0fd161c51b66c4fe7b1209edf5e694cdbddca
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-15 13:26:01 +0100

    Define user's initial state

--- src/common/stores/slices/user.ts
+++ src/common/stores/slices/user.ts
@@ -1,7 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+interface UserState {
+  authenticated: boolean;
+}
+
+const initialState: UserState = {
+  authenticated: false,
+};
+
 export const userSlice = createSlice({
   name: "user",
-  initialState: {},
+  initialState,
   reducers: {},
 });
