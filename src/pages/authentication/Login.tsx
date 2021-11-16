f7a0119e58184a33b8fe824693d40040fdf04df0
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-16 10:49:30 +0100

    Fixes for login page spacing

--- src/pages/authentication/Login.tsx
+++ src/pages/authentication/Login.tsx
@@ -81,7 +81,7 @@ export function Login() {
         </div>
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <div className="bg-white mx-4 lg:mx-0 py-8 px-4 shadow rounded-lg sm:px-10">
             <form className="space-y-6" onSubmit={form.handleSubmit}>
               <div>
                 <Input
