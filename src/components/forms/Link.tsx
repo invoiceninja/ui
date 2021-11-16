f694091304308442c94059e70e83dc3c2a28b899
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-16 15:20:06 +0100

    Passing className & target to Link component

--- src/components/forms/Link.tsx
+++ src/components/forms/Link.tsx
@@ -13,15 +13,29 @@ import { Link } from "react-router-dom";
 interface Props {
   to: string;
   children?: any;
+  className?: string;
+  target?: string;
 }
 
 export function LinkStyled(props: Props) {
   return (
     <Link
       to={props.to}
-      className="font-medium text-indigo-600 hover:text-indigo-500"
+      className={`font-medium text-indigo-600 hover:text-indigo-500 ${props.className}`}
     >
       {props.children}
     </Link>
+  );
+}
+
+export function LinkExternal(props: Props) {
+  return (
+    <a
+      className={`font-medium text-indigo-600 hover:text-indigo-500 ${props.className}`}
+      href={props.to}
+      target={props.target ?? "_self"}
+    >
+      {props.children}
+    </a>
   );
 }
