2faf833891919e2fc31b108dee8cd834486837cb
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-17 14:14:28 +0100

    Products page: Scaffold

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -10,6 +10,9 @@
 
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import { Button } from "../components/forms/Button";
+import { Checkbox } from "../components/forms/Checkbox";
+import { Input } from "../components/forms/Input";
 import { Default } from "../components/layouts/Default";
 
 export function Products() {
@@ -31,11 +34,31 @@ export function Products() {
 
   return (
     <Default title={t("products")}>
-      <div className="py-2 align-middle inline-block min-w-full">
-        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-          <table className="min-w-full divide-y divide-gray-200">
+      <div className="flex flex-col">
+        <div className="flex items-center">
+          <div className="flex items-center space-x-2">
+            <Button variant="primary">{t("invoice")}</Button>
+            <Button variant="secondary">{t("archive")}</Button>
+          </div>
+          <div className="flex items-center mt-3 space-x-3 ml-4 -mt-1">
+            <Checkbox id="status.active" label={t("active")} />
+            <Checkbox id="status.archived" label={t("archived")} />
+            <Checkbox id="status.deleted" label={t("deleted")} />
+          </div>
+        </div>
+
+        <div className="mt-2 flex justify-between items-center">
+          <Input placeholder={t("filter")} />
+          <Button>{t("new_product")}</Button>
+        </div>
+      </div>
+
+      <div className="py-2 align-middle inline-block w-full mt-4">
+        <div className="overflow-hidden border border-gray-300 sm:rounded-lg overflow-x-auto">
+          <table className="w-full divide-y divide-gray-200">
             <thead className="bg-gray-50">
               <tr>
+                <th scope="col"></th>
                 <th
                   scope="col"
                   className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
@@ -68,6 +91,9 @@ export function Products() {
             <tbody className="bg-white divide-y divide-gray-200">
               {people.map((person) => (
                 <tr key={person.email}>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    <Checkbox />
+                  </td>
                   <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                     {person.name}
                   </td>
