a9272611df347a35dc124da9182fd636763f851f
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-17 10:36:23 +0100

    Improve formatting

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -8,17 +8,16 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
- import { useEffect } from "react";
- import { useTranslation } from "react-i18next";
- import { Default } from "../components/layouts/Default";
- 
- export function Dashboard() {
-   const [t] = useTranslation();
- 
-   useEffect(() => {
-     document.title = t("dashboard");
-   });
- 
-   return <Default title={t("dashboard")}>Lorem, ipsum dolor.</Default>;
- }
- 
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { Default } from "../components/layouts/Default";
+
+export function Dashboard() {
+  const [t] = useTranslation();
+
+  useEffect(() => {
+    document.title = t("dashboard");
+  });
+
+  return <Default title={t("dashboard")}>Lorem, ipsum dolor.</Default>;
+}
