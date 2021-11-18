31616450bb443bc0015a3b8acd6cf9baa3df8d0e
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-18 12:25:39 +0100

    Define Product dto

---
+++ src/common/dtos/product.tsx
@@ -0,0 +1,16 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+export interface Product {
+  id?: string;
+  product_key?: string;
+  notes?: string;
+  price?: string;
+}
