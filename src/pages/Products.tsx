fae8f7c5cbad474b06e50cbe6bed01b1a684c613
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-18 11:40:27 +0100

    Products: Extract actions into separate component

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -10,14 +10,13 @@
 
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Button } from "../components/forms/Button";
 import { Checkbox } from "../components/forms/Checkbox";
-import { Input } from "../components/forms/Input";
 import { Default } from "../components/layouts/Default";
 import { useProductsQuery } from "../common/queries/products";
 import classNames from "classnames";
 import { Link } from "react-router-dom";
 import Loading from "../components/icons/Loading";
+import { Actions } from "./products/Actions";
 
 export function Products() {
   useEffect(() => {
@@ -34,24 +33,7 @@ export function Products() {
 
   return (
     <Default title={t("products")}>
-      <div className="flex flex-col">
-        <div className="flex items-center">
-          <div className="flex items-center space-x-2">
-            <Button variant="primary">{t("invoice")}</Button>
-            <Button variant="secondary">{t("archive")}</Button>
-          </div>
-          <div className="flex items-center mt-3 space-x-3 ml-4 -mt-1">
-            <Checkbox id="status.active" label={t("active")} />
-            <Checkbox id="status.archived" label={t("archived")} />
-            <Checkbox id="status.deleted" label={t("deleted")} />
-          </div>
-        </div>
-
-        <div className="mt-2 flex justify-between items-center">
-          <Input placeholder={t("filter")} />
-          <Button>{t("new_product")}</Button>
-        </div>
-      </div>
+      <Actions />
 
       <div className="py-2 align-middle inline-block w-full mt-4">
         <div className="overflow-hidden border border-gray-300 sm:rounded-lg overflow-x-auto">
---
+++ src/pages/products/Actions.tsx
@@ -0,0 +1,39 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { useTranslation } from "react-i18next";
+import { Button } from "../../components/forms/Button";
+import { Checkbox } from "../../components/forms/Checkbox";
+import { Input } from "../../components/forms/Input";
+
+export function Actions() {
+  const [t] = useTranslation();
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex items-center">
+        <div className="flex items-center space-x-2">
+          <Button variant="primary">{t("invoice")}</Button>
+          <Button variant="secondary">{t("archive")}</Button>
+        </div>
+        <div className="flex items-center mt-3 space-x-3 ml-4 -mt-1">
+          <Checkbox id="status.active" label={t("active")} />
+          <Checkbox id="status.archived" label={t("archived")} />
+          <Checkbox id="status.deleted" label={t("deleted")} />
+        </div>
+      </div>
+
+      <div className="mt-2 flex justify-between items-center">
+        <Input placeholder={t("filter")} />
+        <Button>{t("new_product")}</Button>
+      </div>
+    </div>
+  );
+}
