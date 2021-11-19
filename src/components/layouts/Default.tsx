3421199e91eac30f3d44b2bbe365d53968b974d6
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-19 22:30:11 +0100

    Components clean up

--- src/components/layouts/Default.tsx
+++ src/components/layouts/Default.tsx
@@ -8,12 +8,9 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { useState } from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { Box } from "@mui/system";
-import { CssBaseline } from "@mui/material";
-import { AppBar } from "../appbar/AppBar";
 
 interface Props {
   title?: string;
@@ -24,25 +21,5 @@ export function Default(props: Props) {
   const location = useLocation();
   const [t] = useTranslation();
 
-  const navigation = [
-    {
-      name: "Dashboard",
-      href: "/app/dashboard",
-      current: location.pathname === "/app/dashboard",
-    },
-    {
-      name: "Products",
-      href: "/app/products",
-      current: location.pathname === "/app/products",
-    },
-  ];
-
-  const userNavigation = [{ name: t("logout"), href: "/app/logout" }];
-
-  return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar />
-    </Box>
-  );
+  return <div></div>;
 }
