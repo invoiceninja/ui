9ccbfa58b72413062aad2196fde975b9ca77da79
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-20 00:01:43 +0100

    Extract CommonProps interface

---
+++ src/common/interfaces/common-props.interface.ts
@@ -0,0 +1,16 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { ReactNode } from "react";
+
+export default interface CommonProps {
+  className?: string;
+  children?: ReactNode;
+}
