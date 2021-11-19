bc5b71dbef0e0b29ead17385c462cfda4a1deeb8
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-19 22:27:17 +0100

    Register: clean up

--- src/pages/authentication/Register.tsx
+++ src/pages/authentication/Register.tsx
@@ -8,9 +8,9 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import React, { useEffect, useState } from "react";
 import { AxiosError, AxiosResponse } from "axios";
 import { useFormik } from "formik";
-import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
 import { RegisterForm } from "../../common/dtos/authentication";
@@ -19,19 +19,6 @@ import { register } from "../../common/stores/slices/user";
 import { RegisterValidation } from "./common/ValidationInterface";
 import Logo from "../../resources/images/invoiceninja-logox53.png";
 import { Link } from "react-router-dom";
-import {
-  Checkbox,
-  FormControlLabel,
-  FormGroup,
-  Grid,
-  Stack,
-  TextField,
-  Typography,
-  Link as LinkComponent,
-  Alert,
-  Button
-} from "@mui/material";
-import { LoadingButton } from "@mui/lab";
 
 export function Register() {
   const [t] = useTranslation();
@@ -93,145 +80,27 @@ export function Register() {
 
   return (
     <>
-      <div
-        style={{
-          backgroundColor: "#3c3b3b",
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          padding: "28px 55px",
-        }}
-      >
-        <Link to="/">
-          <img width="231" src={Logo} alt="Invoice Ninja Logo" />
-        </Link>
-      </div>
+      <Link to="/">
+        <img width="231" src={Logo} alt="Invoice Ninja Logo" />
+      </Link>
 
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Grid item xs={10} sm={7} md={5} lg={4} xl={2}>
-          <Typography
-            sx={{ marginTop: 10, mx: "auto", textAlign: "center" }}
-            variant="h5"
-          >
-            {t("register_label")}
-          </Typography>
+      <form onSubmit={form.handleSubmit}>
+        <input id="email" type="email" onChange={form.handleChange} />
 
-          <form onSubmit={form.handleSubmit}>
-            <Stack sx={{ mt: 4 }}>
-              <TextField
-                error={Boolean(errors?.email)}
-                helperText={errors?.email}
-                id="email"
-                label={t("email_address")}
-                variant="outlined"
-                type="email"
-                onChange={form.handleChange}
-              />
+        <input id="password" type="password" onChange={form.handleChange} />
 
-              <TextField
-                sx={{ mt: 2 }}
-                error={Boolean(errors?.password)}
-                helperText={errors?.password}
-                id="password"
-                label={t("password")}
-                variant="outlined"
-                type="password"
-                onChange={form.handleChange}
-              />
+        <input
+          id="password_confirmation"
+          type="password"
+          onChange={form.handleChange}
+        />
 
-              <TextField
-                sx={{ mt: 2 }}
-                error={Boolean(errors?.password_confirmation)}
-                helperText={errors?.password_confirmation}
-                id="password_confirmation"
-                label={t("confirm_password")}
-                variant="outlined"
-                type="password"
-                onChange={form.handleChange}
-              />
+        <button type="submit">{t("register")}</button>
+      </form>
 
-              <FormGroup sx={{ mt: 2 }}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      required
-                      id="terms_of_service"
-                      onChange={form.handleChange}
-                    />
-                  }
-                  label={
-                    <div style={{ display: "flex", alignItems: "center" }}>
-                      <span>{t("i_agree_to_the")}</span>
-                      <LinkComponent
-                        target="_blank"
-                        href="https://www.invoiceninja.com/terms"
-                        style={{ marginLeft: 5 }}
-                      >
-                        {t("terms_of_service_link")}
-                      </LinkComponent>
-                    </div>
-                  }
-                />
+      {message && <div>{message}</div>}
 
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      required
-                      id="privacy_policy"
-                      onChange={form.handleChange}
-                    />
-                  }
-                  label={
-                    <div style={{ display: "flex", alignItems: "center" }}>
-                      <span>{t("i_agree_to_the")}</span>
-                      <LinkComponent
-                        target="_blank"
-                        href="https://www.invoiceninja.com/privacy-policy"
-                        style={{ marginLeft: 5 }}
-                      >
-                        {t("privacy_policy_link")}
-                      </LinkComponent>
-                    </div>
-                  }
-                />
-              </FormGroup>
-
-              <LoadingButton
-                type="submit"
-                loading={isFormBusy}
-                sx={{ mt: 2 }}
-                disableElevation
-                size="large"
-                variant="contained"
-              >
-                {t("register")}
-              </LoadingButton>
-            </Stack>
-          </form>
-
-          {message && (
-            <Alert sx={{ marginTop: 2 }} severity="error">
-              {message}
-            </Alert>
-          )}
-
-          <Stack
-            direction="column"
-            justifyContent="space-evenly"
-            alignItems="center"
-            sx={{ marginTop: 2 }}
-          >
-            <Button component={Link} to="/login">
-              {t("login")}
-            </Button>
-          </Stack>
-        </Grid>
-      </Grid>
+      <Link to="/login">{t("login")}</Link>
     </>
   );
 }
