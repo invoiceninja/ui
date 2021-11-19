9b52dd457a7a1564ebf7c22c728dc6d8807ce649
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-19 22:26:51 +0100

    Login: clean up

--- src/pages/authentication/Login.tsx
+++ src/pages/authentication/Login.tsx
@@ -8,8 +8,8 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import React, { useEffect, useState } from "react";
 import { useFormik } from "formik";
-import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import {
   AuthenticationTypes,
@@ -20,18 +20,8 @@ import { authenticate } from "../../common/stores/slices/user";
 import { AxiosError, AxiosResponse } from "axios";
 import { LoginValidation } from "./common/ValidationInterface";
 import { useTranslation } from "react-i18next";
-import { Link as RouterLink } from "react-router-dom";
+import { Link, Link as RouterLink } from "react-router-dom";
 import Logo from "../../resources/images/invoiceninja-logox53.png";
-
-import {
-  Grid,
-  Typography,
-  Stack,
-  TextField,
-  Button,
-  Alert,
-} from "@mui/material";
-import { LoadingButton } from "@mui/lab";
 
 export function Login() {
   const dispatch = useDispatch();
@@ -77,99 +67,26 @@ export function Login() {
 
   return (
     <>
-      <div
-        style={{
-          backgroundColor: "#3c3b3b",
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          padding: "28px 55px",
-        }}
-      >
-        <RouterLink to="/">
-          <img width="231" src={Logo} alt="Invoice Ninja Logo" />
-        </RouterLink>
-      </div>
+      <form onSubmit={form.handleSubmit}>
+        <input
+          // error={Boolean(errors?.email)}
+          // helperText={errors?.email}
+          id="email"
+          // label={t("email_address")}
+          type="email"
+          onChange={form.handleChange}
+        />
 
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Grid item xs={10} sm={7} md={5} lg={4} xl={2}>
-          <Typography
-            sx={{ marginTop: 10, mx: "auto", textAlign: "center" }}
-            variant="h4"
-          >
-            {t("account_login")}
-          </Typography>
+        <input id="password" type="password" onChange={form.handleChange} />
 
-          <form onSubmit={form.handleSubmit}>
-            <Stack sx={{ mt: 4 }}>
-              <TextField
-                error={Boolean(errors?.email)}
-                helperText={errors?.email}
-                id="email"
-                label={t("email_address")}
-                variant="outlined"
-                type="email"
-                onChange={form.handleChange}
-              />
+        <button type="submit">{t("login")}</button>
+      </form>
 
-              <TextField
-                error={Boolean(errors?.password)}
-                helperText={errors?.password}
-                sx={{ mt: 2 }}
-                id="password"
-                label={t("password")}
-                variant="outlined"
-                type="password"
-                onChange={form.handleChange}
-              />
+      {message && <div>{message}</div>}
 
-              <LoadingButton
-                type="submit"
-                loading={isFormBusy}
-                sx={{ mt: 2 }}
-                disableElevation
-                size="large"
-                variant="contained"
-              >
-                {t("login")}
-              </LoadingButton>
-            </Stack>
-          </form>
+      <Link to="/forgot-password">{t("forgot_password")}</Link>
 
-          {message && (
-            <Alert sx={{ marginTop: 2 }} severity="error">
-              {message}
-            </Alert>
-          )}
-
-          <Stack
-            direction="column"
-            justifyContent="space-evenly"
-            alignItems="center"
-            sx={{ marginTop: 2 }}
-          >
-            <Button component={RouterLink} to="/forgot-password">
-              {t("forgot_password")}
-            </Button>
-
-            {isHosted() && (
-              <Button
-                variant="outlined"
-                component={RouterLink}
-                to="/register"
-                sx={{ marginTop: 2 }}
-              >
-                {t("register_label")}
-              </Button>
-            )}
-          </Stack>
-        </Grid>
-      </Grid>
+      {isHosted() && <Link to="/register">{t("register_label")}</Link>}
     </>
   );
 }
