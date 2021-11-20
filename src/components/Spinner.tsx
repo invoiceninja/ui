9c96cef9e280102ce984b9825e6c2ee9d672b433
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-20 13:41:21 +0100

    Spinner: Fixes for strokeWidth

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -28,7 +28,7 @@ export function Spinner(props: Props) {
         cy="12"
         r="10"
         stroke="currentColor"
-        stroke-width="4"
+        strokeWidth="4"
       ></circle>
       <path
         className="opacity-75"
