fff097d33fa25b0893993f54c74724d9a821b1cb
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-20 01:04:20 +0100

    Add environment variable for page title

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv extends Readonly<Record<string, string>> {
   readonly VITE_API_URL: string;
   readonly VITE_IS_HOSTED: string;
   readonly VITE_ROUTER: string;
+  readonly VITE_APP_TITLE: string;
 }
 
 interface ImportMeta {
