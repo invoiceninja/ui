493077ba5a4bd32735c091fd9f179fa23cac59ac
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-20 01:05:21 +0100

    Spinner component

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,42 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import React from "react";
+
+interface Props {
+  variant?: "light" | "dark";
+}
+
+export function Spinner(props: Props) {
+  return (
+    <svg
+      className={`animate-spin h-6 w-6 ${
+        props.variant ?? "text-black"
+      }`}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        className="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        stroke-width="4"
+      ></circle>
+      <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      ></path>
+    </svg>
+  );
+}
