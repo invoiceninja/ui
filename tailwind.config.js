ea0e716797223184b24efdb398d8fb8a83cfd72b
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-20 13:49:32 +0100

    Set "Inter" as font UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -5,7 +7,11 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
