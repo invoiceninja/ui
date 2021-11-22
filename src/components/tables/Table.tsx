c51429931a24c97c84f71504e08e73788bfd6c0f
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-22 17:22:31 +0100

    Table component

---
+++ src/components/tables/Table.tsx
@@ -0,0 +1,32 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import React from "react";
+import CommonProps from "../../common/interfaces/common-props.interface";
+
+interface Props extends CommonProps {}
+
+export function Table(props: Props) {
+  return (
+    <div className="flex flex-col mt-2">
+      <div className="overflow-x-auto">
+        <div className="py-2 align-middle inline-block min-w-full">
+          <div className="overflow-hidden border border-gray-200 rounded border-b border-t">
+            <table className="min-w-full divide-y divide-gray-200">
+              {props.children}
+            </table>
+          </div>
+        </div>
+      </div>
+
+      {/*  */}
+    </div>
+  );
+}
