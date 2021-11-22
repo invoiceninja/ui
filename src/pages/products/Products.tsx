6e3bbbec108e24a0fb14e12139d069aeff7fe1da
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-22 16:32:40 +0100

    Scaffold products page

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -8,12 +8,22 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import React from "react";
-import { ChangeEvent, useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import { useProductsQuery } from "../../common/queries/products";
 import { RootState } from "../../common/stores/store";
+import { Button } from "../../components/forms/Button";
+import { Default } from "../../components/layouts/Default";
+import Select from "react-select";
+import { InputField } from "../../components/forms/InputField";
+import {
+  CheckSquare,
+  ChevronLeft,
+  ChevronRight,
+  PlusCircle,
+} from "react-feather";
+import { Link } from "../../components/forms/Link";
 
 export function Products() {
   useEffect(() => {
@@ -27,25 +37,177 @@ export function Products() {
   );
 
   const filter = useSelector((state: RootState) => state.products.filter);
-  const [selected, setSelected] = useState<string[]>([]);
+  const colors = useSelector((state: RootState) => state.settings.colors);
 
-  function checkboxHandler(event: ChangeEvent<HTMLInputElement>): void {
-    selected.includes(event.target.value)
-      ? setSelected(
-          selected.filter((value, index, array) => value !== event.target.value)
-        )
-      : setSelected([...selected, event.target.value]);
-  }
+  const people = [
+    {
+      name: "Jane Cooper",
+      title: "Regional Paradigm Technician",
+      role: "Admin",
+      email: "jane.cooper@example.com",
+    },
+  ];
 
-  const { data, isLoading } = useProductsQuery({
-    perPage: 10,
-    currentPage,
-    filter,
-  });
-  
-  console.log(data, isLoading);
+  const options = [
+    { value: "archive", label: "Active" },
+    { value: "archived", label: "Archived" },
+    { value: "deleted", label: "Deleted" },
+  ];
 
-  return <div>
-    Products page
-  </div>;
+  // const { data, isLoading } = useProductsQuery({
+  //   perPage: 10,
+  //   currentPage,
+  //   filter,
+  // });
+
+  return (
+    <Default title={t("products")}>
+      <div className="flex flex-col md:flex-row md:items-center md:justify-between">
+        <div className="flex items-center space-x-2">
+          <Button>
+            <span>{t("invoice")}</span>
+            <CheckSquare size="20" />
+          </Button>
+          <Button type="secondary">{t("archive")}</Button>
+          <Select
+            defaultValue={options[0]}
+            placeholder={t("status")}
+            options={options}
+            isMulti
+          />
+        </div>
+        <div className="mt-2 md:mt-0 flex items-center space-x-4">
+          <InputField id="filter" placeholder={t("filter")} />
+          <Button>
+            <span>{t("new_product")}</span>
+            <PlusCircle size="20" />
+          </Button>
+        </div>
+      </div>
+
+      <div className="flex flex-col mt-2">
+        <div className="overflow-x-auto">
+          <div className="py-2 align-middle inline-block min-w-full">
+            <div className="overflow-hidden border border-gray-200 rounded border-b border-t">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead style={{ backgroundColor: colors.primary }}>
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                    >
+                      Title
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                    >
+                      Email
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                    >
+                      Role
+                    </th>
+                    <th scope="col" className="relative px-6 py-3">
+                      <span className="sr-only">Edit</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {people.map((person, personIdx) => (
+                    <tr
+                      key={person.email}
+                      className={
+                        personIdx % 2 === 0 ? "bg-white" : "bg-gray-50"
+                      }
+                    >
+                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        {person.name}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                        {person.title}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                        {person.email}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                        {person.role}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                        <Link to="/edit">{t("edit")}</Link>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col md:flex-row md:items-center justify-between space-x-2 my-3">
+          <div className="flex justify-center md:justify-start items-center space-x-4">
+            <span className="text-sm">Showing 1 to 1 of 1 entires</span>
+            <div className="flex items-center space-x-2">
+              <select
+                id="location"
+                name="location"
+                className="block pl-3 pr-10 py-2 text-base border-gray-300 sm:text-sm rounded-md"
+                defaultValue="10"
+              >
+                <option>10</option>
+                <option>50</option>
+                <option>100</option>
+              </select>
+              <label
+                htmlFor="location"
+                className="block text-sm font-medium text-gray-700"
+              >
+                {t("rows")}
+              </label>
+            </div>
+          </div>
+          <nav className="flex justify-center md:justify-end my-4 md:my-0 items-center">
+            <a
+              className="py-1.5 px-2 bg-white border-b border-t rounded-l hover:bg-gray-50 border"
+              href="#"
+            >
+              <ChevronLeft />
+            </a>
+            <a
+              className="py-1.5 px-4 bg-white border-b border-t hover:bg-gray-50"
+              href="#"
+            >
+              1
+            </a>
+            <a
+              className="py-1.5 px-4 bg-white border-b border-t bg-green-700 text-white"
+              href="#"
+            >
+              2
+            </a>
+            <a
+              className="py-1.5 px-4 bg-white border-b border-t hover:bg-gray-50"
+              href="#"
+            >
+              3
+            </a>
+            <a
+              className="py-1.5 px-2 bg-white border-b border-t border-r rounded-r hover:bg-gray-50"
+              href="#"
+            >
+              <ChevronRight />
+            </a>
+          </nav>
+        </div>
+      </div>
+    </Default>
+  );
 }
