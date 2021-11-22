9e3ea14a4cbeeba1e75509ddfff861fcceced4ea
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-22 17:22:34 +0100

    Tbody component

---
+++ src/components/tables/Tbody.tsx
@@ -0,0 +1,18 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import React from "react";
+import CommonProps from "../../common/interfaces/common-props.interface";
+
+interface Props extends CommonProps {}
+
+export function Tbody(props: Props) {
+  return <tbody>{props.children}</tbody>;
+}
