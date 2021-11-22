37e11823c5e3e639ebe9da8dfe167f9f953aa70a
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-22 17:22:44 +0100

    Thead component

---
+++ src/components/tables/Thead.tsx
@@ -0,0 +1,26 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import React from "react";
+import { useSelector } from "react-redux";
+import CommonProps from "../../common/interfaces/common-props.interface";
+import { RootState } from "../../common/stores/store";
+
+interface Props extends CommonProps {}
+
+export function Thead(props: Props) {
+  const colors = useSelector((state: RootState) => state.settings.colors);
+
+  return (
+    <thead style={{ backgroundColor: colors.primary }}>
+      <tr>{props.children}</tr>
+    </thead>
+  );
+}
