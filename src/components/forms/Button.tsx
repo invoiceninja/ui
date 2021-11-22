9b66675cec02aa2f43d6e35cf6ab6abc592280d1
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-22 16:32:20 +0100

    Add secondary variant to Button

--- src/components/forms/Button.tsx
+++ src/components/forms/Button.tsx
@@ -9,6 +9,7 @@
  */
 
 import classNames from "classnames";
+import { produceWithPatches } from "immer";
 import React, { ReactNode } from "react";
 import { useSelector } from "react-redux";
 import CommonProps from "../../common/interfaces/common-props.interface";
@@ -19,21 +20,31 @@ interface Props extends CommonProps {
   children?: ReactNode;
   variant?: "block";
   disabled?: boolean;
+  type?: "primary" | "secondary";
 }
 
+const defaultProps: Props = {
+  type: "primary",
+};
+
 export function Button(props: Props) {
+  props = { ...defaultProps, ...props };
+
   const colors = useSelector((state: RootState) => state.settings.colors);
+
   const css: React.CSSProperties = {
-    backgroundColor: colors.primary,
+    backgroundColor: props.type === "primary" ? colors.primary : "white",
   };
 
   return (
     <button
       disabled={props.disabled}
       className={classNames(
-        `inline-flex justify-center py-2 px-3 text-white rounded ${props.className}`,
+        `inline-flex items-center space-x-2 justify-center py-2 tracking-wide px-4 rounded text-sm ${props.className}`,
         {
           "w-full": props.variant === "block",
+          "text-white": props.type == "primary",
+          "text-gray-900 border border-gray-300": props.type == "secondary",
         }
       )}
       style={css}
