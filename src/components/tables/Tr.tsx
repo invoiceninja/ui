66f0cac35c2fc0ff9193e067be18e75240409384
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-22 17:22:47 +0100

    Tr component

---
+++ src/components/tables/Tr.tsx
@@ -0,0 +1,18 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import React from "react";
+import CommonProps from "../../common/interfaces/common-props.interface";
+
+interface Props extends CommonProps {}
+
+export function Tr(props: Props) {
+  return <tr className="bg-white hover:bg-gray-50">{props.children}</tr>;
+}
