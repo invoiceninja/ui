405aed0bb6f3567a9ede7784e460733adcfa5f75
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-24 17:03:40 +0100

    Fixes for fetcher

--- src/common/queries/products.tsx
+++ src/common/queries/products.tsx
@@ -14,19 +14,16 @@ import { Params } from "./common/params.interface";
 
 export function useProductsQuery(params: Params) {
   return useSWR(
-    [
-      "GET",
-      endpoint(
-        "/api/v1/products?per_page=:perPage&page=:currentPage&filter=:filter&status=:status&sort=:sort",
-        {
-          perPage: params.perPage,
-          currentPage: params.currentPage,
-          filter: params.filter,
-          status: params.status,
-          sort: params.sort ?? "id|asc",
-        }
-      ),
-    ],
+    endpoint(
+      "/api/v1/products?per_page=:perPage&page=:currentPage&filter=:filter&status=:status&sort=:sort",
+      {
+        perPage: params.perPage,
+        currentPage: params.currentPage,
+        filter: params.filter,
+        status: params.status,
+        sort: params.sort ?? "id|asc",
+      }
+    ),
     fetcher
   );
 }
