95470c9073e4f530390af0de8b2d41837727b89c
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-24 12:11:23 +0100

    Scaffold token slice

---
+++ src/common/stores/slices/token.ts
@@ -0,0 +1,31 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { createSlice } from "@reduxjs/toolkit";
+
+interface TokenState {
+  api: any;
+}
+
+const initialState: TokenState = {
+  api: {},
+};
+
+export const tokenSlice = createSlice({
+  name: "token",
+  initialState,
+  reducers: {
+    updateToken: (state, action: any) => {
+      state.api = action.payload;
+    },
+  },
+});
+
+export const { updateToken } = tokenSlice.actions;
