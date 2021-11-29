813a5c9d7d32a7ea23b012520425b72bcb3f4a0f
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-29 16:08:18 +0100

    Drop safelist for purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   purge: {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    safelist: ["bg-green-800"],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
