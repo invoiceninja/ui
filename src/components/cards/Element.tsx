b523f5e5df5af7108ae682c935c646ff62284229
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-01 14:06:27 +0100

    Extract card element component

---
+++ src/components/cards/Element.tsx
@@ -0,0 +1,26 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import React, { ReactNode } from 'react';
+
+export default function Element(props: {
+  leftSide?: ReactNode;
+  rightSide?: ReactNode;
+  children: ReactNode;
+}) {
+  return (
+    <div className="py-3 sm:py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 flex flex-col lg:flex-row lg:items-center">
+      <dt className="text-sm font-medium text-gray-500">{props.leftSide}</dt>
+      <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+        {props.children}
+      </dd>
+    </div>
+  );
+}
