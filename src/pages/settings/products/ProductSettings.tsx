7721d48fdd8a16962f9a05d52f10272e3f60a77e
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-02 15:30:33 +0100

    Product settings

--- src/pages/settings/products/ProductSettings.tsx
+++ src/pages/settings/products/ProductSettings.tsx
@@ -10,6 +10,8 @@
 
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Card, Element } from '../../../components/cards';
+import Toggle from '../../../components/forms/Toggle';
 import { Settings } from '../../../components/layouts/Settings';
 
 export function ProductSettings() {
@@ -21,5 +23,63 @@ export function ProductSettings() {
     )}`;
   });
 
-  return <Settings title={t('product_settings')}>{/*  */}</Settings>;
+  return (
+    <Settings title={t('product_settings')}>
+      <Card withSaveButton title={t('Settings')}>
+        <Element
+          leftSide={t('show_product_discount')}
+          leftSideHelp={t('show_product_discount_help')}
+          pushContentToRight
+        >
+          <Toggle />
+        </Element>
+        <Element
+          leftSide={t('show_product_cost')}
+          leftSideHelp={t('show_product_cost_help')}
+          pushContentToRight
+        >
+          <Toggle />
+        </Element>
+        <Element
+          leftSide={t('show_product_quantity')}
+          leftSideHelp={t('show_product_quantity_help')}
+          pushContentToRight
+        >
+          <Toggle />
+        </Element>
+        <Element
+          leftSide={t('default_quantity')}
+          leftSideHelp={t('default_quantity_help')}
+          pushContentToRight
+        >
+          <Toggle />
+        </Element>
+
+        <div className="py-4 border-b"></div>
+
+        <Element
+          className="mt-4"
+          leftSide={t('fill_products')}
+          leftSideHelp={t('fill_products_help')}
+          pushContentToRight
+        >
+          <Toggle />
+        </Element>
+        <Element
+          leftSide={t('update_products')}
+          leftSideHelp={t('update_products_help')}
+          pushContentToRight
+        >
+          <Toggle />
+        </Element>
+        <Element
+          leftSide={t('convert_products')}
+          leftSideHelp={t('convert_products_help')}
+          pushContentToRight
+        >
+          <Toggle />
+        </Element>
+      </Card>
+    </Settings>
+  );
 }
