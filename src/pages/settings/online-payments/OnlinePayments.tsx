f3b3baa644d49c9019cb168d1b470183e0eb6a6a
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-02 14:46:44 +0100

    Online payments page

--- src/pages/settings/online-payments/OnlinePayments.tsx
+++ src/pages/settings/online-payments/OnlinePayments.tsx
@@ -10,7 +10,18 @@
 
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Card, Element } from '../../../components/cards';
+import { Button, Checkbox, SelectField } from '../../../components/forms';
 import { Settings } from '../../../components/layouts/Settings';
+import {
+  Pagination,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '../../../components/tables';
 
 export function OnlinePayments() {
   const [t] = useTranslation();
@@ -21,5 +32,63 @@ export function OnlinePayments() {
     )}`;
   });
 
-  return <Settings title={t('online_payments')}>{/*  */}</Settings>;
+  return (
+    <Settings title={t('online_payments')}>
+      <Card withSaveButton title={t('settings')}>
+        <Element leftSide={t('auto_bill_on')}>
+          <SelectField>
+            <option value="due_date">{t('due_date')}</option>
+            <option value="send_date">{t('send_date')}</option>
+          </SelectField>
+        </Element>
+        <Element leftSide={t('use_available_credits')}>
+          <SelectField>
+            <option value="enabled">{t('enabled')}</option>
+            <option value="show_option">{t('show_option')}</option>
+            <option value="off">{t('off')}</option>
+          </SelectField>
+        </Element>
+        <Element leftSide={t('allow_over_payment')}>
+          <Checkbox
+            label={t('allow_over_payment_help')}
+            id="allow_over_payment"
+          />
+        </Element>
+        <Element leftSide={t('allow_under_payment')}>
+          <Checkbox
+            label={t('allow_under_payment_help')}
+            id="allow_under_payment"
+          />
+        </Element>
+      </Card>
+
+      <div className="flex justify-end mt-8">
+        <Button to="/gateways/create">{t('add_gateway')}</Button>
+      </div>
+
+      <Table>
+        <Thead>
+          <Th>{t('gateway')}</Th>
+          <Th>{t('limits')}</Th>
+          <Th>{t('fees')}</Th>
+          <Th>{t('action')}</Th>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>PayPal Express</Td>
+            <Td>No Limits</Td>
+            <Td>Fees are disabled</Td>
+            <Td></Td>
+          </Tr>
+        </Tbody>
+      </Table>
+      
+      <Pagination
+        currentPage={1}
+        onPageChange={() => {}}
+        onRowsChange={() => {}}
+        totalPages={1}
+      />
+    </Settings>
+  );
 }
