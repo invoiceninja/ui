4ce6811db7fe8171d208f888863e40677d80c3df
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-02 15:30:17 +0100

    Element: Support for pushing elements to the end

--- src/components/cards/Element.tsx
+++ src/components/cards/Element.tsx
@@ -8,12 +8,13 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import classNames from 'classnames';
 import React, { ReactNode } from 'react';
 
 export function Element(props: {
   leftSide?: ReactNode;
   leftSideHelp?: ReactNode;
-  rightSide?: ReactNode;
+  pushContentToRight?: boolean;
   children: ReactNode;
   className?: any;
 }) {
@@ -21,13 +22,23 @@ export function Element(props: {
     <div
       className={`py-3 sm:py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 flex flex-col lg:flex-row lg:items-center ${props.className}`}
     >
-      <dt className="text-sm font-medium text-gray-500 flex flex-col">
-        <span className="text-gray-600">{props.leftSide}</span>
+      <dt className="text-sm text-gray-500 flex flex-col">
+        <span className="text-gray-500 font-medium ">{props.leftSide}</span>
         {props.leftSideHelp && (
-          <span className="text-gray-400 mt-2">{props.leftSideHelp}</span>
+          <span className="text-gray-400 mt-2 text-xs">
+            {props.leftSideHelp}
+          </span>
         )}
       </dt>
-      <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+      <dd
+        className={classNames(
+          'mt-4 text-sm text-gray-900 sm:mt-0 sm:col-span-2',
+          {
+            'flex flex-col sm:flex-row sm:justify-end':
+              props.pushContentToRight,
+          }
+        )}
+      >
         {props.children}
       </dd>
     </div>
