2fe883192b1e202b8d32feaab88dc8e13647b737
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-06 15:29:10 +0100

    Organise custom label by categories

--- src/pages/settings/custom-fields/CustomFields.tsx
+++ src/pages/settings/custom-fields/CustomFields.tsx
@@ -10,7 +10,7 @@
 
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Card } from '../../../components/cards';
+import { Card, ClickableElement } from '../../../components/cards';
 import { Settings } from '../../../components/layouts/Settings';
 import { Field } from './components';
 
@@ -21,14 +21,27 @@ export function CustomFields() {
     document.title = `${import.meta.env.VITE_APP_TITLE}: ${t('custom_fields')}`;
   });
 
+  const modules = [
+    { label: t('company'), link: '/settings/custom_fields/company' },
+    { label: t('clients'), link: '/settings/custom_fields/clients' },
+    { label: t('products'), link: '/settings/custom_fields/products' },
+    { label: t('invoices'), link: '/settings/custom_fields/invoices' },
+    { label: t('payments'), link: '/settings/custom_fields/payments' },
+    { label: t('projects'), link: '/settings/custom_fields/projects' },
+    { label: t('tasks'), link: '/settings/custom_fields/tasks' },
+    { label: t('vendors'), link: '/settings/custom_fields/vendors' },
+    { label: t('expenses'), link: '/settings/custom_fields/expenses' },
+    { label: t('users'), link: '/settings/custom_fields/users' },
+  ];
+
   return (
     <Settings title={t('custom_fields')}>
-      <Card title={t('company')}>
-        {['company1', 'company2', 'company3', 'company4'].map((field) => {
-          return (
-            <Field key={field} id={field} placeholder={t('company_field')} />
-          );
-        })}
+      <Card title={t('custom_fields')}>
+        {modules.map((module, index) => (
+          <ClickableElement key={index} to={module.link}>
+            {module.label}
+          </ClickableElement>
+        ))}
       </Card>
     </Settings>
   );
