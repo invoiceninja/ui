138a19d3bc7da8002c97e82f5ea7a82d20423d40
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-06 16:22:58 +0100

    Surchages custom fields

--- src/pages/settings/custom-fields/invoices/Invoices.tsx
+++ src/pages/settings/custom-fields/invoices/Invoices.tsx
@@ -10,7 +10,9 @@
 
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Card } from '../../../../components/cards';
+import { Card, Element } from '../../../../components/cards';
+import { InputField } from '../../../../components/forms';
+import Toggle from '../../../../components/forms/Toggle';
 import { Settings } from '../../../../components/layouts/Settings';
 import { Field } from '../components';
 
@@ -29,9 +31,15 @@ export function Invoices() {
         ))}
       </Card>
 
-      <Card title={`${t('custom_fields')}: ${t('contacts')}`}>
-        {['contact1', 'contact2', 'contact3', 'contact4'].map((field) => (
-          <Field field={field} placeholder={t('contact_field')} />
+      <Card>
+        {['surchage1', 'surchage2', 'surchage3', 'surchage4'].map((field) => (
+          <Element
+            leftSide={
+              <InputField id={field} placeholder={t('surcharge_field')} />
+            }
+          >
+            <Toggle label={t('charge_taxes')} />
+          </Element>
         ))}
       </Card>
     </Settings>
