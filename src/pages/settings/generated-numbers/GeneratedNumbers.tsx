f227b8f0bd0c1e855dd0aec5e88491fb74cf36ad
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-07 12:58:12 +0100

    Scaffold list of all modules for generated numbers

--- src/pages/settings/generated-numbers/GeneratedNumbers.tsx
+++ src/pages/settings/generated-numbers/GeneratedNumbers.tsx
@@ -8,16 +8,70 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Card } from '../../../components/cards';
+import { Card, ClickableElement } from '../../../components/cards';
 import { Settings } from '../../../components/layouts/Settings';
+import { Settings as SettingsComponent } from './components';
 
 export function GeneratedNumbers() {
   const [t] = useTranslation();
 
+  useEffect(() => {
+    document.title = `${import.meta.env.VITE_APP_TITLE}: ${t(
+      'generated_numbers'
+    )}`;
+  });
+
   return (
     <Settings title={t('generated_numbers')}>
-      <Card title={t('settings')}>{/*  */}</Card>
+      <SettingsComponent />
+
+      <Card>
+        <ClickableElement to="/settings/generated_numbers/clients">
+          {t('clients')}
+        </ClickableElement>
+
+        <ClickableElement to="/settings/generated_numbers/invoices">
+          {t('invoices')}
+        </ClickableElement>
+
+        <ClickableElement to="/settings/generated_numbers/recurring_invoices">
+          {t('recurring_invoices')}
+        </ClickableElement>
+
+        <ClickableElement to="/settings/generated_numbers/payments">
+          {t('payments')}
+        </ClickableElement>
+
+        <ClickableElement to="/settings/generated_numbers/quotes">
+          {t('quotes')}
+        </ClickableElement>
+
+        <ClickableElement to="/settings/generated_numbers/credits">
+          {t('credits')}
+        </ClickableElement>
+
+        <ClickableElement to="/settings/generated_numbers/projects">
+          {t('projects')}
+        </ClickableElement>
+
+        <ClickableElement to="/settings/generated_numbers/tasks">
+          {t('tasks')}
+        </ClickableElement>
+
+        <ClickableElement to="/settings/generated_numbers/vendors">
+          {t('vendors')}
+        </ClickableElement>
+
+        <ClickableElement to="/settings/generated_numbers/expenses">
+          {t('expenses')}
+        </ClickableElement>
+
+        <ClickableElement to="/settings/generated_numbers/recurring_expenses">
+          {t('recurring_expenses')}
+        </ClickableElement>
+      </Card>
     </Settings>
   );
 }
---
+++ src/pages/settings/generated-numbers/components/Settings.tsx
@@ -0,0 +1,53 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { useTranslation } from 'react-i18next';
+import { Card, Element } from '../../../../components/cards';
+import { InputField, SelectField } from '../../../../components/forms';
+import Toggle from '../../../../components/forms/Toggle';
+
+export function Settings() {
+  const [t] = useTranslation();
+
+  return (
+    <Card title={t('settings')}>
+      <Element leftSide={t('number_padding')}>
+        <SelectField>
+          <option value="0001">0001</option>
+        </SelectField>
+      </Element>
+
+      <Element leftSide={t('generate_number')}>
+        <SelectField>
+          <option value="when_saved">{t('when_saved')}</option>
+          <option value="when_sent">{t('when_sent')}</option>
+        </SelectField>
+      </Element>
+
+      <Element leftSide={t('recurring_prefix')}>
+        <InputField id="recurring_prefix" />
+      </Element>
+
+      <Element leftSide={t('shared_invoice_quote_counter')}>
+        <Toggle />
+      </Element>
+
+      <Element leftSide={t('shared_invoice_credit_counter')}>
+        <Toggle />
+      </Element>
+
+      <Element leftSide={t('reset_counter')}>
+        <SelectField>
+          <option value="never">{t('never')}</option>
+        </SelectField>
+      </Element>
+    </Card>
+  );
+}
