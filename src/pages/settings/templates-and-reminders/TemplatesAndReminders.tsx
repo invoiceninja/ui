5d98b22c48f3185545d4cc6f3a5e6e259ba2312e
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-07 16:37:25 +0100

    Templates and reminders

--- src/pages/settings/templates-and-reminders/TemplatesAndReminders.tsx
+++ src/pages/settings/templates-and-reminders/TemplatesAndReminders.tsx
@@ -8,7 +8,8 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { Card } from '@invoiceninja/cards';
+import { Card, ClickableElement, Element } from '@invoiceninja/cards';
+import { InputField, SelectField, Textarea } from '@invoiceninja/forms';
 import { Settings } from 'components/layouts/Settings';
 import { useTranslation } from 'react-i18next';
 
@@ -16,10 +17,37 @@ export function TemplatesAndReminders() {
   const [t] = useTranslation();
 
   return (
-      <Settings title={t('templates_and_reminders')}>
-          <Card title={t('edit')}>
-              {/*  */}
-          </Card>
-      </Settings>
-  )
+    <Settings title={t('templates_and_reminders')}>
+      <Card title={t('edit')}>
+        <Element leftSide={t('template')}>
+          <SelectField>
+            <option value="invoices">{t('invoices')}</option>
+          </SelectField>
+        </Element>
+
+        <Element leftSide={t('subject')}>
+          <InputField id="subject" />
+        </Element>
+
+        <Element leftSide={t('body')}>
+          <Textarea />
+        </Element>
+
+        <ClickableElement>{t('variables')}</ClickableElement>
+      </Card>
+
+      <Card title={t('preview')}>
+        <div className="flex flex-col md:flex-row md:items-center px-5">
+          <button
+            type="button"
+            className="relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            <span className="mt-2 block text-sm font-medium text-gray-900">
+              Preview will show here
+            </span>
+          </button>
+        </div>
+      </Card>
+    </Settings>
+  );
 }
