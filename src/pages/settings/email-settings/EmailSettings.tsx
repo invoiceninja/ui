cb55403ccd96acf39e446f91111e42ea3504b4d3
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-07 14:33:46 +0100

    Email settings page

--- src/pages/settings/email-settings/EmailSettings.tsx
+++ src/pages/settings/email-settings/EmailSettings.tsx
@@ -10,6 +10,9 @@
 
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Card, Element } from '../../../components/cards';
+import { InputField, SelectField, Textarea } from '../../../components/forms';
+import Toggle from '../../../components/forms/Toggle';
 import { Settings } from '../../../components/layouts/Settings';
 
 export function EmailSettings() {
@@ -21,5 +24,66 @@ export function EmailSettings() {
     )}`;
   });
 
-  return <Settings title={t('email_settings')}>{/*  */}</Settings>;
+  return (
+    <Settings title={t('email_settings')}>
+      <Card title={t('settings')}>
+        <Element leftSide={t('send_from_gmail')}>
+          <Toggle />
+        </Element>
+
+        <div className="pt-4 border-b"></div>
+
+        <Element className="mt-4" leftSide={t('from_name')}>
+          <InputField id="from_name" />
+        </Element>
+
+        <Element leftSide={t('reply_to_name')}>
+          <InputField id="reply_to_name" />
+        </Element>
+
+        <Element leftSide={t('reply_to_email')}>
+          <InputField id="reply_to_email" />
+        </Element>
+
+        <Element leftSide={t('bcc_email')}>
+          <InputField id="bcc_email" />
+        </Element>
+
+        <Element leftSide={t('send_time')}>
+          <SelectField>
+            <option value="7am">7 AM</option>
+          </SelectField>
+        </Element>
+
+        <div className="pt-4 border-b"></div>
+
+        <Element className="mt-4" leftSide={t('email_design')}>
+          <SelectField>
+            <option value="plain">{t('plain')}</option>
+            <option value="light">{t('light')}</option>
+            <option value="dark">{t('dark')}</option>
+            <option value="custom">{t('custom')}</option>
+          </SelectField>
+        </Element>
+
+        <Element leftSide={t('signature')}>
+          <Textarea />
+        </Element>
+
+        <div className="pt-4 border-b"></div>
+
+        <Element className="mt-4" leftSide={t('attach_pdf')}>
+          <Toggle />
+        </Element>
+
+        <Element leftSide={t('attach_documents')}>
+          <Toggle />
+        </Element>
+
+        <Element leftSide={t('attach_ubl')}>
+          <Toggle />
+        </Element>
+      </Card>
+    </Settings>
+  );
 }
