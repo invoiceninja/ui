257fb941c8efb41ebf76d296d3b3bdc3bf86eab3
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-08 15:08:44 +0100

    Wrap card into Form

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -14,43 +14,48 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../common/stores/store';
 import { Button } from '../forms';
 
-export function Card(props: {
+interface Props {
   children: ReactNode;
   title?: string;
   description?: string;
   withSaveButton?: boolean;
+  onFormSubmit?: any;
   onSaveClick?: any;
   saveButtonLabel?: string;
-}) {
+}
+
+export function Card(props: Props) {
   const colors = useSelector((state: RootState) => state.settings.colors);
   const [t] = useTranslation();
 
   return (
     <div className="bg-white shadow overflow-hidden rounded">
-      {props.title && (
-        <div className="px-4 py-5 sm:px-6">
-          <h3 className="text-lg leading-6 font-medium text-gray-900">
-            {props.title}
-          </h3>
-          {props.description && (
-            <p className="mt-1 max-w-2xl text-sm text-gray-500">
-              {props.description}
-            </p>
-          )}
+      <form onSubmit={props.onFormSubmit}>
+        {props.title && (
+          <div className="px-4 py-5 sm:px-6">
+            <h3 className="text-lg leading-6 font-medium text-gray-900">
+              {props.title}
+            </h3>
+            {props.description && (
+              <p className="mt-1 max-w-2xl text-sm text-gray-500">
+                {props.description}
+              </p>
+            )}
+          </div>
+        )}
+        <div className="border-t border-gray-200 py-0">
+          <dl className="py-4">{props.children}</dl>
         </div>
-      )}
-      <div className="border-t border-gray-200 py-0">
-        <dl className="py-4">{props.children}</dl>
-      </div>
-      {props.withSaveButton && (
-        <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
-          <dl className="sm:divide-y sm:divide-gray-200">
-            <div className="sm:py-5 sm:px-6 flex justify-end">
-              <Button>{props.saveButtonLabel ?? t('save')}</Button>
-            </div>
-          </dl>
-        </div>
-      )}
+        {props.withSaveButton && (
+          <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
+            <dl className="sm:divide-y sm:divide-gray-200">
+              <div className="sm:py-5 sm:px-6 flex justify-end">
+                <Button>{props.saveButtonLabel ?? t('save')}</Button>
+              </div>
+            </dl>
+          </div>
+        )}
+      </form>
     </div>
   );
 }
