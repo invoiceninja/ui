efa58d41ee17cda1f20563df9ab350e5d2908d51
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-10 11:32:50 +0100

    Tailwind CSS: Change `purge` to `content`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,17 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  purge: {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  },
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         ninja: {
-          gray: "#454544",
-          "gray-darker": "#2F2E2E",
+          gray: '#454544',
+          'gray-darker': '#2F2E2E',
         },
       },
     },
@@ -21,7 +19,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-  ],
+  plugins: [require('@tailwindcss/forms')],
 };
