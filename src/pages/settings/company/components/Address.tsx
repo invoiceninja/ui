d1739246ace5a7eeee4cb3b9aa9efd4be962ac1d
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-10 15:51:52 +0100

    Company: Updating address

--- src/pages/settings/company/components/Address.tsx
+++ src/pages/settings/company/components/Address.tsx
@@ -8,35 +8,83 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { useStaticsQuery } from 'common/queries/statics';
+import { updateChanges } from 'common/stores/slices/company';
+import { RootState } from 'common/stores/store';
+import { ChangeEvent } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
 import { Card, Element } from '../../../../components/cards';
 import { InputField, SelectField } from '../../../../components/forms';
 
 export function Address() {
   const [t] = useTranslation();
+  const company = useSelector((state: RootState) => state.company);
+  const { data } = useStaticsQuery();
+  const dispatch = useDispatch();
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    dispatch(
+      updateChanges({ property: event.target.id, value: event.target.value })
+    );
+  }
 
   return (
-    <Card title={t('address')}>
-      <Element leftSide={t('address1')}>
-        <InputField id="address1" />
-      </Element>
-      <Element leftSide={t('address2')}>
-        <InputField id="address2" />
-      </Element>
-      <Element leftSide={t('city')}>
-        <InputField id="city" />
-      </Element>
-      <Element leftSide={t('state')}>
-        <InputField id="state" />
-      </Element>
-      <Element leftSide={t('postal_code')}>
-        <InputField id="postal_code" />
-      </Element>
-      <Element leftSide={t('country')}>
-        <SelectField>
-          <option>Bosnia and Herzegovina</option>
-        </SelectField>
-      </Element>
-    </Card>
+    <>
+      {company.current?.company?.settings && (
+        <Card title={t('address')}>
+          <Element leftSide={t('address1')}>
+            <InputField
+              onChange={handleChange}
+              value={company.current.company.settings.address1 || ''}
+              id="settings.address1"
+            />
+          </Element>
+          <Element leftSide={t('address2')}>
+            <InputField
+              onChange={handleChange}
+              value={company.current.company.settings.address2 || ''}
+              id="settings.address2"
+            />
+          </Element>
+          <Element leftSide={t('city')}>
+            <InputField
+              onChange={handleChange}
+              value={company.current.company.settings.city || ''}
+              id="settings.city"
+            />
+          </Element>
+          <Element leftSide={t('state')}>
+            <InputField
+              onChange={handleChange}
+              value={company.current.company.settings.state || ''}
+              id="settings.state"
+            />
+          </Element>
+          <Element leftSide={t('postal_code')}>
+            <InputField
+              onChange={handleChange}
+              value={company.current.company.settings.postal_code || ''}
+              id="settings.postal_code"
+            />
+          </Element>
+          <Element leftSide={t('country')}>
+            <SelectField
+              value={company.current.company.settings.country_id || '1'}
+              onChange={handleChange}
+              id="settings.country_id"
+            >
+              {data?.data.countries.map(
+                (size: { id: string; name: string }) => (
+                  <option key={size.id} value={size.id}>
+                    {size.name}
+                  </option>
+                )
+              )}
+            </SelectField>
+          </Element>
+        </Card>
+      )}
+    </>
   );
 }
