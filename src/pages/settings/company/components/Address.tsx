4af80e838d776cbf2ed5010649d618f5c0ef3bae
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-13 13:36:39 +0100

    Update adress to use the hook

--- src/pages/settings/company/components/Address.tsx
+++ src/pages/settings/company/components/Address.tsx
@@ -8,69 +8,68 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { useCurrentCompany } from 'common/hooks/useCurrentCompany';
 import { useStaticsQuery } from 'common/queries/statics';
 import { updateChanges } from 'common/stores/slices/company';
-import { RootState } from 'common/stores/store';
 import { ChangeEvent } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Card, Element } from '../../../../components/cards';
 import { InputField, SelectField } from '../../../../components/forms';
 
 export function Address() {
   const [t] = useTranslation();
-  const company = useSelector((state: RootState) => state.company);
+  const company = useCurrentCompany();
   const { data } = useStaticsQuery();
   const dispatch = useDispatch();
 
-  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) =>
     dispatch(
       updateChanges({ property: event.target.id, value: event.target.value })
     );
-  }
 
   return (
     <>
-      {company.current?.company?.settings && (
+      {company?.settings && (
         <Card title={t('address')}>
           <Element leftSide={t('address1')}>
             <InputField
               onChange={handleChange}
-              value={company.current.company.settings.address1 || ''}
+              value={company.settings.address1 || ''}
               id="settings.address1"
             />
           </Element>
           <Element leftSide={t('address2')}>
             <InputField
               onChange={handleChange}
-              value={company.current.company.settings.address2 || ''}
+              value={company.settings.address2 || ''}
               id="settings.address2"
             />
           </Element>
           <Element leftSide={t('city')}>
             <InputField
               onChange={handleChange}
-              value={company.current.company.settings.city || ''}
+              value={company.settings.city || ''}
               id="settings.city"
             />
           </Element>
           <Element leftSide={t('state')}>
             <InputField
               onChange={handleChange}
-              value={company.current.company.settings.state || ''}
+              value={company.settings.state || ''}
               id="settings.state"
             />
           </Element>
           <Element leftSide={t('postal_code')}>
             <InputField
               onChange={handleChange}
-              value={company.current.company.settings.postal_code || ''}
+              value={company.settings.postal_code || ''}
               id="settings.postal_code"
             />
           </Element>
           <Element leftSide={t('country')}>
             <SelectField
-              value={company.current.company.settings.country_id || '1'}
+              value={company.settings.country_id || '1'}
               onChange={handleChange}
               id="settings.country_id"
             >
