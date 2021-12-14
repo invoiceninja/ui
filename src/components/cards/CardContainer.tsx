04f2195b7a90293f0ff0bf16361dd40c58272cad
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-14 16:25:22 +0100

    CardContainer component

---
+++ src/components/cards/CardContainer.tsx
@@ -0,0 +1,13 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+export function CardContainer(props: { children: React.ReactNode }) {
+  return <div className="p-8 space-y-6">{props.children}</div>;
+}
