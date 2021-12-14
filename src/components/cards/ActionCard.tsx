d0ddaf7fce213f386d88c83ab5517335814372d5
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-14 16:50:59 +0100

    ActionCard component

---
+++ src/components/cards/ActionCard.tsx
@@ -0,0 +1,31 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import React from 'react';
+
+export function ActionCard(props: {
+  label: string;
+  help?: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="mt-2 bg-white w-full p-8 rounded shadow my-4">
+      <div className={`flex justify-between items-center`}>
+        <section>
+          <h2>{props.label}</h2>
+          {props.help && (
+            <span className="text-xs text-gray-600">{props.help}</span>
+          )}
+        </section>
+        {props.children}
+      </div>
+    </div>
+  );
+}
