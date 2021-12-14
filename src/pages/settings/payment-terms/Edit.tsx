97f6072a4ac17192a5c3734076b71d26a381379c
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-14 16:25:53 +0100

    Editing payment terms

--- src/pages/settings/payment-terms/Edit.tsx
+++ src/pages/settings/payment-terms/Edit.tsx
@@ -8,12 +8,22 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { useProductQuery } from 'common/queries/products';
+import { Card, CardContainer } from '@invoiceninja/cards';
+import { InputField } from '@invoiceninja/forms';
+import axios, { AxiosError } from 'axios';
+import { endpoint } from 'common/helpers';
+import { PaymentTerm } from 'common/interfaces/payment-term';
+import { defaultHeaders } from 'common/queries/common/headers';
+import { usePaymentTermQuery } from 'common/queries/payment-terms';
+import { Container } from 'components/Container';
 import { Settings } from 'components/layouts/Settings';
 import { Spinner } from 'components/Spinner';
+import { useFormik } from 'formik';
 import { useEffect } from 'react';
+import toast from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
+import { useSWRConfig } from 'swr';
 
 export function Edit() {
   const [t] = useTranslation();
@@ -23,16 +33,66 @@ export function Edit() {
   });
 
   const { id } = useParams();
-  const { data: paymentTerm } = useProductQuery({ id });
+  const { data } = usePaymentTermQuery({ id });
+  const { mutate } = useSWRConfig();
 
-  console.log(paymentTerm);
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      num_days: data?.data.data.num_days || 0,
+    },
+    onSubmit: (values: Partial<PaymentTerm>) => {
+      toast.loading(t('processing'));
+
+      axios
+        .put(
+          endpoint('/api/v1/payment_terms/:id', { id: data?.data.data.id }),
+          values,
+          { headers: defaultHeaders }
+        )
+        .then(() => {
+          mutate(data?.request.responseURL);
+
+          toast.dismiss();
+          toast.success(t('updated_payment_term'));
+        })
+        .catch((error: AxiosError) => {
+          console.error(error);
+
+          toast.dismiss();
+          toast.error(t('error_title'));
+        })
+        .finally(() => formik.setSubmitting(false));
+    },
+  });
 
   return (
     <Settings title={t('payment_terms')}>
-      {!paymentTerm && (
+      {!data && (
         <div className="flex justify-center">
           <Spinner />
         </div>
+      )}
+
+      {data && (
+        <Container>
+          <Card
+            withSaveButton
+            disableSubmitButton={formik.isSubmitting}
+            onFormSubmit={formik.handleSubmit}
+            title={data.data.data.name}
+          >
+            <CardContainer>
+              <InputField
+                value={formik.values.num_days}
+                type="number"
+                id="num_days"
+                label={t('number_of_days')}
+                onChange={formik.handleChange}
+              />
+            </CardContainer>
+          </Card>
+        </Container>
       )}
     </Settings>
   );
