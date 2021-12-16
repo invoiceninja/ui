cabf1b9359b493b18e165e5600aa234691d9174c
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-16 15:23:14 +0100

    Replace swr with react-query for documents table

--- src/pages/settings/company/documents/components/Table.tsx
+++ src/pages/settings/company/documents/components/Table.tsx
@@ -22,17 +22,14 @@ import { endpoint } from 'common/helpers';
 import { Document } from 'common/interfaces/document.interface';
 import { defaultHeaders } from 'common/queries/common/headers';
 import { useDocumentsQuery } from 'common/queries/documents';
-import { bulk } from 'common/queries/payment-terms';
-import { updateLatestQueryUrl } from 'common/stores/slices/company-documents';
 import { Dropdown } from 'components/dropdown/Dropdown';
 import { DropdownElement } from 'components/dropdown/DropdownElement';
 import { FileIcon } from 'components/FileIcon';
 import { Spinner } from 'components/Spinner';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import toast from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
-import { useDispatch } from 'react-redux';
-import { useSWRConfig } from 'swr';
+import { useQueryClient } from 'react-query';
 
 export function Table() {
   const [t] = useTranslation();
@@ -40,15 +37,7 @@ export function Table() {
   const [perPage, setPerPage] = useState<string>('10');
 
   const { data, isLoading } = useDocumentsQuery({ perPage, currentPage });
-  const { mutate } = useSWRConfig();
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(
-      updateLatestQueryUrl({ url: data?.request.responseURL as string })
-    );
-  }, [data]);
+  const queryClient = useQueryClient();
 
   const _delete = (id: string) => {
     toast.loading(t('processing'));
@@ -60,15 +49,14 @@ export function Table() {
       .then(() => {
         toast.dismiss();
         toast.success(t('deleted_payment_term'));
-
-        mutate(data?.request.responseURL);
       })
       .catch((error) => {
         console.error(error);
 
         toast.dismiss();
         toast.error(t('error_title'));
-      });
+      })
+      .finally(() => queryClient.invalidateQueries('/api/v1/documents'));
   };
 
   return (
