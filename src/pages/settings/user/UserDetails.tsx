e56344bdbc315530ea6fd63ff3cb9021d1d8c0de
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-19 12:34:08 +0100

    show confirmation dialog for password

--- src/pages/settings/user/UserDetails.tsx
+++ src/pages/settings/user/UserDetails.tsx
@@ -8,6 +8,7 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { Button, InputField } from '@invoiceninja/forms';
 import { Modal } from 'components/Modal';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -22,15 +23,32 @@ import {
 
 export function UserDetails() {
   const [t] = useTranslation();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     document.title = `${import.meta.env.VITE_APP_TITLE}: ${t('user_details')}`;
   });
 
-  const onSave = () => {};
+  const onSave = () => {
+    setIsModalOpen(true);
+  };
 
   return (
     <Settings onSaveClick={onSave} title={t('user_details')}>
+      <Modal
+        onClose={setIsModalOpen}
+        visible={isModalOpen}
+        title={t('confirmation')}
+        text={t('please_enter_your_password')}
+      >
+        <InputField
+          id="current_password"
+          type="password"
+          label={t('current_password')}
+        />
+        <Button>{t('continue')}</Button>
+      </Modal>
+
       <div className="space-y-6 mt-6">
         <Details />
         <Password />
