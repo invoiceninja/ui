ea076d9256d2170a5805b7e48cd313bd992c02d5
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-20 16:32:13 +0100

    useAccentColor hook

---
+++ src/common/hooks/useAccentColor.ts
@@ -0,0 +1,25 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import colors from 'common/constants/colors';
+import { RootState } from 'common/stores/store';
+import { useSelector } from 'react-redux';
+import { useCurrentUser } from './useCurrentUser';
+
+export function useAccentColor() {
+  const user = useCurrentUser();
+  const userState = useSelector((state: RootState) => state.user);
+
+  return (
+    userState.changes?.company_user?.settings?.accent_color ||
+    user?.company_user?.settings?.accent_color ||
+    colors.primary
+  );
+}
