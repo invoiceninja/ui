b01ebcd8eb1226b53267ea986c237dbb0101e24a
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-21 13:47:04 +0100

    define company-users store

---
+++ src/common/stores/slices/company-users.ts
@@ -0,0 +1,82 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { property, set } from 'lodash';
+
+interface CompanyUser {
+  currentIndex: number;
+  api: any;
+  changes: {
+    company: any;
+  };
+}
+
+type AvailableResources = 'company';
+
+const initialState: CompanyUser = {
+  currentIndex: 0,
+  api: {},
+  changes: {
+    company: undefined,
+  },
+};
+
+export const companyUserSlice = createSlice({
+  name: 'companyUser',
+  initialState,
+  reducers: {
+    updateCompanyUsers: (state, action) => {
+      state.api = action.payload;
+    },
+    injectInChanges: (
+      state,
+      action: PayloadAction<{ object: AvailableResources; data: any }>
+    ) => {
+      state.changes[action.payload.object] = action.payload.data;
+    },
+    updateChanges: (
+      state,
+      action: PayloadAction<{
+        object: AvailableResources;
+        property: string;
+        value: any;
+      }>
+    ) => {
+      state.changes[action.payload.object] = set(
+        state.changes[action.payload.object],
+        action.payload.property,
+        action.payload.value
+      );
+    },
+    resetChanges: (state, action: PayloadAction<AvailableResources>) => {
+      state.changes[action.payload] =
+        state.api[state.currentIndex][action.payload];
+    },
+    updateRecord: (
+      state,
+      action: PayloadAction<{
+        object: AvailableResources;
+        data: any;
+      }>
+    ) => {
+      state.api[state.currentIndex][action.payload.object] =
+        action.payload.data;
+    },
+  },
+});
+
+export const {
+  updateCompanyUsers,
+  injectInChanges,
+  resetChanges,
+  updateChanges,
+  updateRecord,
+} = companyUserSlice.actions;
