19ece27829ccc7302676188b896165f209c7e940
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-21 17:01:43 +0100

    use password confirmation from external component

--- src/pages/settings/user/UserDetails.tsx
+++ src/pages/settings/user/UserDetails.tsx
@@ -8,7 +8,6 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { Button, InputField } from '@invoiceninja/forms';
 import axios, { AxiosError } from 'axios';
 import { endpoint } from 'common/helpers';
 import { useCurrentUser } from 'common/hooks/useCurrentUser';
@@ -20,8 +19,8 @@ import {
   updateUser,
 } from 'common/stores/slices/user';
 import { RootState } from 'common/stores/store';
-import { Modal } from 'components/Modal';
-import { ChangeEvent, useEffect, useState } from 'react';
+import { PasswordConfirmation } from 'components/PasswordConfirmation';
+import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
@@ -40,10 +39,10 @@ export function UserDetails() {
   const user = useCurrentUser();
   const dispatch = useDispatch();
 
-  const userState = useSelector((state: RootState) => state.user);
+  const [isPasswordConfirmModalOpen, setPasswordConfirmModalOpen] =
+    useState(false);
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [currentPassword, setCurrentPassword] = useState('');
+  const userState = useSelector((state: RootState) => state.user);
 
   useEffect(() => {
     document.title = `${import.meta.env.VITE_APP_TITLE}: ${t('user_details')}`;
@@ -51,8 +50,7 @@ export function UserDetails() {
     dispatch(injectInChanges());
   }, [user]);
 
-  const onSave = () => {
-    setIsModalOpen(false);
+  const onSave = (password: string, passwordIsRequired: boolean) => {
     toast.loading(t('processing'));
 
     axios
@@ -60,7 +58,7 @@ export function UserDetails() {
         endpoint('/api/v1/users/:id?include=company_user', { id: user.id }),
         userState.changes,
         {
-          headers: { 'X-Api-Password': currentPassword, ...defaultHeaders },
+          headers: { 'X-Api-Password': password, ...defaultHeaders },
         }
       )
       .then((response) => {
@@ -71,38 +69,26 @@ export function UserDetails() {
 
         window.dispatchEvent(new CustomEvent('user.updated'));
       })
-      .catch((error: AxiosError) => {
-        toast.dismiss();
+      .catch((error: AxiosError | any) => {
+        console.error(error);
 
-        error.response?.status === 412
-          ? toast.error(error.response?.data.message)
-          : toast.error(t('error_title'));
+        toast.dismiss();
+        toast.error(t('error_title'));
       })
       .finally(() => dispatch(deletePassword()));
   };
 
   return (
     <Settings
-      onSaveClick={() => setIsModalOpen(true)}
+      onSaveClick={() => setPasswordConfirmModalOpen(true)}
       onCancelClick={() => dispatch(resetChanges())}
       title={t('user_details')}
     >
-      <Modal
-        onClose={setIsModalOpen}
-        visible={isModalOpen}
-        title={t('confirmation')}
-        text={t('please_enter_your_password')}
-      >
-        <InputField
-          id="current_password"
-          type="password"
-          label={t('current_password')}
-          onChange={(event: ChangeEvent<HTMLInputElement>) =>
-            setCurrentPassword(event.target.value)
-          }
-        />
-        <Button onClick={onSave}>{t('continue')}</Button>
-      </Modal>
+      <PasswordConfirmation
+        show={isPasswordConfirmModalOpen}
+        onClose={setPasswordConfirmModalOpen}
+        onSave={onSave}
+      />
 
       <div className="space-y-6 mt-6">
         <Details />
