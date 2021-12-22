068e6ebd38a35b43e70fde01f4290191cf693e50
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-22 14:34:04 +0100

    define timezone interface

---
+++ src/common/interfaces/timezone.ts
@@ -0,0 +1,16 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+export interface Timezone {
+  id: string;
+  location: string;
+  name: string;
+  utc_offset: number;
+}
