9db8b34077cffb379684576e5237c5d3b64b39b0
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-22 14:33:56 +0100

    define dateformat interface

---
+++ src/common/interfaces/date-format.ts
@@ -0,0 +1,16 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+export interface DateFormat {
+  id: string;
+  format: string;
+  format_dart: string;
+  format_moment: string;
+}
