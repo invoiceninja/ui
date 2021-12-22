d0fb2426e41f9cdc5e153135a53a27c77a92d703
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-22 14:34:35 +0100

    localization: settings

--- src/pages/settings/localization/Localization.tsx
+++ src/pages/settings/localization/Localization.tsx
@@ -8,20 +8,68 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { endpoint } from 'common/helpers';
+import { useCompanyChanges } from 'common/hooks/useCompanyChanges';
+import { useCurrentCompany } from 'common/hooks/useCurrentCompany';
+import { defaultHeaders } from 'common/queries/common/headers';
+import {
+  injectInChanges,
+  resetChanges,
+  updateRecord,
+} from 'common/stores/slices/company-users';
 import { useEffect } from 'react';
+import toast from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
 import { Settings } from '../../../components/layouts/Settings';
 import { CustomLabels, Settings as SettingsComponent } from './components';
 
 export function Localization() {
   const [t] = useTranslation();
+  const company = useCurrentCompany();
+  const dispatch = useDispatch();
+  const companyChanges = useCompanyChanges();
 
   useEffect(() => {
     document.title = `${import.meta.env.VITE_APP_TITLE}: ${t('localization')}`;
-  });
+
+    dispatch(injectInChanges({ object: 'company', data: company }));
+  }, [company]);
+
+  const onSave = () => {
+    toast.loading(t('processing'));
+
+    axios
+      .put(
+        endpoint('/api/v1/companies/:id', { id: companyChanges.id }),
+        companyChanges,
+        { headers: defaultHeaders }
+      )
+      .then((response: AxiosResponse) => {
+        dispatch(updateRecord({ object: 'company', data: response.data.data }));
+
+        toast.dismiss();
+        toast.success(t('updated_settings'));
+      })
+      .catch((error: AxiosError) => {
+        console.error(error);
+
+        toast.dismiss();
+        toast.success(t('error_title'));
+      });
+  };
+
+  const onCancel = () => {
+    dispatch(resetChanges('company'));
+  };
 
   return (
-    <Settings title={t('localization')}>
+    <Settings
+      onSaveClick={onSave}
+      onCancelClick={onCancel}
+      title={t('localization')}
+    >
       <div className="space-y-6 mt-6">
         <SettingsComponent />
         <CustomLabels />
--- src/pages/settings/localization/components/Settings.tsx
+++ src/pages/settings/localization/components/Settings.tsx
@@ -8,57 +8,177 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { Currency } from 'common/interfaces/currency';
+import { DateFormat } from 'common/interfaces/date-format';
+import { Language } from 'common/interfaces/language';
+import { Timezone } from 'common/interfaces/timezone';
+import { useStaticsQuery } from 'common/queries/statics';
+import { updateChanges } from 'common/stores/slices/company-users';
+import { RootState } from 'common/stores/store';
+import dayjs from 'dayjs';
+import { ChangeEvent } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
 import { Card, Element } from '../../../../components/cards';
-import { Checkbox, Radio, SelectField } from '../../../../components/forms';
+import { Radio, SelectField } from '../../../../components/forms';
 import Toggle from '../../../../components/forms/Toggle';
 
 export function Settings() {
   const [t] = useTranslation();
+  const { data: statics } = useStaticsQuery();
+  const dispatch = useDispatch();
+
+  const companyChanges = useSelector(
+    (state: RootState) => state.companyUsers.changes.company
+  );
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    dispatch(
+      updateChanges({
+        object: 'company',
+        property: event.target.id,
+        value: event.target.value,
+      })
+    );
+  };
 
   const currencyFormats = [
-    { id: 'symbol', title: 'Symbol: $1,000.00' },
-    { id: 'code', title: 'Code: 1,000.00 USD' },
+    {
+      id: 'symbol',
+      title: `${t('currency_symbol')}: $1,000.00`,
+      value: 'false',
+    },
+    { id: 'code', title: `${t('currency_code')}: 1,000.00 USD`, value: 'true' },
   ];
+
+  console.log(statics);
 
   return (
     <>
       <Card title={t('settings')}>
         <Element leftSide={t('currency')}>
-          <SelectField>
-            <option>US Dollar</option>
+          <SelectField
+            value={companyChanges?.settings?.currency_id || ''}
+            id="settings.currency_id"
+            onChange={handleChange}
+          >
+            <option value=""></option>
+            {statics &&
+              statics.data.currencies.map((currency: Currency) => (
+                <option value={currency.id} key={currency.id}>
+                  {currency.name}
+                </option>
+              ))}
           </SelectField>
         </Element>
         <Element leftSide={t('currency_format')}>
-          <Radio options={currencyFormats} defaultSelected="code" />
+          <Radio
+            onClick={(event: ChangeEvent<HTMLInputElement>) =>
+              dispatch(
+                updateChanges({
+                  object: 'company',
+                  property: 'settings.show_currency_code',
+                  value: event.target.value === 'true' ? true : false,
+                })
+              )
+            }
+            name="show_currency_code"
+            options={currencyFormats}
+            defaultSelected={companyChanges?.settings?.show_currency_code.toString()}
+          />
         </Element>
         <Element leftSide={t('language')}>
-          <SelectField>
-            <option>English</option>
+          <SelectField
+            onChange={handleChange}
+            id="settings.language_id"
+            value={companyChanges?.settings?.language_id || '1'}
+          >
+            {statics &&
+              statics.data.languages.map((language: Language) => (
+                <option value={language.id} key={language.id}>
+                  {language.name}
+                </option>
+              ))}
           </SelectField>
         </Element>
         <Element leftSide={t('timezone')}>
-          <SelectField>
-            <option>Europe/Sarajevo</option>
+          <SelectField
+            onChange={handleChange}
+            id="settings.timezone_id"
+            value={companyChanges?.settings?.timezone_id || '1'}
+          >
+            {statics &&
+              statics.data.timezones.map((timezone: Timezone) => (
+                <option value={timezone.id} key={timezone.id}>
+                  {timezone.name}
+                </option>
+              ))}
           </SelectField>
         </Element>
         <Element leftSide={t('date_format')}>
-          <SelectField>
-            <option>January 31, 2000</option>
+          <SelectField
+            onChange={handleChange}
+            id="settings.date_format_id"
+            value={companyChanges?.settings?.date_format_id || '1'}
+          >
+            {statics &&
+              statics.data.date_formats.map((dateFormat: DateFormat) => (
+                <option value={dateFormat.id} key={dateFormat.id}>
+                  {dayjs().format(dateFormat.format_moment)}
+                </option>
+              ))}
           </SelectField>
         </Element>
         <Element leftSide={t('military_time')}>
-          <Toggle />
+          <Toggle
+            checked={companyChanges?.settings?.military_time}
+            onChange={(value: boolean) =>
+              dispatch(
+                updateChanges({
+                  object: 'company',
+                  property: 'settings.military_time',
+                  value,
+                })
+              )
+            }
+          />
         </Element>
         <Element leftSide={t('decimal_comma')}>
-          <Toggle />
+          <Toggle
+            checked={companyChanges?.use_comma_as_decimal_place}
+            onChange={(value: boolean) =>
+              dispatch(
+                updateChanges({
+                  object: 'company',
+                  property: 'use_comma_as_decimal_place',
+                  value,
+                })
+              )
+            }
+          />
         </Element>
-      </Card>
 
-      <Card>
-        <Element leftSide={t('first_month_of_the_year')}>
-          <SelectField>
-            <option>January</option>
+        <div className="pt-6 border-b"></div>
+
+        <Element className="mt-4" leftSide={t('first_month_of_the_year')}>
+          <SelectField
+            id="first_month_of_year"
+            value={companyChanges?.first_month_of_year || ''}
+            onChange={handleChange}
+          >
+            <option value="">{/*  */}</option>
+            <option value="1">{t('january')}</option>
+            <option value="2">{t('february')}</option>
+            <option value="3">{t('march')}</option>
+            <option value="4">{t('april')}</option>
+            <option value="5">{t('may')}</option>
+            <option value="6">{t('june')}</option>
+            <option value="7">{t('july')}</option>
+            <option value="8">{t('august')}</option>
+            <option value="9">{t('september')}</option>
+            <option value="10">{t('october')}</option>
+            <option value="11">{t('november')}</option>
+            <option value="12">{t('december')}</option>
           </SelectField>
         </Element>
       </Card>
