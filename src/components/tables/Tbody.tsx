84805cc99cb5875dfb87d0e35a8bf3460dcf2bbc
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-23 17:16:53 +0100

    optional placeholder for tbody

--- src/components/tables/Tbody.tsx
+++ src/components/tables/Tbody.tsx
@@ -8,10 +8,40 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { Spinner } from 'components/Spinner';
+import { useTranslation } from 'react-i18next';
+import { Td, Tr } from '.';
 import CommonProps from '../../common/interfaces/common-props.interface';
 
-interface Props extends CommonProps {}
+interface Props extends CommonProps {
+  data?: any;
+  showHelperPlaceholders?: boolean;
+}
 
 export function Tbody(props: Props) {
-  return <tbody>{props.children}</tbody>;
+  const [t] = useTranslation();
+
+  return (
+    <tbody>
+      {!props.data && props.showHelperPlaceholders && (
+        <Tr>
+          <Td colSpan={20}>
+            <Spinner />
+          </Td>
+        </Tr>
+      )}
+
+      {props.data &&
+        props.showHelperPlaceholders &&
+        props.data.data.meta.pagination.total === 0 && (
+          <Tr>
+            <Td colSpan={20}>
+              <p>{t('empty_table')}</p>
+            </Td>
+          </Tr>
+        )}
+
+      {props.children}
+    </tbody>
+  );
 }
