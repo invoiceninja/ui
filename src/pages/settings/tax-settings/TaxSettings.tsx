998504828e7e84a176d41cc5c5c24bcd1aaf6413
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-23 16:33:36 +0100

    updating tax rate settings

--- src/pages/settings/tax-settings/TaxSettings.tsx
+++ src/pages/settings/tax-settings/TaxSettings.tsx
@@ -8,85 +8,135 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { useEffect } from 'react';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { endpoint } from 'common/helpers';
+import { useCompanyChanges } from 'common/hooks/useCompanyChanges';
+import { useCurrentCompany } from 'common/hooks/useCurrentCompany';
+import { defaultHeaders } from 'common/queries/common/headers';
+import {
+  injectInChanges,
+  resetChanges,
+  updateChanges,
+  updateRecord,
+} from 'common/stores/slices/company-users';
+import { ChangeEvent, useEffect } from 'react';
+import toast from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
 import { Card, Element } from '../../../components/cards';
-import { Button, Checkbox, SelectField } from '../../../components/forms';
+import { SelectField } from '../../../components/forms';
 import Toggle from '../../../components/forms/Toggle';
 import { Settings } from '../../../components/layouts/Settings';
-import {
-  Pagination,
-  Table,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
-} from '../../../components/tables';
+import { Table } from './components/index';
 
 export function TaxSettings() {
   const [t] = useTranslation();
+  const company = useCurrentCompany();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     document.title = `${import.meta.env.VITE_APP_TITLE}: ${t('tax_settings')}`;
-  });
+
+    dispatch(injectInChanges({ object: 'company', data: company }));
+  }, [company]);
+
+  const companyChanges = useCompanyChanges();
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    dispatch(
+      updateChanges({
+        object: 'company',
+        property: event.target.id,
+        value: event.target.value,
+      })
+    );
+  };
+
+  const handleToggleChange = (id: string, value: boolean) => {
+    dispatch(
+      updateChanges({
+        object: 'company',
+        property: id,
+        value,
+      })
+    );
+  };
+
+  const onSave = () => {
+    toast.loading(t('processing'));
+
+    axios
+      .put(
+        endpoint('/api/v1/companies/:id', { id: companyChanges.id }),
+        companyChanges,
+        { headers: defaultHeaders }
+      )
+      .then((response: AxiosResponse) => {
+        dispatch(updateRecord({ object: 'company', data: response.data.data }));
+
+        toast.dismiss();
+        toast.success(t('updated_settings'));
+      })
+      .catch((error: AxiosError) => {
+        console.error(error);
+
+        toast.dismiss();
+        toast.success(t('error_title'));
+      });
+  };
 
   return (
-    <Settings title={t('tax_settings')}>
-      <Card withSaveButton title={t('tax_settings')}>
+    <Settings
+      onSaveClick={onSave}
+      onCancelClick={() => dispatch(resetChanges('company'))}
+      title={t('tax_settings')}
+    >
+      <Card title={t('tax_settings')}>
         <Element leftSide={t('invoice_tax_rates')}>
-          <SelectField>
-            <option value="disabled">{t('disabled')}</option>
-            <option value="one_tax_rate">{t('one_tax_rate')}</option>
-            <option value="two_tax_rates">{t('two_tax_rates')}</option>
-            <option value="three_tax_rates">{t('three_tax_rates')}</option>
+          <SelectField
+            id="enabled_tax_rates"
+            onChange={handleChange}
+            value={companyChanges?.enabled_tax_rates || 0}
+          >
+            <option value="0">{t('disabled')}</option>
+            <option value="1">{t('one_tax_rate')}</option>
+            <option value="2">{t('two_tax_rates')}</option>
+            <option value="3">{t('three_tax_rates')}</option>
           </SelectField>
         </Element>
+
         <Element leftSide={t('item_tax_rates')}>
-          <SelectField>
-            <option value="disabled">{t('disabled')}</option>
-            <option value="one_tax_rate">{t('one_tax_rate')}</option>
-            <option value="two_tax_rates">{t('two_tax_rates')}</option>
-            <option value="three_tax_rates">{t('three_tax_rates')}</option>
+          <SelectField
+            id="enabled_item_tax_rates"
+            onChange={handleChange}
+            value={companyChanges?.enabled_item_tax_rates || 0}
+          >
+            <option value="0">{t('disabled')}</option>
+            <option value="1">{t('one_tax_rate')}</option>
+            <option value="2">{t('two_tax_rates')}</option>
+            <option value="3">{t('three_tax_rates')}</option>
           </SelectField>
         </Element>
+
         <Element
           leftSide={t('inclusive_taxes')}
           leftSideHelp={
             <span className="flex flex-col">
-              <span>Exclusive: 100+10% = 100 + 10</span>
-              <span>Inclusive: 100+10% = 90.91 + 9.09</span>
+              <span>{t('exclusive')}: 100 + 10% = 100 + 10</span>
+              <span>{t('inclusive')}: 100 + 10% = 90.91 + 9.09</span>
             </span>
           }
         >
-          <Toggle />
+          <Toggle
+            onChange={(value: boolean) =>
+              handleToggleChange('settings.inclusive_taxes', value)
+            }
+            checked={companyChanges?.settings?.inclusive_taxes || false}
+          />
         </Element>
       </Card>
 
-      <div className="flex justify-end mt-8">
-        <Button to="/tax_rates/create">{t('create_tax_rate')}</Button>
-      </div>
-
-      <Table>
-        <Thead>
-          <Th>{t('name')}</Th>
-          <Th>{t('rate')}</Th>
-          <Th>{t('type')}</Th>
-          <Th>{t('action')}</Th>
-        </Thead>
-        <Tbody>
-          <Tr>
-            <Td colSpan={4}>{t('empty_table')}</Td>
-          </Tr>
-        </Tbody>
-      </Table>
-
-      <Pagination
-        currentPage={1}
-        onPageChange={() => {}}
-        onRowsChange={() => {}}
-        totalPages={1}
-      />
+      <Table />
     </Settings>
   );
 }
