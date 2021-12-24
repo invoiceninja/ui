f1926fef2989e5b60738ae7b25ca0c8bcf1f9733
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-24 14:43:28 +0100

    task settings

--- src/pages/settings/task-settings/TaskSettings.tsx
+++ src/pages/settings/task-settings/TaskSettings.tsx
@@ -8,100 +8,190 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { useEffect } from 'react';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { endpoint } from 'common/helpers';
+import { useCompanyChanges } from 'common/hooks/useCompanyChanges';
+import { useCurrentCompany } from 'common/hooks/useCurrentCompany';
+import { defaultHeaders } from 'common/queries/common/headers';
+import {
+  injectInChanges,
+  resetChanges,
+  updateChanges,
+  updateRecord,
+} from 'common/stores/slices/company-users';
+import { Alert } from 'components/Alert';
+import { Divider } from 'components/cards/Divider';
+import { ChangeEvent, useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
-import { Card, Element } from '../../../components/cards';
-import { Button, InputField } from '../../../components/forms';
+import { useDispatch } from 'react-redux';
+import { Card, ClickableElement, Element } from '../../../components/cards';
+import { InputField } from '../../../components/forms';
 import Toggle from '../../../components/forms/Toggle';
 import { Settings } from '../../../components/layouts/Settings';
-import {
-  Pagination,
-  Table,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
-} from '../../../components/tables';
 
 export function TaskSettings() {
   const [t] = useTranslation();
+  const dispatch = useDispatch();
+
+  const [errors, setErrors] = useState<any>(undefined);
+
+  const company = useCurrentCompany();
+  const companyChanges = useCompanyChanges();
 
   useEffect(() => {
     document.title = `${import.meta.env.VITE_APP_TITLE}: ${t('task_settings')}`;
-  });
+
+    dispatch(injectInChanges({ object: 'company', data: company }));
+  }, [company]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) =>
+    dispatch(
+      updateChanges({
+        object: 'company',
+        property: event.target.id,
+        value: event.target.value,
+      })
+    );
+
+  const handleToggleChange = (id: string, value: boolean) =>
+    dispatch(
+      updateChanges({
+        object: 'company',
+        property: id,
+        value,
+      })
+    );
+
+  const onSave = () => {
+    toast.loading(t('processing'));
+    setErrors(undefined);
+
+    axios
+      .put(
+        endpoint('/api/v1/companies/:id', { id: companyChanges.id }),
+        companyChanges,
+        { headers: defaultHeaders }
+      )
+      .then((response: AxiosResponse) => {
+        dispatch(updateRecord({ object: 'company', data: response.data.data }));
+
+        toast.dismiss();
+        toast.success(t('updated_settings'));
+      })
+      .catch((error: AxiosError) => {
+        console.error(error);
+        toast.dismiss();
+
+        error.response?.status === 422
+          ? setErrors(error.response.data)
+          : toast.error(t('error_title'));
+      });
+  };
 
   return (
-    <Settings title={t('task_settings')}>
+    <Settings
+      onSaveClick={onSave}
+      onCancelClick={() => dispatch(resetChanges('company'))}
+      title={t('task_settings')}
+    >
+      {errors?.errors?.settings && (
+        <Alert type="danger">
+          {errors?.errors?.settings.map((element: string) => (
+            <p>{element}</p>
+          ))}
+        </Alert>
+      )}
+
       <Card title={t('settings')}>
         <Element leftSide={t('default_task_rate')}>
-          <InputField id="default_task_rate" />
+          <InputField
+            id="settings.default_task_rate"
+            onChange={handleChange}
+            value={companyChanges?.settings?.default_task_rate || ''}
+          />
         </Element>
+
         <Element
           leftSide={t('auto_start_tasks')}
           leftSideHelp={t('auto_start_tasks_help')}
         >
-          <Toggle />
+          <Toggle
+            checked={companyChanges?.auto_start_tasks || false}
+            onChange={(value: boolean) =>
+              handleToggleChange('auto_start_tasks', value)
+            }
+          />
         </Element>
+
         <Element
           leftSide={t('show_task_end_date')}
           leftSideHelp={t('show_task_end_date_help')}
         >
-          <Toggle />
+          <Toggle
+            checked={companyChanges?.show_task_end_date || false}
+            onChange={(value: boolean) =>
+              handleToggleChange('show_task_end_date', value)
+            }
+          />
         </Element>
 
-        <div className="pt-6 border-b"></div>
+        <Divider />
 
         <Element
-          className="mt-6"
           leftSide={t('show_tasks_table')}
           leftSideHelp={t('show_tasks_table_help')}
         >
-          <Toggle />
+          <Toggle
+            checked={companyChanges?.show_tasks_table || false}
+            onChange={(value: boolean) =>
+              handleToggleChange('show_tasks_table', value)
+            }
+          />
         </Element>
+
         <Element
           leftSide={t('invoice_task_datelog')}
           leftSideHelp={t('invoice_task_datelog_help')}
         >
-          <Toggle />
+          <Toggle
+            checked={companyChanges?.invoice_task_datelog || false}
+            onChange={(value: boolean) =>
+              handleToggleChange('invoice_task_datelog', value)
+            }
+          />
         </Element>
+
         <Element
           leftSide={t('invoice_task_timelog')}
           leftSideHelp={t('invoice_task_timelog_help')}
         >
-          <Toggle />
+          <Toggle
+            checked={companyChanges?.invoice_task_timelog || false}
+            onChange={(value: boolean) =>
+              handleToggleChange('invoice_task_timelog', value)
+            }
+          />
         </Element>
+
         <Element
           leftSide={t('add_documents_to_invoice')}
           leftSideHelp={t('add_documents_to_invoice_help')}
         >
-          <Toggle />
+          <Toggle
+            checked={companyChanges?.invoice_task_documents || false}
+            onChange={(value: boolean) =>
+              handleToggleChange('invoice_task_documents', value)
+            }
+          />
         </Element>
       </Card>
 
-      <div className="flex justify-end mt-8">
-        <Button to="/task_settings/statuses/create">Create status</Button>
-      </div>
-
-      <Table>
-        <Thead>
-          <Th>{t('status')}</Th>
-          <Th>{t('total')}</Th>
-          <Th>{t('action')}</Th>
-        </Thead>
-        <Tbody>
-          <Tr>
-            <Td colSpan={3}>{t('empty_table')}</Td>
-          </Tr>
-        </Tbody>
-      </Table>
-
-      <Pagination
-        currentPage={1}
-        onPageChange={() => {}}
-        onRowsChange={() => {}}
-        totalPages={1}
-      />
+      <Card>
+        <ClickableElement to="/settings/task_statuses">
+          {t('configure_statuses')}
+        </ClickableElement>
+      </Card>
     </Settings>
   );
 }
