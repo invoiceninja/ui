e47d2b12bf31d174b406d2f15b5e86439753e57a
Benjamin Beganović <k1pstabug@gmail.com>
2021-12-28 12:12:26 +0100

    creating new task status

--- src/pages/settings/task-statuses/Create.tsx
+++ src/pages/settings/task-statuses/Create.tsx
@@ -8,6 +8,92 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { Card, CardContainer } from '@invoiceninja/cards';
+import { InputField, InputLabel } from '@invoiceninja/forms';
+import axios, { AxiosError } from 'axios';
+import { endpoint } from 'common/helpers';
+import { defaultHeaders } from 'common/queries/common/headers';
+import { Container } from 'components/Container';
+import { Settings } from 'components/layouts/Settings';
+import { useFormik } from 'formik';
+import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+import { useTranslation } from 'react-i18next';
+import { useQueryClient } from 'react-query';
+import { generatePath, useNavigate } from 'react-router-dom';
+
 export function Create() {
-  return <div>Creating a task status</div>;
+  const [t] = useTranslation();
+  const [errors, setErrors] = useState<Record<string, any>>({});
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    document.title = `${import.meta.env.VITE_APP_TITLE}: ${t(
+      'new_task_status'
+    )}`;
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      color: '',
+    },
+    onSubmit: (values) => {
+      setErrors({});
+
+      axios
+        .post(endpoint('/api/v1/task_statuses'), values, {
+          headers: defaultHeaders,
+        })
+        .then((response) => {
+          toast.dismiss();
+          toast.success(t('created_task_status'));
+
+          queryClient.invalidateQueries('/api/v1/task_statuses');
+
+          navigate(
+            generatePath('/settings/task_statuses/:id/edit', {
+              id: response.data.data.id,
+            })
+          );
+        })
+        .catch((error: AxiosError) => {
+          toast.dismiss();
+          console.error(error);
+
+          error.response?.status === 422
+            ? setErrors(error.response.data)
+            : toast.error(t('error_title'));
+        })
+        .finally(() => formik.setSubmitting(false));
+    },
+  });
+
+  return (
+    <Settings title={t('task_statuses')}>
+      <Container className="space-y-6">
+        <Card
+          withSaveButton
+          disableSubmitButton={formik.isSubmitting}
+          onFormSubmit={formik.handleSubmit}
+          title={t('new_task_status')}
+        >
+          <CardContainer>
+            <InputField
+              type="text"
+              id="name"
+              label={t('name')}
+              errorMessage={errors?.errors?.name}
+              required
+              onChange={formik.handleChange}
+            />
+
+            <InputLabel>{t('color')}</InputLabel>
+            <input type="color" id="color" onChange={formik.handleChange} />
+          </CardContainer>
+        </Card>
+      </Container>
+    </Settings>
+  );
 }
