5ac58bc44ea6a60ff13a5e45e7e3dd099dc56aad
Benjamin Beganović <k1pstabug@gmail.com>
2022-01-06 16:30:25 +0100

    Updating security settings

--- src/pages/settings/account-management/component/SecuritySettings.tsx
+++ src/pages/settings/account-management/component/SecuritySettings.tsx
@@ -8,35 +8,108 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { useCompanyChanges } from 'common/hooks/useCompanyChanges';
+import { updateChanges } from 'common/stores/slices/company-users';
+import Toggle from 'components/forms/Toggle';
+import { ChangeEvent } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
 import { generatePath } from 'react-router';
 import { Card, Element } from '../../../../components/cards';
 import { SelectField } from '../../../../components/forms';
-import Toggle from '../../../../components/forms/Toggle';
 
 export function SecuritySettings() {
   const [t] = useTranslation();
+  const companyChanges = useCompanyChanges();
+  const dispatch = useDispatch();
+
+  const options = [
+    {
+      value: 1_800_000,
+      label: generatePath(t('count_minutes'), { count: '30' }),
+    },
+    {
+      value: 7_200_000,
+      label: generatePath(t('count_hours'), { count: '2' }),
+    },
+    {
+      value: 28_800_000,
+      label: generatePath(t('count_hours'), { count: '8' }),
+    },
+    {
+      value: 86_400_000,
+      label: generatePath(t('count_day'), { count: '1' }),
+    },
+    {
+      value: 604_800_000,
+      label: generatePath(t('count_days'), { count: '7' }),
+    },
+    {
+      value: 2_592_000_000,
+      label: generatePath(t('count_days'), { count: '30' }),
+    },
+    {
+      value: 0,
+      label: t('never'),
+    },
+  ];
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) =>
+    dispatch(
+      updateChanges({
+        object: 'company',
+        property: event.target.id,
+        value: event.target.value,
+      })
+    );
+
+  const handleToggleChange = (id: string, value: boolean) =>
+    dispatch(
+      updateChanges({
+        object: 'company',
+        property: id,
+        value,
+      })
+    );
 
   return (
     <Card title={t('security_settings')}>
       <Element leftSide={t('password_timeout')}>
-        <SelectField>
-          <option value="7_days">
-            {generatePath(t('count_days'), { count: '7' })}
-          </option>
+        <SelectField
+          id="default_password_timeout"
+          value={companyChanges?.default_password_timeout || false}
+          onChange={handleChange}
+        >
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
         </SelectField>
       </Element>
 
       <Element leftSide={t('web_session_timeout')}>
-        <SelectField>
-          <option value="7_days">
-            {generatePath(t('count_days'), { count: '7' })}
-          </option>
+        <SelectField
+          id="session_timeout"
+          value={companyChanges?.session_timeout || false}
+          onChange={handleChange}
+        >
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
         </SelectField>
       </Element>
 
       <Element leftSide={t('require_password_with_social_login')}>
-        <Toggle />
+        <Toggle
+          checked={companyChanges?.oauth_password_required}
+          id="oauth_password_required"
+          onChange={(value: boolean) =>
+            handleToggleChange('oauth_password_required', value)
+          }
+        />
       </Element>
     </Card>
   );
