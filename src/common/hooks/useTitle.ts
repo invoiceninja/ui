21af67056379392af79c2c60f1728875de6c5180
Benjamin Beganović <k1pstabug@gmail.com>
2022-01-17 14:24:46 +0100

    Support for changing title with useTitle hook

--- src/common/hooks/useTitle.ts
+++ src/common/hooks/useTitle.ts
@@ -11,14 +11,19 @@
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-export function useTitle(title: string, translate = true): void {
+export function useTitle(title: string, translate = true) {
   const [t] = useTranslation();
 
-  const [documentTitle] = useState(
-    `${import.meta.env.VITE_APP_TITLE}: ${translate ? t(title) : title}`
+  const [documentTitle, setDocumentTitle] = useState(
+    translate ? t(title) : title
   );
 
   useEffect(() => {
-    document.title = documentTitle;
+    document.title = `${import.meta.env.VITE_APP_TITLE}: ${documentTitle}`;
   }, [documentTitle]);
+
+  return {
+    documentTitle,
+    setDocumentTitle,
+  };
 }
