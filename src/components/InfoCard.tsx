c04f8dea626b6ee53bf120acec084f022c2cbd5f
Benjamin Beganović <k1pstabug@gmail.com>
2022-01-17 17:06:38 +0100

    Rename Statistic to InfoCard

---
+++ src/components/InfoCard.tsx
@@ -0,0 +1,28 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { ReactNode } from 'react';
+
+interface Props {
+  title: string;
+  value: ReactNode;
+  className?: string;
+}
+
+export function InfoCard(props: Props) {
+  return (
+    <div
+      className={`px-4 py-5 bg-white shadow rounded overflow-hidden sm:p-6 space-y-2 ${props.className}`}
+    >
+      <dd className="text-xl font-medium text-gray-900">{props.title}</dd>
+      <dt className="text-sm text-gray-500 truncate">{props.value}</dt>
+    </div>
+  );
+}
