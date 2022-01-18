2048e4e629da533dd4abf0b46f87d8afb84ab04a
Benjamin Beganović <k1pstabug@gmail.com>
2022-01-18 14:20:19 +0100

    Show invoices for the client

--- src/pages/clients/pages/Invoices.tsx
+++ src/pages/clients/pages/Invoices.tsx
@@ -8,6 +8,39 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { Link } from '@invoiceninja/forms';
+import { DataTable, DataTableColumns } from 'components/DataTable';
+import { useTranslation } from 'react-i18next';
+import { generatePath, useParams } from 'react-router-dom';
+
 export function Invoices() {
-  return <div>Show client invoices</div>;
+  const [t] = useTranslation();
+  const { id } = useParams();
+
+  const columns: DataTableColumns = [
+    {
+      id: 'number',
+      label: t('number'),
+      format: (value, resource) => (
+        <Link to={generatePath('/invoices/:id', { id: resource.id })}>
+          {value}
+        </Link>
+      ),
+    },
+    { id: 'date', label: t('date') },
+    { id: 'amount', label: t('amount') },
+    { id: 'balance', label: t('balance') },
+    { id: 'due_date', label: t('due_date') },
+    { id: 'status_id', label: t('status') },
+  ];
+
+  return (
+    <DataTable
+      resource="invoice"
+      endpoint={`/api/v1/invoices?client_id=${id}`}
+      columns={columns}
+      withResourcefulActions
+      bulkRoute="/api/v1/invoices/bulk"
+    />
+  );
 }
