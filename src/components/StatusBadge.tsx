e72c8d540118e801de5d39d2e7e6e3784779696c
Benjamin Beganović <k1pstabug@gmail.com>
2022-01-18 17:17:26 +0100

    StatusBadge component

---
+++ src/components/StatusBadge.tsx
@@ -0,0 +1,26 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { useTranslation } from 'react-i18next';
+
+interface Props {
+  for: any;
+  code: string | number;
+}
+
+export function StatusBadge(props: Props) {
+  const [t] = useTranslation();
+
+  return (
+    <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
+      {t(props.for[props.code])}
+    </span>
+  );
+}
