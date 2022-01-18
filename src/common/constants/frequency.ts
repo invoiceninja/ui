51958097e729a585b060789441a9a61d86ceb5f3
Benjamin Beganović <k1pstabug@gmail.com>
2022-01-18 17:41:11 +0100

    Declare frequency const

---
+++ src/common/constants/frequency.ts
@@ -0,0 +1,24 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+export default {
+  '1': 'freq_daily',
+  '2': 'freq_weekly',
+  '3': 'freq_two_weeks',
+  '4': 'freq_four_weeks',
+  '5': 'freq_monthly',
+  '6': 'freq_two_months',
+  '7': 'freq_three_months',
+  '8': 'freq_four_months',
+  '9': 'freq_six_months',
+  '10': 'freq_annually',
+  '11': 'freq_two_years',
+  '12': 'freq_three_years',
+};
