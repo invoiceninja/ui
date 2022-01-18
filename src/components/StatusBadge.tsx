1299c49cb94459b84912aa92ba79f9eab0460e2b
Benjamin Beganović <k1pstabug@gmail.com>
2022-01-18 17:41:20 +0100

    Headless mode for status badge

--- src/components/StatusBadge.tsx
+++ src/components/StatusBadge.tsx
@@ -8,18 +8,25 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import classNames from 'classnames';
 import { useTranslation } from 'react-i18next';
 
 interface Props {
   for: any;
   code: string | number;
+  headless?: boolean;
 }
 
 export function StatusBadge(props: Props) {
   const [t] = useTranslation();
 
   return (
-    <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
+    <span
+      className={classNames({
+        'inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800':
+          !props.headless,
+      })}
+    >
       {t(props.for[props.code])}
     </span>
   );
