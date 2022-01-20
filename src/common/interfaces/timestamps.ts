beb18fc2f27535703383dd8a0e28a682b77f4c3d
Benjamin Beganović <k1pstabug@gmail.com>
2022-01-20 14:54:36 +0100

    Declare Timestamps interface

---
+++ src/common/interfaces/timestamps.ts
@@ -0,0 +1,15 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+export interface Timestamps {
+  created_at: number;
+  archived_at: number;
+  updated_at: number;
+}
