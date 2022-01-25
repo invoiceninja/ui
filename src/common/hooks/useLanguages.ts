4432e86fe157fd753ead812f774d82feb07edf64
Benjamin Beganović <k1pstabug@gmail.com>
2022-01-25 12:41:06 +0100

    useLanguages hook

---
+++ src/common/hooks/useLanguages.ts
@@ -0,0 +1,28 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { Language } from 'common/interfaces/language';
+import { useStaticsQuery } from 'common/queries/statics';
+import { useEffect, useState } from 'react';
+
+export function useLanguages(): Language[] {
+  const { data: statics } = useStaticsQuery();
+  const [languages, setLanguages] = useState<Language[]>([]);
+
+  console.log(statics);
+
+  useEffect(() => {
+    if (statics?.data?.languages) {
+      setLanguages(statics.data.languages);
+    }
+  }, [statics]);
+
+  return languages;
+}
