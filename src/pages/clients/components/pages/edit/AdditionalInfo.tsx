f001e38f25e3a15861a9d6822372aabbc9ad8c2f
Benjamin Beganović <k1pstabug@gmail.com>
2022-01-25 12:30:56 +0100

    Scaffold settings part of additional info

--- src/pages/clients/components/pages/edit/AdditionalInfo.tsx
+++ src/pages/clients/components/pages/edit/AdditionalInfo.tsx
@@ -8,7 +8,10 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { Card } from '@invoiceninja/cards';
+import { Tab } from '@headlessui/react';
+import { Card, Element } from '@invoiceninja/cards';
+import { InputField, SelectField } from '@invoiceninja/forms';
+import { TabGroup } from 'components/TabGroup';
 import { useTranslation } from 'react-i18next';
 
 export function AdditionalInfo() {
@@ -16,7 +19,43 @@ export function AdditionalInfo() {
 
   return (
     <Card className="mt-4" title={t('additional_info')}>
-      {/*  */}
+      <TabGroup
+        className="px-5"
+        tabs={[t('settings'), t('notes'), t('classify')]}
+      >
+        <Tab.Panel>
+          <Element leftSide={t('currency')}>
+            <SelectField />
+          </Element>
+
+          <Element leftSide={t('language')}>
+            <InputField id="language" />
+          </Element>
+
+          <Element leftSide={t('payment_terms')}>
+            <InputField id="invoice_payment_terms" />
+          </Element>
+
+          <Element leftSide={t('quote_valid_until')}>
+            <InputField id="quote_valid_until" />
+          </Element>
+
+          <Element leftSide={t('task_rate')}>
+            <InputField id="task_rate" />
+          </Element>
+
+          <Element leftSide={t('send_reminders')}>
+            <SelectField>
+              <option value=""></option>
+              <option value="enabled">{t('enabled')}</option>
+              <option value="disabled">{t('disabled')}</option>
+            </SelectField>
+          </Element>
+        </Tab.Panel>
+
+        <Tab.Panel></Tab.Panel>
+        <Tab.Panel></Tab.Panel>
+      </TabGroup>
     </Card>
   );
 }
