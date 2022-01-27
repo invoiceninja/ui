c7ce7505be0a7b56c79f2f05db88da46162f8598
Benjamin Beganović <k1pstabug@gmail.com>
2022-01-27 16:41:31 +0100

    Prefilling value for CustomField

--- src/pages/settings/custom-fields/components/Field.tsx
+++ src/pages/settings/custom-fields/components/Field.tsx
@@ -8,7 +8,7 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { ChangeEvent, useRef, useState } from 'react';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { CardContainer, Element } from '../../../../components/cards';
 import { InputField, SelectField } from '../../../../components/forms';
@@ -23,6 +23,7 @@ export enum AvailableTypes {
 
 interface Props {
   field: string;
+  initialValue?: string;
   placeholder: string;
   onChange?: (value: string, field: string, type: AvailableTypes) => unknown;
 }
@@ -30,13 +31,39 @@ interface Props {
 export function Field(props: Props) {
   const [t] = useTranslation();
 
+  const [initialValue, setInitialValue] = useState('');
+  const [dropdownInitialValue, setDropdownInitialValue] = useState('');
+
   const [dropdownType, setDropdownType] = useState<AvailableTypes>(
     AvailableTypes.SingleLineText
   );
 
+  const dropdownTypes = [
+    AvailableTypes.SingleLineText,
+    AvailableTypes.MultiLineText,
+    AvailableTypes.Switch,
+    AvailableTypes.Dropdown,
+    AvailableTypes.Date,
+  ];
+
   const inputRef = useRef<HTMLInputElement>();
   const dropdownInputRef = useRef<HTMLInputElement>();
   const dropdownTypeRef = useRef<HTMLSelectElement>();
+
+  useEffect(() => {
+    if (props.initialValue) {
+      const initialValueParts = props.initialValue.split('|');
+
+      if (dropdownTypes.includes(initialValueParts[1] as AvailableTypes)) {
+        setDropdownType(initialValueParts[1] as AvailableTypes);
+      } else {
+        setDropdownType(AvailableTypes.Dropdown);
+        setDropdownInitialValue(initialValueParts[1]);
+      }
+
+      setInitialValue(initialValueParts[0]);
+    }
+  }, []);
 
   const handleChange = () => {
     const type =
@@ -65,6 +92,7 @@ export function Field(props: Props) {
             id={props.field}
             placeholder={props.placeholder}
             onChange={handleChange}
+            value={initialValue}
           />
         }
       >
@@ -74,7 +102,7 @@ export function Field(props: Props) {
             setDropdownType(event.target.value as AvailableTypes);
             handleChange();
           }}
-          defaultValue={dropdownType}
+          value={dropdownType}
         >
           <option value="single_line_text">{t('single_line_text')}</option>
           <option value="multi_line_text">{t('multi_line_text')}</option>
@@ -91,6 +119,7 @@ export function Field(props: Props) {
             id="multi_line_text"
             placeholder={t('comma_sparated_list')}
             onChange={handleChange}
+            value={dropdownInitialValue}
           />
         </CardContainer>
       )}
