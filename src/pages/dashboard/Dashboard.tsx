c06167004d17dbf642342552f787036d957a7b2c
THEO-184 <theophilusboakye47@gmail.com>
2022-01-27 17:27:31 +0000

    setup dashboard cards

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -12,9 +12,37 @@ import { Breadcrumbs } from 'components/Breadcrumbs';
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Default } from '../../components/layouts/Default';
+import { Card } from '@invoiceninja/cards';
+import { useAccentColor } from 'common/hooks/useAccentColor';
+
+export interface DashboardCard {
+  title: string;
+  amount: string;
+  sold: string;
+}
 
 export function Dashboard() {
+  const accentColor = useAccentColor();
   const [t] = useTranslation();
+  // columns
+  const columns: DashboardCard[] = [
+    {
+      title: 'TOTAL REVENUE',
+      amount: 'ZK2,974.37',
+      sold: 'ZK0.99',
+    },
+    {
+      title: 'TOTAL EXPENSES',
+      amount: 'ZK0.00',
+      sold: 'ZK0.00',
+    },
+    {
+      title: 'OUTSTANDING',
+      amount: 'ZK7,434.00',
+      sold: 'ZK0.00',
+    },
+  ];
+  const css: React.CSSProperties = {};
 
   useEffect(() => {
     document.title = `${import.meta.env.VITE_APP_TITLE}: ${t('dashboard')}`;
@@ -25,6 +53,27 @@ export function Dashboard() {
   return (
     <Default title={t('dashboard')}>
       <Breadcrumbs pages={navigation} />
+      <div className="container mx-auto my-6">
+        <div className="grid grid-cols-12 space-y-4 lg:space-y-0 lg:gap-4">
+          {columns.map((card) => {
+            const { title, amount, sold } = card;
+            return (
+              <Card className="col-span-12 lg:col-span-4 py-3 px-5">
+                <h1 className="text-gray-800">{title}</h1>
+                <div className="flex flex-row justify-between my-1">
+                  <div className="font-bold">{amount}</div>
+                  <div style={{ color: accentColor }}>
+                    {sold}
+                    <p className="text-sm text-right text-sky-700">
+                      Last 30 days
+                    </p>
+                  </div>
+                </div>
+              </Card>
+            );
+          })}
+        </div>
+      </div>
     </Default>
   );
 }
--- src/pages/settings/company/CompanyDetails.tsx
+++ src/pages/settings/company/CompanyDetails.tsx
@@ -92,3 +92,4 @@ export function CompanyDetails() {
     </Settings>
   );
 }
+
