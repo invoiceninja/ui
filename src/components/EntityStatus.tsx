1168cb838d6c23b14bc9cbaf028ff3ed4af7f82b
Benjamin Beganović <k1pstabug@gmail.com>
2022-02-01 14:36:49 +0100

    Component to show status badge for entity state

---
+++ src/components/EntityStatus.tsx
@@ -0,0 +1,39 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { EntityState } from 'common/enums/entity-state';
+import { getEntityState } from 'common/helpers';
+import { Product } from 'common/interfaces/product';
+import { Badge } from 'components/Badge';
+import { useTranslation } from 'react-i18next';
+
+interface Props {
+  entity: unknown;
+}
+
+export function EntityStatus(props: Props) {
+  const [t] = useTranslation();
+
+  const state = getEntityState(props.entity);
+
+  if (state === EntityState.Active) {
+    return <Badge variant="primary">{t('active')}</Badge>;
+  }
+
+  if (state === EntityState.Archived) {
+    return <Badge variant="yellow">{t('archived')}</Badge>;
+  }
+
+  if (state === EntityState.Deleted) {
+    return <Badge variant="red">{t('deleted')}</Badge>;
+  }
+
+  return <></>;
+}
