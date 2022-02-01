c8a6c209562da3845fb67903885d97ccc47bcb32
Benjamin Beganović <k1pstabug@gmail.com>
2022-02-01 15:16:27 +0100

    Deleting products

--- src/pages/products/edit/Edit.tsx
+++ src/pages/products/edit/Edit.tsx
@@ -22,6 +22,7 @@ import { useTranslation } from 'react-i18next';
 import { useQueryClient } from 'react-query';
 import { generatePath, useParams } from 'react-router-dom';
 import { Archive } from './components/Archive';
+import { Delete } from './components/Delete';
 import { Restore } from './components/Restore';
 
 export function Edit() {
@@ -118,12 +119,20 @@ export function Edit() {
       )}
 
       {product &&
-        getEntityState(product.data.data) === EntityState.Archived && (
+        (getEntityState(product.data.data) === EntityState.Archived ||
+          getEntityState(product.data.data) === EntityState.Deleted) && (
           <Restore
             id={product.data.data.id}
             endpoint={generatePath('/api/v1/products/:id', { id })}
           />
         )}
+
+      {product && getEntityState(product.data.data) === EntityState.Active && (
+        <Delete
+          id={product.data.data.id}
+          endpoint={generatePath('/api/v1/products/:id', { id })}
+        />
+      )}
     </>
   );
 }
