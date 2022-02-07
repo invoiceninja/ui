21b142f157cf58a8638f2a35c8027daa07692c90
Benjamin Beganović <k1pstabug@gmail.com>
2022-02-07 14:57:18 +0100

    Fetching and showing clients on invoice create page

--- src/pages/invoices/create/Create.tsx
+++ src/pages/invoices/create/Create.tsx
@@ -8,6 +8,22 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { Card } from '@invoiceninja/cards';
+import { useTitle } from 'common/hooks/useTitle';
+import { Default } from 'components/layouts/Default';
+import { useTranslation } from 'react-i18next';
+import { Client } from './components/Client';
+
 export function Create() {
-  return <div>Creating invoices</div>;
+  const [t] = useTranslation();
+
+  useTitle('new_invoice');
+
+  return (
+    <Default title={t('new_invoice')}>
+      <div className="grid grid-cols-12 gap-4">
+        <Client />
+      </div>
+    </Default>
+  );
 }
---
+++ src/pages/invoices/create/components/Client.tsx
@@ -0,0 +1,64 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { Card } from '@invoiceninja/cards';
+import { Button, InputLabel } from '@invoiceninja/forms';
+import axios from 'axios';
+import { endpoint } from 'common/helpers';
+import { Client as IClient } from 'common/interfaces/client';
+import { defaultHeaders } from 'common/queries/common/headers';
+import { debounce } from 'lodash';
+import { useTranslation } from 'react-i18next';
+import AsyncSelect from 'react-select/async';
+
+interface IClientArrayRecord {
+  value: string;
+  label: string;
+}
+
+export function Client() {
+  const [t] = useTranslation();
+
+  const debouncedSearch = debounce((query, resolve) => {
+    axios
+      .get(endpoint(`/api/v1/clients?filter=${query}`), {
+        headers: defaultHeaders,
+      })
+      .then((response) => {
+        const array: IClientArrayRecord[] = [];
+
+        response?.data?.data?.map((client: IClient) =>
+          array.push({ value: client.id, label: client.display_name })
+        );
+
+        resolve(array);
+      });
+  }, 500);
+
+  const loadOptions = (query: string) => {
+    return new Promise((resolve) => debouncedSearch(query, resolve));
+  };
+
+  return (
+    <Card className="col-span-12 lg:col-span-6 xl:col-span-4" withContainer>
+      <div className="flex items-center justify-between">
+        <InputLabel>{t('client')}</InputLabel>
+        <Button type="minimal">{t('new_client')}</Button>
+      </div>
+
+      <AsyncSelect
+        cacheOptions
+        defaultOptions
+        loadOptions={loadOptions}
+        menuPortalTarget={document.body}
+      />
+    </Card>
+  );
+}
