70af0fbe0cb524263351cf7dd6b4d8785ae21d0f
Benjamin Beganović <k1pstabug@gmail.com>
2022-02-08 17:02:34 +0100

    wip

--- src/common/helpers/invoices/invoice-sum.ts
+++ src/common/helpers/invoices/invoice-sum.ts
@@ -2,13 +2,22 @@ import { InvoiceItemSum } from './invoice-item-sum';
 import { Invoice } from 'common/interfaces/invoice';
 
 export class InvoiceSum {
-  protected taxMap = new Map();
+  protected taxMap: Record<string, unknown>[] = [];
   protected invoiceItems = new InvoiceItemSum(this.invoice);
+  protected totalTaxMap: Record<string, unknown>[] = [];
+
+  protected totalDiscount = 0;
+  protected total = 0;
+  protected totalTaxes = 0;
+  protected totalCustomValues = 0;
 
   constructor(protected invoice: Invoice) {}
 
   public async build() {
     await this.calculateLineItems();
+    await this.calculateDiscount();
+    await this.calculateInvoiceTaxes();
+    await this.calculateCustomValues();
 
     return this;
   }
@@ -17,5 +26,142 @@ export class InvoiceSum {
     await this.invoiceItems.process();
 
     return this;
+  }
+
+  protected async calculateDiscount() {
+    this.totalDiscount = this.discount(this.invoiceItems.subTotal);
+    this.total -= this.totalDiscount;
+
+    return this;
+  }
+
+  protected async calculateInvoiceTaxes() {
+    if (this.invoice.tax_name1.length >= 1) {
+      let tax = this.taxer(this.total, this.invoice.tax_rate1);
+
+      tax += this.getSurchargeTaxTotalForKey(
+        this.invoice.tax_name1,
+        this.invoice.tax_rate1
+      );
+
+      this.totalTaxes += tax;
+
+      this.totalTaxMap.push({
+        name: `${this.invoice.tax_name1} ${parseFloat(
+          this.invoice.tax_rate1.toFixed(2)
+        )} %`,
+      });
+    }
+
+    if (this.invoice.tax_name2.length >= 1) {
+      let tax = this.taxer(this.total, this.invoice.tax_rate2);
+
+      tax += this.getSurchargeTaxTotalForKey(
+        this.invoice.tax_name2,
+        this.invoice.tax_rate2
+      );
+
+      this.totalTaxes += tax;
+
+      this.totalTaxMap.push({
+        name: `${this.invoice.tax_name2} ${parseFloat(
+          this.invoice.tax_rate2.toFixed(2)
+        )} %`,
+      });
+    }
+
+    if (this.invoice.tax_name3.length >= 1) {
+      let tax = this.taxer(this.total, this.invoice.tax_rate3);
+
+      tax += this.getSurchargeTaxTotalForKey(
+        this.invoice.tax_name3,
+        this.invoice.tax_rate3
+      );
+
+      this.totalTaxes += tax;
+
+      this.totalTaxMap.push({
+        name: `${this.invoice.tax_name3} ${parseFloat(
+          this.invoice.tax_rate3.toFixed(2)
+        )} %`,
+      });
+    }
+
+    return this;
+  }
+
+  protected async calculateCustomValues() {
+    this.totalCustomValues += this.valuer(this.invoice.custom_surcharge1);
+    this.totalCustomValues += this.valuer(this.invoice.custom_surcharge2);
+    this.totalCustomValues += this.valuer(this.invoice.custom_surcharge3);
+    this.totalCustomValues += this.valuer(this.invoice.custom_surcharge4);
+
+    return this;
+  }
+
+  protected getSurchargeTaxTotalForKey(taxName: string, rate: number) {
+    let taxComponent = 0;
+
+    if (this.invoice.custom_surcharge_tax1) {
+      taxComponent += parseFloat(
+        (this.invoice.custom_surcharge1 * (rate / 100)).toFixed(2)
+      );
+    }
+
+    if (this.invoice.custom_surcharge_tax2) {
+      taxComponent += parseFloat(
+        (this.invoice.custom_surcharge2 * (rate / 100)).toFixed(2)
+      );
+    }
+
+    if (this.invoice.custom_surcharge_tax3) {
+      taxComponent += parseFloat(
+        (this.invoice.custom_surcharge3 * (rate / 100)).toFixed(2)
+      );
+    }
+
+    if (this.invoice.custom_surcharge_tax4) {
+      taxComponent += parseFloat(
+        (this.invoice.custom_surcharge4 * (rate / 100)).toFixed(2)
+      );
+    }
+
+    return taxComponent;
+  }
+
+  protected async setTaxMap() {
+    if (this.invoice.is_amount_discount) {
+      this.invoiceItems.calculateTaxesWithAmountDiscount();
+    }
+
+    this.taxMap = [];
+  }
+
+  /////////////
+
+  protected discount(amount: number) {
+    // This needs extraction in the invoice service/class.
+
+    if (this.invoice.is_amount_discount) {
+      return this.invoice.discount;
+    }
+
+    return parseFloat((amount * (this.invoice.discount / 100)).toFixed(2));
+  }
+
+  protected taxer(amount: number, tax_rate: number) {
+    // This needs extraction in the taxer service/class.
+
+    return parseFloat((amount * ((tax_rate ?? 0) / 100)).toFixed(2));
+  }
+
+  protected valuer(customValue: number | undefined): number {
+    // This needs extraction in the custom valuer service/class.
+
+    if (typeof customValue === 'number') {
+      return customValue;
+    }
+
+    return 0;
   }
 }
