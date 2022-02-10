8ca93e03a81952d03a35d9a0a21d9a213cc25c17
Benjamin Beganović <k1pstabug@gmail.com>
2022-02-10 16:54:15 +0100

    Statics service to fetch with support for caching

---
+++ src/common/helpers/statics.ts
@@ -0,0 +1,29 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import axios from 'axios';
+import { defaultHeaders } from './../queries/common/headers';
+import { endpoint } from 'common/helpers';
+import { Statics as IStatics } from './../interfaces/statics';
+
+export class Statics {
+  protected static cache: IStatics;
+
+  public static query(): Promise<IStatics> {
+    if (this.cache) {
+      return new Promise((resolve) => resolve(this.cache));
+    }
+
+    return axios
+      .get(endpoint('/api/v1/statics'), { headers: defaultHeaders })
+      .then((response) => (this.cache = response.data))
+      .then(() => this.cache);
+  }
+}
