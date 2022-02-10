a951de424556e471ef27ebd4a8cc3a08fd191396
David Bomba <turbo124@gmail.com>
2022-02-11 10:35:40 +1100

    Tests for invoice calculation

--- tests/unit/helpers/invoice-sum.test.ts
+++ tests/unit/helpers/invoice-sum.test.ts
@@ -1,5 +1,9 @@
 import { InvoiceSum } from './../../../src/common/helpers/invoices/invoice-sum';
 import invoice from '../../helpers/data/invoice';
+import invoice_item from '../../helpers/data/invoice_item';
+import { InvoiceItem } from './../../../src/common/interfaces/invoice-item';
+import { InvoiceItemSum } from '../../../src/common/helpers/invoices/invoice-item-sum';
+
 
 describe('InvoiceSum test', () => {
   test('correct instance', () => {
@@ -13,7 +17,19 @@ describe('InvoiceSum test', () => {
 
 describe('InvoiceSum test invoice calculation', () => {
 
-  it('playground', async () => {
+
+  it('One Line Item', async () => {
+
+    invoice.line_items = invoice_item;
+
+    const invoiceSum = await new InvoiceSum(invoice).build();
+
+    expect(invoiceSum.invoice.amount).toEqual(10);
+    expect(invoiceSum.invoice.balance).toEqual(0);
+
+  });
+
+  it('Zero sum invoice', async () => {
 
     invoice.line_items = [];
     const invoiceSum = await new InvoiceSum(invoice).build();
@@ -22,5 +38,6 @@ describe('InvoiceSum test invoice calculation', () => {
     expect(invoiceSum.invoice.balance).toEqual(0);
 
   });
+
 
 });
