e2dc3d1e16ceaff9141fe2632c9219e499ef840a
David Bomba <turbo124@gmail.com>
2022-02-11 09:37:11 +1100

    Tests for number formatter

---
+++ tests/unit/helpers/invoice-number.test.ts
@@ -0,0 +1,27 @@
+import { InvoiceSum } from './../../../src/common/helpers/invoices/invoice-sum';
+import currencies from '../../helpers/data/currencies';
+import { Number } from './../../../src/common/helpers/number';
+
+describe('Test Access Currency Object', () => {
+
+    test('access', () => {
+
+        const usd_currency = currencies.filter(currency => currency.code == 'USD')[0];
+        expect(usd_currency.id).toEqual("1");
+        expect(usd_currency.precision).toEqual(2);
+    })
+
+})
+
+describe('Test Number Formatting USD', () => {
+
+    it('playground', async () => {
+
+        const usd_currency = currencies.filter(currency => currency.code == 'USD')[0];
+
+        const formatValue = Number.numberFormat(10, usd_currency);
+        expect(formatValue).toEqual("10.00");
+
+    });
+
+});
