2e240cb747f3c410e5e11025172d255a1e9b0b5f
Benjamin Beganović <k1pstabug@gmail.com>
2022-02-14 16:17:42 +0100

    Resolving column translations

--- src/pages/invoices/create/components/tables/Products.tsx
+++ src/pages/invoices/create/components/tables/Products.tsx
@@ -8,6 +8,41 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { Table, Tbody, Th, Thead } from '@invoiceninja/tables';
+import { useCurrentCompany } from 'common/hooks/useCurrentCompany';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
 export function Products() {
-  return <div>Products table</div>;
+  const [t] = useTranslation();
+  const company = useCurrentCompany();
+  const [columns, setColumns] = useState([]);
+
+  useEffect(() => {
+    setColumns(company?.settings.pdf_variables.product_columns || []);
+  }, [company]);
+
+  const resolveTranslation = (key: string) => {
+    if (key.startsWith('$product')) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const [product, string] = key.split('.');
+
+      return t(string);
+    }
+
+    return t(key);
+  };
+
+  return (
+    <div>
+      <Table>
+        <Thead>
+          {columns.map((column, index) => (
+            <Th key={index}>{resolveTranslation(column)}</Th>
+          ))}
+        </Thead>
+        <Tbody></Tbody>
+      </Table>
+    </div>
+  );
 }
