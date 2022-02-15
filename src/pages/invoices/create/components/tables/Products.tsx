5f4c24df7b7814df0544c8950101074d7c0dda7c
Benjamin Beganović <k1pstabug@gmail.com>
2022-02-15 14:00:43 +0100

    Basic type & key resolving

--- src/pages/invoices/create/components/tables/Products.tsx
+++ src/pages/invoices/create/components/tables/Products.tsx
@@ -8,41 +8,79 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { Table, Tbody, Th, Thead } from '@invoiceninja/tables';
+import { InputField } from '@invoiceninja/forms';
+import { Table, Tbody, Td, Th, Thead, Tr } from '@invoiceninja/tables';
 import { useCurrentCompany } from 'common/hooks/useCurrentCompany';
+import { InvoiceItem } from 'common/interfaces/invoice-item';
+import { RootState } from 'common/stores/store';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 
 export function Products() {
   const [t] = useTranslation();
   const company = useCurrentCompany();
+
   const [columns, setColumns] = useState([]);
+  const [lineItems, setLineItems] = useState<Partial<InvoiceItem>[]>([]);
+  const invoice = useSelector((state: RootState) => state.invoices.current);
 
   useEffect(() => {
     setColumns(company?.settings.pdf_variables.product_columns || []);
   }, [company]);
 
-  const resolveTranslation = (key: string) => {
-    if (key.startsWith('$product')) {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const [product, string] = key.split('.');
+  useEffect(() => {
+    if (typeof invoice?.line_items === 'string') {
+      // We are dealing with empty invoice from /create endpoint.
+      // An "empty" line item push is needed.
 
-      return t(string);
+      lineItems.push({
+        product_key: '',
+        quantity: 0,
+      });
+
+      setLineItems(lineItems);
     }
+  }, [invoice]);
 
-    return t(key);
+  const resolveKey = (key: string) => {
+    const [resource, property] = key.split('.');
+
+    return { resource, property };
+  };
+
+  const resolveType = (key: string) => {
+    const { property } = resolveKey(key);
+
+    return <InputField id={property} />
+  };
+
+  const resolveTranslation = (key: string) => {
+    const { property } = resolveKey(key);
+
+    return property ? t(property) : t(key);
   };
 
   return (
     <div>
-      <Table>
-        <Thead>
-          {columns.map((column, index) => (
-            <Th key={index}>{resolveTranslation(column)}</Th>
-          ))}
-        </Thead>
-        <Tbody></Tbody>
-      </Table>
+      {invoice && (
+        <Table>
+          <Thead>
+            {columns.map((column, index) => (
+              <Th key={index}>{resolveTranslation(column)}</Th>
+            ))}
+          </Thead>
+          <Tbody>
+            {lineItems.map((lineItem, index) => (
+              <Tr key={index}>
+                {columns.map((column, index) => (
+                  <Td key={index}>{resolveType(column)}</Td>
+                ))}
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      )}
     </div>
   );
 }
