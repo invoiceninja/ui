cfca89fa247ce7df24363cfa5e7ae02412e715db
Benjamin Beganović <k1pstabug@gmail.com>
2022-02-15 13:30:07 +0100

    Initialize blank invoice when visiting create page

--- src/pages/invoices/create/Create.tsx
+++ src/pages/invoices/create/Create.tsx
@@ -8,9 +8,12 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { useQuery } from 'common/hooks/useQuery';
 import { useTitle } from 'common/hooks/useTitle';
+import { Invoice } from 'common/interfaces/invoice';
 import { BreadcrumRecord } from 'components/Breadcrumbs';
 import { Default } from 'components/layouts/Default';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { generatePath } from 'react-router-dom';
 import { Client } from './components/clients/Client';
@@ -32,15 +35,27 @@ export function Create() {
     },
   ];
 
+  const [invoice, setInvoice] = useState<Invoice>();
+
+  const { data } = useQuery('/api/v1/invoices/create', {
+    refetchOnWindowFocus: false,
+  });
+
+  useEffect(() => {
+    setInvoice(data?.data.data || undefined);
+  }, [data]);
+
   return (
     <Default title={t('new_invoice')} breadcrumbs={pages}>
-      <div className="grid grid-cols-12 gap-4">
-        <Client />
-        <Details />
-        <Table />
-        <Footer />
-        <Totals />
-      </div>
+      {invoice && (
+        <div className="grid grid-cols-12 gap-4">
+          <Client />
+          <Details />
+          <Table />
+          <Footer />
+          <Totals />
+        </div>
+      )}
     </Default>
   );
 }
