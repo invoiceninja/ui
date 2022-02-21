789b6541d047053ba8e300c6d36c49f2ed3486fc
Benjamin Beganović <k1pstabug@gmail.com>
2022-02-21 15:02:40 +0100

    Extract resolving property

---
+++ src/pages/invoices/create/helpers/resolve-property.ts
@@ -0,0 +1,22 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { InvoiceItem } from 'common/interfaces/invoice-item';
+import { aliases } from 'common/stores/slices/invoices';
+import { resolveKey } from './resolve-key';
+
+export function resolveProperty(property: string) {
+  const { property: _property } = resolveKey(property);
+
+  return (
+    (aliases[_property] as keyof InvoiceItem) ||
+    (_property as keyof InvoiceItem)
+  );
+}
