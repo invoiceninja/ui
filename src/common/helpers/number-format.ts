9377525ae9915191a4b7e85d44d3a52ab75c6794
Benjamin Beganović <k1pstabug@gmail.com>
2022-02-22 13:10:09 +0100

    Extract numberFormat function to be currency-agnostic

---
+++ src/common/helpers/number-format.ts
@@ -0,0 +1,30 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+export function numberFormat(
+  value: string | number,
+  decimals = 0,
+  decimalSeparator = '.',
+  thousandSeparator = ','
+) {
+  const number = typeof value === 'string' ? parseFloat(value) : value;
+
+  const str = number.toFixed(decimals).toString().split('.');
+
+  const parts = [];
+
+  for (let i = str[0].length; i > 0; i -= 3) {
+    parts.unshift(str[0].substring(Math.max(0, i - 3), i));
+  }
+
+  str[0] = parts.join(thousandSeparator);
+
+  return str.join(decimalSeparator);
+}
