6a18741fc68d5321817f1b4aa14a836d1292c42c
Kemal <kemal.selimbasic.2016@gmail.com>
2022-02-23 10:45:48 +0100

    added recurring invoice constants

---
+++ src/common/enums/recurring-invoice.ts
@@ -0,0 +1,16 @@
+/**
+* Invoice Ninja (https://invoiceninja.com).
+*
+* @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+*
+* @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+*
+* @license https://www.elastic.co/licensing/elastic-license
+*/
+export const enum RecurringInvoice{
+    DRAFT = '1',
+    ACTIVE = '2',
+    PAUSED = '3',
+    COMPLETED = '4',
+    PENDING = '-1',
+}
