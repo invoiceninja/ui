8c28fa9fe067d260a5c413701c5b54b549edb3fa
Kemal <kemal.selimbasic.2016@gmail.com>
2022-02-23 20:54:17 +0100

    added data table to expenses page

--- src/pages/expenses/index/Expenses.tsx
+++ src/pages/expenses/index/Expenses.tsx
@@ -8,8 +8,55 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { date } from 'common/helpers';
+import { useCurrentCompanyDateFormats } from 'common/hooks/useCurrentCompanyDateFormats';
+import { DataTable, DataTableColumns } from 'components/DataTable';
+import { EntityStatus } from 'components/EntityStatus';
 import { Default } from 'components/layouts/Default';
+import { useTranslation } from 'react-i18next';
 
 export function Expenses() {
-  return <Default></Default>;
+  const [t] = useTranslation();
+  const { dateFormat } = useCurrentCompanyDateFormats();
+  const pages = [{ name: t('expenses'), href: '/expenses' }];
+  const columns: DataTableColumns = [
+    {
+      //no status field
+      id: 'status_id',
+      label: t('status'),
+    },
+    {
+      id: 'number',
+      label: t('number'),
+    },
+    { id: 'client_id', label: t('client') },
+    {
+      id: 'date',
+      label: t('date'),
+      format: (value) => date(value, dateFormat),
+    },
+    { id: 'amount', label: t('amount') },
+    { id: 'public_notes', label: t('public_notes') },
+    {
+      id: 'entity_status',
+      label: t('entity_status'),
+      format: (value, resource) => <EntityStatus entity={resource} />,
+    },
+  ];
+  return (
+    <Default
+      title={t('expenses')}
+      breadcrumbs={pages}
+      docsLink="docs/expenses/"
+    >
+      <DataTable
+        resource="expense"
+        endpoint="/api/v1/expenses"
+        columns={columns}
+        linkToCreate="/expenses/create"
+        linkToEdit="/expenses/:id/edit"
+        withResourcefulActions
+      />
+    </Default>
+  );
 }
