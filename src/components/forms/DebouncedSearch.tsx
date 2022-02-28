498f9a538c0ea7faa758d6d293bb5973dc57dfa1
Benjamin Beganović <k1pstabug@gmail.com>
2022-02-28 13:10:26 +0100

    Update debounced search component

--- src/components/forms/DebouncedSearch.tsx
+++ src/components/forms/DebouncedSearch.tsx
@@ -1,62 +1,100 @@
-/**
- * Invoice Ninja (https://invoiceninja.com).
- *
- * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
- *
- * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
- *
- * @license https://www.elastic.co/licensing/elastic-license
- */
-
+import { useEffect, useState } from 'react';
+import { Combobox } from '@headlessui/react';
+import { ChevronDown } from 'react-feather';
+import { clone, debounce } from 'lodash';
 import axios from 'axios';
 import { endpoint } from 'common/helpers';
 import { defaultHeaders } from 'common/queries/common/headers';
-import { debounce } from 'lodash';
-import AsyncSelect from 'react-select/async';
 
+interface Record {
+  value: string | number;
+  label: string;
+  internal: boolean;
+}
 interface Props {
   endpoint: string;
   value?: string;
   label: string;
-  onChange?: (value: { value: unknown; label: unknown }) => any;
-  className?: string;
+  placeholder?: string;
+  onChange: (value: Record) => any;
 }
 
+const internalRecord = { value: '', label: '', internal: true };
+
 export function DebouncedSearch(props: Props) {
-  const debouncedSearch = debounce((query, resolve) => {
+  const [records, setRecords] = useState<Record[]>([internalRecord]);
+  const [selectedOption, setSelectedOption] = useState(records[0]);
+
+  const debouncedSearch = debounce((query) => {
     axios
       .get(endpoint(`${props.endpoint}?filter=${query}`), {
         headers: defaultHeaders,
       })
       .then((response) => {
-        const array: Record<string, unknown>[] = [{ value: '', label: '' }];
+        const array: Record[] = [{ value: '', label: '', internal: true }];
 
         response?.data?.data?.map((resource: any) =>
           array.push({
             value: resource[props.value ?? 'id'],
             label: resource[props.label],
+            internal: false,
           })
         );
 
-        resolve(array);
+        setRecords(array);
       });
+
+    const internal = records.findIndex((record) => record.internal);
+
+    setRecords((current) => {
+      current[internal].label = query;
+      current[internal].value = query;
+
+      return current;
+    });
+
+    setSelectedOption(clone(records[0]));
   }, 500);
 
-  const loadOptions = (query: string) => {
-    return new Promise((resolve) => debouncedSearch(query, resolve));
-  };
+  useEffect(() => {
+    props.onChange(selectedOption);
+  }, [selectedOption]);
 
   return (
-    <AsyncSelect
-      className={props.className}
-      cacheOptions
-      defaultOptions
-      loadOptions={loadOptions}
-      menuPortalTarget={document.body}
-      onChange={(value) =>
-        props.onChange &&
-        props.onChange(value as { value: unknown; label: unknown })
-      }
-    />
+    <div className="w-full">
+      <Combobox value={selectedOption} onChange={setSelectedOption}>
+        <div className="relative mt-1">
+          <div className="relative w-full">
+            <Combobox.Input
+              placeholder={props.placeholder || ''}
+              className="w-full py-2 px-3 rounded text-sm text-gray-900 dark:bg-gray-800 dark:border-transparent dark:text-gray-100 border border-gray-300"
+              onChange={(event) => debouncedSearch(event.target.value)}
+              displayValue={(record) => record.label}
+            />
+            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronDown
+                className="w-5 h-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </Combobox.Button>
+          </div>
+        </div>
+        {records.filter((record) => !record.internal).length >= 1 && (
+          <Combobox.Options className="absolute w-80 py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-xl max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            {records
+              .filter((record) => !record.internal)
+              .map((record, index) => (
+                <Combobox.Option
+                  className="cursor-pointer select-none relative py-2 px-3 text-gray-900 hover:bg-gray-100"
+                  key={index}
+                  value={record}
+                >
+                  {record.label}
+                </Combobox.Option>
+              ))}
+          </Combobox.Options>
+        )}
+      </Combobox>
+    </div>
   );
 }
