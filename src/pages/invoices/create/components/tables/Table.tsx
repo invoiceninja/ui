ffd91dae99e03391e056928d050d5389ec85934a
Benjamin Beganović <k1pstabug@gmail.com>
2022-03-01 13:32:30 +0100

    Show only products table

--- src/pages/invoices/create/components/tables/Table.tsx
+++ src/pages/invoices/create/components/tables/Table.tsx
@@ -8,27 +8,12 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { Tab } from '@headlessui/react';
-import { TabGroup } from 'components/TabGroup';
-import { useTranslation } from 'react-i18next';
 import { Products } from './Products';
-import { Tasks } from './Tasks';
 
 export function Table() {
-  const [t] = useTranslation();
-
   return (
-    <>
-      <div className="col-span-12">
-        <TabGroup tabs={[t('products'), t('tasks')]}>
-          <Tab.Panel>
-            <Products />
-          </Tab.Panel>
-          <Tab.Panel>
-            <Tasks />
-          </Tab.Panel>
-        </TabGroup>
-      </div>
-    </>
+    <div className="col-span-12">
+      <Products />
+    </div>
   );
 }
