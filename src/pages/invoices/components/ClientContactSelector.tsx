cf7e0356de7a75dff99d41a03c7430db7b6fd6b6
Benjamin Beganović <k1pstabug@gmail.com>
2022-03-03 13:58:52 +0100

    Extract client contact selector for invoices

---
+++ src/pages/invoices/components/ClientContactSelector.tsx
@@ -0,0 +1,93 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { Checkbox } from '@invoiceninja/forms';
+import { ClientResolver } from 'common/helpers/clients/client-resolver';
+import { useCurrentInvoice } from 'common/hooks/useCurrentInvoice';
+import { Client } from 'common/interfaces/client';
+import {
+  setCurrentInvoicePropertySync,
+  toggleCurrentInvoiceInvitation,
+} from 'common/stores/slices/invoices';
+import { ChangeEvent, useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
+
+export function ClientContactSelector() {
+  const [client, setClient] = useState<Client>();
+  const invoice = useCurrentInvoice();
+  const clientResolver = new ClientResolver();
+  const dispatch = useDispatch();
+  const [t] = useTranslation();
+
+  const handleContactCheckboxChange = (contactId: string, value: boolean) => {
+    dispatch(toggleCurrentInvoiceInvitation({ contactId, checked: value }));
+  };
+
+  const handleCheckedState = (contactId: string) => {
+    const potential = invoice?.invitations.find(
+      (i) => i.client_contact_id === contactId
+    );
+
+    return Boolean(potential);
+  };
+
+  useEffect(() => {
+    if (invoice?.client_id) {
+      clientResolver
+        .find(invoice.client_id)
+        .then((client) => {
+          setClient(client);
+
+          dispatch(
+            setCurrentInvoicePropertySync({
+              property: 'invitations',
+              value: [],
+            })
+          );
+
+          client.contacts.map((contact) => {
+            if (contact.send_email) {
+              handleContactCheckboxChange(contact.id, true);
+            }
+          });
+        })
+        .catch((error) => console.error(error));
+    }
+  }, [invoice?.client_id]);
+
+  return (
+    <>
+      {client &&
+        client.contacts.map((contact, index) => (
+          <div key={index}>
+            <Checkbox
+              id={contact.id}
+              value={contact.id}
+              label={
+                contact.first_name.length >= 1
+                  ? `${contact.first_name} ${contact.last_name}`
+                  : t('blank_contact')
+              }
+              checked={handleCheckedState(contact.id)}
+              onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                handleContactCheckboxChange(
+                  event.target.value,
+                  event.target.checked
+                )
+              }
+            />
+
+            <span className="text-sm text-gray-700">{contact.email}</span>
+          </div>
+        ))}
+    </>
+  );
+}
