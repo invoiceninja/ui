eaeed744db2bc8fc2c14e73332199cdbce45c525
Benjamin Beganović <k1pstabug@gmail.com>
2022-03-03 14:26:54 +0100

    Extract handling line item propety change into hook

---
+++ src/pages/invoices/common/hooks/useHandleLineItemPropertyChange.ts
@@ -0,0 +1,27 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { InvoiceItem } from 'common/interfaces/invoice-item';
+import { setCurrentLineItemProperty } from 'common/stores/slices/invoices';
+import { useDispatch } from 'react-redux';
+
+export function useHandleLineItemPropertyChange() {
+  const dispatch = useDispatch();
+
+  return (key: keyof InvoiceItem, value: unknown, index: number) => {
+    dispatch(
+      setCurrentLineItemProperty({
+        position: index,
+        property: key,
+        value,
+      })
+    );
+  };
+}
