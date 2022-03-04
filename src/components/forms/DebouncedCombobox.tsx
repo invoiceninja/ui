06276e4d828753b431d69e8e9fabf156ae70a8ba
Benjamin Beganović <k1pstabug@gmail.com>
2022-03-04 10:44:10 +0100

    Relative position dropdown icon as indicator

--- src/components/forms/DebouncedCombobox.tsx
+++ src/components/forms/DebouncedCombobox.tsx
@@ -111,8 +111,12 @@ export function DebouncedCombobox(props: Props) {
               displayValue={(record: Record) => record.label}
               onFocus={() => openDropdownButton.current?.click()}
             />
+            <ChevronDown
+              className="absolute inset-y-0 mt-2 right-0 flex items-center pr-2 w-8 h-5 text-gray-400"
+              aria-hidden="true"
+            />
             <Combobox.Button
-              className="absolute inset-y-0 right-0 flex items-center pr-2"
+              className="hidden"
               // eslint-disable-next-line @typescript-eslint/ban-ts-comment
               // @ts-ignore
               ref={openDropdownButton}
