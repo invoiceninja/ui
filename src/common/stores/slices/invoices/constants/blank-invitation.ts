12b2d57fbf5d7f5e67b617757dbb7574198b1086
Benjamin Beganović <k1pstabug@gmail.com>
2022-03-04 17:14:14 +0100

    Export blank line item

---
+++ src/common/stores/slices/invoices/constants/blank-invitation.ts
@@ -0,0 +1,23 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+export const blankInvitation = {
+  client_contact_id: '',
+  is_deleted: false,
+  isChanged: false,
+  key: '',
+  link: '',
+  opened_date: '',
+  sent_date: '',
+  viewed_date: '',
+  created_at: 0,
+  archived_at: 0,
+  updated_at: 0,
+};
