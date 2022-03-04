2d912abb10d1b3de2003260ad8a554e5b6a87df7
Benjamin Beganović <k1pstabug@gmail.com>
2022-03-04 17:15:17 +0100

    Extract `setCurrentLineItemProperty` reducer

---
+++ src/common/stores/slices/invoices/extra-reducers/set-current-line-item-property.ts
@@ -0,0 +1,31 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { ClientResolver } from 'common/helpers/clients/client-resolver';
+import { CurrencyResolver } from 'common/helpers/currencies/currency-resolver';
+
+const clientResolver = new ClientResolver();
+const currencyResolver = new CurrencyResolver();
+
+export const setCurrentLineItemProperty = createAsyncThunk(
+  'invoices/setCurrentLineItemProperty',
+  async (payload: any, thunkApi) => {
+    const state = thunkApi.getState() as any;
+
+    const client = await clientResolver.find(state.invoices.current.client_id);
+    const currency = await currencyResolver.find(client.settings.currency_id); // or company currency
+
+    payload.client = client;
+    payload.currency = currency;
+
+    return payload;
+  }
+);
