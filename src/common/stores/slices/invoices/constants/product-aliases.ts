097a8a9564036a20cfbcde83bec88ac1e15f59a7
Benjamin Beganović <k1pstabug@gmail.com>
2022-03-04 17:14:44 +0100

    Export product aliases

---
+++ src/common/stores/slices/invoices/constants/product-aliases.ts
@@ -0,0 +1,15 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+export const aliases: Record<string, string> = {
+  item: 'product_key',
+  description: 'notes',
+  unit_cost: 'cost',
+};
