a1965211f11136f166f19b7d40ba6afaa4a470ae
Benjamin Beganović <k1pstabug@gmail.com>
2022-03-09 11:25:24 +0100

    useFormatMoney hook for recurring invoices

---
+++ src/pages/recurring-invoices/common/hooks/useFormatMoney.ts
@@ -0,0 +1,69 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import axios from 'axios';
+import { endpoint } from 'common/helpers';
+import { Number } from 'common/helpers/number';
+import { useCurrentRecurringInvoice } from 'common/hooks/useCurrentRecurringInvoice';
+import { useResolveClientCurrency } from 'common/hooks/useResolveClientCurrency';
+import { useResolveCountry } from 'common/hooks/useResolveCountry';
+import { Client } from 'common/interfaces/client';
+import { Country } from 'common/interfaces/country';
+import { Currency } from 'common/interfaces/currency';
+import { defaultHeaders } from 'common/queries/common/headers';
+import { useEffect, useState } from 'react';
+import { useQueryClient } from 'react-query';
+import { generatePath } from 'react-router-dom';
+
+export function useFormatMoney() {
+  const resolveCurrency = useResolveClientCurrency();
+  const resolveCountry = useResolveCountry();
+
+  const invoice = useCurrentRecurringInvoice();
+  const queryClient = useQueryClient();
+
+  const [country, setCountry] = useState<Country>();
+  const [currency, setCurrency] = useState<Currency>();
+  const [client, setClient] = useState<Client>();
+
+  useEffect(() => {
+    if (invoice?.client_id) {
+      queryClient
+        .fetchQuery(
+          generatePath('/api/v1/clients/:id', { id: invoice.client_id }),
+          () =>
+            axios.get(
+              endpoint('/api/v1/clients/:id', { id: invoice.client_id }),
+              {
+                headers: defaultHeaders,
+              }
+            ),
+          { staleTime: Infinity }
+        )
+        .then((response) => setClient(response.data.data))
+        .catch((error) => console.error(error));
+    }
+  }, [invoice]);
+
+  useEffect(() => {
+    if (client) {
+      setCurrency(resolveCurrency(client));
+      setCountry(resolveCountry(client.country_id));
+    }
+  }, [client]);
+
+  return (value: number | string) => {
+    if (currency && country) {
+      return Number.formatMoney(value, currency, country);
+    }
+
+    return value;
+  };
+}
