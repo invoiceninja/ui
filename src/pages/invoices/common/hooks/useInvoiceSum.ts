e62d622b4878980b611195d514124004ee3cf562
Benjamin Beganović <k1pstabug@gmail.com>
2022-03-14 17:22:38 +0100

    useInvoiceSum hook

---
+++ src/pages/invoices/common/hooks/useInvoiceSum.ts
@@ -0,0 +1,16 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { RootState } from 'common/stores/store';
+import { useSelector } from 'react-redux';
+
+export function useInvoiceSum() {
+  return useSelector((state: RootState) => state.invoices.invoiceSum);
+}
