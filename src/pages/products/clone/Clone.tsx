60f4decd41c289096d37d04325cf2df9f0cb23dd
notks <kemal.selimbasic.2016@gmail.com>
2022-03-15 17:54:48 +0100

    fixed product clone layout to work with router outlet

--- src/pages/products/clone/Clone.tsx
+++ src/pages/products/clone/Clone.tsx
@@ -14,9 +14,6 @@ import { endpoint } from 'common/helpers';
 import { defaultHeaders } from 'common/queries/common/headers';
 import { useProductQuery } from 'common/queries/products';
 import { Alert } from 'components/Alert';
-import { Breadcrumbs } from 'components/Breadcrumbs';
-import { Container } from 'components/Container';
-import { Default } from 'components/layouts/Default';
 import { Spinner } from 'components/Spinner';
 import { useFormik } from 'formik';
 import { useEffect, useState } from 'react';
@@ -27,14 +24,6 @@ import { CreateProductDto } from '..';
 export function Clone() {
   const [t] = useTranslation();
   const { id } = useParams();
-
-  const pages = [
-    { name: t('products'), href: '/products' },
-    {
-      name: t('clone_product'),
-      href: generatePath('/products/:id/clone', { id }),
-    },
-  ];
 
   const { data } = useProductQuery({ id });
   const [errors, setErrors] = useState<any>();
@@ -93,69 +82,61 @@ export function Clone() {
 
   if (!data) {
     return (
-      <Default>
-        <Container>
-          <div className="flex justify-center">
-            <Spinner />
-          </div>
-        </Container>
-      </Default>
+      <div className="flex justify-center">
+        <Spinner />
+      </div>
     );
   }
 
   return (
-    <Default>
-      <Container>
-        <Breadcrumbs pages={pages} />
+    <>
+      <h2 className="text-2xl">
+        {t('clone')}: {data.data.data.product_key}
+      </h2>
+      <div className="bg-white w-full p-8 rounded shadow my-4">
+        <form onSubmit={form.handleSubmit} className="space-y-6">
+          <InputField
+            label={t('product')}
+            id="product_key"
+            required
+            value={form.values.product_key || ''}
+            onChange={form.handleChange}
+          />
 
-        <h2 className="text-2xl">
-          {t('clone')}: {data.data.data.product_key}
-        </h2>
-        <div className="bg-white w-full p-8 rounded shadow my-4">
-          <form onSubmit={form.handleSubmit} className="space-y-6">
-            <InputField
-              label={t('product')}
-              id="product_key"
-              required
-              value={form.values.product_key || ''}
-              onChange={form.handleChange}
-            />
+          {errors?.product_key && (
+            <Alert type="danger">{errors.product_key}</Alert>
+          )}
 
-            {errors?.product_key && (
-              <Alert type="danger">{errors.product_key}</Alert>
+          <Textarea
+            label={t('notes')}
+            id="notes"
+            onChange={form.handleChange}
+            value={form.values.notes || ''}
+          />
+
+          {errors?.notes && <Alert type="danger">{errors.notes}</Alert>}
+
+          <InputField
+            label={t('cost')}
+            id="cost"
+            value={form.values.cost || ''}
+            onChange={form.handleChange}
+          />
+
+          {errors?.cost && <Alert type="danger">{errors.cost}</Alert>}
+
+          <div className="flex justify-end items-center space-x-2">
+            {!isFormBusy && (
+              <Button to="/products" type="secondary">
+                {t('cancel')}
+              </Button>
             )}
 
-            <Textarea
-              label={t('notes')}
-              id="notes"
-              onChange={form.handleChange}
-              value={form.values.notes || ''}
-            />
-
-            {errors?.notes && <Alert type="danger">{errors.notes}</Alert>}
-
-            <InputField
-              label={t('cost')}
-              id="cost"
-              value={form.values.cost || ''}
-              onChange={form.handleChange}
-            />
-
-            {errors?.cost && <Alert type="danger">{errors.cost}</Alert>}
-
-            <div className="flex justify-end items-center space-x-2">
-              {!isFormBusy && (
-                <Button to="/products" type="secondary">
-                  {t('cancel')}
-                </Button>
-              )}
-
-              <Button disabled={isFormBusy}>{t('save')}</Button>
-            </div>
-          </form>
-        </div>
-        {alert && <Alert type={alert?.type}>{alert?.message}.</Alert>}
-      </Container>
-    </Default>
+            <Button disabled={isFormBusy}>{t('save')}</Button>
+          </div>
+        </form>
+      </div>
+      {alert && <Alert type={alert?.type}>{alert?.message}.</Alert>}
+    </>
   );
 }
