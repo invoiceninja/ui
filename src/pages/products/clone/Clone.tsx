c2893ddcb6931a2639d3c8ac55c8fec9f8f28b78
notks <kemal.selimbasic.2016@gmail.com>
2022-03-16 21:02:16 +0100

    extracted create component and updated clone component to use create component

--- src/pages/products/clone/Clone.tsx
+++ src/pages/products/clone/Clone.tsx
@@ -8,135 +8,34 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { Button, InputField, Textarea } from '@invoiceninja/forms';
-import axios, { AxiosError, AxiosResponse } from 'axios';
-import { endpoint } from 'common/helpers';
-import { defaultHeaders } from 'common/queries/common/headers';
 import { useProductQuery } from 'common/queries/products';
-import { Alert } from 'components/Alert';
 import { Spinner } from 'components/Spinner';
-import { useFormik } from 'formik';
 import { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import { generatePath, useNavigate, useParams } from 'react-router';
-import { CreateProductDto } from '..';
+import { useParams } from 'react-router';
+import { CreateComponent, CreateProductDto } from '../create/CreateComponent';
 
 export function Clone() {
-  const [t] = useTranslation();
   const { id } = useParams();
 
-  const { data } = useProductQuery({ id });
-  const [errors, setErrors] = useState<any>();
-  const [isFormBusy, setIsFormBusy] = useState<boolean>(false);
-  const navigate = useNavigate();
-  const [alert, setAlert] = useState<
-    { type: string; message: string } | undefined
-  >(undefined);
-
-  const [initialValues, setInitialValues] = useState<CreateProductDto>({
-    product_key: '',
-    notes: '',
-    cost: '',
-  });
+  const { data: product } = useProductQuery({ id });
+  const [initialValues, setInitialValues] = useState<CreateProductDto>();
 
   useEffect(() => {
-    document.title = `${import.meta.env.VITE_APP_TITLE}: ${
-      data?.data.data.product_key
-    }`;
+    if (product) {
+      setInitialValues({
+        product_key: product?.data.data.product_key,
+        notes: product?.data.data.notes,
+        cost: product?.data.data.cost,
+        custom_value1: product?.data.data.custom_value1,
+        custom_value2: product?.data.data.custom_value2,
+        custom_value3: product?.data.data.custom_value3,
+        custom_value4: product?.data.data.custom_value4,
+      });
+    }
+    console.log('data:', initialValues);
+  }, [product]);
 
-    setInitialValues({
-      product_key: data?.data.data.product_key,
-      notes: data?.data.data.notes,
-      cost: data?.data.data.cost,
-    });
-  }, [data, location]);
-
-  const form = useFormik({
-    enableReinitialize: true,
-    initialValues,
-    onSubmit: (values: CreateProductDto) => {
-      setIsFormBusy(true);
-
-      axios
-        .post(endpoint('/api/v1/products'), values, { headers: defaultHeaders })
-        .then((response: AxiosResponse) =>
-          navigate(
-            generatePath('/products/:id/edit', { id: response.data.data.id }),
-            { state: { message: t('created_product') } }
-          )
-        )
-        .catch((error: AxiosError) => {
-          if (error.response?.status === 403) {
-            return navigate('/logout');
-          }
-
-          if (error.response?.status === 422) {
-            setErrors(error.response.data.errors);
-          }
-
-          setAlert(error?.response?.data.message);
-        })
-        .finally(() => setIsFormBusy(false));
-    },
-  });
-
-  if (!data) {
-    return (
-      <div className="flex justify-center">
-        <Spinner />
-      </div>
-    );
-  }
-
-  return (
-    <>
-      <h2 className="text-2xl">
-        {t('clone')}: {data.data.data.product_key}
-      </h2>
-      <div className="bg-white w-full p-8 rounded shadow my-4">
-        <form onSubmit={form.handleSubmit} className="space-y-6">
-          <InputField
-            label={t('product')}
-            id="product_key"
-            required
-            value={form.values.product_key || ''}
-            onChange={form.handleChange}
-          />
-
-          {errors?.product_key && (
-            <Alert type="danger">{errors.product_key}</Alert>
-          )}
-
-          <Textarea
-            label={t('notes')}
-            id="notes"
-            onChange={form.handleChange}
-            value={form.values.notes || ''}
-          />
-
-          {errors?.notes && <Alert type="danger">{errors.notes}</Alert>}
-
-          <InputField
-            label={t('cost')}
-            id="cost"
-            value={form.values.cost || ''}
-            onChange={form.handleChange}
-          />
-
-          {errors?.cost && <Alert type="danger">{errors.cost}</Alert>}
-
-          <div className="flex justify-end items-center space-x-2">
-            {!isFormBusy && (
-              <Button to="/products" type="secondary">
-                {t('cancel')}
-              </Button>
-            )}
-
-            <Button disabled={isFormBusy}>{t('save')}</Button>
-          </div>
-        </form>
-      </div>
-      {alert && <Alert type={alert?.type}>{alert?.message}.</Alert>}
-    </>
-  );
+  return initialValues?<CreateComponent product={initialValues} />:<Spinner/>;
+  
+  
 }
--- src/pages/products/create/Create.tsx
+++ src/pages/products/create/Create.tsx
@@ -1,148 +1,31 @@
 /**
- * Invoice Ninja (https://invoiceninja.com).
- *
- * @link https://github.com/invoiceninja/invoiceninja source repository
- *
- * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
- *
- * @license https://www.elastic.co/licensing/elastic-license
- */
+* Invoice Ninja (https://invoiceninja.com).
+*
+* @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+*
+* @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+*
+* @license https://www.elastic.co/licensing/elastic-license
+*/
 
-import axios, { AxiosError, AxiosResponse } from 'axios';
-import { defaultHeaders } from 'common/queries/common/headers';
-import { useFormik } from 'formik';
-import { useEffect, useState } from 'react';
-import toast from 'react-hot-toast';
-import { useTranslation } from 'react-i18next';
-import { generatePath, useNavigate } from 'react-router';
-import { InputField, Textarea } from '@invoiceninja/forms';
-import { endpoint } from 'common/helpers';
-import { Default } from 'components/layouts/Default';
-import { Container } from 'components/Container';
-import { Alert } from 'components/Alert';
-import { Card, Element } from '@invoiceninja/cards';
-import { useCurrentCompany } from 'common/hooks/useCurrentCompany';
-import { CustomField } from 'components/CustomField';
+import { Container } from "components/Container";
+import { Default } from "components/layouts/Default";
+import { useTranslation } from "react-i18next";
+import { CreateComponent } from "./CreateComponent";
 
-export interface CreateProductDto {
-  product_key: string;
-  notes: string;
-  cost: string;
-  custom_value1: string;
-  custom_value2: string;
-  custom_value3: string;
-  custom_value4: string;
-}
 
 export function Create() {
   const [t] = useTranslation();
-  const company = useCurrentCompany();
+
   const pages = [
     { name: t('products'), href: '/products' },
     { name: t('new_product'), href: '/products/create' },
   ];
-
-  useEffect(() => {
-    document.title = `${import.meta.env.VITE_APP_TITLE}: ${t('new_product')}`;
-  });
-
-  const navigate = useNavigate();
-  const [errors, setErrors] = useState<any>();
-
-  const formik = useFormik({
-    initialValues: {
-      product_key: '',
-      notes: '',
-      cost: '',
-      custom_value1: '',
-      custom_value2: '',
-      custom_value3: '',
-      custom_value4: '',
-    },
-    onSubmit: (values: CreateProductDto) => {
-      axios
-        .post(endpoint('/api/v1/products'), values, { headers: defaultHeaders })
-        .then((response: AxiosResponse) => {
-          toast.success(t('created_product'));
-
-          navigate(
-            generatePath('/products/:id/edit', { id: response.data.data.id }),
-            { state: { message: t('created_product') } }
-          );
-        })
-        .catch((error: AxiosError) =>
-          error.response?.status === 422
-            ? setErrors(error.response.data.errors)
-            : toast.error(t('error_title'))
-        )
-        .finally(() => formik.setSubmitting(false));
-    },
-  });
-
   return (
-    <Default breadcrumbs={pages}>
-      <Container>
-        <Card
-          title={t('new_product')}
-          withSaveButton
-          disableSubmitButton={formik.isSubmitting}
-          onFormSubmit={formik.handleSubmit}
-        >
-          <Element leftSide={t('product')} required>
-            <InputField
-              id="product_key"
-              required
-              onChange={formik.handleChange}
-            />
-            {errors?.product_key && (
-              <Alert type="danger">{errors.product_key}</Alert>
-            )}
-          </Element>
-          <Element leftSide={t('notes')}>
-            <Textarea id="notes" onChange={formik.handleChange} />
-            {errors?.notes && <Alert type="danger">{errors.notes}</Alert>}
-          </Element>
-          <Element leftSide={t('cost')}>
-            <InputField id="cost" onChange={formik.handleChange} />
-            {errors?.cost && <Alert type="danger">{errors.cost}</Alert>}
-          </Element>
-          {company?.custom_fields?.product1 && (
-            <CustomField
-              field="custom_value1"
-              defaultValue={formik.values.custom_value1}
-              value={company.custom_fields.product1}
-              onChange={(value) => formik.setFieldValue('custom_value1', value)}
-            />
-          )}
-
-          {company?.custom_fields?.product2 && (
-            <CustomField
-              field="custom_value2"
-              defaultValue={formik.values.custom_value2}
-              value={company.custom_fields.product2}
-              onChange={(value) => formik.setFieldValue('custom_value2', value)}
-            />
-          )}
-
-          {company?.custom_fields?.product3 && (
-            <CustomField
-              field="custom_value3"
-              defaultValue={formik.values.custom_value3}
-              value={company.custom_fields.product3}
-              onChange={(value) => formik.setFieldValue('custom_value3', value)}
-            />
-          )}
-
-          {company?.custom_fields?.product4 && (
-            <CustomField
-              field="custom_value4"
-              defaultValue={formik.values.custom_value4}
-              value={company.custom_fields.product4}
-              onChange={(value) => formik.setFieldValue('custom_value4', value)}
-            />
-          )}
-        </Card>
-      </Container>
-    </Default>
-  );
+   <Default breadcrumbs={pages}>
+<Container>
+  <CreateComponent/>
+</Container>
+   </Default>
+  )
 }
