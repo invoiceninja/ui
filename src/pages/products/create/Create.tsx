3c3012f1b3192671c5a9c463f1b366879987f5ed
notks <kemal.selimbasic.2016@gmail.com>
2022-03-16 16:55:13 +0100

    updated products create page tu use custom elements and added custom fields

--- src/pages/products/create/Create.tsx
+++ src/pages/products/create/Create.tsx
@@ -15,22 +15,28 @@ import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
 import { generatePath, useNavigate } from 'react-router';
-import { Button, InputField, Textarea } from '@invoiceninja/forms';
-import { Breadcrumbs } from 'components/Breadcrumbs';
+import { InputField, Textarea } from '@invoiceninja/forms';
 import { endpoint } from 'common/helpers';
 import { Default } from 'components/layouts/Default';
 import { Container } from 'components/Container';
 import { Alert } from 'components/Alert';
+import { Card, Element } from '@invoiceninja/cards';
+import { useCurrentCompany } from 'common/hooks/useCurrentCompany';
+import { CustomField } from 'components/CustomField';
 
 export interface CreateProductDto {
   product_key: string;
   notes: string;
   cost: string;
+  custom_value1: string;
+  custom_value2: string;
+  custom_value3: string;
+  custom_value4: string;
 }
 
 export function Create() {
   const [t] = useTranslation();
-
+  const company = useCurrentCompany();
   const pages = [
     { name: t('products'), href: '/products' },
     { name: t('new_product'), href: '/products/create' },
@@ -43,11 +49,15 @@ export function Create() {
   const navigate = useNavigate();
   const [errors, setErrors] = useState<any>();
 
-  const form = useFormik({
+  const formik = useFormik({
     initialValues: {
       product_key: '',
       notes: '',
       cost: '',
+      custom_value1: '',
+      custom_value2: '',
+      custom_value3: '',
+      custom_value4: '',
     },
     onSubmit: (values: CreateProductDto) => {
       axios
@@ -65,56 +75,73 @@ export function Create() {
             ? setErrors(error.response.data.errors)
             : toast.error(t('error_title'))
         )
-        .finally(() => form.setSubmitting(false));
+        .finally(() => formik.setSubmitting(false));
     },
   });
 
   return (
-    <Default>
+    <Default breadcrumbs={pages}>
       <Container>
-        <Breadcrumbs pages={pages} />
-
-        <h2 className="text-2xl">{t('new_product')}</h2>
-        <div className="bg-white w-full p-8 rounded shadow my-4">
-          <form onSubmit={form.handleSubmit} className="space-y-6">
+        <Card
+          title={t('new_product')}
+          withSaveButton
+          disableSubmitButton={formik.isSubmitting}
+          onFormSubmit={formik.handleSubmit}
+        >
+          <Element leftSide={t('product')} required>
             <InputField
-              label={t('product')}
               id="product_key"
               required
-              onChange={form.handleChange}
+              onChange={formik.handleChange}
             />
-
             {errors?.product_key && (
               <Alert type="danger">{errors.product_key}</Alert>
             )}
-
-            <Textarea
-              label={t('notes')}
-              id="notes"
-              onChange={form.handleChange}
-            />
-
+          </Element>
+          <Element leftSide={t('notes')}>
+            <Textarea id="notes" onChange={formik.handleChange} />
             {errors?.notes && <Alert type="danger">{errors.notes}</Alert>}
-
-            <InputField
-              label={t('cost')}
-              id="cost"
-              onChange={form.handleChange}
-            />
-
+          </Element>
+          <Element leftSide={t('cost')}>
+            <InputField id="cost" onChange={formik.handleChange} />
             {errors?.cost && <Alert type="danger">{errors.cost}</Alert>}
+          </Element>
+          {company?.custom_fields?.product1 && (
+            <CustomField
+              field="custom_value1"
+              defaultValue={formik.values.custom_value1}
+              value={company.custom_fields.product1}
+              onChange={(value) => formik.setFieldValue('custom_value1', value)}
+            />
+          )}
 
-            <div className="flex justify-end items-center space-x-2">
-              {!form.isSubmitting && (
-                <Button to="/products" type="secondary">
-                  {t('cancel')}
-                </Button>
-              )}
+          {company?.custom_fields?.product2 && (
+            <CustomField
+              field="custom_value2"
+              defaultValue={formik.values.custom_value2}
+              value={company.custom_fields.product2}
+              onChange={(value) => formik.setFieldValue('custom_value2', value)}
+            />
+          )}
 
-              <Button disabled={form.isSubmitting}>{t('save')}</Button>
-            </div>
-          </form>
-        </div>
+          {company?.custom_fields?.product3 && (
+            <CustomField
+              field="custom_value3"
+              defaultValue={formik.values.custom_value3}
+              value={company.custom_fields.product3}
+              onChange={(value) => formik.setFieldValue('custom_value3', value)}
+            />
+          )}
+
+          {company?.custom_fields?.product4 && (
+            <CustomField
+              field="custom_value4"
+              defaultValue={formik.values.custom_value4}
+              value={company.custom_fields.product4}
+              onChange={(value) => formik.setFieldValue('custom_value4', value)}
+            />
+          )}
+        </Card>
       </Container>
     </Default>
   );
