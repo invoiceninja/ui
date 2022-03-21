b8c189cf6069b497229f4ec8b2a0927990817383
Benjamin Beganović <k1pstabug@gmail.com>
2022-03-21 12:28:11 +0100

    Update invoice preview to use new component

--- src/pages/invoices/common/components/InvoicePreview.tsx
+++ src/pages/invoices/common/components/InvoicePreview.tsx
@@ -8,46 +8,32 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import axios from 'axios';
 import { endpoint } from 'common/helpers';
 import { useCurrentInvoice } from 'common/hooks/useCurrentInvoice';
-import { defaultHeaders } from 'common/queries/common/headers';
-import { useEffect, useRef } from 'react';
+import { InvoiceViewer } from './InvoiceViewer';
 
 interface Props {
   for: 'create' | 'invoice';
 }
 
 export function InvoicePreview(props: Props) {
-  const iframeRef = useRef<HTMLIFrameElement>(null);
   const invoice = useCurrentInvoice();
 
-  useEffect(() => {
-    if (invoice && invoice?.client_id) {
-      axios
-        .post(
+  if (invoice?.id && invoice?.client_id) {
+    return (
+      <InvoiceViewer
+        link={
           props.for === 'create'
             ? endpoint('/api/v1/live_preview?entity=invoice')
             : endpoint('/api/v1/live_preview?entity=invoice&entity_id=:id', {
                 id: invoice?.id,
-              }),
-          invoice,
-          {
-            responseType: 'arraybuffer',
-            headers: defaultHeaders,
-          }
-        )
-        .then((response) => {
-          const blob = new Blob([response.data], { type: 'application/pdf' });
-          const url = URL.createObjectURL(blob);
+              })
+        }
+        resource={invoice}
+        method="POST"
+      />
+    );
+  }
 
-          if (iframeRef.current) {
-            iframeRef.current.src = url;
-          }
-        })
-        .catch((error) => console.log(error));
-    }
-  }, [invoice]);
-
-  return <iframe ref={iframeRef} width="100%" height={1500} />;
+  return <></>;
 }
