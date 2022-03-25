21acf6ae3f79f429535254531f1bb4932786c1b3
Benjamin Beganović <k1pstabug@gmail.com>
2022-03-25 16:35:36 +0100

    useResolveTemplate based on param properties

--- src/pages/invoices/email/hooks/useResolveTemplate.ts
+++ src/pages/invoices/email/hooks/useResolveTemplate.ts
@@ -23,10 +23,14 @@ export interface EmailTemplate {
 }
 
 export function useResolveTemplate(
-  templateId: string,
+  body: string,
   entity: string,
-  entityId: string
+  entityId: string,
+  subject: string,
+  templateId: string
 ) {
+  console.log(subject);
+
   const queryClient = useQueryClient();
   const [template, setTemplate] = useState<EmailTemplate>();
 
@@ -36,10 +40,10 @@ export function useResolveTemplate(
         .post(
           endpoint('/api/v1/templates'),
           {
-            body: '',
+            body,
             entity,
             entity_id: entityId,
-            subject: '',
+            subject,
             template: templateId,
           },
           { headers: defaultHeaders }
@@ -47,7 +51,7 @@ export function useResolveTemplate(
         .then((response) => setTemplate(response.data))
         .catch((error) => console.error(error))
     );
-  }, [templateId]);
+  }, [body, entity, entityId, subject, templateId]);
 
   return template;
 }
