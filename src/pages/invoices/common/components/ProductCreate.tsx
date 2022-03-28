88133c0613061811233a28e2a98046e0e5bd08fb
Benjamin Beganović <k1pstabug@gmail.com>
2022-03-28 11:08:51 +0200

    Style fixes for the modal

--- src/pages/invoices/common/components/ProductCreate.tsx
+++ src/pages/invoices/common/components/ProductCreate.tsx
@@ -15,15 +15,11 @@ import { useCurrentCompany } from 'common/hooks/useCurrentCompany';
 import { useTitle } from 'common/hooks/useTitle';
 import { defaultHeaders } from 'common/queries/common/headers';
 import { useBlankProductQuery } from 'common/queries/products';
-import { Container } from 'components/Container';
 import { Modal } from 'components/Modal';
-import { Element } from '@invoiceninja/cards';
-
 import { useFormik } from 'formik';
 import { useState } from 'react';
 import toast from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
-import { Alert } from 'components/Alert';
 import { CustomField } from 'components/CustomField';
 import { CreateProductDto } from 'pages/products/common/components/CreateProduct';
 
@@ -31,14 +27,14 @@ interface Props {
   isModalOpen: boolean;
   setIsModalOpen?: any;
 }
+
 export function ProductCreate(props: Props) {
   const [t] = useTranslation();
   const { data: product } = useBlankProductQuery();
 
-  const company = useCurrentCompany();
-
   useTitle(t('new_product'));
 
+  const company = useCurrentCompany();
   const [errors, setErrors] = useState<any>();
 
   const formik = useFormik({
@@ -75,83 +71,81 @@ export function ProductCreate(props: Props) {
       onClose={props.setIsModalOpen}
       backgroundColor="gray"
     >
-      <Container>
-        <Element leftSide={t('product')} required>
-          <InputField
-            id="product_key"
-            value={formik.initialValues.product_key}
-            required
-            onChange={formik.handleChange}
-          />
-          {errors?.product_key && (
-            <Alert type="danger">{errors.product_key}</Alert>
-          )}
-        </Element>
-        <Element leftSide={t('notes')}>
-          <InputField
-            id="notes"
-            element="textarea"
-            value={formik.initialValues.notes}
-            onChange={formik.handleChange}
-          />
-          {errors?.notes && <Alert type="danger">{errors.notes}</Alert>}
-        </Element>
-        <Element leftSide={t('cost')}>
-          <InputField
-            id="cost"
-            value={formik.initialValues.cost}
-            onChange={formik.handleChange}
-          />
-          {errors?.cost && <Alert type="danger">{errors.cost}</Alert>}
-        </Element>
-        <Element leftSide={t('quantity')}>
-          <InputField
-            id="quantity"
-            value={formik.initialValues.quantity}
-            onChange={formik.handleChange}
-          />
-        </Element>
-        {company?.custom_fields?.product1 && (
-          <CustomField
-            field="custom_value1"
-            defaultValue={formik.values.custom_value1}
-            value={company.custom_fields.product1}
-            onChange={(value) => formik.setFieldValue('custom_value1', value)}
-          />
-        )}
+      <InputField
+        id="product_key"
+        value={formik.initialValues.product_key}
+        required
+        onChange={formik.handleChange}
+        label={t('product')}
+        errorMessage={errors?.product_key}
+      />
 
-        {company?.custom_fields?.product2 && (
-          <CustomField
-            field="custom_value2"
-            defaultValue={formik.values.custom_value2}
-            value={company.custom_fields.product2}
-            onChange={(value) => formik.setFieldValue('custom_value2', value)}
-          />
-        )}
+      <InputField
+        id="notes"
+        element="textarea"
+        value={formik.initialValues.notes}
+        onChange={formik.handleChange}
+        label={t('notes')}
+        errorMessage={errors?.notes}
+      />
 
-        {company?.custom_fields?.product3 && (
-          <CustomField
-            field="custom_value3"
-            defaultValue={formik.values.custom_value3}
-            value={company.custom_fields.product3}
-            onChange={(value) => formik.setFieldValue('custom_value3', value)}
-          />
-        )}
+      <InputField
+        id="cost"
+        value={formik.initialValues.cost}
+        onChange={formik.handleChange}
+        label={t('cost')}
+        errorMessage={errors?.cost}
+      />
 
-        {company?.custom_fields?.product4 && (
-          <CustomField
-            field="custom_value4"
-            defaultValue={formik.values.custom_value4}
-            value={company.custom_fields.product4}
-            onChange={(value) => formik.setFieldValue('custom_value4', value)}
-          />
-        )}
-      </Container>
+      <InputField
+        id="quantity"
+        value={formik.initialValues.quantity}
+        onChange={formik.handleChange}
+        label={t('quantity')}
+        errorMessage={errors?.quantity}
+      />
+
+      {company?.custom_fields?.product1 && (
+        <CustomField
+          field="custom_value1"
+          defaultValue={formik.values.custom_value1}
+          value={company.custom_fields.product1}
+          onChange={(value) => formik.setFieldValue('custom_value1', value)}
+        />
+      )}
+
+      {company?.custom_fields?.product2 && (
+        <CustomField
+          field="custom_value2"
+          defaultValue={formik.values.custom_value2}
+          value={company.custom_fields.product2}
+          onChange={(value) => formik.setFieldValue('custom_value2', value)}
+        />
+      )}
+
+      {company?.custom_fields?.product3 && (
+        <CustomField
+          field="custom_value3"
+          defaultValue={formik.values.custom_value3}
+          value={company.custom_fields.product3}
+          onChange={(value) => formik.setFieldValue('custom_value3', value)}
+        />
+      )}
+
+      {company?.custom_fields?.product4 && (
+        <CustomField
+          field="custom_value4"
+          defaultValue={formik.values.custom_value4}
+          value={company.custom_fields.product4}
+          onChange={(value) => formik.setFieldValue('custom_value4', value)}
+        />
+      )}
+
       <Button
         type="primary"
         behavior="button"
         onClick={() => {
-          formik.handleSubmit();
+          formik.submitForm();
         }}
       >
         {t('save')}
