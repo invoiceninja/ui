19472801591ccc7cab325bc69827b777db9ee62d
Benjamin Beganović <k1pstabug@gmail.com>
2022-03-29 16:56:52 +0200

    Spacing & licence

--- src/common/helpers/invoices/resolve-non-numeric-value.ts
+++ src/common/helpers/invoices/resolve-non-numeric-value.ts
@@ -1,9 +1,21 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
 import { ChangeEvent } from 'react';
 
 export function isNonNumericValue(event: ChangeEvent<HTMLInputElement>) {
   if (isNaN(Number(event.target.value)) || event.target.value == '') {
     event.target.value = '0';
+
     return true;
   }
+
   return false;
 }
