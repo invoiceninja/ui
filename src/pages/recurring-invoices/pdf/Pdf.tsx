462aa700809e61efb9e42f4023a992e161248495
Benjamin Beganović <k1pstabug@gmail.com>
2022-03-30 14:58:40 +0200

    Showing PDF for recurring invoice

--- src/pages/recurring-invoices/pdf/Pdf.tsx
+++ src/pages/recurring-invoices/pdf/Pdf.tsx
@@ -8,6 +8,36 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { useTitle } from 'common/hooks/useTitle';
+import { RecurringInvoice } from 'common/interfaces/recurring-invoice';
+import { useRecurringInvoiceQuery } from 'common/queries/recurring-invoices';
+import { Default } from 'components/layouts/Default';
+import { Spinner } from 'components/Spinner';
+import { InvoiceViewer } from 'pages/invoices/common/components/InvoiceViewer';
+import { generatePath, useParams } from 'react-router-dom';
+import { useGeneratePdfUrl } from '../common/hooks/useGeneratePdfUrl';
+
 export function Pdf() {
-  return <div>Show recurring invoice PDF</div>;
+  const { documentTitle } = useTitle('view_pdf');
+  const { id } = useParams();
+  const { data: recurringInvoice, isLoading } = useRecurringInvoiceQuery({
+    id,
+  });
+  const url = useGeneratePdfUrl();
+
+  return (
+    <Default
+      title={documentTitle}
+      onBackClick={generatePath('/recurring_invoices')}
+    >
+      {isLoading && <Spinner />}
+
+      {recurringInvoice && (
+        <InvoiceViewer
+          link={url(recurringInvoice.data.data as RecurringInvoice) as string}
+          method="GET"
+        />
+      )}
+    </Default>
+  );
 }
