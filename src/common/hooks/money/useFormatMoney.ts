b1ad551a607fce847437368abb11ae502e5f6763
Benjamin Beganović <k1pstabug@gmail.com>
2022-04-07 12:52:55 +0200

    useFormatMoney hook

---
+++ src/common/hooks/money/useFormatMoney.ts
@@ -0,0 +1,29 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { Number } from 'common/helpers/number';
+import { useResolveCountry } from '../useResolveCountry';
+import { useResolveCurrency } from '../useResolveCurrency';
+
+export function useFormatMoney() {
+  const resolveCountry = useResolveCountry();
+  const resolveCurrency = useResolveCurrency();
+
+  return (value: string | number, countryId: string, currencyId: string) => {
+    const country = resolveCountry(countryId);
+    const currency = resolveCurrency(currencyId);
+
+    if (country && currency) {
+      return Number.formatMoney(value, currency, country);
+    }
+
+    return value;
+  };
+}
