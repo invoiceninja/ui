8e005561b438a2e6e219f054f3c17ba5e397beeb
Benjamin Beganović <k1pstabug@gmail.com>
2022-04-12 14:24:16 +0200

    useHandleCompanySave hook

---
+++ src/pages/settings/common/hooks/useHandleCompanySave.ts
@@ -0,0 +1,45 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import axios, { AxiosError } from 'axios';
+import toast from 'react-hot-toast';
+import { endpoint } from 'common/helpers';
+import { useCompanyChanges } from 'common/hooks/useCompanyChanges';
+import { defaultHeaders } from 'common/queries/common/headers';
+import { updateRecord } from 'common/stores/slices/company-users';
+import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
+
+export function useHandleCompanySave() {
+  const [t] = useTranslation();
+  const companyChanges = useCompanyChanges();
+  const dispatch = useDispatch();
+
+  return () => {
+    const toastId = toast.loading(t('processing'));
+
+    axios
+      .put(
+        endpoint('/api/v1/companies/:id', { id: companyChanges?.id }),
+        companyChanges,
+        { headers: defaultHeaders }
+      )
+      .then((response) => {
+        dispatch(updateRecord({ object: 'company', data: response.data.data }));
+
+        toast.success(t('updated_settings'), { id: toastId });
+      })
+      .catch((error: AxiosError) => {
+        console.error(error);
+
+        toast.success(t('error_title'), { id: toastId });
+      });
+  };
+}
