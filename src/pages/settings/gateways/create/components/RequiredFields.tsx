58d339a03d18ea7adf2e24a68b046d5a34ef2905
Benjamin Beganović <k1pstabug@gmail.com>
2022-04-13 17:09:51 +0200

    Fixes for lint

--- src/pages/settings/gateways/create/components/RequiredFields.tsx
+++ src/pages/settings/gateways/create/components/RequiredFields.tsx
@@ -18,6 +18,7 @@ interface Props {
   gateway: Gateway;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function RequiredFields(props: Props) {
   const [t] = useTranslation();
 
