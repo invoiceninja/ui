f09bff6974ea413233bcafb92be35cc9a4dd6084
Benjamin Beganović <k1pstabug@gmail.com>
2022-04-14 10:52:43 +0200

    Inject blank company gateway into state

--- src/pages/settings/gateways/create/Create.tsx
+++ src/pages/settings/gateways/create/Create.tsx
@@ -11,9 +11,11 @@
 import { Card, Element } from '@invoiceninja/cards';
 import { SelectField } from '@invoiceninja/forms';
 import { useTitle } from 'common/hooks/useTitle';
+import { CompanyGateway } from 'common/interfaces/company-gateway';
 import { Gateway } from 'common/interfaces/statics';
+import { useBlankCompanyGatewayQuery } from 'common/queries/company-gateways';
 import { Settings } from 'components/layouts/Settings';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useGateways } from '../common/hooks/useGateways';
 import { Credentials } from './components/Credentials';
@@ -23,16 +25,26 @@ import { Settings as GatewaySettings } from './components/Settings';
 
 export function Create() {
   const { documentTitle } = useTitle('online_payments');
+  const { data: blankCompanyGateway } = useBlankCompanyGatewayQuery();
 
   const [t] = useTranslation();
 
   const gateways = useGateways();
 
+  const [companyGateway, setCompanyGateway] = useState<CompanyGateway>();
   const [gateway, setGateway] = useState<Gateway>();
 
   const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
     setGateway(gateways.find((gateway) => gateway.id === event.target.value));
   };
+
+  useEffect(() => {
+    if (blankCompanyGateway?.data.data) {
+      setCompanyGateway(blankCompanyGateway.data.data);
+    }
+  }, [blankCompanyGateway]);
+
+  console.log(companyGateway);
 
   return (
     <Settings title={documentTitle}>
