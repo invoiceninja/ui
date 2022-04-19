47e8990b0e50f9c90bb553ff5dc52d7764dc5796
Benjamin Beganović <k1pstabug@gmail.com>
2022-04-19 17:19:51 +0200

    Update recurring invoices products table to support sorting

--- src/pages/recurring-invoices/common/components/ProductsTable.tsx
+++ src/pages/recurring-invoices/common/components/ProductsTable.tsx
@@ -22,19 +22,27 @@ import { Plus, Trash2 } from 'react-feather';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
 import { useResolveInputField } from '../hooks/useResolveInputField';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import { useHandleSortingRows } from '../hooks/useHandleSortingRows';
 
 export function ProductsTable() {
   const [t] = useTranslation();
+
   const invoice = useCurrentRecurringInvoice();
   const columns = useProductColumns();
+
   const resolveTranslation = useResolveTranslation();
   const dispatch = useDispatch();
+
   const [isTaxModalOpen, setIsTaxModalOpen] = useState(false);
   const [isProductModalOpen, setIsProductModalOpen] = useState(false);
+
   const resolveInputField = useResolveInputField({
     setIsTaxModalOpen,
     setIsProductModalOpen,
   });
+
+  const onDragEnd = useHandleSortingRows();
 
   return (
     <div>
@@ -45,53 +53,78 @@ export function ProductsTable() {
           ))}
           <Th>{/* This is placeholder for "Remove" button. */}</Th>
         </Thead>
-        <Tbody>
-          {invoice?.client_id &&
-            invoice.line_items.map((lineItem, lineItemIndex) => (
-              <Tr key={lineItemIndex}>
-                {columns.map((column, columnIndex) => (
-                  <Td key={columnIndex}>
-                    {resolveInputField(column, lineItemIndex)}
-                  </Td>
-                ))}
-
-                <Td>
-                  {invoice && invoice.line_items.length > 1 && (
-                    <button
-                      className="text-gray-600 hover:text-red-600"
-                      onClick={() =>
-                        dispatch(deleteInvoiceLineItem(lineItemIndex))
-                      }
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="recurring-invoice-product-table">
+            {(provided) => (
+              <Tbody {...provided.droppableProps} innerRef={provided.innerRef}>
+                {invoice?.client_id &&
+                  invoice.line_items.map((lineItem, lineItemIndex) => (
+                    <Draggable
+                      key={lineItemIndex}
+                      draggableId={lineItemIndex.toString()}
+                      index={lineItemIndex}
                     >
-                      <Trash2 size={18} />
-                    </button>
-                  )}
-                </Td>
-              </Tr>
-            ))}
+                      {(provided) => (
+                        <Tr
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          innerRef={provided.innerRef}
+                          key={lineItemIndex}
+                        >
+                          {columns.map((column, columnIndex) => (
+                            <Td key={columnIndex}>
+                              {resolveInputField(column, lineItemIndex)}
+                            </Td>
+                          ))}
 
-          {invoice?.client_id && (
-            <Tr>
-              <Td colSpan={100}>
-                <button
-                  onClick={() => dispatch(injectBlankItemIntoCurrent())}
-                  className="w-full py-2 inline-flex justify-center items-center space-x-2"
-                >
-                  <Plus size={18} />
-                  <span>{t('add_item')}</span>
-                </button>
-              </Td>
-            </Tr>
-          )}
+                          <Td>
+                            {invoice &&
+                              (lineItem.product_key || lineItemIndex > 0) &&
+                              invoice.line_items.length > 0 && (
+                                <button
+                                  className="text-gray-600 hover:text-red-600"
+                                  onClick={() =>
+                                    dispatch(
+                                      deleteInvoiceLineItem(lineItemIndex)
+                                    )
+                                  }
+                                >
+                                  <Trash2 size={18} />
+                                </button>
+                              )}
+                          </Td>
+                        </Tr>
+                      )}
+                    </Draggable>
+                  ))}
 
-          {!invoice?.client_id && (
-            <Tr>
-              <Td colSpan={100}>{t('no_client_selected')}.</Td>
-            </Tr>
-          )}
-        </Tbody>
+                {invoice?.client_id && (
+                  <Tr>
+                    <Td colSpan={100}>
+                      <button
+                        onClick={() => dispatch(injectBlankItemIntoCurrent())}
+                        className="w-full py-2 inline-flex justify-center items-center space-x-2"
+                      >
+                        <Plus size={18} />
+                        <span>{t('add_item')}</span>
+                      </button>
+                    </Td>
+                  </Tr>
+                )}
+
+                {!invoice?.client_id && (
+                  <Tr>
+                    <Td colSpan={100}>{t('no_client_selected')}.</Td>
+                  </Tr>
+                )}
+              </Tbody>
+            )}
+          </Droppable>
+        </DragDropContext>
       </Table>
+
       <TaxCreate isVisible={isTaxModalOpen} onClose={setIsTaxModalOpen} />
+
       <ProductCreate
         setIsModalOpen={setIsProductModalOpen}
         isModalOpen={isProductModalOpen}
