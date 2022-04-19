e93bbd88b0eff3083c3fdaf3517ac222e442538e
Benjamin Beganović <k1pstabug@gmail.com>
2022-04-19 17:19:28 +0200

    useHandleSortingRows for recurring invoices

---
+++ src/pages/recurring-invoices/common/hooks/useHandleSortingRows.ts
@@ -0,0 +1,31 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { arrayMoveImmutable } from 'array-move';
+import { DropResult } from 'react-beautiful-dnd';
+import { useCurrentRecurringInvoice } from './useCurrentRecurringInvoice';
+import { useSetCurrentRecurringInvoiceProperty } from './useSetCurrentRecurringInvoiceProperty';
+
+export function useHandleSortingRows() {
+  const invoice = useCurrentRecurringInvoice();
+  const handleChange = useSetCurrentRecurringInvoiceProperty();
+
+  return (result: DropResult) => {
+    const sorted = invoice
+      ? arrayMoveImmutable(
+          invoice.line_items,
+          result.source.index,
+          result.destination?.index as unknown as number
+        )
+      : [];
+
+    handleChange('line_items', sorted);
+  };
+}
