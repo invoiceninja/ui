0336ad900a6c0e12e3a2e23edbf8ee0a0b17afe9
Benjamin Beganović <k1pstabug@gmail.com>
2022-04-21 23:54:26 +0200

    useCompanyGatewaysQuery

--- src/common/queries/company-gateways.ts
+++ src/common/queries/company-gateways.ts
@@ -14,6 +14,17 @@ import { useQuery } from 'react-query';
 import { generatePath } from 'react-router-dom';
 import { defaultHeaders } from './common/headers';
 
+export function useCompanyGatewaysQuery() {
+  return useQuery(
+    generatePath('/api/v1/company_gateways'),
+    () =>
+      axios.get(endpoint('/api/v1/company_gateways'), {
+        headers: defaultHeaders(),
+      }),
+    { staleTime: Infinity }
+  );
+}
+
 export function useCompanyGatewayQuery(params: { id: string | undefined }) {
   return useQuery(
     generatePath('/api/v1/company_gateways/:id', { id: params.id }),
