3355180684617e714c268e9a601dbc96d2c9ecb7
notks <kemal.selimbasic.2016@gmail.com>
2022-04-27 21:13:13 +0200

    CurrencyInput code cleanup

--- src/components/forms/CurrencyInput.tsx
+++ src/components/forms/CurrencyInput.tsx
@@ -14,15 +14,10 @@ import currency from 'currency.js';
 import { DebounceInput } from 'react-debounce-input';
 
 import CommonProps from '../../common/interfaces/common-props.interface';
-import { InputLabel } from './InputLabel';
 
 interface Props extends CommonProps {
-  label?: string;
   id?: string;
-  placeholder?: string;
-  required?: boolean;
   border?: boolean;
-  name?: string;
   errorMessage?: string | string[];
   currency?: Currency;
 
@@ -32,35 +27,39 @@ interface Props extends CommonProps {
 export function CurrencyInput(props: Props) {
   return (
     <section>
-      {props.label && (
-        <InputLabel className="mb-2" for={props.id}>
-          {props.label}
-          {props.required && <span className="ml-1 text-red-600">*</span>}
-        </InputLabel>
+      {props.currency && (
+        <DebounceInput
+          element={'input'}
+          debounceTimeout={300}
+          id={props.id}
+          type={'text'}
+          className={classNames(
+            `w-full py-2 px-3 rounded text-sm text-gray-900 dark:bg-gray-800 dark:border-transparent dark:text-gray-100 disabled:bg-gray-100 disabled:cursor-not-allowed ${props.className}`,
+            {
+              'border border-gray-300': props.border !== false,
+            }
+          )}
+          onChange={(event) => {
+            props.onChange &&
+              props.onChange(
+                String(
+                  currency(event.target.value, {
+                    separator: props.currency?.thousand_separator,
+                    decimal: props.currency?.decimal_separator,
+                    symbol: '',
+                    precision: props.currency?.precision,
+                  }).value
+                )
+              );
+          }}
+          value={currency(props.value, {
+            separator: props.currency?.thousand_separator,
+            decimal: props.currency?.decimal_separator,
+            symbol: '',
+            precision: props.currency?.precision,
+          }).format()}
+        />
       )}
-      <DebounceInput
-        element={'input'}
-        debounceTimeout={300}
-        id={props.id}
-        type={'text'}
-        className={classNames(
-          `w-full py-2 px-3 rounded text-sm text-gray-900 dark:bg-gray-800 dark:border-transparent dark:text-gray-100 disabled:bg-gray-100 disabled:cursor-not-allowed ${props.className}`,
-          {
-            'border border-gray-300': props.border !== false,
-          }
-        )}
-        onChange={(event) => {
-          props.onChange &&
-            props.onChange(String(currency(event.target.value).value));
-        }}
-        value={currency(props.value, {
-          separator: props.currency?.thousand_separator,
-          decimal: props.currency?.decimal_separator,
-          symbol: props.currency?.symbol,
-          precision: props.currency?.precision,
-        }).format()}
-      />
-
       {props.errorMessage && (
         <Alert className="mt-2" type="danger">
           {props.errorMessage}
