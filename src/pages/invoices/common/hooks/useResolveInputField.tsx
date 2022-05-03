312d79c0e99d4e8a4308b4bd78e4f3b0ac08176e
David Bomba <turbo124@gmail.com>
2022-05-03 21:35:17 +1000

    Clear taxes for invoices

--- src/pages/invoices/common/hooks/useResolveInputField.tsx
+++ src/pages/invoices/common/hooks/useResolveInputField.tsx
@@ -144,7 +144,7 @@ export function useResolveInputField(props: Props) {
               index);
           }}
           clearButton={Boolean(invoice?.line_items[index][property])}
-          disabled={Boolean(invoice?.line_items[index][property] as number > 0)}
+          disabled={Boolean(invoice?.line_items[index][property].value as number > 1)}
         />
       );
     }
