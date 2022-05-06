b25a52593c22eaea2dfff5cca1cc68a5b3e9a85d
Benjamin Beganović <k1pstabug@gmail.com>
2022-05-06 17:11:45 +0200

    Return same instance of client resolver (useClientResolver hook)

---
+++ src/common/hooks/clients/useClientResolver.ts
@@ -0,0 +1,17 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { ClientResolver } from 'common/helpers/clients/client-resolver';
+
+const clientResolver = new ClientResolver();
+
+export function useClientResolver() {
+  return clientResolver;
+}
