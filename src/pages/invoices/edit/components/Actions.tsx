cbe2ffd23a612c834355298cbe70318b316a98b9
Benjamin Beganović <k1pstabug@gmail.com>
2022-05-16 19:36:43 +0200

    Fixes for dropdown elements not dismissing after clicking on action

--- src/pages/invoices/edit/components/Actions.tsx
+++ src/pages/invoices/edit/components/Actions.tsx
@@ -37,84 +37,78 @@ export function Actions() {
   const restore = useHandleRestore();
   const destroy = useHandleDelete();
 
+  if (!invoice) {
+    return <Spinner />;
+  }
+
   return (
     <Dropdown label={t('more_actions')}>
-      {!invoice && (
-        <DropdownElement>
-          <Spinner />
+      <DropdownElement to={generatePath('/invoices/:id/pdf', { id })}>
+        {t('view_pdf')}
+      </DropdownElement>
+
+      <DropdownElement onClick={() => downloadPdf(invoice)}>
+        {t('download')}
+      </DropdownElement>
+
+      <DropdownElement to={generatePath('/invoices/:id/email', { id })}>
+        {t('email_invoice')}
+      </DropdownElement>
+
+      {invoice.status_id === InvoiceStatus.Draft && !invoice.is_deleted && (
+        <DropdownElement onClick={() => markSent(invoice)}>
+          {t('mark_sent')}
         </DropdownElement>
       )}
 
-      {invoice && (
-        <>
-          <DropdownElement to={generatePath('/invoices/:id/pdf', { id })}>
-            {t('view_pdf')}
+      {parseInt(invoice.status_id) < parseInt(InvoiceStatus.Paid) &&
+        !invoice.is_deleted && (
+          <DropdownElement onClick={() => markPaid(invoice)}>
+            {t('mark_paid')}
           </DropdownElement>
+        )}
 
-          <DropdownElement onClick={() => downloadPdf(invoice)}>
-            {t('download')}
-          </DropdownElement>
+      <DropdownElement onClick={() => invoice && openClientPortal(invoice)}>
+        {t('client_portal')}
+      </DropdownElement>
 
-          <DropdownElement to={generatePath('/invoices/:id/email', { id })}>
-            {t('email_invoice')}
-          </DropdownElement>
-
-          {invoice.status_id === InvoiceStatus.Draft && !invoice.is_deleted && (
-            <DropdownElement onClick={() => markSent(invoice)}>
-              {t('mark_sent')}
-            </DropdownElement>
+      {invoice && parseInt(invoice.status_id) < 4 && (
+        <DropdownElement
+          to={generatePath(
+            '/payments/create?invoice=:invoiceId&client=:clientId',
+            { invoiceId: invoice.id, clientId: invoice.client_id }
           )}
+        >
+          {t('apply_payment')}
+        </DropdownElement>
+      )}
 
-          {parseInt(invoice.status_id) < parseInt(InvoiceStatus.Paid) &&
-            !invoice.is_deleted && (
-              <DropdownElement onClick={() => markPaid(invoice)}>
-                {t('mark_paid')}
-              </DropdownElement>
-            )}
+      {invoice && <Divider withoutPadding />}
 
-          <DropdownElement onClick={() => invoice && openClientPortal(invoice)}>
-            {t('client_portal')}
-          </DropdownElement>
+      <DropdownElement
+        to={generatePath('/invoices/:id/clone', { id: invoice.id })}
+      >
+        {t('clone_to_invoice')}
+      </DropdownElement>
 
-          {invoice && parseInt(invoice.status_id) < 4 && (
-            <DropdownElement
-              to={generatePath(
-                '/payments/create?invoice=:invoiceId&client=:clientId',
-                { invoiceId: invoice.id, clientId: invoice.client_id }
-              )}
-            >
-              {t('apply_payment')}
-            </DropdownElement>
-          )}
+      <Divider withoutPadding />
 
-          {invoice && <Divider withoutPadding />}
+      {invoice.archived_at === 0 && (
+        <DropdownElement onClick={() => archive(invoice)}>
+          {t('archive')}
+        </DropdownElement>
+      )}
 
-          <DropdownElement
-            to={generatePath('/invoices/:id/clone', { id: invoice.id })}
-          >
-            {t('clone_to_invoice')}
-          </DropdownElement>
+      {invoice.archived_at > 0 && (
+        <DropdownElement onClick={() => restore(invoice)}>
+          {t('restore')}
+        </DropdownElement>
+      )}
 
-          <Divider withoutPadding />
-
-          {invoice.archived_at === 0 && (
-            <DropdownElement onClick={() => archive(invoice)}>
-              {t('archive')}
-            </DropdownElement>
-          )}
-
-          {invoice.archived_at > 0 && (
-            <DropdownElement onClick={() => restore(invoice)}>
-              {t('restore')}
-            </DropdownElement>
-          )}
-
-          {!invoice.is_deleted && (
-            <DropdownElement onClick={() => destroy(invoice)}>
-              {t('delete')}
-            </DropdownElement>
-          )}
-        </>
+      {!invoice.is_deleted && (
+        <DropdownElement onClick={() => destroy(invoice)}>
+          {t('delete')}
+        </DropdownElement>
       )}
     </Dropdown>
   );
