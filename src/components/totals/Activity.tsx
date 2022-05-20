e5a59a2737d4abcde8225970deac4f7e85b5d7a5
Benjamin Beganović <k1pstabug@gmail.com>
2022-05-20 17:02:47 +0200

    Refactor Activity component and remove redundant table

--- src/components/totals/Activity.tsx
+++ src/components/totals/Activity.tsx
@@ -8,78 +8,52 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { Table, Tbody, Tr, Td } from "@invoiceninja/tables";
-import { date, endpoint, trans } from "common/helpers";
-import { useCurrentCompanyDateFormats } from "common/hooks/useCurrentCompanyDateFormats";
-import { Spinner } from "components/Spinner";
+import { date, endpoint, trans } from 'common/helpers';
+import { useCurrentCompanyDateFormats } from 'common/hooks/useCurrentCompanyDateFormats';
+import { Spinner } from 'components/Spinner';
 import { request } from 'common/helpers/request';
-import { t } from "i18next";
-import { useQuery } from "react-query";
+import { useQuery } from 'react-query';
+import { Card } from '@invoiceninja/cards';
+import { useTranslation } from 'react-i18next';
+import { NonClickableElement } from 'components/cards/NonClickableElement';
+import { ActivityRecord } from 'common/interfaces/activity-record';
 
 export function Activity() {
-    
-    const { dateFormat } = useCurrentCompanyDateFormats();
+  const [t] = useTranslation();
+  const { dateFormat } = useCurrentCompanyDateFormats();
 
-    const { data, isLoading, isError } = useQuery('/api/v1/activities', () =>
-        request('GET', endpoint('/api/v1/activities'))
-    );
+  const { data, isLoading, isError } = useQuery('/api/v1/activities', () =>
+    request('GET', endpoint('/api/v1/activities'))
+  );
 
-    return (
-        <div className="bg-white shadow sm:rounded-lg">
-            <div className="divide-y divide-gray-200">
-                <div className="px-2 py-2 sm:px-6">
-                    <h2 id="notes-title" className="text-lg font-medium text-gray-900">{t('activity')}</h2>
-                </div>
-                <div className="">
-                    <Table>
-                        <Tbody>
-                            {isLoading && (
-                                <Tr>
-                                    <Td colSpan={100}>
-                                        <Spinner />
-                                    </Td>
-                                </Tr>
-                            )}
+  return (
+    <Card title={t('activity')}>
+      {isLoading && (
+        <NonClickableElement>
+          <Spinner />
+        </NonClickableElement>
+      )}
 
-                            {isError && (
-                                <Tr>
-                                    <Td className="text-center" colSpan={100}>
-                                        {t('error_refresh_page')}
-                                    </Td>
-                                </Tr>
-                            )}
+      {isError && (
+        <NonClickableElement>{t('error_refresh_page')}</NonClickableElement>
+      )}
 
-                            {data && data.data.data.length === 0 && (
-                                <Tr>
-                                    <Td colSpan={100}>{t('no_records_found')}</Td>
-                                </Tr>
-                            )}
+      {data?.data.data &&
+        data.data.data.map((record: ActivityRecord, index: number) => (
+          <NonClickableElement key={index}>
+            <span className="mr-1">{date(record.created_at, dateFormat)}:</span>
 
-                            {data &&
-                                data?.data?.data?.map((resource: any, index: number) => (
-                                    <Tr
-                                        key={index}
-                                        onClick={() => document.getElementById(resource.id)?.click()}
-                                    >
-                                        <td className='px-2 lg:px-2.5 xl:px-4 py-2 whitespace-nowrap text-sm text-gray-900'>
-                                            {date(resource.created_at, dateFormat)}  {trans(`activity_${resource.activity_type_id}`,
-                                                {
-                                                'client':resource.client_id, 
-                                                'contact':resource.client_contact_id, 
-                                                'quote':resource.quote_id,
-                                                'user':resource.user_id,
-                                                'expense':resource.expense_id,
-                                                'invoice':resource.invoice_id,
-                                                'recurring_invoice':resource.recurring_invoice_id,
-                                                }
-                                            )}
-                                        </td>
-                                    </Tr>
-                                ))}
-                        </Tbody>
-                    </Table>
-                </div>
-            </div>
-        </div>
-    );
+            {trans(`activity_${record.activity_type_id}`, {
+              client: record.client_id,
+              contact: record.contact_id,
+              quote: record.quote_id,
+              user: record.user_id,
+              expense: record.expense_id,
+              invoice: record.invoice_id,
+              recurring_invoice: record.recurring_invoice_id,
+            })}
+          </NonClickableElement>
+        ))}
+    </Card>
+  );
 }
