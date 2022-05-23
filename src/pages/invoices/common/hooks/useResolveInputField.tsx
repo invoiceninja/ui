6caba13f310b253c22470017693a6f5529d24541
Benjamin Beganović <k1pstabug@gmail.com>
2022-05-23 16:30:17 +0200

    Invoices: Custom fields support

--- src/pages/invoices/common/hooks/useResolveInputField.tsx
+++ src/pages/invoices/common/hooks/useResolveInputField.tsx
@@ -25,6 +25,8 @@ import { TaxRateSelector } from 'components/tax-rates/TaxRateSelector';
 import { useDispatch } from 'react-redux';
 import { setCurrentLineItemProperty } from 'common/stores/slices/invoices/extra-reducers/set-current-line-item-property';
 import { ProductSelector } from 'components/products/ProductSelector';
+import { useCurrentCompany } from 'common/hooks/useCurrentCompany';
+import { CustomField } from 'components/CustomField';
 
 const numberInputs = ['discount', 'cost', 'unit_cost', 'quantity'];
 const taxInputs = ['tax_rate1', 'tax_rate2', 'tax_rate3'];
@@ -41,6 +43,7 @@ export function useResolveInputField() {
   const handleProductChange = useHandleProductChange();
   const onChange = useHandleLineItemPropertyChange();
 
+  const company = useCurrentCompany();
   const invoice = useCurrentInvoice();
   const formatMoney = useFormatMoney();
   const getCurrency = useGetCurrencySeparators(setInputCurrencySeparators);
@@ -151,6 +154,31 @@ export function useResolveInputField() {
 
     if (['line_total'].includes(property)) {
       return formatMoney(invoice?.line_items[index][property] as number);
+    }
+
+    if (['product1', 'product2', 'product3', 'product4'].includes(property)) {
+      const field = property.replace(
+        'product',
+        'custom_value'
+      ) as keyof InvoiceItem;
+
+      return company.custom_fields?.[property] ? (
+        <CustomField
+          field={property}
+          defaultValue={invoice?.line_items[index][field]}
+          value={company.custom_fields?.[property]}
+          onChange={(value) => onChange(field, value, index)}
+          fieldOnly
+        />
+      ) : (
+        <InputField
+          id={property}
+          value={invoice?.line_items[index][property]}
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            onChange(property, event.target.value, index)
+          }
+        />
+      );
     }
 
     return (
