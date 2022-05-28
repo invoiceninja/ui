558e18839254484840cc4db58663387d926bef32
Benjamin Beganović <k1pstabug@gmail.com>
2022-05-28 15:19:59 +0200

    Update default layout with new components

--- src/components/layouts/Default.tsx
+++ src/components/layouts/Default.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable */
-
 /**
  * Invoice Ninja (https://invoiceninja.com).
  *
@@ -10,12 +8,10 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { Fragment, ReactNode, useState } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
+import { ReactNode, useState } from 'react';
 import {
   Home,
   Menu as MenuIcon,
-  X,
   Box,
   FileText,
   Settings,
@@ -23,26 +19,16 @@ import {
   PlusCircle,
   Repeat,
   CreditCard,
-  File,
-  Briefcase,
-  Truck,
-  DollarSign,
-  RefreshCcw,
 } from 'react-feather';
 import CommonProps from '../../common/interfaces/common-props.interface';
 import { useTranslation } from 'react-i18next';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { Button } from '@invoiceninja/forms';
-import { CompanySwitcher } from 'components/CompanySwitcher';
 import { Breadcrumbs, BreadcrumRecord } from 'components/Breadcrumbs';
-import { HelpSidebarIcons } from 'components/HelpSidebarIcons';
-import { useLogo } from 'common/hooks/useLogo';
 import { useSelector } from 'react-redux';
 import { RootState } from 'common/stores/store';
-
-function classNames(...classes: any) {
-  return classes.filter(Boolean).join(' ');
-}
+import { DesktopSidebar } from './components/DesktopSidebar';
+import { MobileSidebar } from './components/MobileSidebar';
 
 interface Props extends CommonProps {
   title?: string;
@@ -68,7 +54,6 @@ export function Default(props: Props) {
   const [t] = useTranslation();
 
   const location = useLocation();
-  const logo = useLogo();
 
   const navigation = [
     {
@@ -198,169 +183,14 @@ export function Default(props: Props) {
   return (
     <>
       <div>
-        <Transition.Root show={sidebarOpen} as={Fragment}>
-          <Dialog
-            as="div"
-            className="fixed inset-0 flex z-40 md:hidden"
-            onClose={setSidebarOpen}
-          >
-            <Transition.Child
-              as={Fragment}
-              enter="transition-opacity ease-linear duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity ease-linear duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75" />
-            </Transition.Child>
-            <Transition.Child
-              as={Fragment}
-              enter="transition ease-in-out duration-300"
-              enterFrom="-translate-x-full"
-              enterTo="translate-x-0"
-              leave="transition ease-in-out duration-300"
-              leaveFrom="translate-x-0"
-              leaveTo="-translate-x-full"
-            >
-              <div className="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-ninja-gray dark:bg-gray-900">
-                <Transition.Child
-                  as={Fragment}
-                  enter="ease-in-out duration-300"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="ease-in-out duration-300"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
-                  <div className="absolute top-0 right-0 -mr-12 pt-2">
-                    <button
-                      type="button"
-                      className="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                      onClick={() => setSidebarOpen(false)}
-                    >
-                      <span className="sr-only">Close sidebar</span>
-                      <X className="text-white" />
-                    </button>
-                  </div>
-                </Transition.Child>
+        <MobileSidebar
+          navigation={navigation}
+          sidebarOpen={sidebarOpen}
+          setSidebarOpen={setSidebarOpen}
+        />
 
-                <div className="flex-shrink-0 flex items-center px-4">
-                  <CompanySwitcher />
-                </div>
+        <DesktopSidebar navigation={navigation} docsLink={props.docsLink} />
 
-                <div className="mt-5 flex-1 h-0 overflow-y-auto">
-                  <nav className="space-y-1">
-                    {navigation.map((item) => (
-                      <div
-                        key={item.name}
-                        className={classNames(
-                          'flex items-center justify-between',
-                          item.current
-                            ? 'border-l-4 border-transparent bg-ninja-gray-darker text-gray-100 dark:bg-gray-700 dark:text-gray-100'
-                            : 'border-l-4 border-transparent text-gray-100 hover:bg-ninja-gray-darker dark:hover:bg-gray-700 dark:hover:text-gray-100',
-                          'group flex items-center justify-between px-4 text-sm font-medium'
-                        )}
-                      >
-                        <Link to={item.href} className="w-full">
-                          <div className="flex justify-start items-center my-2">
-                            <item.icon
-                              className={classNames(
-                                item.current
-                                  ? 'dark:text-gray-100'
-                                  : 'dark:group-hover:text-gray-100',
-                                'text-gray-100 mr-3 flex-shrink-0 h-6 w-6'
-                              )}
-                              aria-hidden="true"
-                            />
-                            {item.name}
-                          </div>
-                        </Link>
-
-                        {item.rightButton && (
-                          <Link
-                            to={item.rightButton.to}
-                            title={item.rightButton.label}
-                            className="hover:bg-gray-200 hover:bg-opacity-10 rounded-full p-1.5"
-                          >
-                            <item.rightButton.icon />
-                          </Link>
-                        )}
-                      </div>
-                    ))}
-                  </nav>
-                </div>
-              </div>
-            </Transition.Child>
-            <div className="flex-shrink-0 w-14" aria-hidden="true">
-              {/* Dummy element to force sidebar to shrink to fit close icon */}
-            </div>
-          </Dialog>
-        </Transition.Root>
-
-        {/* Static sidebar for desktop */}
-        <div
-          className={`hidden md:flex ${
-            isMiniSidebar ? 'md:w-16' : 'md:w-64'
-          } md:flex-col md:fixed md:inset-y-0 border-r`}
-          style={{ zIndex: 100 }}
-        >
-          {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex flex-col flex-grow border-gray-200 pt-5 bg-ninja-gray dark:bg-gray-800 dark:border-transparent overflow-y-auto">
-            <div className="flex items-center flex-shrink-0 px-4">
-              {isMiniSidebar ? (
-                <img className="w-8" src={logo} alt="Company logo" />
-              ) : (
-                <CompanySwitcher />
-              )}
-            </div>
-
-            <div className="mt-5 flex-grow flex flex-col">
-              <nav className="flex-1 pb-4 space-y-1">
-                {navigation.map((item) => (
-                  <div
-                    key={item.name}
-                    className={classNames(
-                      'flex items-center justify-between',
-                      item.current
-                        ? 'border-l-4 border-transparent bg-ninja-gray-darker text-gray-100 dark:bg-gray-700 dark:text-gray-100'
-                        : 'border-l-4 border-transparent text-gray-100 hover:bg-ninja-gray-darker dark:hover:bg-gray-700 dark:hover:text-gray-100',
-                      'group flex items-center justify-between px-4 text-sm font-medium'
-                    )}
-                  >
-                    <Link to={item.href} className="w-full">
-                      <div className="flex justify-start items-center my-2">
-                        <item.icon
-                          className={classNames(
-                            item.current
-                              ? 'dark:text-gray-100'
-                              : 'dark:group-hover:text-gray-100',
-                            'text-gray-100 mr-3 flex-shrink-0 h-6 w-6'
-                          )}
-                          aria-hidden="true"
-                        />
-                        {!isMiniSidebar && item.name}
-                      </div>
-                    </Link>
-
-                    {item.rightButton && !isMiniSidebar && (
-                      <Link
-                        to={item.rightButton.to}
-                        title={item.rightButton.label}
-                        className="hover:bg-gray-200 hover:bg-opacity-10 rounded-full p-1.5"
-                      >
-                        <item.rightButton.icon />
-                      </Link>
-                    )}
-                  </div>
-                ))}
-              </nav>
-
-              <HelpSidebarIcons docsLink={props.docsLink} />
-            </div>
-          </div>
-        </div>
         <div
           className={`${
             isMiniSidebar ? 'md:pl-16' : 'md:pl-64'
