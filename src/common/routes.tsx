8a011bc96ad4cf64429f1ed9bbae2c0c7f71d829
Benjamin Beganović <k1pstabug@gmail.com>
2022-06-01 16:36:20 +0200

    Extract authentication routes into separate file

--- src/common/routes.tsx
+++ src/common/routes.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable */
-
 /**
  * Invoice Ninja (https://invoiceninja.com).
  *
@@ -11,47 +9,21 @@
  */
 
 import { Route, Routes } from 'react-router';
-import { HostedRoute } from '../components/HostedRoute';
 import { PrivateRoute } from '../components/PrivateRoute';
-import { PublicRoute } from '../components/PublicRoute';
 import { Index } from '../pages/Index';
 import { Dashboard } from '../pages/dashboard/Dashboard';
-
-import * as Authentication from '../pages/authentication';
-import * as Products from '../pages/products';
-import * as Invoices from '../pages/invoices';
-import * as Settings from '../pages/settings';
-import * as Clients from '../pages/clients';
-import * as Payments from '../pages/payments';
-import * as RecurringInvoices from '../pages/recurring-invoices';
-import * as Credits from '../pages/credits';
-import * as Projects from '../pages/projects';
-import * as Vendors from '../pages/vendors';
-import * as Expenses from '../pages/expenses';
-import * as RecurringExpenses from '../pages/recurring-expenses';
-import { Unauthorized } from 'pages/errors/401';
-import { Guard } from './guards/Guard';
-import { permission } from './guards/guards/permission';
 import { invoiceRoutes } from 'pages/invoices/routes';
 import { clientRoutes } from 'pages/clients/routes';
 import { productRoutes } from 'pages/products/routes';
 import { recurringInvoiceRoutes } from 'pages/recurring-invoices/routes';
 import { paymentRoutes } from 'pages/payments/routes';
 import { settingsRoutes } from 'pages/settings/routes';
+import { authenticationRoutes } from 'pages/authentication/routes';
 
 export const routes = (
   <Routes>
     <Route path="/" element={<Index />} />
-    <Route element={<PublicRoute />}>
-      <Route path="/login" element={<Authentication.Login />} />
-      <Route
-        path="/recover_password"
-        element={<Authentication.RecoverPassword />}
-      />
-      <Route element={<HostedRoute />}>
-        <Route path="/register" element={<Authentication.Register />} />
-      </Route>
-    </Route>
+    {authenticationRoutes}
     <Route element={<PrivateRoute />}>
       <Route path="/dashboard" element={<Dashboard />} />
       {invoiceRoutes}
@@ -60,7 +32,6 @@ export const routes = (
       {recurringInvoiceRoutes}
       {paymentRoutes}
       {settingsRoutes}
-      <Route path="/logout" element={<Authentication.Logout />} />
     </Route>
   </Routes>
 );
---
+++ src/pages/authentication/routes.tsx
@@ -0,0 +1,34 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { HostedRoute } from 'components/HostedRoute';
+import { PrivateRoute } from 'components/PrivateRoute';
+import { PublicRoute } from 'components/PublicRoute';
+import { Route } from 'react-router-dom';
+import { Login } from './Login';
+import { Logout } from './Logout';
+import { RecoverPassword } from './RecoverPassword';
+import { Register } from './Register';
+
+export const authenticationRoutes = (
+  <>
+    <Route element={<PublicRoute />}>
+      <Route path="/login" element={<Login />} />
+      <Route path="/recover_password" element={<RecoverPassword />} />
+      <Route element={<HostedRoute />}>
+        <Route path="/register" element={<Register />} />
+      </Route>
+    </Route>
+
+    <Route element={<PrivateRoute />}>
+      <Route path="/logout" element={<Logout />} />
+    </Route>
+  </>
+);
