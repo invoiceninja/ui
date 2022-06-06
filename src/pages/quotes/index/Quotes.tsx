927945c6a2b5547bdf65e0d33d7320e705972023
Benjamin Beganović <k1pstabug@gmail.com>
2022-06-06 13:49:30 +0200

    Quotes datatable

--- src/pages/quotes/index/Quotes.tsx
+++ src/pages/quotes/index/Quotes.tsx
@@ -8,6 +8,79 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { Link } from '@invoiceninja/forms';
+import quoteStatus from 'common/constants/quote-status';
+import { date } from 'common/helpers';
+import { useFormatMoney } from 'common/hooks/money/useFormatMoney';
+import { useCurrentCompanyDateFormats } from 'common/hooks/useCurrentCompanyDateFormats';
+import { useTitle } from 'common/hooks/useTitle';
+import { Quote } from 'common/interfaces/quote';
+import { BreadcrumRecord } from 'components/Breadcrumbs';
+import { DataTable, DataTableColumns } from 'components/DataTable';
+import { Default } from 'components/layouts/Default';
+import { StatusBadge } from 'components/StatusBadge';
+import { useTranslation } from 'react-i18next';
+import { generatePath } from 'react-router-dom';
+
 export function Quotes() {
-  return <div>Page for quotes</div>;
+  const { documentTitle } = useTitle('quotes');
+  const { dateFormat } = useCurrentCompanyDateFormats();
+
+  const [t] = useTranslation();
+
+  const pages: BreadcrumRecord[] = [
+    { name: t('quotes'), href: generatePath('/quotes') },
+  ];
+
+  const formatMoney = useFormatMoney();
+
+  const columns: DataTableColumns = [
+    {
+      id: 'status_id',
+      label: t('status'),
+      format: (value) => <StatusBadge for={quoteStatus} code={value} />,
+    },
+    { id: 'number', label: t('number') },
+    {
+      id: 'client_id',
+      label: t('client'),
+      format: (_, resource: Quote) => (
+        <Link to={generatePath('/clients/:id', { id: resource.client_id })}>
+          {resource.client?.display_name}
+        </Link>
+      ),
+    },
+    {
+      id: 'amount',
+      label: t('amount'),
+      format: (value, resource) =>
+        formatMoney(
+          value,
+          resource?.client.country_id,
+          resource?.client.settings.currency_id
+        ),
+    },
+    {
+      id: 'date',
+      label: t('date'),
+      format: (value) => date(value, dateFormat),
+    },
+    {
+      id: 'valid_until',
+      label: t('valid_until'),
+      format: (value) => date(value, dateFormat),
+    },
+  ];
+
+  return (
+    <Default title={documentTitle} breadcrumbs={pages}>
+      <DataTable
+        resource="quote"
+        columns={columns}
+        endpoint="/api/v1/quotes?include=client"
+        linkToEdit="/quotes/:id/edit"
+        withResourcefulActions
+      />
+    </Default>
+  );
 }
