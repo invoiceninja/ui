4b2f55415fe63c6b8960a846a8a238d2b0f9422b
Benjamin Beganović <k1pstabug@gmail.com>
2022-06-06 17:12:09 +0200

    Allow delayed permission check

--- src/pages/errors/401.tsx
+++ src/pages/errors/401.tsx
@@ -8,19 +8,38 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { useTitle } from 'common/hooks/useTitle';
 import { Default } from 'components/layouts/Default';
+import { Spinner } from 'components/Spinner';
+import { useEffect, useState } from 'react';
 import { AlertTriangle } from 'react-feather';
+import { useTranslation } from 'react-i18next';
 
 export function Unauthorized() {
-  const { documentTitle } = useTitle('not_allowed');
+  const [t] = useTranslation();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+
+    return () => {
+      setIsLoading(true);
+    };
+  }, []);
 
   return (
     <Default>
       <div className="flex flex-col items-center mt-14 space-y-4">
-        <AlertTriangle size={128} />
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <>
+            <AlertTriangle size={128} />
 
-        <h1 className="text-2xl">{documentTitle}.</h1>
+            <h1 className="text-2xl">{t('not_allowed')}.</h1>
+          </>
+        )}
       </div>
     </Default>
   );
