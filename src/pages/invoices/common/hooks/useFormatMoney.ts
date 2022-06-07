fae8edecf6561876033bd343eefdb4058459969c
Benjamin Beganović <k1pstabug@gmail.com>
2022-06-07 17:36:20 +0200

    Make `useFormatMoney` hook resource-agnostic

--- src/pages/invoices/common/hooks/useFormatMoney.ts
+++ src/pages/invoices/common/hooks/useFormatMoney.ts
@@ -11,21 +11,25 @@
 import { endpoint } from 'common/helpers';
 import { Number } from 'common/helpers/number';
 import { request } from 'common/helpers/request';
-import { useCurrentInvoice } from 'common/hooks/useCurrentInvoice';
 import { useResolveClientCurrency } from 'common/hooks/useResolveClientCurrency';
 import { useResolveCountry } from 'common/hooks/useResolveCountry';
 import { Client } from 'common/interfaces/client';
 import { Country } from 'common/interfaces/country';
 import { Currency } from 'common/interfaces/currency';
+import { Invoice } from 'common/interfaces/invoice';
 import { useEffect, useState } from 'react';
 import { useQueryClient } from 'react-query';
 import { generatePath } from 'react-router-dom';
 
-export function useFormatMoney() {
+interface Props {
+  resource: Invoice;
+}
+
+export function useFormatMoney(props: Props) {
   const resolveCurrency = useResolveClientCurrency();
   const resolveCountry = useResolveCountry();
 
-  const invoice = useCurrentInvoice();
+  const resource = props.resource;
   const queryClient = useQueryClient();
 
   const [country, setCountry] = useState<Country>();
@@ -33,21 +37,21 @@ export function useFormatMoney() {
   const [client, setClient] = useState<Client>();
 
   useEffect(() => {
-    if (invoice?.client_id) {
+    if (resource?.client_id) {
       queryClient
         .fetchQuery(
-          generatePath('/api/v1/clients/:id', { id: invoice.client_id }),
+          generatePath('/api/v1/clients/:id', { id: resource.client_id }),
           () =>
             request(
               'GET',
-              endpoint('/api/v1/clients/:id', { id: invoice.client_id })
+              endpoint('/api/v1/clients/:id', { id: resource.client_id })
             ),
           { staleTime: Infinity }
         )
         .then((response) => setClient(response.data.data))
         .catch((error) => console.error(error));
     }
-  }, [invoice]);
+  }, [resource]);
 
   useEffect(() => {
     if (client) {
