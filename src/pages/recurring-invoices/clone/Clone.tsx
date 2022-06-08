293f6683d3af40a94e3fa01ff5caa0fbf03deca5
Benjamin Beganović <k1pstabug@gmail.com>
2022-06-08 11:31:07 +0200

    Recurring invoices Clone: Update usage of ClientSelector, InvoiceTotals

--- src/pages/recurring-invoices/clone/Clone.tsx
+++ src/pages/recurring-invoices/clone/Clone.tsx
@@ -12,7 +12,10 @@ import { useTitle } from 'common/hooks/useTitle';
 import { RecurringInvoice } from 'common/interfaces/recurring-invoice';
 import { ValidationBag } from 'common/interfaces/validation-bag';
 import { useRecurringInvoiceQuery } from 'common/queries/recurring-invoices';
-import { injectBlankItemIntoCurrent } from 'common/stores/slices/recurring-invoices';
+import {
+  injectBlankItemIntoCurrent,
+  toggleCurrentRecurringInvoiceInvitation,
+} from 'common/stores/slices/recurring-invoices';
 import { deleteRecurringInvoiceItem } from 'common/stores/slices/recurring-invoices/extra-reducers/delete-recurring-invoice-item';
 import { setCurrentLineItemProperty } from 'common/stores/slices/recurring-invoices/extra-reducers/set-current-line-item-property';
 import { setCurrentRecurringInvoice } from 'common/stores/slices/recurring-invoices/extra-reducers/set-current-recurring-invoice';
@@ -20,17 +23,18 @@ import { setCurrentRecurringInvoiceLineItem } from 'common/stores/slices/recurri
 import { BreadcrumRecord } from 'components/Breadcrumbs';
 import { Default } from 'components/layouts/Default';
 import { ValidationAlert } from 'components/ValidationAlert';
+import { ClientSelector } from 'pages/invoices/common/components/ClientSelector';
 import { InvoicePreview } from 'pages/invoices/common/components/InvoicePreview';
+import { InvoiceTotals } from 'pages/invoices/common/components/InvoiceTotals';
 import { ProductsTable } from 'pages/invoices/common/components/ProductsTable';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
 import { generatePath, useParams } from 'react-router-dom';
-import { ClientSelector } from '../common/components/ClientSelector';
 import { InvoiceDetails } from '../common/components/InvoiceDetails';
 import { InvoiceFooter } from '../common/components/InvoiceFooter';
-import { InvoiceTotals } from '../common/components/InvoiceTotals';
 import { useCurrentRecurringInvoice } from '../common/hooks/useCurrentRecurringInvoice';
+import { useInvoiceSum } from '../common/hooks/useInvoiceSum';
 import { useSetCurrentRecurringInvoiceProperty } from '../common/hooks/useSetCurrentRecurringInvoiceProperty';
 import { useHandleCreate } from '../create/hooks/useHandleCreate';
 
@@ -46,6 +50,8 @@ export function Clone() {
   const handleCreate = useHandleCreate(setErrors);
   const currentRecurringInvoice = useCurrentRecurringInvoice();
   const handleChange = useSetCurrentRecurringInvoiceProperty();
+
+  const invoiceSum = useInvoiceSum();
 
   const pages: BreadcrumRecord[] = [
     { name: t('recurring_invoices'), href: '/recurring_invoices' },
@@ -78,7 +84,21 @@ export function Clone() {
       {errors && <ValidationAlert errors={errors} />}
 
       <div className="grid grid-cols-12 gap-4">
-        <ClientSelector />
+        {currentRecurringInvoice && (
+          <ClientSelector
+            resource={currentRecurringInvoice}
+            onChange={(id) => handleChange('client_id', id)}
+            onClearButtonClick={() => handleChange('client_id', '')}
+            onContactCheckboxChange={(contactId, value) =>
+              dispatch(
+                toggleCurrentRecurringInvoiceInvitation({
+                  contactId,
+                  checked: value,
+                })
+              )
+            }
+          />
+        )}
         <InvoiceDetails />
 
         <div className="col-span-12">
@@ -109,7 +129,16 @@ export function Clone() {
         </div>
 
         <InvoiceFooter page="create" />
-        <InvoiceTotals />
+
+        {currentRecurringInvoice && (
+          <InvoiceTotals
+            resource={currentRecurringInvoice}
+            invoiceSum={invoiceSum}
+            onChange={(property, value) =>
+              handleChange(property as keyof RecurringInvoice, value)
+            }
+          />
+        )}
       </div>
 
       <div className="my-4">
