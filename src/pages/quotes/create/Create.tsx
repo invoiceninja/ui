c0ccb5ec114c38bd843ea53c34293e9f323495eb
Benjamin Beganović <k1pstabug@gmail.com>
2022-06-10 14:35:27 +0200

    Scaffold page for creating the quotes

--- src/pages/quotes/create/Create.tsx
+++ src/pages/quotes/create/Create.tsx
@@ -8,6 +8,123 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { useTitle } from 'common/hooks/useTitle';
+import { Quote } from 'common/interfaces/quote';
+import { useBlankQuoteQuery } from 'common/queries/quotes';
+import {
+  dismissCurrentQuote,
+  injectBlankItemIntoCurrent,
+  toggleCurrentQuoteInvitation,
+} from 'common/stores/slices/quotes';
+import { deleteQuoteLineItem } from 'common/stores/slices/quotes/extra-reducers/delete-invoice-item copy';
+import { setCurrentLineItemProperty } from 'common/stores/slices/quotes/extra-reducers/set-current-line-item-property';
+import { setCurrentQuote } from 'common/stores/slices/quotes/extra-reducers/set-current-quote';
+import { setCurrentQuoteLineItem } from 'common/stores/slices/quotes/extra-reducers/set-current-quote-line-item';
+import { BreadcrumRecord } from 'components/Breadcrumbs';
+import { Default } from 'components/layouts/Default';
+import { ClientSelector } from 'pages/invoices/common/components/ClientSelector';
+import { InvoicePreview } from 'pages/invoices/common/components/InvoicePreview';
+import { InvoiceTotals } from 'pages/invoices/common/components/InvoiceTotals';
+import { ProductsTable } from 'pages/invoices/common/components/ProductsTable';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
+import { generatePath } from 'react-router-dom';
+import { QuoteDetails } from '../common/components/QuoteDetails';
+import { QuoteFooter } from '../common/components/QuoteFooter';
+import { useCurrentQuote } from '../common/hooks/useCurrentQuote';
+import { useInvoiceSum } from '../common/hooks/useInvoiceSum';
+import { useSetCurrentQuoteProperty } from '../common/hooks/useSetCurrentQuoteProperty';
+
 export function Create() {
-  return <div>Page to create quote</div>;
+  const { documentTitle } = useTitle('new_quote');
+  const { data: blankQuote } = useBlankQuoteQuery();
+
+  const dispatch = useDispatch();
+  const handleChange = useSetCurrentQuoteProperty();
+
+  const currentQuote = useCurrentQuote();
+  const invoiceSum = useInvoiceSum();
+
+  useEffect(() => {
+    if (blankQuote?.data.data) {
+      dispatch(setCurrentQuote(blankQuote.data.data));
+    }
+
+    return () => {
+      dispatch(dismissCurrentQuote());
+    };
+  }, [blankQuote]);
+
+  const [t] = useTranslation();
+
+  const pages: BreadcrumRecord[] = [
+    { name: t('quotes'), href: '/quotes' },
+    {
+      name: t('new_quote'),
+      href: generatePath('/quotes/create'),
+    },
+  ];
+
+  return (
+    <Default title={documentTitle} breadcrumbs={pages}>
+      <div className="grid grid-cols-12 gap-4">
+        {currentQuote && (
+          <ClientSelector
+            resource={currentQuote}
+            onChange={(id) => handleChange('client_id', id)}
+            onClearButtonClick={() => handleChange('client_id', '')}
+            onContactCheckboxChange={(contactId, value) =>
+              dispatch(
+                toggleCurrentQuoteInvitation({ contactId, checked: value })
+              )
+            }
+          />
+        )}
+
+        <QuoteDetails />
+
+        <div className="col-span-12">
+          {currentQuote && (
+            <ProductsTable
+              resource={currentQuote}
+              onProductChange={(index, lineItem) =>
+                dispatch(setCurrentQuoteLineItem({ index, lineItem }))
+              }
+              onLineItemPropertyChange={(key, value, index) =>
+                dispatch(
+                  setCurrentLineItemProperty({
+                    position: index,
+                    property: key,
+                    value,
+                  })
+                )
+              }
+              onSort={(lineItems) => handleChange('line_items', lineItems)}
+              onDeleteRowClick={(index) => dispatch(deleteQuoteLineItem(index))}
+              onCreateItemClick={() => dispatch(injectBlankItemIntoCurrent())}
+            />
+          )}
+        </div>
+
+        <QuoteFooter page="create" />
+
+        {currentQuote && (
+          <InvoiceTotals
+            resource={currentQuote}
+            invoiceSum={invoiceSum}
+            onChange={(property, value) =>
+              handleChange(property as keyof Quote, value)
+            }
+          />
+        )}
+      </div>
+
+      <div className="my-4">
+        {currentQuote && (
+          <InvoicePreview for="create" resource={currentQuote} entity="quote" />
+        )}
+      </div>
+    </Default>
+  );
 }
