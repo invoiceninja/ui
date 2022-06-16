64bbe0413836dde1f63330d88b75ebe42857dd93
Benjamin Beganović <k1pstabug@gmail.com>
2022-06-16 12:57:26 +0200

    Flutter icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -7,8 +7,10 @@
  *
  * @license https://www.elastic.co/licensing/elastic-license
  */
+
 export * from './File';
 export * from './Jpg';
 export * from './Pdf';
 export * from './Png';
 export * from './Svg';
+export * from './Flutter';
