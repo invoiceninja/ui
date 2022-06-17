650f606ec83a653b56d2b7975f690cc0ef7ed9ed
David Bomba <turbo124@gmail.com>
2022-06-17 13:04:41 +1000

    Fixes for oauth

--- src/pages/authentication/Logout.tsx
+++ src/pages/authentication/Logout.tsx
@@ -9,19 +9,26 @@
  */
 
 import { request } from 'common/helpers/request';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useQueryClient } from 'react-query';
 import { useNavigate } from 'react-router';
 import { endpoint } from '../../common/helpers';
+import { useCurrentUser } from 'common/hooks/useCurrentUser';
 
 export function Logout() {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
-
+  const user = useCurrentUser();
+  const [msalInstance, onMsalInstanceChange] = useState();
+  
   useEffect(() => {
     request('POST', endpoint('/api/v1/logout'), {}).then(() => {
       localStorage.removeItem('X-NINJA-TOKEN');
       localStorage.removeItem('X-CURRENT-INDEX');
+
+      if (user.oauth_provider_id == 'microsoft'){
+          onMsalInstanceChange(msalInstance);
+      }
 
       queryClient.invalidateQueries();
 
