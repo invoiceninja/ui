b4d17eea12a8f791b884efe81cb0284d345c899d
Benjamin Beganović <k1pstabug@gmail.com>
2022-06-17 17:11:06 +0200

    Scaffold credits store slice

---
+++ src/common/stores/slices/credits.ts
@@ -0,0 +1,32 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { createSlice } from '@reduxjs/toolkit';
+import { InvoiceSum } from 'common/helpers/invoices/invoice-sum';
+import { Credit } from 'common/interfaces/credit';
+
+interface CreditState {
+  api?: any;
+  current?: Credit;
+  invoiceSum?: InvoiceSum;
+}
+
+const initialState: CreditState = {
+  api: {},
+};
+
+export const quoteSlice = createSlice({
+  name: 'quotes',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {},
+});
+
+export const {} = quoteSlice.actions;
