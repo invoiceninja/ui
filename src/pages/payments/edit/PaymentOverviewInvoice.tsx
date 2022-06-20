43c9df0de30bacccfc09a948fda7e6ce1276cf72
Benjamin Beganović <k1pstabug@gmail.com>
2022-06-20 13:13:00 +0200

    Replace filter() with find() for searching the invoice

--- src/pages/payments/edit/PaymentOverviewInvoice.tsx
+++ src/pages/payments/edit/PaymentOverviewInvoice.tsx
@@ -8,49 +8,54 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
- import { useFormatMoney } from 'common/hooks/money/useFormatMoney';
- import { useCurrentCompany } from 'common/hooks/useCurrentCompany';
- import { useTranslation } from 'react-i18next';
- import { Payment, Paymentable } from 'common/interfaces/payment';
- import { Invoice } from 'common/interfaces/invoice';
- import { useCurrentCompanyDateFormats } from 'common/hooks/useCurrentCompanyDateFormats';
- import { date as formatDate } from 'common/helpers';
+import { useFormatMoney } from 'common/hooks/money/useFormatMoney';
+import { useCurrentCompany } from 'common/hooks/useCurrentCompany';
+import { useTranslation } from 'react-i18next';
+import { Payment, Paymentable } from 'common/interfaces/payment';
+import { Invoice } from 'common/interfaces/invoice';
+import { useCurrentCompanyDateFormats } from 'common/hooks/useCurrentCompanyDateFormats';
+import { date as formatDate } from 'common/helpers';
 
- 
- interface Props {
-   payment: Payment;
-   paymentable: Paymentable
- }
- 
-
- export function setLabel(payment: Payment, paymentable: Paymentable):string  {
-
-     const invoice: any = payment?.invoices?.filter(
-        (invoice: Invoice) => invoice.id == paymentable.invoice_id
-      );
-
-      return invoice[0].number;
+interface Props {
+  payment: Payment;
+  paymentable: Paymentable;
 }
 
- export function PaymentOverviewInvoice(props: Props) {
-   const [t] = useTranslation();
-   const formatMoney = useFormatMoney();
-   const company = useCurrentCompany();
-   const { dateFormat } = useCurrentCompanyDateFormats();
+export function setLabel(payment: Payment, paymentable: Paymentable): string {
+  const invoice = payment?.invoices?.find(
+    (invoice: Invoice) => invoice.id == paymentable.invoice_id
+  ) as Invoice;
 
+  return invoice.number;
+}
 
-   return (
-     <div className="grid grid-cols-1 gap-2 my-2 border border-x-5 py-4">
-        <div className="flex items-center justify-center">
+export function PaymentOverviewInvoice(props: Props) {
+  const [t] = useTranslation();
+  const formatMoney = useFormatMoney();
+  const company = useCurrentCompany();
+  const { dateFormat } = useCurrentCompanyDateFormats();
+
+  return (
+    <div className="grid grid-cols-1 gap-2 my-2 border border-x-5 py-4">
+      <div className="flex items-center justify-center">
         <span className="text-gray-800">
           {`${t('invoice')} ${setLabel(props.payment, props.paymentable)}`}
         </span>
       </div>
       <div className="flex items-center justify-center">
-        <span className='text-gray-400'>{formatMoney(props?.payment?.amount || 0,
+        <span className="text-gray-400">
+          {formatMoney(
+            props?.payment?.amount || 0,
             company.settings.country_id,
-            props.payment?.currency_id)} - {formatDate(new Date(props.paymentable.created_at*1000).toString(), dateFormat) } 
+            props.payment?.currency_id
+          )}
+
+          {formatDate(
+            new Date(props.paymentable.created_at * 1000).toString(),
+            dateFormat
+          )}
         </span>
-        </div>
-    </div> 
-)}
+      </div>
+    </div>
+  );
+}
