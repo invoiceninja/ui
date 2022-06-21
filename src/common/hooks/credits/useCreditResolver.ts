197ab3a0f735e2d2b54eedc8cfb640a01eb7cef0
Benjamin Beganović <k1pstabug@gmail.com>
2022-06-21 17:21:55 +0200

    Credit resolver functions

---
+++ src/common/hooks/credits/useCreditResolver.ts
@@ -0,0 +1,17 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { CreditResolver } from 'common/helpers/credits/credit-resolver';
+
+const creditResolver = new CreditResolver();
+
+export function useCreditResolver() {
+  return creditResolver;
+}
