76a82e626fe252cbecdd328eda1e85acebf5838a
Benjamin Beganović <k1pstabug@gmail.com>
2022-06-26 03:04:58 +0200

    Client portal: Messages settings

--- src/pages/settings/client-portal/components/Messages.tsx
+++ src/pages/settings/client-portal/components/Messages.tsx
@@ -9,28 +9,56 @@
  */
 
 import { Card, Element } from '@invoiceninja/cards';
-import { Textarea } from '@invoiceninja/forms';
+import { InputField } from '@invoiceninja/forms';
+import { useCompanyChanges } from 'common/hooks/useCompanyChanges';
+import { useHandleCurrentCompanyChangeProperty } from 'pages/settings/common/hooks/useHandleCurrentCompanyChange';
 import { useTranslation } from 'react-i18next';
 
 export function Messages() {
   const [t] = useTranslation();
+  const company = useCompanyChanges();
+  const handleChange = useHandleCurrentCompanyChangeProperty();
 
   return (
     <Card title={t('messages')}>
       <Element leftSide={t('dashboard')}>
-        <Textarea />
+        <InputField
+          element="textarea"
+          value={company?.settings.custom_message_dashboard}
+          onValueChange={(value) =>
+            handleChange('settings.custom_message_dashboard', value)
+          }
+        />
       </Element>
 
       <Element leftSide={t('unpaid_invoice')}>
-        <Textarea />
+        <InputField
+          element="textarea"
+          value={company?.settings.custom_message_unpaid_invoice}
+          onValueChange={(value) =>
+            handleChange('settings.custom_message_unpaid_invoice', value)
+          }
+        />
       </Element>
 
       <Element leftSide={t('paid_invoice')}>
-        <Textarea />
+        <InputField
+          element="textarea"
+          value={company?.settings.custom_message_paid_invoice}
+          onValueChange={(value) =>
+            handleChange('settings.custom_message_paid_invoice', value)
+          }
+        />
       </Element>
 
       <Element leftSide={t('unapproved_quote')}>
-        <Textarea />
+        <InputField
+          element="textarea"
+          value={company?.settings.custom_message_unapproved_quote}
+          onValueChange={(value) =>
+            handleChange('settings.custom_message_unapproved_quote', value)
+          }
+        />
       </Element>
     </Card>
   );
