f4f422358891b9108f8bfed2d0b29b2e5a8cb178
Benjamin Beganović <k1pstabug@gmail.com>
2022-06-27 01:43:43 +0200

    Scaffold index page

--- src/pages/settings/users/index/Users.tsx
+++ src/pages/settings/users/index/Users.tsx
@@ -8,6 +8,27 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { useTitle } from 'common/hooks/useTitle';
+import { Settings } from 'components/layouts/Settings';
+import { useTranslation } from 'react-i18next';
+
 export function Users() {
-  return <div>Page for users</div>;
+  useTitle('user_management');
+
+  const [t] = useTranslation();
+
+  const pages = [
+    { name: t('settings'), href: '/settings' },
+    { name: t('user_managment'), href: '/settings/users' },
+  ];
+
+  return (
+    <Settings
+      title={t('user_details')}
+      breadcrumbs={pages}
+      docsLink="/docs/advanced-settings/#user_management"
+    >
+      {/*  */}
+    </Settings>
+  );
 }
