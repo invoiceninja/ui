e58a1fd9ef7b93a627973d61b3f5c1e6385b95c2
Benjamin Beganović <k1pstabug@gmail.com>
2022-06-27 01:49:01 +0200

    Show datatable with all users

--- src/pages/settings/users/index/Users.tsx
+++ src/pages/settings/users/index/Users.tsx
@@ -8,9 +8,13 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { Link } from '@invoiceninja/forms';
 import { useTitle } from 'common/hooks/useTitle';
+import { User } from 'common/interfaces/user';
+import { DataTable, DataTableColumns } from 'components/DataTable';
 import { Settings } from 'components/layouts/Settings';
 import { useTranslation } from 'react-i18next';
+import { generatePath } from 'react-router-dom';
 
 export function Users() {
   useTitle('user_management');
@@ -22,13 +26,33 @@ export function Users() {
     { name: t('user_management'), href: '/settings/users' },
   ];
 
+  const columns: DataTableColumns = [
+    {
+      id: 'name',
+      label: 'name',
+      format: (field, resource: User) => (
+        <Link
+          to={generatePath('/settings/users/:id/edit', { id: resource.id })}
+        >
+          {resource.first_name} {resource.last_name}
+        </Link>
+      ),
+    },
+    { id: 'email', label: 'email' },
+  ];
+
   return (
     <Settings
       title={t('user_details')}
       breadcrumbs={pages}
       docsLink="/docs/advanced-settings/#user_management"
     >
-      {/*  */}
+      <DataTable
+        resource="user"
+        columns={columns}
+        endpoint="/api/v1/users"
+        withoutActions
+      />
     </Settings>
   );
 }
