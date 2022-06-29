4f0575031b1568d1b4a41d25e9326d07b88f7eb9
Benjamin Beganović <k1pstabug@gmail.com>
2022-06-29 14:32:58 +0200

    Saving basic settings

--- src/pages/settings/email-settings/EmailSettings.tsx
+++ src/pages/settings/email-settings/EmailSettings.tsx
@@ -8,29 +8,104 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { Card, Element } from '@invoiceninja/cards';
+import { InputField, SelectField } from '@invoiceninja/forms';
+import { useInjectCompanyChanges } from 'common/hooks/useInjectCompanyChanges';
 import { useTitle } from 'common/hooks/useTitle';
+import { Settings } from 'components/layouts/Settings';
+import dayjs from 'dayjs';
+import { useHandleCancel } from 'pages/invoices/edit/hooks/useHandleCancel';
 import { useTranslation } from 'react-i18next';
-import { Card } from '../../../components/cards';
-import { Settings } from '../../../components/layouts/Settings';
+import { useHandleCompanySave } from '../common/hooks/useHandleCompanySave';
+import { useHandleCurrentCompanyChangeProperty } from '../common/hooks/useHandleCurrentCompanyChange';
 
 export function EmailSettings() {
   useTitle('email_settings');
-  
+
   const [t] = useTranslation();
 
-  const pages =
-   [
+  const pages = [
     { name: t('settings'), href: '/settings' },
     { name: t('email_settings'), href: '/settings/email_settings' },
   ];
+
+  const company = useInjectCompanyChanges();
+  const handleChange = useHandleCurrentCompanyChangeProperty();
+
+  const onSave = useHandleCompanySave();
+  const onCancel = useHandleCancel();
 
   return (
     <Settings
       title={t('email_settings')}
       breadcrumbs={pages}
       docsLink="docs/advanced-settings/#email_settings"
+      onSaveClick={onSave}
+      onCancelClick={onCancel}
     >
-      <Card title={t('settings')}>{/*  */}</Card>
+      <Card title={t('settings')}>
+        <Element leftSide={t('from_name')}>
+          <InputField
+            value={company?.settings.email_from_name}
+            onValueChange={(value) =>
+              handleChange('settings.email_from_name', value)
+            }
+          />
+        </Element>
+
+        <Element leftSide={t('reply_to_name')}>
+          <InputField
+            value={company?.settings.reply_to_name}
+            onValueChange={(value) =>
+              handleChange('settings.reply_to_name', value)
+            }
+          />
+        </Element>
+
+        <Element leftSide={t('reply_to_email')}>
+          <InputField
+            value={company?.settings.reply_to_email}
+            onValueChange={(value) =>
+              handleChange('settings.reply_to_email', value)
+            }
+          />
+        </Element>
+
+        <Element
+          leftSide={t('bcc_email')}
+          leftSideHelp={t('comma_sparated_list')}
+        >
+          <InputField
+            value={company?.settings.bcc_email}
+            onValueChange={(value) => handleChange('settings.bcc_email', value)}
+          />
+        </Element>
+
+        <Element
+          leftSide={t('send_time')}
+          leftSideHelp={t('comma_sparated_list')}
+        >
+          <SelectField
+            value={company?.settings.entity_send_time}
+            onValueChange={(value) =>
+              handleChange(
+                'settings.entity_send_time',
+                value.length > 0 ? value : 6
+              )
+            }
+            withBlank
+          >
+            {[...Array(24).keys()].map((number, index) => (
+              <option key={index} value={number + 1}>
+                {dayjs()
+                  .startOf('day')
+                  .add(number + 1, 'hour')
+                  .format('h:ss A')}
+              </option>
+            ))}
+          </SelectField>
+        </Element>
+      </Card>
     </Settings>
   );
 }
