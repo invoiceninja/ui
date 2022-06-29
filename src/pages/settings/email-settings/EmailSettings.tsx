25c135a64f17dbfcedb14d38d880d68592130803
Benjamin Beganović <k1pstabug@gmail.com>
2022-06-29 14:15:20 +0200

    Clean up EmailSettings component

--- src/pages/settings/email-settings/EmailSettings.tsx
+++ src/pages/settings/email-settings/EmailSettings.tsx
@@ -8,24 +8,21 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { useEffect } from 'react';
+import { useTitle } from 'common/hooks/useTitle';
 import { useTranslation } from 'react-i18next';
-import { Card, Element } from '../../../components/cards';
-import { InputField, SelectField, Textarea } from '../../../components/forms';
-import Toggle from '../../../components/forms/Toggle';
+import { Card } from '../../../components/cards';
 import { Settings } from '../../../components/layouts/Settings';
 
 export function EmailSettings() {
+  useTitle('email_settings');
+  
   const [t] = useTranslation();
-  const pages = [
+
+  const pages =
+   [
     { name: t('settings'), href: '/settings' },
     { name: t('email_settings'), href: '/settings/email_settings' },
   ];
-  useEffect(() => {
-    document.title = `${import.meta.env.VITE_APP_TITLE}: ${t(
-      'email_settings'
-    )}`;
-  });
 
   return (
     <Settings
@@ -33,64 +30,7 @@ export function EmailSettings() {
       breadcrumbs={pages}
       docsLink="docs/advanced-settings/#email_settings"
     >
-      <Card title={t('settings')}>
-        <Element leftSide={t('send_from_gmail')}>
-          <Toggle />
-        </Element>
-
-        <div className="pt-4 border-b"></div>
-
-        <Element className="mt-4" leftSide={t('from_name')}>
-          <InputField id="from_name" />
-        </Element>
-
-        <Element leftSide={t('reply_to_name')}>
-          <InputField id="reply_to_name" />
-        </Element>
-
-        <Element leftSide={t('reply_to_email')}>
-          <InputField id="reply_to_email" />
-        </Element>
-
-        <Element leftSide={t('bcc_email')}>
-          <InputField id="bcc_email" />
-        </Element>
-
-        <Element leftSide={t('send_time')}>
-          <SelectField>
-            <option value="7am">7 AM</option>
-          </SelectField>
-        </Element>
-
-        <div className="pt-4 border-b"></div>
-
-        <Element className="mt-4" leftSide={t('email_design')}>
-          <SelectField>
-            <option value="plain">{t('plain')}</option>
-            <option value="light">{t('light')}</option>
-            <option value="dark">{t('dark')}</option>
-            <option value="custom">{t('custom')}</option>
-          </SelectField>
-        </Element>
-
-        <Element leftSide={t('signature')}>
-          <Textarea />
-        </Element>
-
-        <div className="pt-4 border-b"></div>
-
-        <Element className="mt-4" leftSide={t('attach_pdf')}>
-          <Toggle />
-        </Element>
-
-        <Element leftSide={t('attach_documents')}>
-          <Toggle />
-        </Element>
-
-        <Element leftSide={t('attach_ubl')}>
-          <Toggle />
-        </Element>
-      </Card>
+      <Card title={t('settings')}>{/*  */}</Card>
     </Settings>
   );
 }
