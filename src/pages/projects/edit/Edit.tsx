4f3e8a8b14ab138d0c82c84af022a7b6d007d6ac
Benjamin Beganović <k1pstabug@gmail.com>
2022-06-30 17:29:03 +0200

    Scaffold edit page for projects

---
+++ src/pages/projects/edit/Edit.tsx
@@ -0,0 +1,32 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { Card } from '@invoiceninja/cards';
+import { useTitle } from 'common/hooks/useTitle';
+import { Project } from 'common/interfaces/project';
+import { useProjectQuery } from 'common/queries/projects';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+export function Edit() {
+  const { documentTitle } = useTitle('edit_project');
+  const { id } = useParams();
+  const { data } = useProjectQuery({ id });
+
+  const [project, setProject] = useState<Project>();
+
+  useEffect(() => {
+    if (data) {
+      setProject({ ...data });
+    }
+  }, [data]);
+
+  return <Card title={documentTitle}>{/*  */}</Card>;
+}
