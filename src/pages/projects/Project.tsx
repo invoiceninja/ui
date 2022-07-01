d2fb0a8ef590dcceaed5c18aa58626cba6c6d285
Benjamin Beganović <k1pstabug@gmail.com>
2022-07-01 10:30:44 +0200

    Set document tile from project name

--- src/pages/projects/Project.tsx
+++ src/pages/projects/Project.tsx
@@ -9,16 +9,19 @@
  */
 
 import { useTitle } from 'common/hooks/useTitle';
+import { useProjectQuery } from 'common/queries/projects';
 import { BreadcrumRecord } from 'components/Breadcrumbs';
 import { Container } from 'components/Container';
 import { Default } from 'components/layouts/Default';
 import { Tab, Tabs } from 'components/Tabs';
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { generatePath, Outlet, useParams } from 'react-router-dom';
 
 export function Project() {
-  const { documentTitle } = useTitle('project');
+  const { documentTitle, setDocumentTitle } = useTitle('project');
   const { id } = useParams();
+  const { data } = useProjectQuery({ id });
 
   const [t] = useTranslation();
 
@@ -40,6 +43,10 @@ export function Project() {
       href: generatePath('/projects/:id/documents', { id }),
     },
   ];
+
+  useEffect(() => {
+    data?.name && setDocumentTitle(data.name);
+  }, [data]);
 
   return (
     <Default title={documentTitle} breadcrumbs={pages}>
