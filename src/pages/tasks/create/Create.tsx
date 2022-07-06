aa7227fc9341c7c17f123dba90e423dd044d70fe
Benjamin Beganović <k1pstabug@gmail.com>
2022-07-06 15:17:17 +0200

    Basic task details

--- src/pages/tasks/create/Create.tsx
+++ src/pages/tasks/create/Create.tsx
@@ -9,11 +9,14 @@
  */
 
 import { Card } from '@invoiceninja/cards';
+import { InputField, SelectField } from '@invoiceninja/forms';
+import { useCurrentCompany } from 'common/hooks/useCurrentCompany';
 import { useTitle } from 'common/hooks/useTitle';
 import { Task } from 'common/interfaces/task';
 import { User } from 'common/interfaces/user';
 import { useBlankTaskQuery } from 'common/queries/tasks';
 import { ClientSelector } from 'components/clients/ClientSelector';
+import { CustomField } from 'components/CustomField';
 import { DebouncedCombobox, Record } from 'components/forms/DebouncedCombobox';
 import { Default } from 'components/layouts/Default';
 import { ProjectSelector } from 'components/projects/ProjectSelector';
@@ -30,6 +33,8 @@ export function Create() {
   const handleChange = (property: keyof Task, value: unknown) => {
     setTask((current) => current && { ...current, [property]: value });
   };
+
+  const company = useCurrentCompany();
 
   useEffect(() => {
     blankTask && setTask(blankTask);
@@ -67,6 +72,51 @@ export function Create() {
             }
             onClearButtonClick={() => handleChange('assigned_user_id', '')}
           />
+        </Card>
+
+        <Card className="col-span-12 xl:col-span-4 h-max" withContainer>
+          <InputField
+            label={t('task_number')}
+            value={task?.number}
+            onValueChange={(value) => handleChange('number', value)}
+          />
+
+          <InputField
+            label={t('rate')}
+            value={task?.rate}
+            onValueChange={(value) => handleChange('rate', value)}
+          />
+
+          <SelectField
+            label={t('status')}
+            value={task?.status_id}
+            onValueChange={(value) => handleChange('status_id', value)}
+          >
+            <option value="">{t('backlog')}</option>
+            <option value="">{t('ready_to_do')}</option>
+            <option value="">{t('in_progress')}</option>
+            <option value="">{t('done')}</option>
+          </SelectField>
+
+          {task && company?.custom_fields?.task1 && (
+            <CustomField
+              field="task1"
+              defaultValue={task?.custom_value1 || ''}
+              value={company.custom_fields.task1}
+              onChange={(value) => handleChange('custom_value1', value)}
+              noExternalPadding
+            />
+          )}
+
+          {task && company?.custom_fields?.task2 && (
+            <CustomField
+              field="task2"
+              defaultValue={task?.custom_value2 || ''}
+              value={company.custom_fields.task2}
+              onChange={(value) => handleChange('custom_value2', value)}
+              noExternalPadding
+            />
+          )}
         </Card>
       </div>
     </Default>
