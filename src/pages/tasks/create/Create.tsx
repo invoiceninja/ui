1bcaab29959e12ff8dce47c9761097fcf146c737
Benjamin Beganović <k1pstabug@gmail.com>
2022-07-06 14:34:58 +0200

    Scaffold create page for tasks

--- src/pages/tasks/create/Create.tsx
+++ src/pages/tasks/create/Create.tsx
@@ -8,6 +8,34 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { Card } from '@invoiceninja/cards';
+import { useTitle } from 'common/hooks/useTitle';
+import { Task } from 'common/interfaces/task';
+import { useBlankTaskQuery } from 'common/queries/tasks';
+import { Default } from 'components/layouts/Default';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
 export function Create() {
-  return <div>Page to create tasks!</div>;
+  const { documentTitle } = useTitle('new_task');
+  const { data: blankTask } = useBlankTaskQuery();
+
+  const [t] = useTranslation();
+  const [task, setTask] = useState<Task>();
+
+  const handleChange = (property: keyof Task, value: unknown) => {
+    setTask((current) => current && { ...current, [property]: value });
+  };
+
+  useEffect(() => {
+    blankTask && setTask(blankTask);
+  }, [blankTask]);
+
+  return (
+    <Default title={documentTitle}>
+      <div className="grid grid-cols-12 gap-4">
+        <Card className="col-span-12 xl:col-span-4 h-max" withContainer></Card>
+      </div>
+    </Default>
+  );
 }
