03242f184b70455e72b951bb000b151aa4fdd6c8
Benjamin Beganović <k1pstabug@gmail.com>
2022-07-06 14:34:51 +0200

    Define blank task query

---
+++ src/common/queries/tasks.ts
@@ -0,0 +1,23 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { endpoint } from 'common/helpers';
+import { request } from 'common/helpers/request';
+import { Task } from 'common/interfaces/task';
+import { useQuery } from 'react-query';
+import { generatePath } from 'react-router-dom';
+
+export function useBlankTaskQuery() {
+  return useQuery<Task>(generatePath('/api/v1/tasks/create'), () =>
+    request('GET', endpoint('/api/v1/tasks/create')).then(
+      (response) => response.data.data
+    )
+  );
+}
