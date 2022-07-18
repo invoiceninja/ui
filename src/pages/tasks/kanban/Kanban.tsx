c53248587576be1539c0c3de179b14c39711bab4
Benjamin Beganović <k1pstabug@gmail.com>
2022-07-18 16:16:20 +0200

    Sorting the tasks in the kanban

--- src/pages/tasks/kanban/Kanban.tsx
+++ src/pages/tasks/kanban/Kanban.tsx
@@ -10,9 +10,35 @@
 
 import { Link } from '@invoiceninja/forms';
 import { useTitle } from 'common/hooks/useTitle';
+import { useTaskStatusesQuery } from 'common/queries/task-statuses';
+import { useTasksQuery } from 'common/queries/tasks';
 import { Default } from 'components/layouts/Default';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { BsTable } from 'react-icons/bs';
+import Board from '@asseinfo/react-kanban';
+import '@asseinfo/react-kanban/dist/styles.css';
+import { calculateTime } from '../common/helpers/calculate-time';
+import collect from 'collect.js';
+import { toast } from 'common/helpers/toast/toast';
+import { request } from 'common/helpers/request';
+import { endpoint } from 'common/helpers';
+
+interface Card {
+  id: string;
+  title: string;
+  description: string;
+}
+
+interface Column {
+  id: string;
+  title: string;
+  cards: Card[];
+}
+
+interface Board {
+  columns: Column[];
+}
 
 export function Kanban() {
   const { documentTitle } = useTitle('kanban');
@@ -23,6 +49,65 @@ export function Kanban() {
     { name: t('kanban'), href: '/tasks/kanban' },
   ];
 
+  const { data: taskStatuses } = useTaskStatusesQuery();
+  const { data: tasks } = useTasksQuery();
+
+  const [board, setBoard] = useState<Board>();
+
+  useEffect(() => {
+    if (taskStatuses && tasks) {
+      const columns: Column[] = [];
+
+      taskStatuses.data.map((taskStatus) =>
+        columns.push({ id: taskStatus.id, title: taskStatus.name, cards: [] })
+      );
+
+      tasks.data.map((task) => {
+        const index = columns.findIndex(
+          (column) => column.id === task.status_id
+        );
+
+        if (index >= 0) {
+          columns[index].cards.push({
+            id: task.id,
+            title: task.description,
+            description: calculateTime(task.time_log),
+          });
+        }
+      });
+
+      setBoard((current) => ({ ...current, columns }));
+    }
+  }, [taskStatuses, tasks]);
+
+  const handleDragging = (board: Board) => {
+    const taskIds: Record<string, string[]> = {};
+    const statusIds = collect(board.columns).pluck('id').toArray() as string[];
+
+    statusIds.forEach((id) => (taskIds[id] = []));
+
+    board.columns.forEach((column) => {
+      taskIds[column.id] = collect(column.cards)
+        .pluck('id')
+        .toArray() as string[];
+    });
+
+    const payload = {
+      status_ids: statusIds,
+      task_ids: taskIds,
+    };
+
+    toast.processing();
+
+    request('POST', endpoint('/api/v1/tasks/sort'), payload)
+      .then(() => toast.success('updated_task'))
+      .catch((error) => {
+        console.error(error);
+
+        toast.error();
+      });
+  };
+
   return (
     <Default
       title={documentTitle}
@@ -32,6 +117,14 @@ export function Kanban() {
           <BsTable size={20} />
         </Link>
       }
-    ></Default>
+    >
+      {board && (
+        <Board
+          initialBoard={board}
+          onCardDragEnd={handleDragging}
+          disableColumnDrag
+        />
+      )}
+    </Default>
   );
 }
