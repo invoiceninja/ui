eea056ba29ba3369a66cd2a88952d64db826795e
Benjamin Beganović <k1pstabug@gmail.com>
2022-08-17 15:50:11 +0200

    Update wrapper component

--- src/pages/expenses/Expense.tsx
+++ src/pages/expenses/Expense.tsx
@@ -8,10 +8,15 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { Button } from '@invoiceninja/forms';
 import { useTitle } from 'common/hooks/useTitle';
 import { useExpenseQuery } from 'common/queries/expenses';
 import { BreadcrumRecord } from 'components/Breadcrumbs';
+import { Container } from 'components/Container';
+import { Dropdown } from 'components/dropdown/Dropdown';
+import { Inline } from 'components/Inline';
 import { Default } from 'components/layouts/Default';
+import { Tab, Tabs } from 'components/Tabs';
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { generatePath, Outlet, useParams } from 'react-router-dom';
@@ -32,9 +37,39 @@ export function Expense() {
     { name: documentTitle, href: generatePath('/expenses/:id', { id }) },
   ];
 
+  const tabs: Tab[] = [
+    {
+      name: t('edit'),
+      href: generatePath('/expenses/:id/edit', { id }),
+    },
+    {
+      name: t('documents'),
+      href: generatePath('/expenses/:id/documents', { id }),
+    },
+  ];
+
   return (
-    <Default title={documentTitle} breadcrumbs={pages}>
-      <Outlet />
+    <Default
+      title={documentTitle}
+      breadcrumbs={pages}
+      topRight={
+        <Inline>
+          <Button to={generatePath('/expenses/:id/edit', { id })}>
+            {t('edit_expense')}
+          </Button>
+
+          <Dropdown label={t('more_actions')}>
+            {/* <DropdownElement>A</DropdownElement>
+            <DropdownElement>B</DropdownElement> */}
+          </Dropdown>
+        </Inline>
+      }
+    >
+      <Container>
+        <Tabs tabs={tabs} />
+
+        <Outlet />
+      </Container>
     </Default>
   );
 }
