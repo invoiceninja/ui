39a53d8d7a5160268f53d666c9e54c223bcd85b5
Benjamin Beganović <k1pstabug@gmail.com>
2022-08-17 15:42:54 +0200

    Update expense wrapper component

--- src/pages/expenses/Expense.tsx
+++ src/pages/expenses/Expense.tsx
@@ -9,14 +9,31 @@
  */
 
 import { useTitle } from 'common/hooks/useTitle';
+import { useExpenseQuery } from 'common/queries/expenses';
+import { BreadcrumRecord } from 'components/Breadcrumbs';
 import { Default } from 'components/layouts/Default';
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { generatePath, Outlet, useParams } from 'react-router-dom';
 
 export function Expense() {
-  const { documentTitle } = useTitle('expense');
+  const { documentTitle, setDocumentTitle } = useTitle('expense');
+  const { id } = useParams();
+  const { data: expense } = useExpenseQuery({ id });
+
+  const [t] = useTranslation();
+
+  useEffect(() => {
+    expense && setDocumentTitle(`${t('expense')} ${expense.number}`);
+  }, [expense]);
+
+  const pages: BreadcrumRecord[] = [
+    { name: t('expenses'), href: '/expenses' },
+    { name: documentTitle, href: generatePath('/expenses/:id', { id }) },
+  ];
 
   return (
-    <Default title={documentTitle}>
+    <Default title={documentTitle} breadcrumbs={pages}>
       <Outlet />
     </Default>
   );
