7614e4303add2ce16e4b8c9579d16154d6a6f705
Benjamin Beganović <k1pstabug@gmail.com>
2022-08-17 16:00:35 +0200

    Provide cards for editing the expense

--- src/pages/expenses/edit/Edit.tsx
+++ src/pages/expenses/edit/Edit.tsx
@@ -8,6 +8,61 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { Expense } from 'common/interfaces/expense';
+import { useExpenseQuery } from 'common/queries/expenses';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { AdditionalInfo } from '../create/components/AdditionalInfo';
+import { Details } from '../create/components/Details';
+import { Notes } from '../create/components/Notes';
+import { TaxSettings } from '../create/components/Taxes';
+
 export function Edit() {
-  return <div>Edit page for expenses!</div>;
+  const { id } = useParams();
+  const { data } = useExpenseQuery({ id });
+
+  const [expense, setExpense] = useState<Expense>();
+  const [taxInputType, setTaxInputType] = useState<'by_rate' | 'by_amount'>(
+    'by_rate'
+  );
+
+  const handleChange = <T extends keyof Expense>(
+    property: T,
+    value: Expense[typeof property]
+  ) => {
+    setExpense((expense) => expense && { ...expense, [property]: value });
+  };
+
+  useEffect(() => {
+    if (data) {
+      setExpense(data);
+    }
+  }, [data]);
+
+  return (
+    <div className="grid grid-cols-12 gap-4">
+      <div className="col-span-12 xl:col-span-4">
+        <Details
+          expense={expense}
+          handleChange={handleChange}
+          taxInputType={taxInputType}
+        />
+      </div>
+
+      <div className="col-span-12 xl:col-span-4">
+        <Notes expense={expense} handleChange={handleChange} />
+      </div>
+
+      <div className="col-span-12 xl:col-span-4 space-y-4">
+        <AdditionalInfo expense={expense} handleChange={handleChange} />
+
+        <TaxSettings
+          expense={expense}
+          handleChange={handleChange}
+          taxInputType={taxInputType}
+          setTaxInputType={setTaxInputType}
+        />
+      </div>
+    </div>
+  );
 }
