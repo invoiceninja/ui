88641e1f96a5f97cd2dce2550f9924064db47471
Benjamin Beganović <k1pstabug@gmail.com>
2022-09-07 14:16:03 +0200

    Initial render of ProductsTable

--- src/pages/invoices/create/CreateNext.tsx
+++ src/pages/invoices/create/CreateNext.tsx
@@ -12,18 +12,24 @@ import { useClientResolver } from 'common/hooks/clients/useClientResolver';
 import { useCurrentCompany } from 'common/hooks/useCurrentCompany';
 import { useTitle } from 'common/hooks/useTitle';
 import { Invoice } from 'common/interfaces/invoice';
+import { InvoiceItem, InvoiceItemType } from 'common/interfaces/invoice-item';
 import { Invitation } from 'common/interfaces/purchase-order';
 import { ValidationBag } from 'common/interfaces/validation-bag';
 import { useBlankInvoiceQuery } from 'common/queries/invoices';
 import { blankInvitation } from 'common/stores/slices/invoices/constants/blank-invitation';
 import { Default } from 'components/layouts/Default';
+import { Spinner } from 'components/Spinner';
+import { TabGroup } from 'components/TabGroup';
 import { useAtom } from 'jotai';
 import { cloneDeep } from 'lodash';
 import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useSearchParams } from 'react-router-dom';
 import { invoiceAtom } from '../common/atoms';
 import { ClientSelector } from '../common/components/ClientSelector';
 import { InvoiceDetails } from '../common/components/InvoiceDetails';
+import { ProductsTable } from '../common/components/ProductsTable';
+import { useProductColumns } from '../common/hooks/useProductColumns';
 
 export type ChangeHandler = <T extends keyof Invoice>(
   property: T,
@@ -31,11 +37,13 @@ export type ChangeHandler = <T extends keyof Invoice>(
 ) => void;
 
 export function CreateNext() {
+  const { t } = useTranslation();
   const { documentTitle } = useTitle('new_invoice');
   const { data } = useBlankInvoiceQuery();
 
   const company = useCurrentCompany();
   const clientResolver = useClientResolver();
+  const productColumns = useProductColumns();
 
   const [searchParams] = useSearchParams();
   const [invoice, setInvoice] = useAtom(invoiceAtom);
@@ -88,11 +96,15 @@ export function CreateNext() {
         _invoice.tax_rate3 = company.settings.tax_rate3;
       }
 
+      if (typeof _invoice.line_items === 'string') {
+        _invoice.line_items = [];
+      }
+
       setInvoice(_invoice);
     }
 
     return () => {
-      // setInvoice(undefined);
+      setInvoice(undefined);
     };
   }, [data]);
 
@@ -115,6 +127,26 @@ export function CreateNext() {
       });
   }, [invoice?.client_id]);
 
+  const handleLineItemChange = (index: number, lineItem: InvoiceItem) => {
+    console.log(index, lineItem);
+  };
+
+  const handleLineItemPropertyChange = (
+    key: keyof InvoiceItem,
+    value: unknown,
+    index: number
+  ) => {
+    console.log(key, value, index);
+  };
+
+  const handleCreateLineItem = () => {
+    console.log('Create a line item');
+  };
+
+  const handleDeleteLineItem = (index: number) => {
+    console.log('Delete line item', index);
+  };
+
   return (
     <Default title={documentTitle}>
       <div className="grid grid-cols-12 gap-4">
@@ -128,6 +160,35 @@ export function CreateNext() {
         />
 
         <InvoiceDetails invoice={invoice} handleChange={handleChange} />
+
+        <div className="col-span-12">
+          <TabGroup
+            tabs={[t('products'), t('tasks')]}
+            defaultTabIndex={searchParams.get('table') === 'tasks' ? 1 : 0}
+          >
+            <div>
+              {invoice ? (
+                <ProductsTable
+                  type="product"
+                  resource={invoice}
+                  items={invoice.line_items.filter(
+                    (item) => item.type_id === InvoiceItemType.Product
+                  )}
+                  columns={productColumns}
+                  relationType="client_id"
+                  onLineItemChange={handleLineItemChange}
+                  onSort={(lineItems) => handleChange('line_items', lineItems)}
+                  onLineItemPropertyChange={handleLineItemPropertyChange}
+                  onCreateItemClick={handleCreateLineItem}
+                  onDeleteRowClick={handleDeleteLineItem}
+                />
+              ) : (
+                <Spinner />
+              )}
+            </div>
+            <div>tasks</div>
+          </TabGroup>
+        </div>
       </div>
     </Default>
   );
