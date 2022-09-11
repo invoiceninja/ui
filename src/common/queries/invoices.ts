e10ee2e4b9fdbe43abfc7d58d2726d0e9f9a07cb
Benjamin Beganović <k1pstabug@gmail.com>
2022-09-11 15:18:24 +0200

    Modify `useInvoiceQuery` hook

--- src/common/queries/invoices.ts
+++ src/common/queries/invoices.ts
@@ -8,41 +8,44 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { AxiosResponse } from 'axios';
-import { endpoint } from 'common/helpers';
-import { request } from 'common/helpers/request';
-import { GenericSingleResourceResponse } from 'common/interfaces/generic-api-response';
-import { Invoice } from 'common/interfaces/invoice';
-import { useQuery } from 'react-query';
-import { generatePath } from 'react-router-dom';
+import { AxiosResponse } from "axios";
+import { endpoint } from "common/helpers";
+import { request } from "common/helpers/request";
+import { GenericSingleResourceResponse } from "common/interfaces/generic-api-response";
+import { Invoice } from "common/interfaces/invoice";
+import { useQuery } from "react-query";
+import { generatePath } from "react-router-dom";
 
 export function useInvoiceQuery(
   params: { id: string | undefined },
-  options: Record<string, any> = {}
 ) {
-  return useQuery(
-    generatePath('/api/v1/invoices/:id', { id: params.id }),
-    () => request('GET', endpoint('/api/v1/invoices/:id', { id: params.id })),
-    { ...options, staleTime: Infinity }
+  return useQuery<Invoice>(
+    generatePath("/api/v1/invoices/:id", { id: params.id }),
+    () =>
+      request("GET", endpoint("/api/v1/invoices/:id", { id: params.id })).then((
+        response: GenericSingleResourceResponse<Invoice>,
+      ) => response.data.data),
+    { staleTime: Infinity },
   );
 }
 
 export function useBlankInvoiceQuery() {
   return useQuery<Invoice>(
-    generatePath('/api/v1/invoices/create'),
+    generatePath("/api/v1/invoices/create"),
     () =>
-      request('GET', endpoint('/api/v1/invoices/create')).then(
-        (response: GenericSingleResourceResponse<Invoice>) => response.data.data
+      request("GET", endpoint("/api/v1/invoices/create")).then(
+        (response: GenericSingleResourceResponse<Invoice>) =>
+          response.data.data,
       ),
-    { staleTime: Infinity }
+    { staleTime: Infinity },
   );
 }
 
 export function bulk(
   id: string[],
-  action: 'archive' | 'restore' | 'delete' | 'cancel'
+  action: "archive" | "restore" | "delete" | "cancel",
 ): Promise<AxiosResponse> {
-  return request('POST', endpoint('/api/v1/invoices/bulk'), {
+  return request("POST", endpoint("/api/v1/invoices/bulk"), {
     action,
     ids: Array.from(id),
   });
