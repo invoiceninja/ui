91fdcb6356836d8bfafe74b9a94ffd5ac0075c80
Benjamin Beganović <k1pstabug@gmail.com>
2022-09-11 15:19:01 +0200

    Refactor `useInvoiceSave`

--- src/pages/invoices/edit/hooks/useInvoiceSave.ts
+++ src/pages/invoices/edit/hooks/useInvoiceSave.ts
@@ -8,53 +8,59 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import axios from 'axios';
-import { endpoint } from 'common/helpers';
-import { Invoice } from 'common/interfaces/invoice';
-import { setCurrentInvoice } from 'common/stores/slices/invoices/extra-reducers/set-current-invoice';
-import toast from 'react-hot-toast';
-import { useTranslation } from 'react-i18next';
-import { useQueryClient } from 'react-query';
-import { useDispatch } from 'react-redux';
-import { generatePath } from 'react-router-dom';
-import { useInjectCompanyChanges } from 'common/hooks/useInjectCompanyChanges';
-import { updateRecord } from 'common/stores/slices/company-users';
-import { request } from 'common/helpers/request';
+import axios from "axios";
+import { endpoint } from "common/helpers";
+import { Invoice } from "common/interfaces/invoice";
+import { useQueryClient } from "react-query";
+import { useDispatch } from "react-redux";
+import { generatePath } from "react-router-dom";
+import { useInjectCompanyChanges } from "common/hooks/useInjectCompanyChanges";
+import { updateRecord } from "common/stores/slices/company-users";
+import { request } from "common/helpers/request";
+import { ValidationBag } from "common/interfaces/validation-bag";
+import { toast } from "common/helpers/toast/toast";
 
-export function useInvoiceSave() {
-  const [t] = useTranslation();
+export function useHandleSave(
+  setErrors: (errors: ValidationBag | undefined) => unknown,
+) {
   const queryClient = useQueryClient();
   const dispatch = useDispatch();
   const company = useInjectCompanyChanges();
 
-  return (id: string, invoice: Invoice) => {
-    const toastId = toast.loading(t('processing'));
+  return (invoice: Invoice) => {
+    setErrors(undefined);
+    toast.processing();
 
     axios
       .all([
-        request('PUT', endpoint('/api/v1/invoices/:id', { id }), invoice),
         request(
-          'PUT',
-          endpoint('/api/v1/companies/:id', { id: company?.id }),
-          company
+          "PUT",
+          endpoint("/api/v1/invoices/:id", { id: invoice.id }),
+          invoice,
+        ),
+        request(
+          "PUT",
+          endpoint("/api/v1/companies/:id", { id: company?.id }),
+          company,
         ),
       ])
       .then((response) => {
-        toast.success(t('updated_invoice'), { id: toastId });
-        dispatch(setCurrentInvoice(response[0].data.data));
+        toast.success("updated_invoice");
 
         dispatch(
-          updateRecord({ object: 'company', data: response[1].data.data })
+          updateRecord({ object: "company", data: response[1].data.data }),
         );
       })
       .catch((error) => {
         console.error(error);
 
-        toast.error(t('error_title'), { id: toastId });
+        error.response?.status === 422
+          ? toast.dismiss() && setErrors(error.response.data)
+          : toast.error();
       })
       .finally(() =>
         queryClient.invalidateQueries(
-          generatePath('/api/v1/invoices/:id', { id })
+          generatePath("/api/v1/invoices/:id", { id: invoice.id }),
         )
       );
   };
