d33947558fa2f1ef56b8abf93c376791039ba70c
Benjamin Beganović <k1pstabug@gmail.com>
2022-09-16 14:46:40 +0200

    Recurring invoices: `invoiceSumAtom`

--- src/pages/recurring-invoices/common/atoms.ts
+++ src/pages/recurring-invoices/common/atoms.ts
@@ -8,9 +8,12 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { InvoiceSum } from "common/helpers/invoices/invoice-sum";
 import { RecurringInvoice } from "common/interfaces/recurring-invoice";
 import { atom } from "jotai";
 
 export const recurringInvoiceAtom = atom<RecurringInvoice | undefined>(
   undefined,
 );
+
+export const invoiceSumAtom = atom<InvoiceSum | undefined>(undefined);
