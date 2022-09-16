bc1e11314bf8f275f9b1b71261fca1f4afe1e45d
Benjamin Beganović <k1pstabug@gmail.com>
2022-09-16 16:44:18 +0200

    `ResourceActions` component

---
+++ src/components/ResourceActions.tsx
@@ -0,0 +1,30 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { Fragment } from 'react';
+import { Dropdown } from './dropdown/Dropdown';
+
+export type Action<T = unknown> = (resource: T) => unknown;
+
+interface Props {
+  resource: unknown;
+  label: string;
+  actions: Action<any>[];
+}
+
+export function ResourceActions(props: Props) {
+  return (
+    <Dropdown label={props.label}>
+      {props.actions.map((action, index) => (
+        <Fragment key={index}>{action(props.resource)}</Fragment>
+      ))}
+    </Dropdown>
+  );
+}
