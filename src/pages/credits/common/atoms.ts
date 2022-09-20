7a8b047d1c9ae9b129621e45b9cca7e1e2e4707a
Benjamin Beganović <k1pstabug@gmail.com>
2022-09-20 13:55:51 +0200

    Credits: Create `credit` and `invoiceSum` atoms

---
+++ src/pages/credits/common/atoms.ts
@@ -0,0 +1,16 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { InvoiceSum } from "common/helpers/invoices/invoice-sum";
+import { Credit } from "common/interfaces/credit";
+import { atom } from "jotai";
+
+export const creditAtom = atom<Credit | undefined>(undefined);
+export const invoiceSumAtom = atom<InvoiceSum | undefined>(undefined);
