3fddeaca2b6a2feb3ff1fdd6b7bdaa8c3d8b55ae
Benjamin Beganović <k1pstabug@gmail.com>
2022-09-21 15:51:47 +0200

    Purchase orders: Define `purchaseOrderAtom`

---
+++ src/pages/purchase-orders/common/atoms.ts
@@ -0,0 +1,14 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { PurchaseOrder } from "common/interfaces/purchase-order";
+import { atom } from "jotai";
+
+export const purchaseOrderAtom = atom<PurchaseOrder | undefined>(undefined);
