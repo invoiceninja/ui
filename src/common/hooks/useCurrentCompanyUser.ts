b9661e5321184c550e8b07f54d30b7e9926f82bb
Benjamin Beganović <k1pstabug@gmail.com>
2022-10-17 16:16:55 +0200

    `useCurrentCompanyUser` hook

---
+++ src/common/hooks/useCurrentCompanyUser.ts
@@ -0,0 +1,20 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { CompanyUser } from 'common/interfaces/company-user';
+import { RootState } from 'common/stores/store';
+import { useSelector } from 'react-redux';
+
+export function useCurrentCompanyUser(): CompanyUser | undefined {
+  return useSelector(
+    (state: RootState) =>
+      state.companyUsers.api[state.companyUsers.currentIndex]
+  );
+}
