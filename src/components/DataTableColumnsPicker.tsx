b752950f5f694f16a207ed973c3ce5ae89553d31
Benjamin Beganović <k1pstabug@gmail.com>
2022-10-19 18:29:59 +0200

    Updating & saving the columns

--- src/components/DataTableColumnsPicker.tsx
+++ src/components/DataTableColumnsPicker.tsx
@@ -8,25 +8,67 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { useCallback, useState } from 'react';
+import { endpoint } from 'common/helpers';
+import { request } from 'common/helpers/request';
+import { User } from 'common/interfaces/user';
+import { RootState } from 'common/stores/store';
+import { cloneDeep, set } from 'lodash';
+import { useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { SelectField } from './forms';
+import { useSelector } from 'react-redux';
+import { Button, SelectField } from './forms';
+import { Inline } from './Inline';
 import { Modal } from './Modal';
 
 interface Props {
-  columns: Record<string, string>;
+  columns: string[];
+  defaultColumns: string[];
   table: string;
 }
 
 export function DataTableColumnsPicker(props: Props) {
+  const currentUser = useSelector((state: RootState) => state.user.user) as
+    | User
+    | undefined;
+
   const { t } = useTranslation();
-  const { columns } = props;
+  const { table } = props;
+
+  const [filteredColumns, setFilteredColumns] = useState(props.columns);
+
+  const [currentColumns, setCurrentColumns] = useState<string[]>(
+    currentUser?.company_user?.settings?.react_table_columns?.[table] || []
+  );
 
   const [isModalVisible, setIsModalVisible] = useState(false);
 
+  useEffect(() => {
+    setFilteredColumns((current) =>
+      current.filter((column) => !currentColumns.includes(column))
+    );
+  }, [currentColumns]);
+
   const handleSelectChange = useCallback((value: string) => {
-    console.log(value);
+    value.length > 1 && setCurrentColumns((current) => [...current, value]);
   }, []);
+
+  const onSave = () => {
+    const user = cloneDeep(currentUser) as User;
+
+    console.log(user);
+
+    set(
+      user,
+      `company_user.settings.react_table_columns.${table}`,
+      currentColumns
+    );
+
+    // toast.processing();
+
+    request('PUT', endpoint('/api/v1/company_users/:id', { id: user.id }), user)
+      .then((response) => console.log(response))
+      .catch((error) => console.error(error));
+  };
 
   return (
     <>
@@ -38,14 +80,27 @@ export function DataTableColumnsPicker(props: Props) {
         <SelectField
           label={t('add_column')}
           onValueChange={handleSelectChange}
+          value=""
           withBlank
         >
-          {Object.entries(columns).map(([column, translation], index) => (
+          {filteredColumns.map((column, index) => (
             <option key={index} value={column}>
-              {t(translation)}
+              {t(column)}
             </option>
           ))}
         </SelectField>
+
+        <div className="max-h-64 overflow-y-auto">
+          {currentColumns.map((column, index) => (
+            <p key={index}>{t(column)}</p>
+          ))}
+        </div>
+
+        <div className="flex lg:flex-row lg:justify-end">
+          <Inline>
+            <Button onClick={onSave}>{t('save')}</Button>
+          </Inline>
+        </div>
       </Modal>
 
       <button
