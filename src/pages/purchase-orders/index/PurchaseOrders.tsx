7f59d6ad7fc6cf7e8a04179bd2c0bd849e8a11e3
Benjamin Beganović <k1pstabug@gmail.com>
2022-10-25 18:00:14 +0200

    Purchase order columns

--- src/pages/purchase-orders/index/PurchaseOrders.tsx
+++ src/pages/purchase-orders/index/PurchaseOrders.tsx
@@ -8,20 +8,17 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { Link } from '@invoiceninja/forms';
-import purchaseOrderStatus from 'common/constants/purchase-order-status';
-import { date } from 'common/helpers';
-import { useFormatMoney } from 'common/hooks/money/useFormatMoney';
-import { useCurrentCompany } from 'common/hooks/useCurrentCompany';
-import { useCurrentCompanyDateFormats } from 'common/hooks/useCurrentCompanyDateFormats';
 import { useTitle } from 'common/hooks/useTitle';
-import { PurchaseOrder } from 'common/interfaces/purchase-order';
 import { Page } from 'components/Breadcrumbs';
-import { DataTable, DataTableColumns } from 'components/DataTable';
+import { DataTable } from 'components/DataTable';
+import { DataTableColumnsPicker } from 'components/DataTableColumnsPicker';
 import { Default } from 'components/layouts/Default';
-import { StatusBadge } from 'components/StatusBadge';
 import { useTranslation } from 'react-i18next';
-import { route } from 'common/helpers/route';
+import {
+  defaultColumns,
+  purchaseOrderColumns,
+  usePurchaseOrderColumns,
+} from '../common/hooks';
 
 export function PurchaseOrders() {
   const { documentTitle } = useTitle('purchase_orders');
@@ -32,83 +29,7 @@ export function PurchaseOrders() {
     { name: t('purchase_orders'), href: '/purchase_orders' },
   ];
 
-  const company = useCurrentCompany();
-  const formatMoney = useFormatMoney();
-
-  const { dateFormat } = useCurrentCompanyDateFormats();
-
-  const columns: DataTableColumns<PurchaseOrder> = [
-    {
-      id: 'status_id',
-      label: t('status'),
-      format: (field, purchaseOrder) => (
-        <Link
-          to={route('/purchase_orders/:id/edit', {
-            id: purchaseOrder.id,
-          })}
-        >
-          <StatusBadge for={purchaseOrderStatus} code={field} />
-        </Link>
-      ),
-    },
-    {
-      id: 'number',
-      label: t('number'),
-      format: (field, purchaseOrder) => (
-        <Link
-          to={route('/purchase_orders/:id/edit', {
-            id: purchaseOrder.id,
-          })}
-        >
-          {field}
-        </Link>
-      ),
-    },
-    {
-      id: 'vendor_id',
-      label: t('vendor'),
-      format: (field, purchaseOrder) =>
-        purchaseOrder.vendor && (
-          <Link
-            to={route('/vendors/:id', { id: purchaseOrder.vendor.id })}
-          >
-            {purchaseOrder.vendor.name}
-          </Link>
-        ),
-    },
-    {
-      id: 'expense_id',
-      label: t('expense'),
-      format: (field, purchaseOrder) =>
-        purchaseOrder.expense && (
-          <Link
-            to={route('/expenses/:id', { id: purchaseOrder.expense.id })}
-          >
-            {purchaseOrder.expense.number}
-          </Link>
-        ),
-    },
-    {
-      id: 'amount',
-      label: t('amount'),
-      format: (amount) =>
-        formatMoney(
-          amount,
-          company?.settings.country_id,
-          company?.settings.currency_id
-        ),
-    },
-    {
-      id: 'date',
-      label: t('date'),
-      format: (value) => date(value, dateFormat),
-    },
-    {
-      id: 'due_date',
-      label: t('due_date'),
-      format: (value) => date(value, dateFormat),
-    },
-  ];
+  const columns = usePurchaseOrderColumns();
 
   return (
     <Default title={documentTitle} breadcrumbs={pages}>
@@ -120,6 +41,13 @@ export function PurchaseOrders() {
         linkToEdit="/purchase_orders/:id/edit"
         columns={columns}
         withResourcefulActions
+        leftSideChevrons={
+          <DataTableColumnsPicker
+            columns={purchaseOrderColumns as unknown as string[]}
+            defaultColumns={defaultColumns}
+            table="purchaseOrder"
+          />
+        }
       />
     </Default>
   );
