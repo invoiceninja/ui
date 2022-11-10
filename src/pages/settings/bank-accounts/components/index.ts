e69308930e9c564e01a27e795e93d478e3cfb7be
Abedin <abedinhalilovic12345@gmail.com>
2022-11-10 21:52:43 +0100

    Created index files for exporting feature components

---
+++ src/pages/settings/bank-accounts/components/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+export { default as Details } from './Details';
+export { default as ConnectAccounts } from './ConnectAccounts';
---
+++ src/pages/settings/bank-accounts/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+export { default as BankAccounts } from './index/BankAccounts';
+export { default as EditBankAccount } from './edit/Edit';
+export { default as BankAccountDetails } from './show/BankAccountDetails';
