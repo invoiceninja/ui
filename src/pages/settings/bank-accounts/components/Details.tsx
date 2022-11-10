275b804a2a34f86600a3928c73f4b5399c0f27fc
Abedin <abedinhalilovic12345@gmail.com>
2022-11-10 21:54:21 +0100

    Created components for show view

---
+++ src/pages/settings/bank-accounts/components/Details.tsx
@@ -0,0 +1,45 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { useFormatMoney } from 'common/hooks/money/useFormatMoney';
+import { useCurrentCompany } from 'common/hooks/useCurrentCompany';
+import { BankAccDetails } from 'common/interfaces/bank-accounts';
+import { useTranslation } from 'react-i18next';
+import { Card, Element } from '../../../../components/cards';
+
+const Details = ({ accountDetails }: { accountDetails: BankAccDetails }) => {
+  const {
+    balance,
+    bank_account_type: bankAccountType,
+    provider_name: providerName,
+    bank_account_status: bankAccountStatus,
+  } = accountDetails;
+
+  const [t] = useTranslation();
+  const company = useCurrentCompany();
+  const formatMoney = useFormatMoney();
+
+  return (
+    <Card title={t('details')}>
+      <Element leftSide={t('balance')}>
+        {formatMoney(
+          balance || 0,
+          company.settings.country_id,
+          company.settings.currency_id
+        )}
+      </Element>
+      <Element leftSide={t('type')}>{bankAccountType}</Element>
+      <Element leftSide={t('provider')}>{providerName}</Element>
+      <Element leftSide={t('status')}>{bankAccountStatus}</Element>
+    </Card>
+  );
+};
+
+export default Details;
---
+++ src/pages/settings/bank-accounts/show/BankAccountDetails.tsx
@@ -0,0 +1,69 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { defaultAccountDetails } from 'common/constants/bank-accounts';
+import { endpoint } from 'common/helpers';
+import { request } from 'common/helpers/request';
+import { route } from 'common/helpers/route';
+import { useTitle } from 'common/hooks/useTitle';
+import { BankAccDetails } from 'common/interfaces/bank-accounts';
+import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+import { useTranslation } from 'react-i18next';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Settings } from '../../../../components/layouts/Settings';
+import { Details } from '../components';
+
+const BankAccountDetails = () => {
+  useTitle('bank_account_details');
+  const [t] = useTranslation();
+  const navigate = useNavigate();
+  const { id: bankAccountId } = useParams();
+  const [bankAccountDetails, setBankAccountDetails] = useState<BankAccDetails>(
+    defaultAccountDetails
+  );
+
+  const pages = [
+    { name: t('settings'), href: '/settings' },
+    { name: t('bank_account_details'), href: '/settings/bank_account_details' },
+  ];
+
+  const fetchBankAccountDetails = async () => {
+    toast.loading(t('processing'));
+
+    try {
+      const { data: responseData } = await request(
+        'GET',
+        endpoint('/api/v1/bank_integrations/:id', { id: bankAccountId })
+      );
+      setBankAccountDetails(responseData?.data);
+    } catch (error) {
+      console.error(error);
+      navigate(route('/settings/bank_accounts'));
+      return;
+    }
+    toast.dismiss();
+  };
+
+  useEffect(() => {
+    fetchBankAccountDetails();
+  }, []);
+
+  return (
+    <Settings
+      title={t('bank_account_details')}
+      breadcrumbs={pages}
+      docsLink="docs/basic-settings/#bank_account_details"
+    >
+      <Details accountDetails={bankAccountDetails} />
+    </Settings>
+  );
+};
+export default BankAccountDetails;
