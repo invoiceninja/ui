f20a35553cc97b17016943a55ecb3cd7835e4465
Abedin <abedinhalilovic12345@gmail.com>
2022-11-10 21:51:14 +0100

    Created needed constants

---
+++ src/common/constants/bank-accounts.ts
@@ -0,0 +1,22 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+export const defaultAccountDetails = {
+  company_id: 'Apnew5aKBz',
+  user_id: 'AzncbfaKBz',
+  provider_bank_name: 'Bank Name',
+  bank_account_id: 1,
+  bank_account_name: 'Acc name',
+  bank_account_number: '111 111 1111',
+  bank_account_status: 'ACTIVE',
+  bank_account_type: 'CREDITCARD',
+  balance: 1,
+  currency: 'USD',
+};
