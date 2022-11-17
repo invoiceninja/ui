a8fb1c4030bde27853198568bc2be20e13bd1c28
Benjamin Beganović <k1pstabug@gmail.com>
2022-11-17 14:17:50 +0100

    Add reports to the sidebar

--- src/components/layouts/Default.tsx
+++ src/components/layouts/Default.tsx
@@ -23,6 +23,7 @@ import {
   ShieldOff,
   Briefcase,
   Clock,
+  PieChart,
 } from 'react-feather';
 import CommonProps from '../../common/interfaces/common-props.interface';
 import { useTranslation } from 'react-i18next';
@@ -229,6 +230,13 @@ export function Default(props: Props) {
         label: t('new_expense'),
         visible: hasPermission('create_expense'),
       },
+    },
+    {
+      name: t('reports'),
+      href: '/reports',
+      icon: PieChart,
+      current: location.pathname.startsWith('/reports'),
+      visible: true,
     },
     {
       name: t('settings'),
