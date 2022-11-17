e07ff8c18a5ba1ca0aea69b6e83396bcc885c89f
Benjamin Beganović <k1pstabug@gmail.com>
2022-11-17 14:17:46 +0100

    Scaffold report routes

---
+++ src/pages/reports/index/Reports.tsx
@@ -0,0 +1,18 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { useTitle } from 'common/hooks/useTitle';
+import { Default } from 'components/layouts/Default';
+
+export function Reports() {
+  const { documentTitle } = useTitle('reports');
+
+  return <Default title={documentTitle}></Default>;
+}
