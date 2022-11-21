a25280ff5dc54f361e008653b366503e06b20892
Benjamin Beganović <k1pstabug@gmail.com>
2022-11-21 11:58:47 +0100

    Exporting reports

--- src/pages/reports/index/Reports.tsx
+++ src/pages/reports/index/Reports.tsx
@@ -9,8 +9,13 @@
  */
 
 import { Card, Element } from '@invoiceninja/cards';
-import { Datepicker, InputField, SelectField } from '@invoiceninja/forms';
+import { InputField, SelectField } from '@invoiceninja/forms';
+import { AxiosError } from 'axios';
+import { endpoint } from 'common/helpers';
+import { request } from 'common/helpers/request';
+import { toast } from 'common/helpers/toast/toast';
 import { useTitle } from 'common/hooks/useTitle';
+import { ValidationBag } from 'common/interfaces/validation-bag';
 import { Page } from 'components/Breadcrumbs';
 import { Default } from 'components/layouts/Default';
 import { useState } from 'react';
@@ -57,7 +62,7 @@ const reports: Report[] = [
       start_date: '',
       end_date: '',
       date_key: '',
-      date_range: '',
+      date_range: 'all',
       report_keys: [],
       send_email: false,
     },
@@ -70,7 +75,7 @@ const reports: Report[] = [
       start_date: '',
       end_date: '',
       date_key: '',
-      date_range: '',
+      date_range: 'all',
       report_keys: [],
       send_email: false,
     },
@@ -99,7 +104,8 @@ export function Reports() {
   const { t } = useTranslation();
 
   const [report, setReport] = useState<Report>(reports[0]);
-  const [range, setRange] = useState<Range>();
+  const [isPendingExport, setIsPendingExport] = useState(false);
+  const [errors, setErrors] = useState<ValidationBag>();
 
   const pages: Page[] = [{ name: t('reports'), href: '/reports' }];
 
@@ -115,7 +121,10 @@ export function Reports() {
     const range = ranges.find((range) => range.identifier === identifier);
 
     if (range) {
-      setRange(range);
+      setReport((current) => ({
+        ...current,
+        payload: { ...current.payload, date_range: range.identifier },
+      }));
     }
   };
 
@@ -129,8 +138,55 @@ export function Reports() {
     }));
   };
 
+  const handleExport = () => {
+    toast.processing();
+
+    setIsPendingExport(true);
+    setErrors(undefined);
+
+    request('POST', endpoint(report.endpoint), report.payload, {
+      responseType: 'blob',
+    })
+      .then((response) => {
+        const blob = new Blob([response.data], { type: 'text/csv' });
+        const url = URL.createObjectURL(blob);
+
+        const link = document.createElement('a');
+
+        link.download = `${report.identifier}.csv`;
+        link.href = url;
+        link.target = '_blank';
+
+        document.body.appendChild(link);
+
+        link.click();
+
+        document.body.removeChild(link);
+
+        toast.success();
+      })
+      .catch((error: AxiosError) => {
+        console.error(error);
+
+        if (error.response?.status === 422) {
+          const blob = error.response.data as Blob;
+
+          blob.text().then((text) => setErrors(JSON.parse(text)));
+        }
+
+        toast.error();
+      })
+      .finally(() => setIsPendingExport(false));
+  };
+
   return (
-    <Default title={documentTitle} breadcrumbs={pages}>
+    <Default
+      title={documentTitle}
+      breadcrumbs={pages}
+      onSaveClick={handleExport}
+      saveButtonLabel={t('export')}
+      disableSaveButton={isPendingExport}
+    >
       <div className="grid grid-cols-12 gap-4">
         <Card className="col-span-6 h-max">
           <Element leftSide={t('report')}>
@@ -157,24 +213,26 @@ export function Reports() {
             </SelectField>
           </Element>
 
-          {range?.identifier === 'custom' && (
+          {report.payload.date_range === 'custom' && (
             <Element leftSide={t('start_date')}>
               <InputField
                 type="date"
                 onValueChange={(value) =>
                   handleCustomDateChange('start_date', value)
                 }
+                errorMessage={errors?.errors?.start_date}
               />
             </Element>
           )}
 
-          {range?.identifier === 'custom' && (
+          {report.payload.date_range === 'custom' && (
             <Element leftSide={t('end_date')}>
               <InputField
                 type="date"
                 onValueChange={(value) =>
                   handleCustomDateChange('end_date', value)
                 }
+                errorMessage={errors?.errors?.end_date}
               />
             </Element>
           )}
