9c38d8a6808848a915587bf1b4e487758d442c46
Abedin <abedinhalilovic12345@gmail.com>
2022-12-13 20:42:18 +0100

    Implemented solution for multi languages in the app

--- src/index.tsx
+++ src/index.tsx
@@ -22,12 +22,108 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { createRoot } from 'react-dom/client';
 
 import './resources/css/app.css';
-import en from './resources/lang/en/en.json';
+import * as LanguageResources from './resources/lang/index';
 
 i18n.use(initReactI18next).init({
   resources: {
+    ar: {
+      translation: LanguageResources.ar,
+    },
+    ca: {
+      translation: LanguageResources.ca,
+    },
+    cs: {
+      translation: LanguageResources.cs,
+    },
+    da: {
+      translation: LanguageResources.da,
+    },
+    de: {
+      translation: LanguageResources.de,
+    },
+    el: {
+      translation: LanguageResources.el,
+    },
     en: {
-      translation: en,
+      translation: LanguageResources.en,
+    },
+    en_GB: {
+      translation: LanguageResources.en_GB,
+    },
+    es: {
+      translation: LanguageResources.es,
+    },
+    es_ES: {
+      translation: LanguageResources.es_ES,
+    },
+    fa: {
+      translation: LanguageResources.fa,
+    },
+    fi: {
+      translation: LanguageResources.fi,
+    },
+    fr: {
+      translation: LanguageResources.fr,
+    },
+    fr_CA: {
+      translation: LanguageResources.fr_CA,
+    },
+    hr: {
+      translation: LanguageResources.hr,
+    },
+    it: {
+      translation: LanguageResources.it,
+    },
+    ja: {
+      translation: LanguageResources.ja,
+    },
+    lt: {
+      translation: LanguageResources.lt,
+    },
+    lv_LV: {
+      translation: LanguageResources.lv_LV,
+    },
+    mk_MK: {
+      translation: LanguageResources.mk_MK,
+    },
+    nb_NO: {
+      translation: LanguageResources.nb_NO,
+    },
+    nl: {
+      translation: LanguageResources.nl,
+    },
+    pl: {
+      translation: LanguageResources.pl,
+    },
+    pt_BR: {
+      translation: LanguageResources.pt_BR,
+    },
+    pt_PT: {
+      translation: LanguageResources.pt_PT,
+    },
+    ro: {
+      translation: LanguageResources.ro,
+    },
+    ru_RU: {
+      translation: LanguageResources.ru_RU,
+    },
+    sl: {
+      translation: LanguageResources.sl,
+    },
+    sq: {
+      translation: LanguageResources.sq,
+    },
+    sv: {
+      translation: LanguageResources.sv,
+    },
+    th: {
+      translation: LanguageResources.th,
+    },
+    tr_TR: {
+      translation: LanguageResources.tr_TR,
+    },
+    zh_TW: {
+      translation: LanguageResources.zh_TW,
     },
   },
   lng: 'en',
---
+++ src/resources/lang/index.ts
@@ -0,0 +1,45 @@
+/**
+* Invoice Ninja (https://invoiceninja.com).
+*
+* @link https://github.com/invoiceninja/invoiceninja source repository
+*
+* @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+*
+* @license https://www.elastic.co/licensing/elastic-license
+*/
+
+import ar from './ar/ar.json';
+import ca from './ca/ca.json';
+import cs from './cs/cs.json';
+import da from './da/da.json';
+import de from './de/de.json';
+import el from './el/el.json';
+import en from './en/en.json';
+import en_GB from './en_GB/en_GB.json';
+import es from './es/es.json';
+import es_ES from './es_ES/es_ES.json';
+import fa from './fa/fa.json';
+import fi from './fi/fi.json';
+import fr from './fr/fr.json';
+import fr_CA from './fr_CA/fr_CA.json';
+import hr from './hr/hr.json';
+import it from './it/it.json';
+import ja from './ja/ja.json';
+import lt from './lt/lt.json';
+import lv_LV from './lv_LV/lv_LV.json';
+import mk_MK from './mk_MK/mk_MK.json';
+import nb_NO from './nb_NO/nb_NO.json';
+import nl from './nl/nl.json';
+import pl from './pl/pl.json';
+import pt_BR from './pt_BR/pt_BR.json';
+import pt_PT from './pt_PT/pt_PT.json';
+import ro from './ro/ro.json';
+import ru_RU from './ru_RU/ru_RU.json';
+import sl from './sl/sl.json';
+import sq from './sq/sq.json';
+import sv from './sv/sv.json';
+import th from './th/th.json';
+import tr_TR from './tr_TR/tr_TR.json';
+import zh_TW from './zh_TW/zh_TW.json';
+
+export { it, ar, ca, cs, da, de, el, en, en_GB, es, es_ES, fa, fi, fr, fr_CA, hr, ja, lt, lv_LV, mk_MK, nb_NO, nl, pl, pt_BR, pt_PT, ro, ru_RU, sl, sq, sv, th, tr_TR, zh_TW };
