a9fcc40eb62191fc5e409c0a6c4d349a19c3a46f
Abedin <abedinhalilovic12345@gmail.com>
2022-12-14 23:05:08 +0100

    Added necessary translation

--- src/components/import/UploadImport.tsx
+++ src/components/import/UploadImport.tsx
@@ -214,7 +214,7 @@ export function UploadImport(props: Props) {
                 <Image className="mx-auto h-12 w-12 text-gray-400" />
                 <span className="mt-2 block text-sm font-medium text-gray-900">
                   {isDragActive
-                    ? 'drop_your_files_here'
+                    ? t('drop_your_files_here')
                     : t('dropzone_default_message')}
                 </span>
               </div>
