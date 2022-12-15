b1176b69ab6d5751348582ae155efaa77f9c6b95
Benjamin Beganović <k1pstabug@gmail.com>
2022-12-15 13:43:30 +0100

    Edit form in slider

--- src/pages/tasks/kanban/components/EditSlider.tsx
+++ src/pages/tasks/kanban/components/EditSlider.tsx
@@ -8,6 +8,86 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { Button, InputField } from '@invoiceninja/forms';
+import { current } from '@reduxjs/toolkit';
+import { Task } from 'common/interfaces/task';
+import { TaskStatus } from 'common/interfaces/task-status';
+import { ClientSelector } from 'components/clients/ClientSelector';
+import { DebouncedCombobox, Record } from 'components/forms/DebouncedCombobox';
+import { ProjectSelector } from 'components/projects/ProjectSelector';
+import { TabGroup } from 'components/TabGroup';
+import { UserSelector } from 'components/users/UserSelector';
+import { useAtom } from 'jotai';
+import { useSave } from 'pages/tasks/common/hooks';
+import { useTranslation } from 'react-i18next';
+import { currentTaskAtom } from '../common/atoms';
+
 export function EditSlider() {
-  return <div>Edit slider</div>;
+  const [t] = useTranslation();
+  const [task, setTask] = useAtom(currentTaskAtom);
+
+  const handleChange = (property: keyof Task, value: Task[typeof property]) => {
+    setTask((current) => current && { ...current, [property]: value });
+  };
+
+  const save = useSave();
+
+  return (
+    <TabGroup tabs={[t('details'), t('times')]} width="full">
+      <div className="px-4 space-y-4">
+        <ClientSelector
+          inputLabel={t('client')}
+          value={task?.client_id}
+          onChange={(client) => handleChange('client_id', client.id)}
+        />
+
+        <ProjectSelector
+          inputLabel={t('project')}
+          value={task?.project_id}
+          onChange={(project) => handleChange('project_id', project.id)}
+        />
+
+        <UserSelector
+          inputLabel={t('user')}
+          value={task?.assigned_user_id}
+          onChange={(user) => handleChange('assigned_user_id', user.id)}
+        />
+
+        <InputField
+          label={t('task_number')}
+          value={task?.number}
+          onValueChange={(number) => handleChange('number', number)}
+        />
+
+        <InputField
+          label={t('rate')}
+          value={task?.rate}
+          onValueChange={(rate) => handleChange('rate', rate)}
+        />
+
+        <DebouncedCombobox
+          inputLabel={t('status')}
+          endpoint="/api/v1/task_statuses"
+          label="name"
+          onChange={(value: Record<TaskStatus>) =>
+            value.resource && handleChange('status_id', value.resource.id)
+          }
+          defaultValue={task?.status_id}
+          queryAdditional
+        />
+
+        <InputField
+          element="textarea"
+          value={task?.description}
+          onValueChange={(value) => handleChange('description', value)}
+        />
+
+        <div className="flex justify-end">
+          <Button onClick={() => task && save(task)}>{t('save')}</Button>
+        </div>
+      </div>
+
+      <div>Times</div>
+    </TabGroup>
+  );
 }
