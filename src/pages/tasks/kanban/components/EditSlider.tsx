236d44dc384c61e71e4b223d1b8fb80c72387709
Benjamin Beganović <k1pstabug@gmail.com>
2022-12-16 13:37:42 +0100

    Show time records in the "times" tab

--- src/pages/tasks/kanban/components/EditSlider.tsx
+++ src/pages/tasks/kanban/components/EditSlider.tsx
@@ -8,80 +8,135 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { ClickableElement } from '@invoiceninja/cards';
 import { Button, InputField } from '@invoiceninja/forms';
 import { Task } from 'common/interfaces/task';
 import { TaskStatus } from 'common/interfaces/task-status';
 import { ClientSelector } from 'components/clients/ClientSelector';
 import { DebouncedCombobox, Record } from 'components/forms/DebouncedCombobox';
+import { Modal } from 'components/Modal';
 import { ProjectSelector } from 'components/projects/ProjectSelector';
+import { TabGroup } from 'components/TabGroup';
 import { UserSelector } from 'components/users/UserSelector';
 import { useAtom } from 'jotai';
+import { parseTimeLog } from 'pages/tasks/common/helpers/calculate-time';
 import { useSave } from 'pages/tasks/common/hooks';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { currentTaskAtom } from '../common/atoms';
+import { useFormatTimeLog } from '../common/hooks';
 
 export function EditSlider() {
   const [t] = useTranslation();
   const [task, setTask] = useAtom(currentTaskAtom);
+
+  const [isTimeModalVisible, setIsTimeModalVisible] = useState(false);
+  const [timeLog, setTimeLog] = useState<number[]>([]);
 
   const handleChange = (property: keyof Task, value: Task[typeof property]) => {
     setTask((current) => current && { ...current, [property]: value });
   };
 
   const save = useSave();
+  const formatTimeLog = useFormatTimeLog();
 
   return (
-    <div className="p-4 space-y-4">
-      <ClientSelector
-        inputLabel={t('client')}
-        value={task?.client_id}
-        onChange={(client) => handleChange('client_id', client.id)}
-      />
+    <>
+      <Modal
+        title={t('time_log')}
+        visible={isTimeModalVisible && timeLog.length > 0}
+        onClose={() => {
+          setIsTimeModalVisible(false);
+          setTimeLog([]);
+        }}
+      >
+        <InputField
+          label={t('date')}
+          type="date"
+          // value={parseTimeToDate(timeLog[0])}
+          onValueChange={(value) => console.log(value)}
+        />
 
-      <ProjectSelector
-        inputLabel={t('project')}
-        value={task?.project_id}
-        onChange={(project) => handleChange('project_id', project.id)}
-      />
+        <InputField label={t('start_time')} />
+        <InputField label={t('end_time')} />
+        <InputField label={t('duration')} />
+      </Modal>
 
-      <UserSelector
-        inputLabel={t('user')}
-        value={task?.assigned_user_id}
-        onChange={(user) => handleChange('assigned_user_id', user.id)}
-      />
+      <TabGroup tabs={[t('details'), t('times')]} width="full">
+        <div className="p-4 space-y-4">
+          <ClientSelector
+            inputLabel={t('client')}
+            value={task?.client_id}
+            onChange={(client) => handleChange('client_id', client.id)}
+          />
 
-      <InputField
-        label={t('task_number')}
-        value={task?.number}
-        onValueChange={(number) => handleChange('number', number)}
-      />
+          <ProjectSelector
+            inputLabel={t('project')}
+            value={task?.project_id}
+            onChange={(project) => handleChange('project_id', project.id)}
+          />
 
-      <InputField
-        label={t('rate')}
-        value={task?.rate}
-        onValueChange={(rate) => handleChange('rate', rate)}
-      />
+          <UserSelector
+            inputLabel={t('user')}
+            value={task?.assigned_user_id}
+            onChange={(user) => handleChange('assigned_user_id', user.id)}
+          />
 
-      <DebouncedCombobox
-        inputLabel={t('status')}
-        endpoint="/api/v1/task_statuses"
-        label="name"
-        onChange={(value: Record<TaskStatus>) =>
-          value.resource && handleChange('status_id', value.resource.id)
-        }
-        defaultValue={task?.status_id}
-        queryAdditional
-      />
+          <InputField
+            label={t('task_number')}
+            value={task?.number}
+            onValueChange={(number) => handleChange('number', number)}
+          />
 
-      <InputField
-        element="textarea"
-        value={task?.description}
-        onValueChange={(value) => handleChange('description', value)}
-      />
+          <InputField
+            label={t('rate')}
+            value={task?.rate}
+            onValueChange={(rate) => handleChange('rate', rate)}
+          />
 
-      <div className="flex justify-end">
-        <Button onClick={() => task && save(task)}>{t('save')}</Button>
-      </div>
-    </div>
+          <DebouncedCombobox
+            inputLabel={t('status')}
+            endpoint="/api/v1/task_statuses"
+            label="name"
+            onChange={(value: Record<TaskStatus>) =>
+              value.resource && handleChange('status_id', value.resource.id)
+            }
+            defaultValue={task?.status_id}
+            queryAdditional
+          />
+
+          <InputField
+            element="textarea"
+            value={task?.description}
+            onValueChange={(value) => handleChange('description', value)}
+          />
+
+          <div className="flex justify-end">
+            <Button onClick={() => task && save(task)}>{t('save')}</Button>
+          </div>
+        </div>
+
+        <div>
+          {task &&
+            formatTimeLog(task.time_log).map(([date, start, end], i) => (
+              <ClickableElement
+                key={i}
+                onClick={() => {
+                  setIsTimeModalVisible(true);
+                  setTimeLog(parseTimeLog(task.time_log)[i]);
+                }}
+              >
+                <div>
+                  <p>{date}</p>
+
+                  <small>
+                    {start} - {end}
+                  </small>
+                </div>
+              </ClickableElement>
+            ))}
+        </div>
+      </TabGroup>
+    </>
   );
 }
