602b3a222892b2e0bafebab8b7f82e2c8a74ef3f
Abedin <abedinhalilovic12345@gmail.com>
2022-12-17 15:34:45 +0100

    Returned code to App component until fix VM

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,22 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { useCurrentCompany } from 'common/hooks/useCurrentCompany';
+import { useResolveLanguage } from 'common/hooks/useResolveLanguage';
 import { useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
+import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { routes } from './common/routes';
 import { RootState } from './common/stores/store';
 
 export function App() {
+  const { i18n } = useTranslation();
+
+  const company = useCurrentCompany();
+
+  const resolveLanguage = useResolveLanguage();
+
   const darkMode = useSelector((state: RootState) => state.settings.darkMode);
 
   useEffect(() => {
@@ -23,7 +32,22 @@ export function App() {
     darkMode
       ? document.querySelector('html')?.classList.add('dark')
       : document.querySelector('html')?.classList.remove('dark');
-  }, [darkMode]);
+
+    const resolvedLanguage = resolveLanguage(company?.settings.language_id);
+
+    if (resolvedLanguage?.locale) {
+      if (!i18n.hasResourceBundle(resolvedLanguage.locale, 'translation')) {
+        import(
+          `./resources/lang/${resolvedLanguage.locale}/${resolvedLanguage.locale}.json`
+        ).then((response) => {
+          i18n.addResources(resolvedLanguage.locale, 'translation', response);
+          i18n.changeLanguage(resolvedLanguage.locale);
+        });
+      } else {
+        i18n.changeLanguage(resolvedLanguage.locale);
+      }
+    }
+  }, [darkMode, company]);
 
   return (
     <div className="App">
