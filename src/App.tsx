c801910c2d917e7ee32bd37938c625616f80085b
Abedin <abedinhalilovic12345@gmail.com>
2022-12-17 18:34:27 +0100

    Changed dynamic import logic

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,15 @@ import { useCurrentCompany } from 'common/hooks/useCurrentCompany';
 import { useResolveLanguage } from 'common/hooks/useResolveLanguage';
 import { useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
+import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import { routes } from './common/routes';
 import { RootState } from './common/stores/store';
 
 export function App() {
+  const { i18n } = useTranslation();
+
   const company = useCurrentCompany();
 
   const location = useLocation();
@@ -40,6 +43,21 @@ export function App() {
     darkMode
       ? document.querySelector('html')?.classList.add('dark')
       : document.querySelector('html')?.classList.remove('dark');
+
+    if (resolvedLanguage?.locale) {
+      const fileKey = resolvedLanguage.locale;
+
+      if (!i18n.hasResourceBundle(fileKey, 'translation')) {
+        import(`./resources/lang/${fileKey}/${fileKey}.json`).then(
+          (response) => {
+            i18n.addResources(fileKey, 'translation', response);
+            i18n.changeLanguage(fileKey);
+          }
+        );
+      } else {
+        i18n.changeLanguage(fileKey);
+      }
+    }
   }, [darkMode, company]);
 
   return (
