85ec26d8234b4d8a8c589a5d1990b618c8f9ea5c
Benjamin Beganović <k1pstabug@gmail.com>
2022-12-18 15:00:05 +0100

    Remove duplicate options from ViewSlider

--- src/pages/tasks/kanban/components/ViewSlider.tsx
+++ src/pages/tasks/kanban/components/ViewSlider.tsx
@@ -12,6 +12,7 @@ import { ClickableElement, Element } from '@invoiceninja/cards';
 import { endpoint } from 'common/helpers';
 import { route } from 'common/helpers/route';
 import { useFormatMoney } from 'common/hooks/money/useFormatMoney';
+import { useAccentColor } from 'common/hooks/useAccentColor';
 import { useCurrentCompany } from 'common/hooks/useCurrentCompany';
 import { NonClickableElement } from 'components/cards/NonClickableElement';
 import { DocumentsTable } from 'components/DocumentsTable';
@@ -19,11 +20,7 @@ import { TabGroup } from 'components/TabGroup';
 import { useAtom } from 'jotai';
 import { Upload } from 'pages/settings/company/documents/components';
 import { TaskStatus } from 'pages/tasks/common/components/TaskStatus';
-import { isTaskRunning } from 'pages/tasks/common/helpers/calculate-entity-state';
 import { calculateTime } from 'pages/tasks/common/helpers/calculate-time';
-import { useStart } from 'pages/tasks/common/hooks/useStart';
-import { useStop } from 'pages/tasks/common/hooks/useStop';
-import { Edit, Pause, Play, Plus } from 'react-feather';
 import { useTranslation } from 'react-i18next';
 import { useQueryClient } from 'react-query';
 import { currentTaskAtom } from '../common/atoms';
@@ -36,9 +33,7 @@ export function ViewSlider() {
   const formatMoney = useFormatMoney();
   const company = useCurrentCompany();
   const formatTimeLog = useFormatTimeLog();
-
-  const startTask = useStart();
-  const stopTask = useStop();
+  const accentColor = useAccentColor();
 
   const queryClient = useQueryClient();
 
@@ -70,41 +65,13 @@ export function ViewSlider() {
               <TaskStatus entity={currentTask} />
             </Element>
 
-            <ClickableElement
-              to={route('/tasks/:id/edit', { id: currentTask.id })}
+            <NonClickableElement
+              style={{ backgroundColor: accentColor, color: 'white' }}
             >
-              <div className="inline-flex items-center space-x-1">
-                <Edit size={18} />
-                <span>{t('edit_task')}</span>
+              <div className="inline-flex items-center">
+                <span>{currentTask.description}</span>
               </div>
-            </ClickableElement>
-
-            <ClickableElement>
-              <div className="inline-flex items-center space-x-1">
-                <Plus size={18} />
-                <span>{t('invoice_task')}</span>
-              </div>
-            </ClickableElement>
-
-            {!isTaskRunning(currentTask) && (
-              <ClickableElement onClick={() => startTask(currentTask)}>
-                <div className="inline-flex items-center space-x-1">
-                  <Play size={18} />
-                  <span>{t('start')}</span>
-                </div>
-              </ClickableElement>
-            )}
-
-            {isTaskRunning(currentTask) && (
-              <ClickableElement onClick={() => stopTask(currentTask)}>
-                <div className="inline-flex items-center space-x-1">
-                  <Pause size={18} />
-                  <span>{t('stop')}</span>
-                </div>
-              </ClickableElement>
-            )}
-
-            <NonClickableElement>{currentTask.description}</NonClickableElement>
+            </NonClickableElement>
 
             {formatTimeLog(currentTask.time_log).map(
               ([date, start, end], i) => (
