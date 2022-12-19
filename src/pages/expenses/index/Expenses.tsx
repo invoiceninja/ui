fc778398639cbd0268ca8a0ba381badd978de89a
Abedin <abedinhalilovic12345@gmail.com>
2022-12-20 00:40:46 +0100

    Added custom actions expenses and formatted code

--- src/pages/expenses/index/Expenses.tsx
+++ src/pages/expenses/index/Expenses.tsx
@@ -12,14 +12,14 @@ import { useTitle } from 'common/hooks/useTitle';
 import { DataTable } from 'components/DataTable';
 import { Default } from 'components/layouts/Default';
 import { useTranslation } from 'react-i18next';
-import { Link as ReactRouterLink } from 'react-router-dom';
-import { Download } from 'react-feather';
 import {
   defaultColumns,
   expenseColumns,
+  useActions,
   useExpenseColumns,
 } from '../common/hooks';
 import { DataTableColumnsPicker } from 'components/DataTableColumnsPicker';
+import { ImportButton } from 'components/import/ImportButton';
 
 export function Expenses() {
   useTitle('expenses');
@@ -28,22 +28,9 @@ export function Expenses() {
 
   const pages = [{ name: t('expenses'), href: '/expenses' }];
 
-  const importButton = (
-    <ReactRouterLink to="/expenses/import">
-      <button className="inline-flex items-center justify-center py-2 px-4 rounded text-sm text-white bg-green-500 hover:bg-green-600">
-        <svg
-          className="w-4 h-4 mr-2"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="3 3 20 20"
-        >
-          <Download />
-        </svg>
-        <span>{t('import')}</span>
-      </button>
-    </ReactRouterLink>
-  );
-
   const columns = useExpenseColumns();
+
+  const actions = useActions();
 
   return (
     <Default
@@ -58,8 +45,9 @@ export function Expenses() {
         bulkRoute="/api/v1/expenses/bulk"
         linkToCreate="/expenses/create"
         linkToEdit="/expenses/:id/edit"
+        customActions={actions}
         withResourcefulActions
-        rightSide={importButton}
+        rightSide={<ImportButton route="/expenses/import" />}
         leftSideChevrons={
           <DataTableColumnsPicker
             columns={expenseColumns as unknown as string[]}
