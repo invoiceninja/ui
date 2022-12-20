e036aea11c54c3881747f88743522e02b9a1b375
Abedin <abedinhalilovic12345@gmail.com>
2022-12-20 15:48:17 +0100

    Implemented solution for quote custom filters

--- src/pages/quotes/index/Quotes.tsx
+++ src/pages/quotes/index/Quotes.tsx
@@ -14,39 +14,28 @@ import { DataTable } from 'components/DataTable';
 import { Default } from 'components/layouts/Default';
 import { useTranslation } from 'react-i18next';
 import { route } from 'common/helpers/route';
-import { Link as ReactRouterLink } from 'react-router-dom';
-import { Download } from 'react-feather';
 import {
   defaultColumns,
   quoteColumns,
   useActions,
   useQuoteColumns,
+  useQuoteFilters,
 } from '../common/hooks';
 import { DataTableColumnsPicker } from 'components/DataTableColumnsPicker';
+import { ImportButton } from 'components/import/ImportButton';
 
 export function Quotes() {
   const { documentTitle } = useTitle('quotes');
-  const { t } = useTranslation();
+
+  const [t] = useTranslation();
 
   const pages: Page[] = [{ name: t('quotes'), href: route('/quotes') }];
 
-  const importButton = (
-    <ReactRouterLink to="/quotes/import">
-      <button className="inline-flex items-center justify-center py-2 px-4 rounded text-sm text-white bg-green-500 hover:bg-green-600">
-        <svg
-          className="w-4 h-4 mr-2"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="3 3 20 20"
-        >
-          <Download />
-        </svg>
-        <span>{t('import')}</span>
-      </button>
-    </ReactRouterLink>
-  );
-
   const columns = useQuoteColumns();
+
   const actions = useActions();
+
+  const filters = useQuoteFilters();
 
   return (
     <Default title={documentTitle} breadcrumbs={pages}>
@@ -58,8 +47,11 @@ export function Quotes() {
         linkToCreate="/quotes/create"
         bulkRoute="/api/v1/quotes/bulk"
         customActions={actions}
+        customFilters={filters}
+        customFilterQueryKey="client_status"
+        customFilterPlaceholder="quote_status"
         withResourcefulActions
-        rightSide={importButton}
+        rightSide={<ImportButton route="/quotes/import" />}
         leftSideChevrons={
           <DataTableColumnsPicker
             columns={quoteColumns as unknown as string[]}
