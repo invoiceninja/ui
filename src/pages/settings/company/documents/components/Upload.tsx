523841d8c54861c1aadaf4aee70ab69dfc453849
Abedin <abedinhalilovic12345@gmail.com>
2022-12-24 16:36:24 +0100

    Disabled uploading documents when you don't have permission with a warning message

--- src/pages/settings/company/documents/components/Upload.tsx
+++ src/pages/settings/company/documents/components/Upload.tsx
@@ -8,7 +8,6 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import toast from 'react-hot-toast';
 import { Card, Element } from '@invoiceninja/cards';
 import { useFormik } from 'formik';
 import { useState } from 'react';
@@ -16,6 +15,12 @@ import { useDropzone } from 'react-dropzone';
 import { Image } from 'react-feather';
 import { useTranslation } from 'react-i18next';
 import { request } from 'common/helpers/request';
+import { enterprisePlan } from 'common/guards/guards/enterprise-plan';
+import { isHosted } from 'common/helpers';
+import { Alert } from 'components/Alert';
+import { useCurrentUser } from 'common/hooks/useCurrentUser';
+import { Link } from '@invoiceninja/forms';
+import { toast } from 'common/helpers/toast/toast';
 
 interface Props {
   endpoint: string;
@@ -25,19 +30,22 @@ interface Props {
 
 export function Upload(props: Props) {
   const [t] = useTranslation();
+
+  const user = useCurrentUser();
+
   const [formData, setFormData] = useState(new FormData());
 
   const formik = useFormik({
     enableReinitialize: true,
     initialValues: {},
     onSubmit: () => {
-      const toastId = toast.loading(t('processing'));
+      toast.processing();
 
       request('POST', props.endpoint, formData, {
         headers: { 'Content-Type': 'multipart/form-data' },
       })
         .then(() => {
-          toast.success(t('uploaded_document'), { id: toastId });
+          toast.success('uploaded_document');
 
           setFormData(new FormData());
 
@@ -45,13 +53,13 @@ export function Upload(props: Props) {
         })
         .catch((error) => {
           console.error(error);
-
-          toast.error(t('error_title'), { id: toastId });
+          toast.error();
         });
     },
   });
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    disabled: !enterprisePlan() && isHosted(),
     onDrop: (acceptedFiles) => {
       formData.append('_method', 'PUT');
 
@@ -63,28 +71,27 @@ export function Upload(props: Props) {
     },
   });
 
+  const planWarning = (
+    <Alert className="mb-4" type="warning" disableClosing>
+      {t('documents_upgrade_plan')}
+
+      {user?.company_user && (
+        <Link
+          className="ml-10"
+          external
+          to={user.company_user.ninja_portal_url}
+        >
+          {t('plan_change')}
+        </Link>
+      )}
+    </Alert>
+  );
+
   if (props.widgetOnly) {
     return (
-      <div
-        {...getRootProps()}
-        className="flex flex-col md:flex-row md:items-center"
-      >
-        <div className="relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-          <input {...getInputProps()} />
-          <Image className="mx-auto h-12 w-12 text-gray-400" />
-          <span className="mt-2 block text-sm font-medium text-gray-900">
-            {isDragActive
-              ? 'drop_your_files_here'
-              : t('dropzone_default_message')}
-          </span>
-        </div>
-      </div>
-    );
-  }
+      <>
+        {!enterprisePlan() && isHosted() && planWarning}
 
-  return (
-    <Card title={t('upload')}>
-      <Element leftSide={t('upload')}>
         <div
           {...getRootProps()}
           className="flex flex-col md:flex-row md:items-center"
@@ -99,7 +106,32 @@ export function Upload(props: Props) {
             </span>
           </div>
         </div>
-      </Element>
-    </Card>
+      </>
+    );
+  }
+
+  return (
+    <>
+      {!enterprisePlan() && isHosted() && planWarning}
+
+      <Card title={t('upload')}>
+        <Element leftSide={t('upload')}>
+          <div
+            {...getRootProps()}
+            className="flex flex-col md:flex-row md:items-center"
+          >
+            <div className="relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <input {...getInputProps()} />
+              <Image className="mx-auto h-12 w-12 text-gray-400" />
+              <span className="mt-2 block text-sm font-medium text-gray-900">
+                {isDragActive
+                  ? 'drop_your_files_here'
+                  : t('dropzone_default_message')}
+              </span>
+            </div>
+          </div>
+        </Element>
+      </Card>
+    </>
   );
 }
