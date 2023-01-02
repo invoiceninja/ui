9f5d8105d98c360fbc119a3f472ee9b5fd61d84d
Abedin <abedinhalilovic12345@gmail.com>
2023-01-02 21:40:03 +0100

    Removed unnecessary logic parts and changed style

--- src/components/VerifyModal.tsx
+++ src/components/VerifyModal.tsx
@@ -12,10 +12,7 @@ import { endpoint } from 'common/helpers';
 import { request } from 'common/helpers/request';
 import { toast } from 'common/helpers/toast/toast';
 import { useCurrentUser } from 'common/hooks/useCurrentUser';
-import { useLogo } from 'common/hooks/useLogo';
-import { updateCompanyUsers } from 'common/stores/slices/company-users';
 import { useTranslation } from 'react-i18next';
-import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { Button } from './forms';
 import { Modal } from './Modal';
@@ -30,11 +27,7 @@ export function VerifyModal(props: Props) {
 
   const user = useCurrentUser();
 
-  const logo = useLogo();
-
   const navigate = useNavigate();
-
-  const dispatch = useDispatch();
 
   const handleResendEmail = () => {
     toast.processing();
@@ -49,23 +42,6 @@ export function VerifyModal(props: Props) {
       });
   };
 
-  const handleRefreshData = () => {
-    toast.processing();
-
-    request('POST', endpoint('/api/v1/refresh'))
-      .then((response) => {
-        const companyUsers = response.data.data;
-
-        dispatch(updateCompanyUsers(companyUsers));
-
-        toast.dismiss();
-      })
-      .catch((error) => {
-        console.error(error);
-        toast.error();
-      });
-  };
-
   return (
     <Modal
       title=""
@@ -74,42 +50,36 @@ export function VerifyModal(props: Props) {
       onClose={() => {}}
       disableClosing
     >
-      <div className="flex flex-col space-y-10 pb-1 px-3 text-center">
-        <div className="flex justify-center">
-          <div className="bg-gray-900 rounded-lg py-2 px-2 w-44">
-            <img src={logo} />
-          </div>
-        </div>
+      <div className="flex flex-col justify-center space-y-7 pb-1 px-3 text-left">
+        <span className="text-gray-800 text-xl font-semibold">
+          {props.type === 'email'
+            ? t('confirm_your_email_address')
+            : t('confirm_your_phone_number')}
+        </span>
 
-        <div className="flex flex-col space-y-1">
-          <span className="text-gray-900 text-xl">
-            {props.type === 'email'
-              ? t('confirm_your_email_address')
-              : t('confirm_your_phone_number')}
-          </span>
+        <span className="text-base">
+          {props.type === 'email' ? (
+            <span>
+              {t('confirm_email_help')} <strong>{user?.email}</strong>.
+            </span>
+          ) : (
+            user?.phone
+          )}
+        </span>
 
-          <span className="text-gray-500 text-lg">
-            {props.type === 'email' ? user?.email : user?.phone}
-          </span>
-        </div>
-
-        <div className="flex justify-between items-center w-full">
+        <div className="flex justify-end items-center w-full">
           {props.type === 'email' && (
             <>
-              <Button type="minimal" onClick={handleRefreshData}>
-                {t('refresh_data')}
-              </Button>
-
               <Button
-                type="primary"
-                className="mr-4"
-                onClick={handleResendEmail}
+                type="minimal"
+                className="mr-7"
+                onClick={() => navigate('/logout')}
               >
-                {t('resend_email')}
+                {t('logout')}
               </Button>
 
-              <Button type="minimal" onClick={() => navigate('/logout')}>
-                {t('logout')}
+              <Button type="primary" onClick={handleResendEmail}>
+                {t('resend_email')}
               </Button>
             </>
           )}
