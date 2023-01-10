7c759384711d5f5f69fd9f41946cef236b8d0818
Abedin <abedinhalilovic12345@gmail.com>
2023-01-10 15:58:54 +0100

    Deleted unnecessary condition

--- src/App.tsx
+++ src/App.tsx
@@ -102,7 +102,7 @@ export function App() {
       />
 
       <PhoneVerificationModal
-        visible={Boolean(account) && showSmsVerificationModal && !isHosted()}
+        visible={Boolean(account) && showSmsVerificationModal && isHosted()}
         setVisible={setShowSmsVerificationModal}
       />
 
