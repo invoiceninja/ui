8f463f80c795f22cc776f4d823eba4140c5e41d1
Abedin <abedinhalilovic12345@gmail.com>
2023-01-15 14:43:47 +0100

    Added constraint for calling statics query

--- src/common/queries/statics.ts
+++ src/common/queries/statics.ts
@@ -12,17 +12,17 @@ import { endpoint } from 'common/helpers';
 import { request } from 'common/helpers/request';
 import { Statics } from 'common/interfaces/statics';
 import { useQuery } from 'react-query';
-import { useLocation } from 'react-router-dom';
 
 export function useStaticsQuery() {
-  const location = useLocation();
-
   return useQuery<Statics>(
     '/api/v1/statics',
     () =>
       request('GET', endpoint('/api/v1/statics')).then(
         (response) => response.data
       ),
-    { enabled: !location.pathname.startsWith('/login'), staleTime: Infinity }
+    {
+      enabled: Boolean(localStorage.getItem('X-NINJA-TOKEN')),
+      staleTime: Infinity,
+    }
   );
 }
