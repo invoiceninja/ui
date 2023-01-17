75337cc55cf410e1404a51e666814eccbff9bb96
Benjamin Beganović <k1pstabug@gmail.com>
2023-01-17 17:08:28 +0100

    Scaffold  user interface layout

--- src/pages/settings/designer/Designer.tsx
+++ src/pages/settings/designer/Designer.tsx
@@ -8,6 +8,28 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { Link } from '@invoiceninja/forms';
+import { ArrowLeft } from 'react-feather';
+
 export function Designer() {
-  return <div>Designer</div>;
+  return (
+    <div className="flex">
+      <div className="h-screen w-1/3 bg-gray-100">
+        <div className="inline-flex items-center p-4">
+          <Link className="inline-flex items-center space-x-1" to="/settings">
+            <ArrowLeft size={16} />
+            <span>Back to settings</span>
+          </Link>
+        </div>
+      </div>
+      <div className="h-screen w-full bg-gray-300 flex justify-center overflow-y-auto p-10">
+        <div
+          className="bg-white p-10 rounded"
+          style={{ minWidth: 960, minHeight: 1280 }}
+        >
+          Preview
+        </div>
+      </div>
+    </div>
+  );
 }
