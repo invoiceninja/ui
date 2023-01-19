47219c8857bb5357ad7e588d861fd908d99ca762
Benjamin Beganović <k1pstabug@gmail.com>
2023-01-19 13:57:31 +0100

    Experimental logo control

--- src/pages/settings/designer/Designer.tsx
+++ src/pages/settings/designer/Designer.tsx
@@ -11,10 +11,115 @@
 import { Link } from '@invoiceninja/forms';
 import { useLogo } from 'common/hooks/useLogo';
 import { ColorPicker } from 'components/forms/ColorPicker';
+import { useEffect, useState } from 'react';
 import { ArrowLeft } from 'react-feather';
+
+export const template = `
+  <style>
+    .company-logo {
+      max-width: 65%;
+    }
+  </style>
+  
+  <img src="https://forum.invoiceninja.com/uploads/default/original/2X/6/672414e38c31024298b506d24ffc4d5e3c3b488e.png" class="company-logo" />
+  <p>Primary text</p>
+`;
+
+interface Context {
+  logo: {
+    size: {
+      value: string | null;
+      selector: string;
+      property: string;
+    };
+  };
+}
+
+class Builder {
+  #document!: Document;
+  #css = new CSSStyleSheet();
+
+  constructor(html: string) {
+    this.#document = new DOMParser().parseFromString(html, 'text/html');
+
+    const style = this.#document.querySelector('style');
+
+    if (style) {
+      this.#css.replaceSync(style.innerHTML);
+    }
+  }
+
+  #querySelector(selector: string) {
+    const numbers = [...Array(this.#css.cssRules.length).keys()];
+    let cssRule: CSSStyleRule | undefined;
+
+    numbers.every((i) => {
+      const rule = this.#css.cssRules.item(i) as CSSStyleRule | null;
+
+      if (rule && rule.selectorText === selector) {
+        cssRule = this.#css.cssRules.item(i) as CSSStyleRule;
+
+        return false;
+      }
+    });
+
+    return cssRule ?? null;
+  }
+
+  #html(): string {
+    console.log('Compiling..');
+
+    const cssTexts: string[] = [];
+    const numbers = [...Array(this.#css.cssRules.length).keys()];
+
+    numbers.forEach((number) => {
+      const rule = this.#css.cssRules.item(number);
+
+      rule && cssTexts.push(rule.cssText);
+    });
+
+    this.#document.querySelector('style')!.innerHTML = cssTexts.join('\n');
+
+    return this.#document.documentElement.innerHTML;
+  }
+
+  getSelectorValue(selector: string, property: string) {
+    return (
+      this.#querySelector(selector)?.style.getPropertyValue(property) || null
+    );
+  }
+
+  setSelectorValue(selector: string, property: string, value: string | null) {
+    this.#querySelector(selector)?.style.setProperty(property, value);
+
+    return new Promise((resolve) => {
+      resolve(this.#html());
+    });
+  }
+}
 
 export function Designer() {
   const logo = useLogo();
+  const [html, setHtml] = useState(template);
+  const [context, setContext] = useState<Context>();
+
+  const builder = new Builder(html);
+
+  useEffect(() => {
+    // Initial load, fill the context with data.
+
+    setContext({
+      logo: {
+        size: {
+          value: builder.getSelectorValue('.company-logo', 'max-width'),
+          selector: '.company-logo',
+          property: 'max-width',
+        },
+      },
+    });
+  }, []);
+
+  console.log(context);
 
   return (
     <div className="flex">
@@ -44,8 +149,23 @@ export function Designer() {
                 htmlFor="slider"
                 className="inline-flex items-center space-x-2"
               >
-                <input type="range" min="1" max="100" value="29" id="slider" />
-                <span className="text-sm">29%</span>
+                <input
+                  type="range"
+                  min="1"
+                  max="100"
+                  // value={context?.logo.size.value || 0}
+                  id="slider"
+                  onChange={(event) =>
+                    builder
+                      .setSelectorValue(
+                        '.company-logo',
+                        'max-width',
+                        event.target.value + '%'
+                      )
+                      .then((html) => setHtml(html as string))
+                  }
+                />
+                {/* <span className="text-sm">{context?.logo.size.value || 0}</span> */}
               </label>
             </div>
           </div>
@@ -75,7 +195,7 @@ export function Designer() {
           className="bg-white p-10 rounded"
           style={{ minWidth: 960, minHeight: 1280 }}
         >
-          Preview
+          {html && <iframe srcDoc={html} width={960} height={1280}></iframe>}
         </div>
       </div>
     </div>
