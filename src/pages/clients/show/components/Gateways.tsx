e55bfa2f8f6be544218bc9d6449784a24e198221
Benjamin Beganović <k1pstabug@gail.com>
2023-02-01 14:46:26 +0100

    Improve UX for the payment methods card

--- src/pages/clients/show/components/Gateways.tsx
+++ src/pages/clients/show/components/Gateways.tsx
@@ -11,15 +11,13 @@
 import { Client } from 'common/interfaces/client';
 import { InfoCard } from 'components/InfoCard';
 import { useTranslation } from 'react-i18next';
-import { MdPayment } from 'react-icons/md';
-import { IoIosArrowForward } from 'react-icons/io';
-import { VscLinkExternal } from 'react-icons/vsc';
-import { useNavigate } from 'react-router-dom';
+import { MdChevronRight, MdLaunch, MdPayment } from 'react-icons/md';
 import { route } from 'common/helpers/route';
 import { GatewayTypeIcon } from './GatewayTypeIcon';
 import { useCompanyGatewaysQuery } from 'common/queries/company-gateways';
 import { useEffect, useState } from 'react';
 import { CompanyGateway } from 'common/interfaces/company-gateway';
+import { Link } from '@invoiceninja/forms';
 
 interface Props {
   client: Client;
@@ -28,13 +26,10 @@ interface Props {
 export function Gateways(props: Props) {
   const [t] = useTranslation();
 
-  const navigate = useNavigate();
-
   const { data: companyGatewaysResponse } = useCompanyGatewaysQuery();
+  const { client } = props;
 
   const [companyGateways, setCompanyGateways] = useState<CompanyGateway[]>();
-
-  const { client } = props;
 
   const getCompanyGatewayLabel = (gatewayId: string) => {
     const filteredGateways = companyGateways?.filter(
@@ -51,61 +46,53 @@ export function Gateways(props: Props) {
   }, [companyGatewaysResponse]);
 
   return (
-    <div className="col-span-12 lg:col-span-5 xl:col-span-4">
-      {companyGateways && (
-        <InfoCard
-          title={t('gateway_tokens')}
-          value={
-            <div className="mt-3 space-y-7">
-              {client.gateway_tokens.map((gateway) => (
-                <div
-                  key={gateway.token}
-                  className="flex flex-1 justify-between xl:justify-start items-center cursor-pointer px-2 hover:bg-gray-50"
-                  onClick={() =>
-                    navigate(
-                      route('/settings/gateways/:id/edit', {
-                        id: gateway.company_gateway_id,
-                      })
-                    )
-                  }
-                >
+    <div className="col-span-12 lg:col-span-4">
+      <InfoCard title={t('payment_methods')}>
+        {client.gateway_tokens.map((token) => (
+          <div
+            key={token.id}
+            className="flex items-center justify-between my-2.5"
+          >
+            <div>
+              <div className="inline-flex items-center space-x-1">
+                <div>
                   <MdPayment fontSize={22} />
-
-                  <div className="flex flex-1 justify-between flex-col ml-5">
-                    <div className="flex items-center">
-                      <span className="font-semibold text-base">
-                        {t('gateway')}
-                      </span>
-
-                      <IoIosArrowForward className="ml-1 font-semibold text-base" />
-
-                      <span className="font-semibold text-base ml-1">
-                        {getCompanyGatewayLabel(gateway.company_gateway_id)}
-                      </span>
-                    </div>
-
-                    <div className="flex items-center mt-1">
-                      <GatewayTypeIcon name={gateway.meta.brand} />
-
-                      <div className="flex items-center ml-3">
-                        <span className="mt-1">****</span>
-                        <span className="ml-1">{gateway.meta.last4}</span>
-                      </div>
-
-                      <span className="ml-2">
-                        {gateway.meta.exp_month}/{gateway.meta.exp_year}
-                      </span>
-                    </div>
-                  </div>
-
-                  <VscLinkExternal fontSize={17} />
                 </div>
-              ))}
+                <div className="inline-flex items-center">
+                  <span> {t('gateway')}</span>
+                  <MdChevronRight size={20} />
+                  <span>
+                    {getCompanyGatewayLabel(token.company_gateway_id)}
+                  </span>
+                </div>
+              </div>
+
+              <div className="flex items-center space-x-2">
+                <GatewayTypeIcon name={token.meta.brand} />
+
+                <div className="flex items-center">
+                  <span className="mt-1">****</span>
+                  <span className="ml-1">{token.meta.last4}</span>
+                </div>
+
+                <span>
+                  {token.meta.exp_month}/{token.meta.exp_year}
+                </span>
+              </div>
             </div>
-          }
-          className="h-full"
-        />
-      )}
+
+            <div>
+              <Link
+                to={route('/settings/gateways/:id/edit', {
+                  id: token.company_gateway_id,
+                })}
+              >
+                <MdLaunch size={18} />
+              </Link>
+            </div>
+          </div>
+        ))}
+      </InfoCard>
     </div>
   );
 }
