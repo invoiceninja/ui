803bd2576e27d5c219817b06aa9c8eaa4f06b491
Abedin <abedinhalilovic12345@gmail.com>
2023-02-03 07:55:37 +0100

    Changed translation keyword for statement page dropdown

--- src/pages/clients/statement/Statement.tsx
+++ src/pages/clients/statement/Statement.tsx
@@ -235,7 +235,7 @@ export function Statement() {
               }
               icon={<Icon element={MdSend} />}
             >
-              {t('email_statement')}
+              {t('email')}
             </DropdownElement>
           )}
           <DropdownElement
