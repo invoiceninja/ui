00868dfde7871fab26807eca2b57b26e3b3fc41c
Benjamin Beganović <k1pstabug@gmail.com>
2023-02-10 16:17:06 +0100

    Refactor enabled to hook style

--- src/common/guards/guards/enabled.ts
+++ src/common/guards/guards/enabled.ts
@@ -8,10 +8,12 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { useCurrentCompany } from 'common/hooks/useCurrentCompany';
 import { ModuleBitmask } from 'pages/settings/account-management/component';
-import { GuardContext } from '../Guard';
 
-export function enabled(module: ModuleBitmask) {
-  return ({ company }: GuardContext) =>
+export function useEnabled() {
+  const company = useCurrentCompany();
+
+  return (module: ModuleBitmask) =>
     Boolean(company && company.enabled_modules & module);
 }
