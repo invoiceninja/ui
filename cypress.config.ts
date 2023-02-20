235e292acc38245d807cdb354c6ddbadfc6d8212
Benjamin Beganović <k1pstabug@gmail.com>
2023-02-20 16:27:44 +0100

    Configure e2e for Cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   component: {
     devServer: {
-      framework: "react",
-      bundler: "vite",
+      framework: 'react',
+      bundler: 'vite',
     },
   },
-
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents() {},
+    baseUrl: 'http://localhost:4173',
   },
+  viewportHeight: 900,
+  viewportWidth: 1400,
 });
