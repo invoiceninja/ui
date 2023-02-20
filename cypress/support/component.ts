46de508a62017a3ba78d8170efb42acda53f9c94
Benjamin Beganović <k1pstabug@gmail.com>
2023-02-20 16:39:32 +0100

    Fixes for global Cypress interface

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -1,39 +1,29 @@
-// ***********************************************************
-// This example support/component.ts is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
+import { mount } from 'cypress/react18';
 
-// Import commands.js using ES2015 syntax:
-import './commands'
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
-
-import { mount } from 'cypress/react18'
-
-// Augment the Cypress namespace to include type definitions for
-// your custom command.
-// Alternatively, can be defined in cypress/support/component.d.ts
-// with a <reference path="./component" /> at the top of your spec.
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Cypress {
     interface Chainable {
-      mount: typeof mount
+      mount: typeof mount;
+      login(username?: string, password?: string): Chainable;
+      logout(): Chainable;
     }
   }
 }
 
-Cypress.Commands.add('mount', mount)
+Cypress.Commands.add('mount', mount);
 
-// Example use:
-// cy.mount(<MyComponent />)
+Cypress.Commands.add('login', (email?: string, password?: string) => {
+  cy.clearAllCookies();
+  cy.clearAllLocalStorage();
+
+  cy.visit('/login').contains('Login');
+  cy.get('#email').type(email ?? 'small@example.com');
+  cy.get('#password').type(password ?? 'password');
+  cy.get('[type=submit]').click();
+});
+
+Cypress.Commands.add('logout', () => {
+  cy.clearAllCookies();
+  cy.clearAllLocalStorage();
+});
