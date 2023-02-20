52c1146a77f09d2e40141a13fab753334f4e74f2
Benjamin Beganović <k1pstabug@gmail.com>
2023-02-20 16:28:01 +0100

    cy.login and cy.logout

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,37 +1,24 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+declare namespace Cypress {
+  interface Chainable {
+    login(username?: string, password?: string): Chainable<any>;
+    logout(): Chainable<any>;
+  }
+}
+
+Cypress.Commands.add('login', (email?: string, password?: string) => {
+  cy.clearAllCookies();
+  cy.clearAllLocalStorage();
+
+  cy.visit('/login').contains('Login');
+  cy.get('#email').type(email ?? 'small@example.com');
+  cy.get('#password').type(password ?? 'password');
+  cy.get('[type=submit]').click();
+});
+
+Cypress.Commands.add('logout', () => {
+  cy.clearAllCookies();
+  cy.clearAllLocalStorage();
+});
