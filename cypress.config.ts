ede89c94b8e80d812c5cb0ea8f75d622a607ee8e
Benjamin Beganović <k1pstabug@gmail.com>
2023-02-23 16:33:50 +0100

    Reconfigure Cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,9 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-  component: {
-    devServer: {
-      framework: 'react',
-      bundler: 'vite',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
   },
-  e2e: {
-    setupNodeEvents() {},
-    baseUrl: 'http://localhost:4173',
-  },
-  viewportHeight: 900,
-  viewportWidth: 1400,
 });
