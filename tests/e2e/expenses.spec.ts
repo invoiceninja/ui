2de5e37424b7a66a65d48347234711c5b2d669c6
Civolilah <abedinhalilovic12345@gmail.com>
2023-03-08 14:29:43 +0100

    Created basic expense e2e tests

---
+++ tests/e2e/expenses.spec.ts
@@ -0,0 +1,107 @@
+import { login, logout, permissions } from '$tests/e2e/helpers';
+import test, { expect } from '@playwright/test';
+
+test.skip("can't view expenses without permission", async ({ page }) => {
+  const { clear, save } = permissions(page);
+
+  await login(page);
+  await clear();
+  await save();
+  await logout(page);
+
+  await login(page, 'permissions@example.com', 'password');
+
+  await expect(page.locator('.flex-grow > .flex-1').first()).not.toContainText(
+    'Expenses'
+  );
+});
+
+test.skip('can view expenses', async ({ page }) => {
+  const { clear, save, set } = permissions(page);
+
+  await login(page);
+  await clear();
+  await set('view_expense');
+  await save();
+  await logout(page);
+
+  await login(page, 'permissions@example.com', 'password');
+
+  await page.getByRole('link', { name: 'Expenses', exact: true }).click();
+
+  await page.waitForURL('**/expenses');
+
+  await expect(
+    page.getByRole('heading', {
+      name: "Sorry, you don't have the needed permissions.",
+    })
+  ).not.toBeVisible();
+});
+
+test.skip("can't create an expense", async ({ page }) => {
+  const { clear, save, set } = permissions(page);
+
+  await login(page);
+  await clear();
+  await set('view_expense');
+  await save();
+  await logout(page);
+
+  await login(page, 'permissions@example.com', 'password');
+
+  await page.getByRole('link', { name: 'Expenses', exact: true }).click();
+  await page.getByText('Enter Expense').click();
+
+  await expect(
+    page.getByRole('heading', {
+      name: "Sorry, you don't have the needed permissions.",
+    })
+  ).toBeVisible();
+});
+
+test.skip('can create an expense', async ({ page }) => {
+  const { clear, save, set } = permissions(page);
+
+  await login(page);
+  await clear();
+  await set('create_expense');
+  await save();
+  await logout(page);
+
+  await login(page, 'permissions@example.com', 'password');
+
+  await page.getByRole('link', { name: 'Expenses', exact: true }).click();
+  await page.getByText('Enter Expense').click();
+
+  await expect(
+    page.getByRole('heading', {
+      name: "Sorry, you don't have the needed permissions.",
+    })
+  ).not.toBeVisible();
+});
+
+test.skip('can view assigned expense with create_expense', async ({ page }) => {
+  //Reason for skipping -> Missing vendor
+
+  const { clear, save, set } = permissions(page);
+
+  await login(page);
+  await clear();
+  await set('create_expense');
+  await save();
+  await logout(page);
+
+  await login(page, 'permissions@example.com', 'password');
+
+  await page.getByRole('link', { name: 'Expenses' }).click();
+  await page
+    .getByRole('main')
+    .getByRole('link', { name: 'Enter Expense' })
+    .click();
+  await page.getByRole('option', { name: 'test vendor' }).click();
+  await page.getByRole('button', { name: 'Save' }).click();
+
+  await expect(
+    page.getByRole('heading', { name: 'Edit Expense' })
+  ).toBeVisible();
+});
