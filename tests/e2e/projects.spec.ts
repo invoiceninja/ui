1532dafe56231a995e9ba1f94899cc5fc69aae32
Civolilah <abedinhalilovic12345@gmail.com>
2023-03-08 15:49:34 +0100

    Created basic project e2e tests

---
+++ tests/e2e/projects.spec.ts
@@ -0,0 +1,110 @@
+import { login, logout, permissions } from '$tests/e2e/helpers';
+import test, { expect } from '@playwright/test';
+
+test.skip("can't view projects without permission", async ({ page }) => {
+  const { clear, save } = permissions(page);
+
+  await login(page);
+  await clear();
+  await save();
+  await logout(page);
+
+  await login(page, 'permissions@example.com', 'password');
+
+  await expect(page.locator('.flex-grow > .flex-1').first()).not.toContainText(
+    'Projects'
+  );
+});
+
+test.skip('can view project', async ({ page }) => {
+  const { clear, save, set } = permissions(page);
+
+  await login(page);
+  await clear();
+  await set('view_project');
+  await save();
+  await logout(page);
+
+  await login(page, 'permissions@example.com', 'password');
+
+  await page.getByRole('link', { name: 'Projects', exact: true }).click();
+
+  await page.waitForURL('**/projects');
+
+  await expect(
+    page.getByRole('heading', {
+      name: "Sorry, you don't have the needed permissions.",
+    })
+  ).not.toBeVisible();
+});
+
+test.skip("can't create a project", async ({ page }) => {
+  const { clear, save, set } = permissions(page);
+
+  await login(page);
+  await clear();
+  await set('view_project');
+  await save();
+  await logout(page);
+
+  await login(page, 'permissions@example.com', 'password');
+
+  await page.getByRole('link', { name: 'Projects', exact: true }).click();
+  await page.getByText('New Project').click();
+
+  await expect(
+    page.getByRole('heading', {
+      name: "Sorry, you don't have the needed permissions.",
+    })
+  ).toBeVisible();
+});
+
+test.skip('can create a project', async ({ page }) => {
+  const { clear, save, set } = permissions(page);
+
+  await login(page);
+  await clear();
+  await set('create_project');
+  await save();
+  await logout(page);
+
+  await login(page, 'permissions@example.com', 'password');
+
+  await page.getByRole('link', { name: 'Projects', exact: true }).click();
+  await page.getByText('New Project').click();
+
+  await expect(
+    page.getByRole('heading', {
+      name: "Sorry, you don't have the needed permissions.",
+    })
+  ).not.toBeVisible();
+});
+
+test.skip('can view assigned project with create_project', async ({ page }) => {
+  const { clear, save, set } = permissions(page);
+
+  await login(page);
+  await clear();
+  await set('create_project');
+  await save();
+  await logout(page);
+
+  await login(page, 'permissions@example.com', 'password');
+
+  await page.getByRole('link', { name: 'Projects' }).click();
+  await page
+    .getByRole('main')
+    .getByRole('link', { name: 'New Project' })
+    .click();
+
+  await page.locator('[data-cy="name"]').fill('Project Name');
+
+  await page.locator('#headlessui-combobox-input-\\:rg\\:').click();
+  await page.getByRole('option', { name: 'cypress' }).click();
+
+  await page.getByRole('button', { name: 'Save' }).click();
+
+  await expect(
+    page.getByRole('heading', { name: 'Edit Project' }).first()
+  ).toBeVisible();
+});
