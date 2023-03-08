ff498590506f03cd77dc2f7ad6a308a5d8a4aef4
Civolilah <abedinhalilovic12345@gmail.com>
2023-03-08 15:56:56 +0100

    Created basic purchase order e2e tests

---
+++ tests/e2e/purchase-orders.spec.ts
@@ -0,0 +1,115 @@
+import { login, logout, permissions } from '$tests/e2e/helpers';
+import test, { expect } from '@playwright/test';
+
+test.skip("can't view purchase orders without permission", async ({ page }) => {
+  const { clear, save } = permissions(page);
+
+  await login(page);
+  await clear();
+  await save();
+  await logout(page);
+
+  await login(page, 'permissions@example.com', 'password');
+
+  await expect(page.locator('.flex-grow > .flex-1').first()).not.toContainText(
+    'Purchase Orders'
+  );
+});
+
+test.skip('can view purchase orders', async ({ page }) => {
+  const { clear, save, set } = permissions(page);
+
+  await login(page);
+  await clear();
+  await set('view_purchase_order');
+  await save();
+  await logout(page);
+
+  await login(page, 'permissions@example.com', 'password');
+
+  await page
+    .getByRole('link', { name: 'Purchase Orders', exact: true })
+    .click();
+
+  await page.waitForURL('**/purchase_orders');
+
+  await expect(
+    page.getByRole('heading', {
+      name: "Sorry, you don't have the needed permissions.",
+    })
+  ).not.toBeVisible();
+});
+
+test.skip("can't create a purchase orders", async ({ page }) => {
+  const { clear, save, set } = permissions(page);
+
+  await login(page);
+  await clear();
+  await set('view_purchase_order');
+  await save();
+  await logout(page);
+
+  await login(page, 'permissions@example.com', 'password');
+
+  await page
+    .getByRole('link', { name: 'Purchase Orders', exact: true })
+    .click();
+  await page.getByText('New Purchase Order').click();
+
+  await expect(
+    page.getByRole('heading', {
+      name: "Sorry, you don't have the needed permissions.",
+    })
+  ).toBeVisible();
+});
+
+test.skip('can create a purchase order', async ({ page }) => {
+  const { clear, save, set } = permissions(page);
+
+  await login(page);
+  await clear();
+  await set('create_purchase_order');
+  await save();
+  await logout(page);
+
+  await login(page, 'permissions@example.com', 'password');
+
+  await page
+    .getByRole('link', { name: 'Purchase Orders', exact: true })
+    .click();
+  await page.getByText('New Purchase Orders').click();
+
+  await expect(
+    page.getByRole('heading', {
+      name: "Sorry, you don't have the needed permissions.",
+    })
+  ).not.toBeVisible();
+});
+
+test.skip('can view assigned purchase order with create_purchase_order', async ({
+  page,
+}) => {
+  //Reason for skipping -> Missing vendor
+
+  const { clear, save, set } = permissions(page);
+
+  await login(page);
+  await clear();
+  await set('create_purchase_order');
+  await save();
+  await logout(page);
+
+  await login(page, 'permissions@example.com', 'password');
+
+  await page.getByRole('link', { name: 'Purchase Orders' }).click();
+  await page
+    .getByRole('main')
+    .getByRole('link', { name: 'New Purchase Orders' })
+    .click();
+  await page.getByRole('option', { name: 'test vendor' }).click();
+  await page.getByRole('button', { name: 'Save' }).click();
+
+  await expect(
+    page.getByRole('heading', { name: 'Edit Purchase Order' })
+  ).toBeVisible();
+});
