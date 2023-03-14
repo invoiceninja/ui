3c72d24ce4387d5e4228e130a9bb65670a95c738
Civolilah <abedinhalilovic12345@gmail.com>
2023-03-14 21:43:49 +0100

    Created additional e2e tests for client entity

--- tests/e2e/clients.spec.ts
+++ tests/e2e/clients.spec.ts
@@ -1,5 +1,19 @@
 import { login, logout, permissions } from '$tests/e2e/helpers';
-import test, { expect } from '@playwright/test';
+import test, { expect, Page } from '@playwright/test';
+
+const createClient = async (page: Page) => {
+  await page
+    .getByRole('main')
+    .getByRole('link', { name: 'New Client' })
+    .click();
+
+  await page.locator('#name').fill('Company Name');
+  await page.locator('#first_name_0').fill('First Name');
+  await page.locator('#last_name_0').fill('Last Name');
+  await page.locator('#email_0').fill('first@example.com');
+
+  await page.getByRole('button', { name: 'Save' }).click();
+};
 
 test("can't view clients without permission", async ({ page }) => {
   const { clear, save } = permissions(page);
@@ -16,7 +30,7 @@ test("can't view clients without permission", async ({ page }) => {
   );
 });
 
-test('can view clients', async ({ page }) => {
+test('can view client', async ({ page }) => {
   const { clear, save, set } = permissions(page);
 
   await login(page);
@@ -31,11 +45,49 @@ test('can view clients', async ({ page }) => {
 
   await page.waitForURL('**/clients');
 
-  await expect(
-    page.getByRole('heading', {
-      name: "Sorry, you don't have the needed permissions.",
-    })
-  ).not.toBeVisible();
+  const tableBody = page.locator('tbody').first();
+
+  const tableRow = tableBody.getByRole('row').first();
+
+  await page.waitForTimeout(200);
+
+  const doRecordsExist = await page.getByText('No records found').isHidden();
+
+  if (doRecordsExist) {
+    const moreActionsButton = tableRow
+      .getByRole('button')
+      .filter({ has: page.getByText('More Actions') });
+
+    await moreActionsButton.click();
+
+    await page.waitForTimeout(200);
+
+    await page
+      .getByRole('link')
+      .filter({ has: page.getByText('Edit') })
+      .first()
+      .click();
+
+    await expect(
+      page.getByRole('heading', {
+        name: 'Additional Info',
+      })
+    ).toBeVisible();
+
+    await expect(
+      page.getByRole('heading', {
+        name: 'Details',
+      })
+    ).toBeVisible();
+  } else {
+    await expect(
+      page.getByRole('heading', {
+        name: "Sorry, you don't have the needed permissions.",
+      })
+    ).not.toBeVisible();
+
+    await expect(page.getByText('No records found')).toBeVisible();
+  }
 });
 
 test("can't create a client", async ({ page }) => {
@@ -71,13 +123,32 @@ test('can create a client', async ({ page }) => {
   await login(page, 'permissions@example.com', 'password');
 
   await page.getByRole('link', { name: 'Clients', exact: true }).click();
-  await page.getByText('New Client').click();
+
+  await createClient(page);
 
   await expect(
-    page.getByRole('heading', {
-      name: "Sorry, you don't have the needed permissions.",
+    page.getByText('Details', {
+      exact: true,
     })
-  ).not.toBeVisible();
+  ).toBeVisible();
+
+  await expect(
+    page.getByText('Address', {
+      exact: true,
+    })
+  ).toBeVisible();
+
+  await expect(
+    page.getByText('Contacts', {
+      exact: true,
+    })
+  ).toBeVisible();
+
+  await expect(
+    page.getByText('Standing', {
+      exact: true,
+    })
+  ).toBeVisible();
 });
 
 test('can view assigned client with create_client', async ({ page }) => {
@@ -92,17 +163,231 @@ test('can view assigned client with create_client', async ({ page }) => {
   await login(page, 'permissions@example.com', 'password');
 
   await page.getByRole('link', { name: 'Clients' }).click();
-  await page
-    .getByRole('main')
-    .getByRole('link', { name: 'New Client' })
-    .click();
 
-  await page.locator('#name').fill('Company Name');
-  await page.locator('#first_name_0').fill('First Name');
-  await page.locator('#last_name_0').fill('Last Name');
-  await page.locator('#email_0').fill('first@example.com');
-
-  await page.getByRole('button', { name: 'Save' }).click();
+  await createClient(page);
 
   await expect(page.getByText('Successfully created client')).toBeVisible();
+});
+
+test('deleting client', async ({ page }) => {
+  const { clear, save, set } = permissions(page);
+
+  await login(page);
+  await clear();
+  await set('create_client');
+  await save();
+  await logout(page);
+
+  await login(page, 'permissions@example.com', 'password');
+
+  const tableBody = page.locator('tbody').first();
+
+  await page.getByRole('link', { name: 'Clients', exact: true }).click();
+
+  const tableRow = tableBody.getByRole('row').first();
+
+  await page.waitForURL('**/clients');
+
+  await page.waitForTimeout(200);
+
+  const doRecordsExist = await page.getByText('No records found').isHidden();
+
+  if (!doRecordsExist) {
+    await createClient(page);
+
+    const moreActionsButton = page
+      .getByRole('button')
+      .filter({ has: page.getByText('More Actions') })
+      .first();
+
+    await moreActionsButton.click();
+
+    await page.getByText('Delete').click();
+
+    await expect(page.getByText('Restore')).toBeVisible();
+
+    await expect(page.getByText('Purge')).toBeVisible();
+  } else {
+    const moreActionsButton = tableRow
+      .getByRole('button')
+      .filter({ has: page.getByText('More Actions') });
+
+    await moreActionsButton.click();
+
+    await page.getByText('Delete').click();
+  }
+
+  await expect(page.getByText('Successfully deleted client')).toBeVisible();
+});
+
+test('archiving client', async ({ page }) => {
+  const { clear, save, set } = permissions(page);
+
+  await login(page);
+  await clear();
+  await set('create_client');
+  await save();
+  await logout(page);
+
+  await login(page, 'permissions@example.com', 'password');
+
+  const tableBody = page.locator('tbody').first();
+
+  await page.getByRole('link', { name: 'Clients', exact: true }).click();
+
+  await page.waitForURL('**/clients');
+
+  const tableRow = tableBody.getByRole('row').first();
+
+  await page.waitForTimeout(200);
+
+  const doRecordsExist = await page.getByText('No records found').isHidden();
+
+  if (!doRecordsExist) {
+    await createClient(page);
+
+    const moreActionsButton = page
+      .getByRole('button')
+      .filter({ has: page.getByText('More Actions') })
+      .first();
+
+    await moreActionsButton.click();
+
+    await page.getByText('Archive').click();
+
+    await expect(page.getByText('Restore')).toBeVisible();
+  } else {
+    const moreActionsButton = tableRow
+      .getByRole('button')
+      .filter({ has: page.getByText('More Actions') })
+      .first();
+
+    await moreActionsButton.click();
+
+    await page.getByText('Archive').click();
+  }
+
+  await expect(page.getByText('Successfully archived client')).toBeVisible();
+});
+
+test('client documents preview', async ({ page }) => {
+  const { clear, save, set } = permissions(page);
+
+  await login(page);
+  await clear();
+  await set('create_client');
+  await save();
+  await logout(page);
+
+  await login(page, 'permissions@example.com', 'password');
+
+  const tableBody = page.locator('tbody').first();
+
+  await page.getByRole('link', { name: 'Clients', exact: true }).click();
+
+  await page.waitForURL('**/clients');
+
+  const tableRow = tableBody.getByRole('row').first();
+
+  await page.waitForTimeout(200);
+
+  const doRecordsExist = await page.getByText('No records found').isHidden();
+
+  if (!doRecordsExist) {
+    await createClient(page);
+
+    const moreActionsButton = page
+      .getByRole('button')
+      .filter({ has: page.getByText('More Actions') })
+      .first();
+
+    await moreActionsButton.click();
+  } else {
+    const moreActionsButton = tableRow
+      .getByRole('button')
+      .filter({ has: page.getByText('More Actions') })
+      .first();
+
+    await moreActionsButton.click();
+  }
+
+  await page.getByText('Edit').first().click();
+
+  await page.waitForURL('**/clients/**/edit');
+
+  await page
+    .getByRole('button', {
+      name: 'Documents',
+      exact: true,
+    })
+    .click();
+
+  await expect(
+    page.getByRole('heading', { name: 'Additional Info' })
+  ).toBeVisible();
+
+  await expect(page.getByText('Drop files or click to upload')).toBeVisible();
+});
+
+test('client documents uploading', async ({ page }) => {
+  const { clear, save, set } = permissions(page);
+
+  await login(page);
+  await clear();
+  await set('create_client');
+  await save();
+  await logout(page);
+
+  await login(page, 'permissions@example.com', 'password');
+
+  const tableBody = page.locator('tbody').first();
+
+  await page.getByRole('link', { name: 'Clients', exact: true }).click();
+
+  await page.waitForURL('**/clients');
+
+  const tableRow = tableBody.getByRole('row').first();
+
+  await page.waitForTimeout(200);
+
+  const doRecordsExist = await page.getByText('No records found').isHidden();
+
+  if (!doRecordsExist) {
+    await createClient(page);
+
+    const moreActionsButton = page
+      .getByRole('button')
+      .filter({ has: page.getByText('More Actions') })
+      .first();
+
+    await moreActionsButton.click();
+  } else {
+    const moreActionsButton = tableRow
+      .getByRole('button')
+      .filter({ has: page.getByText('More Actions') })
+      .first();
+
+    await moreActionsButton.click();
+  }
+
+  await page.getByText('Edit').first().click();
+
+  await page.waitForURL('**/clients/**/edit');
+
+  await page
+    .getByRole('button', {
+      name: 'Documents',
+      exact: true,
+    })
+    .click();
+
+  await page
+    .locator('input[type="file"]')
+    .setInputFiles('./tests/assets/images/test-image.png');
+
+  await expect(page.getByText('Successfully uploaded document')).toBeVisible();
+
+  await expect(
+    tableBody.getByText('test-image.png', { exact: true }).first()
+  ).toBeVisible();
 });
