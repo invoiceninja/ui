10a262dcf367e522306d588ce6fbd2aa18087948
Civolilah <abedinhalilovic12345@gmail.com>
2023-03-17 17:35:58 +0100

    Fixed bugs with status badge for expense entity

---
+++ src/common/enums/expense-status.ts
@@ -0,0 +1,17 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+export const enum ExpenseStatus {
+  Logged = '1',
+  Pending = '2',
+  Invoiced = '3',
+  Unpaid = '4',
+  Paid = '5',
+}
