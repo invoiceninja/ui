677233e750ed64f1bb387a5d10508ac1c25c393f
Civolilah <abedinhalilovic12345@gmail.com>
2023-03-22 21:29:45 +0100

    Removed unnecessary messages for validations

--- src/pages/payments/create/Create.tsx
+++ src/pages/payments/create/Create.tsx
@@ -34,7 +34,6 @@ import {
 } from '$app/components/forms/DebouncedCombobox';
 import Toggle from '$app/components/forms/Toggle';
 import { Default } from '$app/components/layouts/Default';
-import { ValidationAlert } from '$app/components/ValidationAlert';
 import { FormEvent, useEffect, useState } from 'react';
 import { X } from 'react-feather';
 import { useTranslation } from 'react-i18next';
@@ -201,8 +200,6 @@ export function Create() {
       disableSaveButton={!payment}
     >
       <Container>
-        {errors && <ValidationAlert errors={errors} />}
-
         <Card title={t('enter_payment')}>
           <Element leftSide={t('client')}>
             <DebouncedCombobox
