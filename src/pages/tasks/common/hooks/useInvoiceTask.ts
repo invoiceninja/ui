62fffb7a41fac2b776790839d5e04603cea57048
Civolilah <abedinhalilovic12345@gmail.com>
2023-03-30 17:37:46 +0200

    Adjusted task invoicing with new task settings

--- src/pages/tasks/common/hooks/useInvoiceTask.ts
+++ src/pages/tasks/common/hooks/useInvoiceTask.ts
@@ -11,7 +11,7 @@
 import { useCurrentCompany } from '$app/common/hooks/useCurrentCompany';
 import { Task } from '$app/common/interfaces/task';
 import { useBlankInvoiceQuery } from '$app/common/queries/invoices';
-import { useNavigate } from 'react-router-dom';
+import { generatePath, useNavigate } from 'react-router-dom';
 import { blankLineItem } from '$app/common/constants/blank-line-item';
 import { parseTimeLog } from '../helpers/calculate-time';
 import { useCurrentCompanyDateFormats } from '$app/common/hooks/useCurrentCompanyDateFormats';
@@ -22,14 +22,23 @@ import {
   InvoiceItemType,
 } from '$app/common/interfaces/invoice-item';
 import collect from 'collect.js';
-import toast from 'react-hot-toast';
 import { useAtom } from 'jotai';
 import { invoiceAtom } from '$app/pages/invoices/common/atoms';
 import { route } from '$app/common/helpers/route';
+import { useTranslation } from 'react-i18next';
+import { request } from '$app/common/helpers/request';
+import { endpoint } from '$app/common/helpers';
+import { AxiosError } from 'axios';
+import { toast } from '$app/common/helpers/toast/toast';
+import { useQueryClient } from 'react-query';
+import { GenericSingleResourceResponse } from '$app/common/interfaces/generic-api-response';
+import { Project } from '$app/common/interfaces/project';
 
 export function useInvoiceTask() {
+  const [t] = useTranslation();
   const navigate = useNavigate();
   const company = useCurrentCompany();
+  const queryClient = useQueryClient();
 
   const { dateFormat } = useCurrentCompanyDateFormats();
   const { data } = useBlankInvoiceQuery();
@@ -55,7 +64,29 @@ export function useInvoiceTask() {
     return hoursSum;
   };
 
-  return (tasks: Task[]) => {
+  const fetchProjectDetails = async (projectId: string) => {
+    let project: Project | undefined;
+
+    await queryClient
+      .fetchQuery(generatePath('/api/v1/projects/:id', { id: projectId }), () =>
+        request(
+          'GET',
+          endpoint('/api/v1/projects/:id', {
+            id: projectId,
+          })
+        )
+      )
+      .then((response: GenericSingleResourceResponse<Project>) => {
+        project = response.data.data;
+      })
+      .catch((error: AxiosError) => {
+        console.error(error);
+      });
+
+    return project;
+  };
+
+  return async (tasks: Task[]) => {
     if (data) {
       const invoice: Invoice = { ...data };
 
@@ -82,19 +113,32 @@ export function useInvoiceTask() {
 
       invoice.client_id = tasks[0].client_id;
 
-      tasks.forEach((task) => {
+      tasks.forEach(async (task) => {
         const logs = parseTimeLog(task.time_log);
         const parsed: string[] = [];
 
+        const taskQuantity = calculateTaskHours(task.time_log);
+
         logs.forEach(([start, stop]) => {
+          const unixStart = dayjs.unix(start);
+          const unixStop = dayjs.unix(stop);
+
+          const hours = company.invoice_task_hours
+            ? (unixStop.diff(unixStart, 'seconds') / 3600).toFixed(4)
+            : null;
+
+          let hoursDescription = '';
+
+          if (hours) {
+            hoursDescription = `• ${hours} ${t('hours')}`;
+          }
+
           parsed.push(
             `${dayjs.unix(start).format(`${dateFormat} hh:mm:ss A`)} - ${dayjs
               .unix(stop)
-              .format('hh:mm:ss A')} <br />`
+              .format('hh:mm:ss A')} ${hoursDescription} <br />`
           );
         });
-
-        const taskQuantity = calculateTaskHours(task.time_log);
 
         const item: InvoiceItem = {
           ...blankLineItem(),
@@ -104,7 +148,18 @@ export function useInvoiceTask() {
           line_total: Number((task.rate * taskQuantity).toFixed(2)),
         };
 
+        let projectDescription = '';
+
+        if (company.invoice_task_project && task.project_id) {
+          const projectResponse = await fetchProjectDetails(task.project_id);
+
+          if (projectResponse) {
+            projectDescription = `## ${projectResponse.name}`;
+          }
+        }
+
         item.notes = [
+          projectDescription,
           task.description,
           '<div class="task-time-details">',
           ...parsed,
@@ -114,18 +169,18 @@ export function useInvoiceTask() {
           .trim();
 
         invoice.line_items = [item];
+
+        setInvoice(invoice);
+
+        navigate(
+          route(
+            '/invoices/create?table=tasks&project=:projectAssigned&action=invoice_task',
+            {
+              projectAssigned: Boolean(tasks[0].project_id),
+            }
+          )
+        );
       });
-
-      setInvoice(invoice);
-
-      navigate(
-        route(
-          '/invoices/create?table=tasks&project=:projectAssigned&action=invoice_task',
-          {
-            projectAssigned: Boolean(tasks[0].project_id),
-          }
-        )
-      );
     }
   };
 }
