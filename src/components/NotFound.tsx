5668147bc7adb1f1815b3e303a1f7ac7bca28c5f
Civolilah <abedinhalilovic12345@gmail.com>
2023-04-09 04:59:18 +0200

    Changed not found page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -8,45 +8,25 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { useLogo } from '$app/common/hooks/useLogo';
-import { Button } from '$app/components/forms';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
+import Logo from '../resources/images/invoiceninja-logo@dark.png';
 
 export function NotFound() {
   const [t] = useTranslation();
-  const navigate = useNavigate();
-  const logo = useLogo();
 
   return (
     <div className="flex flex-col items-center justify-center h-screen space-y-10 py-16 px-5">
       <div className="flex flex-col flex-1 justify-center items-center space-y-7">
-        <h1 className="text-8xl font-bold">404</h1>
-
-        <div className="flex flex-col text-center">
-          <h3 className="text-lg">Oops! Page not found!</h3>
-          <span>
-            <i>
-              We are sorry, it looks like the page you are looking for is not in
-              our system.
-            </i>
-          </span>
-        </div>
-
-        <Button onClick={() => navigate('/dashboard')}>
-          Back To Dashboard
-        </Button>
+        <h1 className="text-2xl md:text-4xl font-bold text-center">
+          {t('api_404')}
+        </h1>
       </div>
 
-      <div className="flex">
-        <div className="bg-gray-900 rounded-lg p-4 self-end">
-          <img
-            src={logo}
-            alt={t('company_logo') ?? 'Company logo'}
-            className="w-64"
-          />
-        </div>
-      </div>
+      <img
+        src={Logo}
+        alt={t('company_logo') ?? 'Company logo'}
+        className="w-52 md:w-64"
+      />
     </div>
   );
 }
