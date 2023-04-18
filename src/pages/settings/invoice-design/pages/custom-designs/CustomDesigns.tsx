62a53aafe98af1f17836563b95a58b8500571a29
Benjamin Beganović <k1pstabug@gmail.com>
2023-04-18 13:23:03 +0200

    Datatable for the designs

--- src/pages/settings/invoice-design/pages/custom-designs/CustomDesigns.tsx
+++ src/pages/settings/invoice-design/pages/custom-designs/CustomDesigns.tsx
@@ -8,6 +8,20 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { DataTable } from '$app/components/DataTable';
+
 export default function CustomDesigns() {
-  return <div>Custom designs</div>;
+  return (
+    <div className="my-4">
+      <DataTable
+        endpoint="/api/v1/designs?custom=true"
+        columns={[{ id: 'name', label: 'Name' }]}
+        resource="design"
+        linkToCreate="/settings/invoice_design/custom_designs/create"
+        bulkRoute="/api/v1/designs/bulk"
+        linkToEdit="/settings/invoice_design/custom_designs/:id/edit"
+        withResourcefulActions
+      />
+    </div>
+  );
 }
