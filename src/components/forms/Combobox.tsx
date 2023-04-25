fd07e982ce7d5d1021a62294a58d511cafb5dff9
Benjamin Beganović <k1pstabug@gmail.com>
2023-04-25 11:25:28 +0200

    Combobox: Manual control of open state

--- src/components/forms/Combobox.tsx
+++ src/components/forms/Combobox.tsx
@@ -13,11 +13,11 @@ import { GenericManyResponse } from '$app/common/interfaces/generic-many-respons
 import { Combobox as HeadlessCombobox } from '@headlessui/react';
 import { AxiosResponse } from 'axios';
 import classNames from 'classnames';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Check, ChevronDown } from 'react-feather';
 import { useTranslation } from 'react-i18next';
 import { useQuery } from 'react-query';
-import { useDebounce } from 'react-use';
+import { useClickAway, useDebounce } from 'react-use';
 
 interface Entry<T = any> {
   id: number | string;
@@ -54,6 +54,7 @@ export function ComboboxStatic({
   const [t] = useTranslation();
   const [selectedValue, setSelectedValue] = useState<Entry | null>(null);
   const [query, setQuery] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
 
   const filteredValues =
     query === ''
@@ -64,6 +65,12 @@ export function ComboboxStatic({
             entry.value.toString().toLowerCase().includes(query.toLowerCase())
           );
         });
+
+  const comboboxRef = useRef<HTMLDivElement>(null);
+
+  useClickAway(comboboxRef, () => {
+    setIsOpen(false);
+  });
 
   useDebounce(
     () => {
@@ -99,18 +106,20 @@ export function ComboboxStatic({
       value={selectedValue}
       onChange={setSelectedValue}
       disabled={readonly}
+      ref={comboboxRef}
     >
       {inputOptions.label && (
         <HeadlessCombobox.Label className="text-sm text-gray-500 font-medium block">
           {inputOptions.label}
         </HeadlessCombobox.Label>
       )}
-      
+
       <div className="relative mt-2">
         <HeadlessCombobox.Input
           className="w-full rounded border-0 bg-white py-2 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           onChange={(event) => setQuery(event.target.value)}
           displayValue={(entry: Nullable<Entry>) => entry?.label ?? ''}
+          onFocus={() => setIsOpen(true)}
         />
 
         {!readonly && (
@@ -119,64 +128,71 @@ export function ComboboxStatic({
           </HeadlessCombobox.Button>
         )}
 
-        <HeadlessCombobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-          {filteredValues.length > 0 &&
-            filteredValues.map((entry) => (
-              <HeadlessCombobox.Option
-                key={entry.id}
-                value={entry}
-                className={({ active }) =>
-                  classNames(
-                    'relative cursor-default select-none py-2 pl-3 pr-9',
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-900'
-                  )
-                }
-              >
-                {({ active, selected }) => (
-                  <>
-                    <span
-                      className={classNames(
-                        'block truncate',
-                        selected && 'font-semibold'
-                      )}
-                    >
-                      {entry.label}
-                    </span>
-
-                    {selected && (
+        {isOpen && (
+          <HeadlessCombobox.Options
+            static
+            className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+          >
+            {filteredValues.length > 0 &&
+              filteredValues.map((entry) => (
+                <HeadlessCombobox.Option
+                  key={entry.id}
+                  value={entry}
+                  className={({ active }) =>
+                    classNames(
+                      'relative cursor-default select-none py-2 pl-3 pr-9',
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-900'
+                    )
+                  }
+                >
+                  {({ active, selected }) => (
+                    <>
                       <span
                         className={classNames(
-                          'absolute inset-y-0 right-0 flex items-center pr-4',
-                          active ? 'text-white' : 'text-gray-600'
+                          'block truncate',
+                          selected && 'font-semibold'
                         )}
                       >
-                        <Check className="h-5 w-5" aria-hidden="true" />
+                        {entry.label}
                       </span>
-                    )}
+
+                      {selected && (
+                        <span
+                          className={classNames(
+                            'absolute inset-y-0 right-0 flex items-center pr-4',
+                            active ? 'text-white' : 'text-gray-600'
+                          )}
+                        >
+                          <Check className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      )}
+                    </>
+                  )}
+                </HeadlessCombobox.Option>
+              ))}
+
+            {filteredValues.length === 0 && (
+              <HeadlessCombobox.Option
+                key="combobox-not-found"
+                className={() =>
+                  classNames(
+                    'relative cursor-default select-none py-2 pl-3 pr-9'
+                  )
+                }
+                value={null}
+                disabled
+              >
+                {() => (
+                  <>
+                    <span className="block truncate">
+                      {t('no_records_found')}.
+                    </span>
                   </>
                 )}
               </HeadlessCombobox.Option>
-            ))}
-
-          {filteredValues.length === 0 && (
-            <HeadlessCombobox.Option
-              key="combobox-not-found"
-              className={() =>
-                classNames('relative cursor-default select-none py-2 pl-3 pr-9')
-              }
-              value={null}
-              disabled
-            >
-              {() => (
-                <>
-                  <span className="block truncate">
-                    {t('no_records_found')}.
-                  </span>
-                </>
-              )}
-            </HeadlessCombobox.Option>
-          )}
-        </HeadlessCombobox.Options>
+            )}
+          </HeadlessCombobox.Options>
+        )}
       </div>
     </HeadlessCombobox>
   );
