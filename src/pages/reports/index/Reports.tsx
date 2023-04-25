51cb3d32ce13485c0e80351b96b9c88a362333ac
Benjamin Beganović <k1pstabug@gmail.com>
2023-04-25 08:01:14 +0200

    Remove unnecessary

--- src/pages/reports/index/Reports.tsx
+++ src/pages/reports/index/Reports.tsx
@@ -426,8 +426,7 @@ export default function Reports() {
       ...styles,
       color: data.color,
     }),
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    multiValueRemove: (styles, { data }) => ({
+    multiValueRemove: (styles) => ({
       ...styles,
       ':hover': {
         color: 'white',
