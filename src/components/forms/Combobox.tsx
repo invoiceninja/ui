384fc1cce6a58248c352fa642e2c17f933702273
Benjamin Beganović <k1pstabug@gmail.com>
2023-04-27 16:23:18 +0200

    Fixes for overflowing

--- src/components/forms/Combobox.tsx
+++ src/components/forms/Combobox.tsx
@@ -132,59 +132,63 @@ export function ComboboxStatic({
   }, [initiallyVisible]);
 
   return (
-    <HeadlessCombobox
-      as="div"
-      value={selectedValue}
-      onChange={(entry) => {
-        entry?.value === comboboxActionId
-          ? action?.onClick()
-          : setSelectedValue(entry);
-      }}
-      disabled={readonly}
-      ref={comboboxRef}
-    >
-      {inputOptions.label && (
-        <HeadlessCombobox.Label className="text-sm text-gray-500 font-medium block">
-          {inputOptions.label}
-        </HeadlessCombobox.Label>
-      )}
-
-      <div className="relative mt-2">
-        <HeadlessCombobox.Input
-          className="w-full rounded border-0 bg-white py-2 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          onChange={(event) => setQuery(event.target.value)}
-          displayValue={(entry: Nullable<Entry>) => entry?.label ?? ''}
-          onFocus={() => setIsOpen(true)}
-        />
-
-        {!readonly && (
-          <HeadlessCombobox.Button
-            onClick={(e) => {
-              if (onDismiss) {
-                e.preventDefault();
-
-                setIsOpen(false);
-
-                return onDismiss();
-              }
-            }}
-            className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none"
-          >
-            {onDismiss && selectedValue ? (
-              <X className="h-5 w-5 text-gray-400" aria-hidden="true" />
-            ) : (
-              <ChevronDown
-                className="h-5 w-5 text-gray-400"
-                aria-hidden="true"
-              />
-            )}
-          </HeadlessCombobox.Button>
+    <div className="w-full">
+      <HeadlessCombobox
+        as="div"
+        value={selectedValue}
+        onChange={(entry) => {
+          entry?.value === comboboxActionId
+            ? action?.onClick()
+            : setSelectedValue(entry);
+        }}
+        disabled={readonly}
+        ref={comboboxRef}
+      >
+        {inputOptions.label && (
+          <HeadlessCombobox.Label className="text-sm text-gray-500 font-medium block">
+            {inputOptions.label}
+          </HeadlessCombobox.Label>
         )}
+
+        <div className="relative mt-1">
+          <div className="relative w-full cursor-default overflow-hidden rounded border border-gray-300 bg-white text-left focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 sm:text-sm">
+            <HeadlessCombobox.Input
+              className="w-full rounded border-0 bg-white py-2 pl-3 pr-10 text-gray-900 shadow-sm ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              onChange={(event) => setQuery(event.target.value)}
+              displayValue={(entry: Nullable<Entry>) => entry?.label ?? ''}
+              onFocus={() => setIsOpen(true)}
+            />
+
+            {!readonly && (
+              <HeadlessCombobox.Button
+                onClick={(e) => {
+                  if (onDismiss) {
+                    e.preventDefault();
+
+                    setIsOpen(false);
+
+                    return onDismiss();
+                  }
+                }}
+                className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none"
+              >
+                {onDismiss && selectedValue ? (
+                  <X className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                ) : (
+                  <ChevronDown
+                    className="h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                )}
+              </HeadlessCombobox.Button>
+            )}
+          </div>
+        </div>
 
         {isOpen && (
           <HeadlessCombobox.Options
             static
-            className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+            className="absolute z-10 mt-1 max-h-60 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
           >
             {action && action.visible && (
               <HeadlessCombobox.Option
@@ -221,7 +225,7 @@ export function ComboboxStatic({
                   value={entry}
                   className={({ active }) =>
                     classNames(
-                      'relative cursor-default select-none py-2 pl-3 pr-9',
+                      'min-w-[19rem] relative cursor-default select-none py-2 pl-3 pr-9',
                       active ? 'bg-gray-100 text-gray-900' : 'text-gray-900'
                     )
                   }
@@ -250,11 +254,7 @@ export function ComboboxStatic({
             {filteredValues.length === 0 && (
               <HeadlessCombobox.Option
                 key="combobox-not-found"
-                className={() =>
-                  classNames(
-                    'relative cursor-default select-none py-2 pl-3 pr-9'
-                  )
-                }
+                className="min-w-[19rem] relative cursor-default select-none py-2 pl-3 pr-9"
                 value={null}
                 disabled
               >
@@ -269,8 +269,8 @@ export function ComboboxStatic({
             )}
           </HeadlessCombobox.Options>
         )}
-      </div>
-    </HeadlessCombobox>
+      </HeadlessCombobox>
+    </div>
   );
 }
 
