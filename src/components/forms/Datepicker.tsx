d5c17c79199774f5a177795b68707a29359ee613
Civolilah <abedinhalilovic12345@gmail.com>
2023-05-04 20:55:40 +0200

    Created custom date picker header for month and year selection

--- src/components/forms/Datepicker.tsx
+++ src/components/forms/Datepicker.tsx
@@ -16,22 +16,84 @@ import {
   useEffect,
   useState,
 } from 'react';
-import ReactDatePicker from 'react-datepicker';
+import ReactDatePicker, { ReactDatePickerProps } from 'react-datepicker';
 import CommonProps from '../../common/interfaces/common-props.interface';
 import 'react-datepicker/dist/react-datepicker.css';
 import { useTranslation } from 'react-i18next';
 import { Icon } from '../icons/Icon';
-import { MdCalendarMonth } from 'react-icons/md';
+import {
+  MdCalendarMonth,
+  MdKeyboardArrowLeft,
+  MdKeyboardArrowRight,
+} from 'react-icons/md';
 import { useAccentColor } from '$app/common/hooks/useAccentColor';
 import dayjs from 'dayjs';
 import { useCurrentCompanyDateFormats } from '$app/common/hooks/useCurrentCompanyDateFormats';
 import { date as formatDate } from '$app/common/helpers';
+import { Button } from './Button';
+import { SelectField } from './SelectField';
 
 interface Props extends CommonProps {
   required?: boolean;
   onValueChange?: (value: string) => unknown;
   minDate?: string;
 }
+
+interface Month {
+  value: number;
+  label: string;
+}
+
+const MONTHS: Month[] = [
+  {
+    value: 0,
+    label: 'january',
+  },
+  {
+    value: 1,
+    label: 'february',
+  },
+  {
+    value: 2,
+    label: 'march',
+  },
+  {
+    value: 3,
+    label: 'april',
+  },
+  {
+    value: 4,
+    label: 'may',
+  },
+  {
+    value: 5,
+    label: 'june',
+  },
+  {
+    value: 6,
+    label: 'july',
+  },
+  {
+    value: 7,
+    label: 'august',
+  },
+  {
+    value: 8,
+    label: 'september',
+  },
+  {
+    value: 9,
+    label: 'october',
+  },
+  {
+    value: 10,
+    label: 'november',
+  },
+  {
+    value: 11,
+    label: 'december',
+  },
+];
 
 export function DatePicker(props: Props) {
   const [t] = useTranslation();
@@ -40,9 +102,22 @@ export function DatePicker(props: Props) {
 
   const [date, setDate] = useState<Date | null>(null);
 
+  const [years, setYears] = useState<number[]>([]);
+
   useEffect(() => {
     setDate(props.value ? new Date(props.value) : null);
   }, [props.value]);
+
+  useEffect(() => {
+    const currentYear = new Date().getFullYear();
+
+    const yearsList: number[] = [];
+    for (let i = currentYear - 5; i <= currentYear + 5; i++) {
+      yearsList.push(i);
+    }
+
+    setYears(yearsList);
+  }, []);
 
   const getFormattedValue = () => {
     let formattedValue = '';
@@ -84,6 +159,70 @@ export function DatePicker(props: Props) {
     </div>
   );
 
+  const CustomHeaderArea: ReactDatePickerProps['renderCustomHeader'] = ({
+    date: currentDate,
+    decreaseMonth,
+    increaseMonth,
+    changeMonth,
+    changeYear,
+  }) => {
+    return (
+      <div className="flex flex-col space-y-2 pb-2">
+        {currentDate && (
+          <span className="text-sm font-bold">{getFormattedValue()}</span>
+        )}
+
+        <div className="flex justify-between items-center px-2">
+          <div className="flex space-x-1">
+            <SelectField
+              className="text-xs"
+              value={currentDate.getMonth()}
+              onValueChange={(value) => changeMonth(parseInt(value))}
+              style={{ width: '6rem', padding: '0.3rem' }}
+            >
+              {MONTHS.map(({ value, label }) => (
+                <option value={value} key={value}>
+                  {t(label)}
+                </option>
+              ))}
+            </SelectField>
+
+            <SelectField
+              className="text-xs"
+              value={currentDate.getFullYear()}
+              onValueChange={(value) => changeYear(parseInt(value))}
+              style={{ width: '4rem', padding: '0.3rem' }}
+            >
+              {years.map((year) => (
+                <option value={year} key={year}>
+                  {year}
+                </option>
+              ))}
+            </SelectField>
+          </div>
+
+          <div className="flex space-x-1">
+            <Button
+              type="minimal"
+              className="border border-gray-300 py-1"
+              onClick={decreaseMonth}
+            >
+              <Icon element={MdKeyboardArrowLeft} />
+            </Button>
+
+            <Button
+              type="minimal"
+              className="border border-gray-300 py-1"
+              onClick={increaseMonth}
+            >
+              <Icon element={MdKeyboardArrowRight} />
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <ReactDatePicker
       portalId="root"
@@ -92,9 +231,12 @@ export function DatePicker(props: Props) {
       showPopperArrow={false}
       required={props.required}
       disabled={props.disabled}
+      onMonthChange={(newDate) => handleOnValueChange(newDate)}
+      onYearChange={(newDate) => handleOnValueChange(newDate)}
+      renderCustomHeader={CustomHeaderArea}
       customInput={createElement(forwardRef(CustomInputArea))}
       minDate={props.minDate ? new Date(props.minDate) : null}
-      onChange={(currentDate) => handleOnValueChange(currentDate)}
+      onChange={(newDate) => handleOnValueChange(newDate)}
       isClearable
       clearButtonClassName={`mr-1 after:bg-[${accentColor}] after:content-['x']`}
       todayButton={t('today')}
