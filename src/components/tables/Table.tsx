5f8c2a40ff72382ff9acdd5bb3d1e27c982468d5
Civolilah <abedinhalilovic12345@gmail.com>
2023-05-14 21:04:23 +0200

    Added new variable in dependency array

--- src/components/tables/Table.tsx
+++ src/components/tables/Table.tsx
@@ -73,7 +73,7 @@ export function Table(props: Props) {
         setIsVerticallyOverflow(false);
       }
     }
-  }, [tableHeight, tableParentHeight]);
+  }, [numberOfCells, tableHeight, tableParentHeight]);
 
   useEffect(() => {
     if (props.style?.height && onVerticalOverflowChange) {
