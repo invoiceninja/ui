c611050eda9aa9c8059e1527ecf07b11078f77e9
David Bomba <turbo124@gmail.com>
2023-05-23 20:12:22 +1000

    Fixes for translation for Payment Terms

--- src/pages/settings/online-payments/OnlinePayments.tsx
+++ src/pages/settings/online-payments/OnlinePayments.tsx
@@ -124,7 +124,7 @@ export function OnlinePayments() {
 
         {paymentTerms && (
           <>
-            <Element leftSide={t('payment_type')}>
+            <Element leftSide={t('payment_terms')}>
               <SelectField
                 value={company?.settings?.payment_terms}
                 id="settings.payment_terms"
