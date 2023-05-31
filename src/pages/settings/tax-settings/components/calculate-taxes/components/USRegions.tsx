02b12ec368aba7937c486adc9f199c6c6227ae85
David Bomba <turbo124@gmail.com>
2023-05-31 20:59:51 +1000

    Clean up

--- src/pages/settings/tax-settings/components/calculate-taxes/components/USRegions.tsx
+++ src/pages/settings/tax-settings/components/calculate-taxes/components/USRegions.tsx
@@ -16,26 +16,24 @@ import { useHandleCurrentCompanyChangeProperty } from "$app/pages/settings/commo
 
 export function USRegions() {
     const [t] = useTranslation();
-    const company = useCurrentCompany();
-
     const handleChange = useHandleCurrentCompanyChangeProperty();
 
     return (
         <>
             <Element leftSide={t('united_states')}>
                 <div className="grid grid-cols-s2 gap-4">
-                <SelectField
-                    id="tax_data.regions.US.tax_all_subregions"
-                    className="col-auto"
-                    onValueChange={(value) => handleChange('tax_data.regions.US.tax_all_subregions', Boolean(value))}
-                >
+                    <SelectField
+                        id="tax_data.regions.US.tax_all_subregions"
+                        className="col-auto"
+                        onValueChange={(value) => handleChange('tax_data.regions.US.tax_all_subregions', Boolean(value))}
+                    >
                     <option value="true">{t('tax_all')}</option>
                     <option value="false">{t('tax_selected')}</option>
                 </SelectField>
                 
                 <Button
                     type="primary"
-                        className="col-auto"
+                    className="col-auto"
                 >
                     Show
                 </Button>
