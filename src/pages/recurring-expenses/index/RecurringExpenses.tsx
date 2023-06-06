4c1529ab6db2ed4a4e79756c1fd63e6982fcbcab
Benjamin Beganović <k1pstabug@gmail.com>
2023-06-06 15:02:01 +0200

    Recurring expense datatable guards

--- src/pages/recurring-expenses/index/RecurringExpenses.tsx
+++ src/pages/recurring-expenses/index/RecurringExpenses.tsx
@@ -18,6 +18,7 @@ import {
   useAllRecurringExpenseColumns,
   useRecurringExpenseColumns,
 } from '../common/hooks';
+import { permission } from '$app/common/guards/guards/permission';
 
 export default function RecurringExpenses() {
   useTitle('recurring_expenses');
@@ -57,6 +58,7 @@ export default function RecurringExpenses() {
             table="recurringExpense"
           />
         }
+        linkToCreateGuards={[permission('create_recurring_expense')]}
       />
     </Default>
   );
