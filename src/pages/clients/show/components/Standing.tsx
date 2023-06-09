769d4fe9e36ab99a7b9496fd4debaab8d50e0cc3
David Bomba <turbo124@gmail.com>
2023-06-09 14:46:06 +1000

    add payment_balance to interface

--- src/pages/clients/show/components/Standing.tsx
+++ src/pages/clients/show/components/Standing.tsx
@@ -65,12 +65,12 @@ export function Standing(props: Props) {
                   </span>
                 </div>
                     
-                {client.unapplied_payments > 0 && (
+                {client.payment_balance > 0 && (
                 <div className="flex items-center justify-between">
                   <p className="font-semibold">{t('payments')}</p>
                   <span>
                     {formatMoney(
-                      client.unapplied_payments,
+                      client.payment_balance,
                       client.country_id,
                       client.settings.currency_id
                     )}
