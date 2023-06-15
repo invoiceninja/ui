24a2e239b4c44d4d5ff0c785df7d88c89bf85b27
Benjamin Beganović <k1pstabug@gmail.com>
2023-06-15 13:50:17 +0200

    Scaffold project overview page

---
+++ src/pages/projects/show/Show.tsx
@@ -0,0 +1,18 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { useTitle } from '$app/common/hooks/useTitle';
+import { Default } from '$app/components/layouts/Default';
+
+export default function Show() {
+  const { documentTitle } = useTitle('project');
+
+  return <Default title={documentTitle}></Default>;
+}
