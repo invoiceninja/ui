6f91bb271622e154991c98e6bf7139b576b1c099
Civolilah <abedinhalilovic12345@gmail.com>
2023-06-23 11:24:56 +0200

    Removed save button

--- src/pages/products/edit/ProductFields.tsx
+++ src/pages/products/edit/ProductFields.tsx
@@ -9,23 +9,15 @@
  */
 
 import { Card } from '$app/components/cards';
-import { endpoint } from '$app/common/helpers';
-import { request } from '$app/common/helpers/request';
-import { toast } from '$app/common/helpers/toast/toast';
 import { useHandleCustomFieldChange } from '$app/common/hooks/useHandleCustomFieldChange';
 import { useInjectCompanyChanges } from '$app/common/hooks/useInjectCompanyChanges';
 import { useShouldDisableCustomFields } from '$app/common/hooks/useShouldDisableCustomFields';
-import { updateRecord } from '$app/common/stores/slices/company-users';
 import { CustomFieldsPlanAlert } from '$app/components/CustomFieldsPlanAlert';
 import { Field } from '$app/pages/settings/custom-fields/components';
-import { FormEvent } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useDispatch } from 'react-redux';
 
 export default function ProductFields() {
   const [t] = useTranslation();
-
-  const dispatch = useDispatch();
 
   const company = useInjectCompanyChanges();
 
@@ -33,37 +25,12 @@ export default function ProductFields() {
 
   const disabledCustomFields = useShouldDisableCustomFields();
 
-  const onSave = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    toast.processing();
-
-    request(
-      'PUT',
-      endpoint('/api/v1/companies/:id', { id: company?.id }),
-      company
-    )
-      .then((response) => {
-        dispatch(
-          updateRecord({ object: 'company', data: response?.data.data })
-        );
-
-        toast.success('updated_product');
-      })
-      .catch((error) => {
-        console.error(error);
-        toast.error();
-      });
-  };
-
   return (
     <>
       <CustomFieldsPlanAlert />
 
       <Card
         title={t('custom_fields')}
-        withSaveButton
-        onFormSubmit={onSave}
         disableSubmitButton={disabledCustomFields}
         disableWithoutIcon={disabledCustomFields}
       >
