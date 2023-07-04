2e6db36799f7983359a7a0737b257c83e95ffbe8
David Bomba <turbo124@gmail.com>
2023-07-04 22:46:04 +1000

    Expand drag and drop zones

--- src/pages/reports/common/components/TwoColumnsDnd.tsx
+++ src/pages/reports/common/components/TwoColumnsDnd.tsx
@@ -18,11 +18,15 @@ import {
 } from '@hello-pangea/dnd';
 import { cloneDeep } from 'lodash';
 import { clientMap } from '$app/common/constants/exports/client-map';
+import { invoiceMap } from '$app/common/constants/exports/invoice-map';
+import { paymentMap } from '$app/common/constants/exports/payment-map';
 import { t } from 'i18next';
 
 export function TwoColumnsDnd() {
     const [data, setData] = useState([
         clientMap,
+        invoiceMap,
+        paymentMap,
         [],
     ]);
 
@@ -57,11 +61,11 @@ export function TwoColumnsDnd() {
     }
 
     return (
-        <div className="max-w-3xl">
+        <div className="max-w-5xl">
             <Card className="my-6">
-
                 <DragDropContext onDragEnd={onDragEnd}>
-                    <div className="flex items-center space-x-4 mx-4">
+                    <div className="flex items-center w-full">
+
                         <Droppable droppableId="0">
                             {(provided) => (
                                 <div
@@ -69,6 +73,7 @@ export function TwoColumnsDnd() {
                                     ref={provided.innerRef}
                                     {...provided.droppableProps}
                                 >
+                                    <h2>{t('client')}</h2>
                                     {data[0].map((record: Record, i: number) => (
                                         <Draggable
                                             key={record.value}
@@ -91,6 +96,8 @@ export function TwoColumnsDnd() {
                             )}
                         </Droppable>
 
+
+
                         <Droppable droppableId="1">
                             {(provided) => (
                                 <div
@@ -98,7 +105,71 @@ export function TwoColumnsDnd() {
                                     ref={provided.innerRef}
                                     {...provided.droppableProps}
                                 >
+                                    <h2>{t('Invoice')}</h2>
                                     {data[1].map((record: Record, i: number) => (
+                                        <Draggable
+                                            key={record.value}
+                                            draggableId={`left-word-${record.value}`}
+                                            index={i}
+                                        >
+                                            {(provided) => (
+                                                <div
+                                                    ref={provided.innerRef}
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}
+                                                >
+                                                    <Element key={i}>{t(`${record.trans}`)}</Element>
+                                                </div>
+                                            )}
+                                        </Draggable>
+                                    ))}
+                                    {provided.placeholder}
+                                </div>
+                            )}
+                        </Droppable>
+
+
+                        <Droppable droppableId="2">
+                            {(provided) => (
+                                <div
+                                    className="w-1/2 border border-dashed flex-column h-screen items-center"
+                                    ref={provided.innerRef}
+                                    {...provided.droppableProps}
+                                >
+                                    <h2>{t('payment')}</h2>
+
+                                    {data[2].map((record: Record, i: number) => (
+                                        <Draggable
+                                            key={record.value}
+                                            draggableId={`left-word-${record.value}`}
+                                            index={i}
+                                        >
+                                            {(provided) => (
+                                                <div
+                                                    ref={provided.innerRef}
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}
+                                                >
+                                                    <Element key={i}>{t(`${record.trans}`)}</Element>
+                                                </div>
+                                            )}
+                                        </Draggable>
+                                    ))}
+                                    {provided.placeholder}
+                                </div>
+                            )}
+                        </Droppable>
+
+                        <Droppable droppableId="3">
+                            {(provided) => (
+                                <>
+                                
+                                <div
+                                    className="w-1/2 border border-dashed flex-column h-screen items-center"
+                                    ref={provided.innerRef}
+                                    {...provided.droppableProps}
+                                >
+                                    {data[3].map((record: Record, i: number) => (
                                         <Draggable
                                             key={record.value}
                                             draggableId={`right-word-${record.value}`}
@@ -117,8 +188,10 @@ export function TwoColumnsDnd() {
                                     ))}
                                     {provided.placeholder}
                                 </div>
+                                </>
                             )}
                         </Droppable>
+
                     </div>
                 </DragDropContext>
             </Card>
