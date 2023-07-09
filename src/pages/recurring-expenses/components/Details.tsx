520e715199556d50a6f66a149cd29fb2bfbb2df1
Civolilah <abedinhalilovic12345@gmail.com>
2023-07-09 19:11:32 +0200

    Added missing validation on Recurring Expense creation page

--- src/pages/recurring-expenses/components/Details.tsx
+++ src/pages/recurring-expenses/components/Details.tsx
@@ -272,6 +272,7 @@ export function Details(props: Props) {
           <CurrencySelector
             value={recurringExpense.currency_id}
             onChange={(currency) => handleChange('currency_id', currency)}
+            errorMessage={errors?.errors.currency_id}
           />
         </Element>
       )}
