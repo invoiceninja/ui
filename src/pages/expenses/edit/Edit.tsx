3b379b36574d88cc7a2e45b5f7f506dda361ec5c
Civolilah <abedinhalilovic12345@gmail.com>
2023-07-20 22:23:04 +0200

    Adjusted Edit Expense component

--- src/pages/expenses/edit/Edit.tsx
+++ src/pages/expenses/edit/Edit.tsx
@@ -8,124 +8,69 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { route } from '$app/common/helpers/route';
-import { useTitle } from '$app/common/hooks/useTitle';
 import { Expense } from '$app/common/interfaces/expense';
 import { ValidationBag } from '$app/common/interfaces/validation-bag';
-import { useExpenseQuery } from '$app/common/queries/expenses';
-import { Page } from '$app/components/Breadcrumbs';
-import { Default } from '$app/components/layouts/Default';
-import { ResourceActions } from '$app/components/ResourceActions';
-import { Tab, Tabs } from '$app/components/Tabs';
-import { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import { useParams } from 'react-router-dom';
-import { useActions, useHandleChange } from '../common/hooks';
+import { Dispatch, SetStateAction } from 'react';
+import { useOutletContext } from 'react-router-dom';
+import { useHandleChange } from '../common/hooks';
 import { AdditionalInfo } from '../create/components/AdditionalInfo';
 import { Details } from '../create/components/Details';
 import { Notes } from '../create/components/Notes';
 import { TaxSettings } from '../create/components/Taxes';
-import { useSave } from './hooks/useSave';
+
+export interface Context {
+  errors: ValidationBag | undefined;
+  setErrors: Dispatch<SetStateAction<ValidationBag | undefined>>;
+  expense: Expense;
+  setExpense: Dispatch<SetStateAction<Expense | undefined>>;
+  taxInputType: 'by_rate' | 'by_amount';
+  setTaxInputType: Dispatch<SetStateAction<'by_rate' | 'by_amount'>>;
+}
 
 export default function Edit() {
-  const [t] = useTranslation();
+  const context: Context = useOutletContext();
 
-  const { documentTitle } = useTitle('edit_expense');
-
-  const { id } = useParams();
-
-  const { data } = useExpenseQuery({ id });
-
-  const pages: Page[] = [
-    { name: t('expenses'), href: '/expenses' },
-    { name: t('edit_expense'), href: route('/expenses/:id', { id }) },
-  ];
-
-  const tabs: Tab[] = [
-    {
-      name: t('edit'),
-      href: route('/expenses/:id/edit', { id }),
-    },
-    {
-      name: t('documents'),
-      href: route('/expenses/:id/documents', { id }),
-    },
-  ];
-
-  const [expense, setExpense] = useState<Expense>();
-
-  const [taxInputType, setTaxInputType] = useState<'by_rate' | 'by_amount'>(
-    'by_rate'
-  );
-
-  const [errors, setErrors] = useState<ValidationBag>();
-
-  const actions = useActions();
-
-  const save = useSave({ setErrors });
+  const {
+    setErrors,
+    setExpense,
+    expense,
+    errors,
+    taxInputType,
+    setTaxInputType,
+  } = context;
 
   const handleChange = useHandleChange({ setExpense, setErrors });
 
-  useEffect(() => {
-    if (data) {
-      setExpense(data);
-      setTaxInputType(data.calculate_tax_by_amount ? 'by_amount' : 'by_rate');
-    }
-  }, [data]);
-
   return (
-    <Default
-      title={documentTitle}
-      breadcrumbs={pages}
-      onSaveClick={() => expense && save(expense)}
-      navigationTopRight={
-        expense && (
-          <ResourceActions
-            resource={expense}
-            label={t('more_actions')}
-            actions={actions}
-          />
-        )
-      }
-    >
-      <div className="space-y-4">
-        <Tabs tabs={tabs} />
-
-        <div className="grid grid-cols-12 gap-4">
-          <div className="col-span-12 xl:col-span-4">
-            <Details
-              expense={expense}
-              handleChange={handleChange}
-              taxInputType={taxInputType}
-              pageType="edit"
-              errors={errors}
-            />
-          </div>
-
-          <div className="col-span-12 xl:col-span-4">
-            <Notes
-              expense={expense}
-              handleChange={handleChange}
-              errors={errors}
-            />
-          </div>
-
-          <div className="col-span-12 xl:col-span-4 space-y-4">
-            <AdditionalInfo
-              expense={expense}
-              handleChange={handleChange}
-              errors={errors}
-            />
-
-            <TaxSettings
-              expense={expense}
-              handleChange={handleChange}
-              taxInputType={taxInputType}
-              setTaxInputType={setTaxInputType}
-            />
-          </div>
-        </div>
+    <div className="grid grid-cols-12 gap-4">
+      <div className="col-span-12 xl:col-span-4">
+        <Details
+          expense={expense}
+          handleChange={handleChange}
+          taxInputType={taxInputType}
+          pageType="edit"
+          errors={errors}
+        />
       </div>
-    </Default>
+
+      <div className="col-span-12 xl:col-span-4">
+        <Notes expense={expense} handleChange={handleChange} errors={errors} />
+      </div>
+
+      <div className="col-span-12 xl:col-span-4 space-y-4">
+        <AdditionalInfo
+          expense={expense}
+          handleChange={handleChange}
+          errors={errors}
+        />
+
+        <TaxSettings
+          expense={expense}
+          handleChange={handleChange}
+          taxInputType={taxInputType}
+          setTaxInputType={setTaxInputType}
+        />
+      </div>
+    </div>
   );
 }
