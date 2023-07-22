39cce44d790ae6cd95094073942308fa77404f88
Civolilah <abedinhalilovic12345@gmail.com>
2023-07-22 21:52:27 +0200

    Added testing properties to Combobox component

--- src/components/forms/Combobox.tsx
+++ src/components/forms/Combobox.tsx
@@ -168,6 +168,7 @@ export function ComboboxStatic({
         <div className="relative mt-1">
           <div className="relative w-full cursor-default overflow-hidden rounded border border-gray-300 bg-white text-left focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 sm:text-sm">
             <HeadlessCombobox.Input
+              data-testid="combobox-input-field"
               className="w-full rounded border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               onChange={(event) => setQuery(event.target.value)}
               displayValue={(entry: Nullable<Entry>) =>
@@ -191,11 +192,16 @@ export function ComboboxStatic({
                 className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none"
               >
                 {onDismiss && selectedValue ? (
-                  <X className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                  <X
+                    className="h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                    data-testid="combobox-clear-icon"
+                  />
                 ) : (
                   <ChevronDown
                     className="h-5 w-5 text-gray-400"
                     aria-hidden="true"
+                    data-testid="combobox-chevrondown-icon"
                   />
                 )}
               </HeadlessCombobox.Button>
@@ -210,6 +216,7 @@ export function ComboboxStatic({
           >
             {action && action.visible && (
               <button
+                data-testid="combobox-action-button"
                 type="button"
                 onClick={action.onClick}
                 className="min-w-[19rem] relative cursor-pointer select-none py-2 pl-3 pr-9 text-gray-900 hover:bg-gray-100"
