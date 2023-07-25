9f47edd42a9635a3e2b7ba993b5b543a7e252a70
Benjamin Beganović <k1pstabug@gmail.com>
2023-07-25 18:33:08 +0200

    Saving client fields

--- src/pages/settings/custom-fields/clients/Clients.tsx
+++ src/pages/settings/custom-fields/clients/Clients.tsx
@@ -10,14 +10,20 @@
 
 import { CustomFieldsPlanAlert } from '$app/components/CustomFieldsPlanAlert';
 import { useTranslation } from 'react-i18next';
-import { Card } from '../../../../components/cards';
-import { Settings } from '../../../../components/layouts/Settings';
-import { Field } from '../components';
 import { useTitle } from '$app/common/hooks/useTitle';
+import { Settings } from '$app/components/layouts/Settings';
+import { Card } from '$app/components/cards';
+import { Field } from '../components/Field';
+import { useHandleCustomFieldChange } from '$app/common/hooks/useHandleCustomFieldChange';
+import { useCurrentCompany } from '$app/common/hooks/useCurrentCompany';
+import { useHandleCompanySave } from '../../common/hooks/useHandleCompanySave';
 
 export function Clients() {
   useTitle('custom_fields');
+
   const [t] = useTranslation();
+
+  const title = `${t('custom_fields')}: ${t('clients')}`;
 
   const pages = [
     { name: t('settings'), href: '/settings' },
@@ -25,23 +31,40 @@ export function Clients() {
     { name: t('clients'), href: '/settings/custom_fields/clients' },
   ];
 
+  const company = useCurrentCompany();
+  const handleChange = useHandleCustomFieldChange();
+  const save = useHandleCompanySave();
+
   return (
     <Settings
       title={t('custom_fields')}
       breadcrumbs={pages}
       docsLink="en/advanced-settings/#custom_fields"
+      onSaveClick={save}
     >
       <CustomFieldsPlanAlert />
 
-      <Card title={`${t('custom_fields')}: ${t('clients')}`}>
+      <Card title={title}>
         {['client1', 'client2', 'client3', 'client4'].map((field) => (
-          <Field key={field} field={field} placeholder={t('client_field')} />
+          <Field
+            key={field}
+            field={field}
+            placeholder={t('client_field')}
+            onChange={(value) => handleChange(field, value)}
+            initialValue={company.custom_fields[field]}
+          />
         ))}
       </Card>
 
       <Card title={`${t('custom_fields')}: ${t('contacts')}`}>
         {['contact1', 'contact2', 'contact3', 'contact4'].map((field) => (
-          <Field key={field} field={field} placeholder={t('contact_field')} />
+          <Field
+            key={field}
+            field={field}
+            placeholder={t('contact_field')}
+            onChange={(value) => handleChange(field, value)}
+            initialValue={company.custom_fields[field]}
+          />
         ))}
       </Card>
     </Settings>
