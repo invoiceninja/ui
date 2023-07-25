45f0cc0d9fa404e6409e5369d49db20d0b55aa2a
Benjamin Beganović <k1pstabug@gmail.com>
2023-07-25 18:44:28 +0200

    Saving expenses fields

--- src/pages/settings/custom-fields/expenses/Expenses.tsx
+++ src/pages/settings/custom-fields/expenses/Expenses.tsx
@@ -10,13 +10,17 @@
 
 import { CustomFieldsPlanAlert } from '$app/components/CustomFieldsPlanAlert';
 import { useTranslation } from 'react-i18next';
-import { Card } from '../../../../components/cards';
-import { Settings } from '../../../../components/layouts/Settings';
-import { Field } from '../components';
 import { useTitle } from '$app/common/hooks/useTitle';
+import { Settings } from '$app/components/layouts/Settings';
+import { Card } from '$app/components/cards';
+import { Field } from '../components/Field';
+import { useHandleCustomFieldChange } from '$app/common/hooks/useHandleCustomFieldChange';
+import { useCurrentCompany } from '$app/common/hooks/useCurrentCompany';
+import { useHandleCompanySave } from '../../common/hooks/useHandleCompanySave';
 
 export function Expenses() {
   useTitle('custom_fields');
+
   const [t] = useTranslation();
 
   const title = `${t('custom_fields')}: ${t('expenses')}`;
@@ -27,17 +31,28 @@ export function Expenses() {
     { name: t('expenses'), href: '/settings/custom_fields/expenses' },
   ];
 
+  const company = useCurrentCompany();
+  const handleChange = useHandleCustomFieldChange();
+  const save = useHandleCompanySave();
+
   return (
     <Settings
       title={t('custom_fields')}
       breadcrumbs={pages}
       docsLink="en/advanced-settings/#custom_fields"
+      onSaveClick={save}
     >
       <CustomFieldsPlanAlert />
 
       <Card title={title}>
         {['expense1', 'expense2', 'expense3', 'expense4'].map((field) => (
-          <Field key={field} field={field} placeholder={t('expense_field')} />
+          <Field
+            key={field}
+            field={field}
+            placeholder={t('expense_field')}
+            onChange={(value) => handleChange(field, value)}
+            initialValue={company.custom_fields[field]}
+          />
         ))}
       </Card>
     </Settings>
