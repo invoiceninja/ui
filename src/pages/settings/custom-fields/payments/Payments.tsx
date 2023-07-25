6565d767fd9b502873c735a8c16b247dee546a7a
Benjamin Beganović <k1pstabug@gmail.com>
2023-07-25 18:41:07 +0200

    Saving payment fields

--- src/pages/settings/custom-fields/payments/Payments.tsx
+++ src/pages/settings/custom-fields/payments/Payments.tsx
@@ -8,17 +8,22 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { useTitle } from '$app/common/hooks/useTitle';
 import { CustomFieldsPlanAlert } from '$app/components/CustomFieldsPlanAlert';
 import { useTranslation } from 'react-i18next';
-import { Card } from '../../../../components/cards';
-import { Settings } from '../../../../components/layouts/Settings';
-import { Field } from '../components';
+import { useTitle } from '$app/common/hooks/useTitle';
+import { Settings } from '$app/components/layouts/Settings';
+import { Card } from '$app/components/cards';
+import { Field } from '../components/Field';
+import { useHandleCustomFieldChange } from '$app/common/hooks/useHandleCustomFieldChange';
+import { useCurrentCompany } from '$app/common/hooks/useCurrentCompany';
+import { useHandleCompanySave } from '../../common/hooks/useHandleCompanySave';
 
 export function Payments() {
-  const { documentTitle } = useTitle('custom_fields');
+  useTitle('custom_fields');
 
   const [t] = useTranslation();
+
+  const title = `${t('custom_fields')}: ${t('payments')}`;
 
   const pages = [
     { name: t('settings'), href: '/settings' },
@@ -26,17 +31,28 @@ export function Payments() {
     { name: t('payments'), href: '/settings/custom_fields/payments' },
   ];
 
+  const company = useCurrentCompany();
+  const handleChange = useHandleCustomFieldChange();
+  const save = useHandleCompanySave();
+
   return (
     <Settings
-      title={documentTitle}
+      title={t('custom_fields')}
       breadcrumbs={pages}
       docsLink="en/advanced-settings/#custom_fields"
+      onSaveClick={save}
     >
       <CustomFieldsPlanAlert />
 
-      <Card title={`${t('custom_fields')}: ${t('payments')}`}>
+      <Card title={title}>
         {['payment1', 'payment2', 'payment3', 'payment4'].map((field) => (
-          <Field key={field} field={field} placeholder={t('payment_field')} />
+          <Field
+            key={field}
+            field={field}
+            placeholder={t('payment_field')}
+            onChange={(value) => handleChange(field, value)}
+            initialValue={company.custom_fields[field]}
+          />
         ))}
       </Card>
     </Settings>
