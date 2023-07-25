78c93485e57559d37a47e928773fa7d890e9cb28
Benjamin Beganović <k1pstabug@gmail.com>
2023-07-25 18:42:00 +0200

    Saving quotes fields

--- src/pages/settings/custom-fields/quotes/Quotes.tsx
+++ src/pages/settings/custom-fields/quotes/Quotes.tsx
@@ -8,17 +8,22 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { useTitle } from '$app/common/hooks/useTitle';
-import { useTranslation } from 'react-i18next';
 import { CustomFieldsPlanAlert } from '$app/components/CustomFieldsPlanAlert';
+import { useTranslation } from 'react-i18next';
+import { useTitle } from '$app/common/hooks/useTitle';
 import { Settings } from '$app/components/layouts/Settings';
 import { Card } from '$app/components/cards';
-import { Field } from '../components';
+import { Field } from '../components/Field';
+import { useHandleCustomFieldChange } from '$app/common/hooks/useHandleCustomFieldChange';
+import { useCurrentCompany } from '$app/common/hooks/useCurrentCompany';
+import { useHandleCompanySave } from '../../common/hooks/useHandleCompanySave';
 
 export function Quotes() {
-  const { documentTitle } = useTitle('custom_fields');
+  useTitle('custom_fields');
 
   const [t] = useTranslation();
+
+  const title = `${t('custom_fields')}: ${t('quotes')}`;
 
   const pages = [
     { name: t('settings'), href: '/settings' },
@@ -26,17 +31,28 @@ export function Quotes() {
     { name: t('quotes'), href: '/settings/custom_fields/quotes' },
   ];
 
+  const company = useCurrentCompany();
+  const handleChange = useHandleCustomFieldChange();
+  const save = useHandleCompanySave();
+
   return (
     <Settings
-      title={documentTitle}
+      title={t('custom_fields')}
       breadcrumbs={pages}
       docsLink="en/advanced-settings/#custom_fields"
+      onSaveClick={save}
     >
       <CustomFieldsPlanAlert />
 
-      <Card title={`${t('custom_fields')}: ${t('quotes')}`}>
-        {['quote1', 'quote2', 'quote3', 'quote4'].map((field) => (
-          <Field key={field} field={field} placeholder={t('quote_field')} />
+      <Card title={title}>
+        {['quote0', 'quote2', 'quote3', 'quote4'].map((field) => (
+          <Field
+            key={field}
+            field={field}
+            placeholder={t('quote_field')}
+            onChange={(value) => handleChange(field, value)}
+            initialValue={company.custom_fields[field]}
+          />
         ))}
       </Card>
     </Settings>
