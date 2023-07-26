e1253a84cb37773302aea0d8697d0c6859a51e24
David Bomba <turbo124@gmail.com>
2023-07-26 10:32:01 +1000

    Add option to set default company

--- src/pages/settings/account-management/component/Overview.tsx
+++ src/pages/settings/account-management/component/Overview.tsx
@@ -12,7 +12,7 @@ import { useCompanyChanges } from '$app/common/hooks/useCompanyChanges';
 import { updateChanges } from '$app/common/stores/slices/company-users';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
-import { Card, ClickableElement, Element } from '../../../../components/cards';
+import { Card, Element } from '../../../../components/cards';
 import Toggle from '../../../../components/forms/Toggle';
 import { useCurrentAccount } from '$app/common/hooks/useCurrentAccount';
 import { endpoint } from '$app/common/helpers';
