a359cc0de4a8e532f00ac83c98bbda9c761e7f31
Civolilah <abedinhalilovic12345@gmail.com>
2023-08-08 21:40:57 +0200

    Created vendor bulk actions

---
+++ src/pages/vendors/common/hooks/useCustomBulkActions.tsx
@@ -0,0 +1,53 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { toast } from '$app/common/helpers/toast/toast';
+import { Vendor } from '$app/common/interfaces/vendor';
+import { useDocumentsBulk } from '$app/common/queries/documents';
+import { CustomBulkAction } from '$app/components/DataTable';
+import { DropdownElement } from '$app/components/dropdown/DropdownElement';
+import { Icon } from '$app/components/icons/Icon';
+import { useTranslation } from 'react-i18next';
+import { MdDownload } from 'react-icons/md';
+
+export const useCustomBulkActions = () => {
+  const [t] = useTranslation();
+
+  const documentsBulk = useDocumentsBulk();
+
+  const shouldDownloadDocuments = (vendors: Vendor[]) => {
+    return vendors.some(({ documents }) => documents.length);
+  };
+
+  const getDocumentsIds = (vendors: Vendor[]) => {
+    return vendors.flatMap(({ documents }) => documents.map(({ id }) => id));
+  };
+
+  const customBulkActions: CustomBulkAction<Vendor>[] = [
+    (_, selectedVendors, onActionCall) => (
+      <DropdownElement
+        onClick={() =>
+          selectedVendors && shouldDownloadDocuments(selectedVendors)
+            ? documentsBulk(
+                getDocumentsIds(selectedVendors),
+                'download',
+                onActionCall
+              )
+            : toast.error('no_documents_to_download')
+        }
+        icon={<Icon element={MdDownload} />}
+      >
+        {t('documents')}
+      </DropdownElement>
+    ),
+  ];
+
+  return customBulkActions;
+};
