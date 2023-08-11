23ce07cdc1cceae0761801331278578a0a4f7aaf
David Bomba <turbo124@gmail.com>
2023-08-11 10:36:58 +1000

    Improve slider with Reminder information and schedules

--- src/common/hooks/emails/useHandleSend.ts
+++ src/common/hooks/emails/useHandleSend.ts
@@ -15,7 +15,6 @@ import { ValidationBag } from '$app/common/interfaces/validation-bag';
 import { AxiosError } from 'axios';
 import { Dispatch, SetStateAction } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useQueryClient } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 
 interface Params {
@@ -25,7 +24,6 @@ interface Params {
 export function useHandleSend({ setErrors }: Params) {
   const [t] = useTranslation();
   const navigate = useNavigate();
-  const queryClient = useQueryClient();
 
   return (
     body: string,
