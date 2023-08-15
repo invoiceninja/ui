2afbe349d9a745343ad3b6d9e9ca1e25f50e4a21
Benjamin Beganović <k1pstabug@gmail.com>
2023-08-16 00:09:27 +0200

    Fix ms logout

--- src/pages/authentication/Logout.tsx
+++ src/pages/authentication/Logout.tsx
@@ -13,15 +13,13 @@ import { useQueryClient } from 'react-query';
 import { useNavigate } from 'react-router';
 import { useCurrentUser } from '$app/common/hooks/useCurrentUser';
 // import { useGoogleLogout } from 'react-google-login';
-import { RootState } from '$app/common/stores/store';
-import { useSelector } from 'react-redux';
 import { isHosted } from '$app/common/helpers';
+import { msal } from './components/SignInProviders';
 
 export function Logout() {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
   const user = useCurrentUser();
-  const msalInstance = useSelector((state: RootState) => state.user.msal);
 
   // const { signOut } = useGoogleLogout({
   //   clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
@@ -31,8 +29,8 @@ export function Logout() {
     localStorage.removeItem('X-NINJA-TOKEN');
     localStorage.removeItem('X-CURRENT-INDEX');
 
-    if (isHosted() && user?.oauth_provider_id === 'microsoft' && msalInstance) {
-      msalInstance.logout();
+    if (isHosted() && user?.oauth_provider_id === 'microsoft' && msal) {
+      msal.logoutPopup();
     }
 
     if (isHosted() && user?.oauth_provider_id === 'google') {
