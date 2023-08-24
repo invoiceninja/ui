3be7b4edac29730319023d1371599437a01b6907
Benjamin Beganović <k1pstabug@gmail.com>
2023-08-24 18:17:55 +0200

    Switch the strategy for dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class', // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
