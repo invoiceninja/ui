4ee030f8dce621ba7ec1409d7b35ee04663fac21
Benjamin Beganović <k1pstabug@gmail.com>
2023-08-24 19:06:02 +0200

    Sidebar icons

--- src/components/HelpSidebarIcons.tsx
+++ src/components/HelpSidebarIcons.tsx
@@ -203,7 +203,7 @@ export function HelpSidebarIcons(props: Props) {
         </div>
       </Modal>
 
-      <nav className="flex p-2 justify-around text-white border-t border-gray-600">
+      <nav className="flex p-2 justify-around text-white border-t border-ninja-gray-lighter">
         {!isMiniSidebar && (
           <>
             {isSelfHosted() && account && !account.is_scheduler_running && (
