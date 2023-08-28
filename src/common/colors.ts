b4ca7e35af85ceb3d45c1a3c16c1dd6cfae31c4e
Benjamin Beganović <k1pstabug@gmail.com>
2023-08-28 17:50:10 +0200

    Define colors

---
+++ src/common/colors.ts
@@ -0,0 +1,35 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+export function useColorScheme() {
+  return {
+    $1: '#182433',
+    $2: '#151f2c',
+    $3: '#ffffff',
+    $4: '#1f2e41',
+    $5: '#1f2e41',
+    $6: '#151f2c',
+    $7: '#151f2c',
+    $8: '#1f2e41',
+    $9: '#ffffff',
+  };
+
+  return {
+    $1: '#ffffff', // Primary background
+    $2: '#f7f7f7', // Secondary background
+    $3: '#0f172a', // Primary text
+    $4: '#f7f7f7', // Primary border 
+    $5: '#d1d5db', // Secondary border (sidebar)
+    $6: '#242930', // Secondary background
+    $7: '#f7f7f7', // Primary hover
+    $8: '#363D47', // Secondary hover
+    $9: '#ffffff', // Accent color text
+  };
+}
