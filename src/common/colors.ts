8233d4998d86d9c6733f3c4642d3f64c8752081a
Benjamin Beganović <k1pstabug@gmail.com>
2023-08-29 18:47:34 +0200

    Extract colors into hook

--- src/common/colors.ts
+++ src/common/colors.ts
@@ -8,28 +8,36 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-export function useColorScheme() {
-  return {
-    $1: '#182433',
-    $2: '#151f2c',
-    $3: '#ffffff',
-    $4: '#1f2e41',
-    $5: '#1f2e41',
-    $6: '#151f2c',
-    $7: '#151f2c',
-    $8: '#1f2e41',
-    $9: '#ffffff',
-  };
+import { atom, useAtom } from 'jotai';
 
-  return {
-    $1: '#ffffff', // Primary background
-    $2: '#f7f7f7', // Secondary background
-    $3: '#0f172a', // Primary text
-    $4: '#f7f7f7', // Primary border 
-    $5: '#d1d5db', // Secondary border (sidebar)
-    $6: '#242930', // Secondary background
-    $7: '#f7f7f7', // Primary hover
-    $8: '#363D47', // Secondary hover
-    $9: '#ffffff', // Accent color text
-  };
+export const $1 = {
+  $1: '#182433',
+  $2: '#151f2c',
+  $3: '#ffffff',
+  $4: '#1f2e41',
+  $5: '#1f2e41',
+  $6: '#151f2c',
+  $7: '#151f2c',
+  $8: '#1f2e41',
+  $9: '#ffffff',
+};
+
+export const $2 = {
+  $1: '#ffffff', // Primary background
+  $2: '#f7f7f7', // Secondary background
+  $3: '#0f172a', // Primary text
+  $4: '#f7f7f7', // Primary border
+  $5: '#d1d5db', // Secondary border (sidebar)
+  $6: '#242930', // Secondary background
+  $7: '#f7f7f7', // Primary hover
+  $8: '#363D47', // Secondary hover
+  $9: '#ffffff', // Accent color text
+};
+
+export const colorSchemeAtom = atom($1);
+
+export function useColorScheme() {
+  const [colorScheme] = useAtom(colorSchemeAtom);
+
+  return colorScheme;
 }
