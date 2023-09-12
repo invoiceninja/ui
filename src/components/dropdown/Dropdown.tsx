043571fa00e67d9ba9895339a24f68a2092ee4d9
Benjamin Beganović <k1pstabug@gmail.com>
2023-09-12 11:17:11 +0200

    Fixes for dropdown button

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -117,7 +117,7 @@ export function Dropdown(props: Props) {
           disabled={props.disabled}
           onClick={() => setVisible(!visible)}
           className={classNames(
-            `inline-flex border border-transparent items-center space-x-2 justify-center py-1.5 px-3 rounded text-sm disabled:cursor-not-allowed disabled:opacity-75 ${props.className}`,
+            `border inline-flex items-center space-x-2 px-4 justify-center rounded text-sm disabled:cursor-not-allowed disabled:opacity-75 py-2 ${props.className}`,
             {
               'hover:bg-white hover:border-gray-300': !props.cardActions,
               'hover:opacity-90': props.cardActions,
