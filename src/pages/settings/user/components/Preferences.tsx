91db9290d1e8a653493dda8429287f6a5b38d0b4
Benjamin Beganović <k1pstabug@gmail.com>
2023-09-20 17:54:57 +0200

    Extract dashboard settings into separate section

--- src/pages/settings/user/components/Preferences.tsx
+++ src/pages/settings/user/components/Preferences.tsx
@@ -43,83 +43,93 @@ export function Preferences() {
   const resolveCurrency = useResolveCurrency();
 
   return (
-    <Card title={t('preferences')}>
-      <Element leftSide={t('show_pdf_preview')}>
-        <Toggle
-          checked={
-            typeof reactSettings.show_pdf_preview === 'boolean'
-              ? reactSettings.show_pdf_preview
-              : true
-          }
-          onValueChange={(value) =>
-            handleChange('company_user.react_settings.show_pdf_preview', value)
-          }
+    <div className="space-y-4">
+      <Card title={t('preferences')}>
+        <Element leftSide={t('show_pdf_preview')}>
+          <Toggle
+            checked={
+              typeof reactSettings.show_pdf_preview === 'boolean'
+                ? reactSettings.show_pdf_preview
+                : true
+            }
+            onValueChange={(value) =>
+              handleChange(
+                'company_user.react_settings.show_pdf_preview',
+                value
+              )
+            }
+          />
+        </Element>
+
+        <Element leftSide={t('show_document_preview')}>
+          <Toggle
+            checked={Boolean(reactSettings.show_document_preview)}
+            onValueChange={(value) =>
+              handleChange(
+                'company_user.react_settings.show_document_preview',
+                value
+              )
+            }
+          />
+        </Element>
+
+        <Element
+          leftSide={t('react_notification_link')}
+          leftSideHelp={t('react_notification_link_help')}
+        >
+          <Toggle
+            checked={reactSettings.react_notification_link}
+            onValueChange={(value) =>
+              handleChange(
+                'company_user.react_settings.react_notification_link',
+                value
+              )
+            }
+          />
+        </Element>
+
+        <Element
+          leftSide={t('number_precision')}
+          leftSideHelp={t('number_precision_help')}
+        >
+          <InputField
+            value={reactSettings?.number_precision}
+            onValueChange={(value) =>
+              handleChange(
+                'company_user.react_settings.number_precision',
+                value
+              )
+            }
+            type="number"
+            placeholder={t('number_precision')}
+          />
+        </Element>
+
+        <Divider />
+      </Card>
+
+      <Card title={t('dashboard')}>
+        <Preference
+          text={t('default_view')}
+          path="dashboard_charts.default_view"
+          format={(value) => <p className="uppercase">{value}</p>}
         />
-      </Element>
 
-      <Element leftSide={t('show_document_preview')}>
-        <Toggle
-          checked={Boolean(reactSettings.show_document_preview)}
-          onValueChange={(value) =>
-            handleChange(
-              'company_user.react_settings.show_document_preview',
-              value
-            )
-          }
+        <Preference
+          text={t('currency')}
+          path="dashboard_charts.currency"
+          format={(value) => {
+            const currency = resolveCurrency(value.toString());
+
+            if (currency) {
+              return currency.code;
+            }
+
+            return value;
+          }}
         />
-      </Element>
-
-      <Element
-        leftSide={t('react_notification_link')}
-        leftSideHelp={t('react_notification_link_help')}
-      >
-        <Toggle
-          checked={reactSettings.react_notification_link}
-          onValueChange={(value) =>
-            handleChange(
-              'company_user.react_settings.react_notification_link',
-              value
-            )
-          }
-        />
-      </Element>
-
-      <Element
-        leftSide={t('number_precision')}
-        leftSideHelp={t('number_precision_help')}
-      >
-        <InputField
-          value={reactSettings?.number_precision}
-          onValueChange={(value) =>
-            handleChange('company_user.react_settings.number_precision', value)
-          }
-          type="number"
-          placeholder={t('number_precision')}
-        />
-      </Element>
-
-      <Divider />
-
-      <Preference
-        text={`${t('dashboard_charts')}: ${'default_view'}`}
-        path="dashboard_charts.default_view"
-        format={(value) => <p className="uppercase">{value}</p>}
-      />
-
-      <Preference
-        text={`${t('dashboard_charts')}: ${'currency'}`}
-        path="dashboard_charts.currency"
-        format={(value) => {
-          const currency = resolveCurrency(value.toString());
-
-          if (currency) {
-            return currency.code;
-          }
-
-          return value;
-        }}
-      />
-    </Card>
+      </Card>
+    </div>
   );
 }
 
@@ -131,26 +141,28 @@ interface PreferenceProps {
 
 function Preference({ text, path, format }: PreferenceProps) {
   const { preferences, update } = usePreferences();
-
-  if (get(preferences, path) === get(preferencesDefaults, path)) {
-    return null;
-  }
+  const { t } = useTranslation();
 
   return (
     <Element leftSide={text}>
       <Inline className="space-x-2">
         <div>{format(get(preferences, path))}</div>
 
-        <button
-          type="button"
-          onClick={() => {
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            update(`preferences.${path}`, get(preferencesDefaults, path));
-          }}
-        >
-          <X size={18} />
-        </button>
+        {get(preferencesDefaults, path) === get(preferences, path) ? (
+          <small>({t('default')})</small>
+        ) : (
+          <button
+            type="button"
+            onClick={() => {
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              update(`preferences.${path}`, get(preferencesDefaults, path));
+            }}
+            aria-label="Reset to default"
+          >
+            <X size={18} />
+          </button>
+        )}
       </Inline>
     </Element>
   );
