d682acc64f5be1540b684b250e08213dcac041b1
Benjamin Beganović <k1pstabug@gmail.com>
2023-10-03 17:53:53 +0200

    Allow specifying the resource

--- src/pages/settings/invoice-design/pages/template-designs/pages/create/Create.tsx
+++ src/pages/settings/invoice-design/pages/template-designs/pages/create/Create.tsx
@@ -18,7 +18,7 @@ import { ValidationBag } from '$app/common/interfaces/validation-bag';
 import { useBlankDesignQuery } from '$app/common/queries/designs';
 import { Container } from '$app/components/Container';
 import { Card, Element } from '$app/components/cards';
-import { InputField } from '$app/components/forms';
+import { Checkbox, InputField } from '$app/components/forms';
 import { useSaveBtn } from '$app/components/layouts/common/hooks';
 import { AxiosError } from 'axios';
 import { useEffect, useState } from 'react';
@@ -27,82 +27,121 @@ import { useQueryClient } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 
 export default function Create() {
-    const { t } = useTranslation();
-    const { data } = useBlankDesignQuery();
+  const { t } = useTranslation();
+  const { data } = useBlankDesignQuery();
 
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    const baseTemplate = "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\t<ninja>\n\n\t\t</ninja>\n\t</body>\n</html>\n";
+  const baseTemplate =
+    '<html>\n\t<head>\n\t</head>\n\t<body>\n\t\t<ninja>\n\n\t\t</ninja>\n\t</body>\n</html>\n';
 
-    const [design, setDesign] = useState<Design | null>(null);
-    const [errors, setErrors] = useState<ValidationBag | null>(null);
+  const [design, setDesign] = useState<Design | null>(null);
+  const [errors, setErrors] = useState<ValidationBag | null>(null);
 
-    const handleChange = <T extends keyof Design>(key: T, value: Design[T]) => {
-        setDesign((current) => current && {
-            ...current,
-            is_template: true,
-            design: {
-                    ...current.design,
-                    header: " ",
-                    body: baseTemplate,
-                    footer: " ",
-                    includes: " ",
-                },
-                [key]: value
-            }
-        );
-    };
-
-    useEffect(() => {
-        if (data) {
-            setDesign(data);
+  const handleChange = <T extends keyof Design>(key: T, value: Design[T]) => {
+    setDesign(
+      (current) =>
+        current && {
+          ...current,
+          is_template: true,
+          design: {
+            ...current.design,
+            header: ' ',
+            body: baseTemplate,
+            footer: ' ',
+            includes: ' ',
+          },
+          [key]: value,
         }
-
-        return () => setDesign(null);
-    }, [data]);
-
-    const navigate = useNavigate();
-
-    useSaveBtn(
-        {
-            onClick() {
-                toast.processing();
-                setErrors(null);
-
-                request('POST', endpoint('/api/v1/designs'), design)
-                    .then((response: GenericSingleResourceResponse<Design>) => {
-                        toast.success('design_created');
-
-                        queryClient.invalidateQueries(['/api/v1/designs']);
-
-                        navigate(
-                            route('/settings/invoice_design/template_designs/:id/edit', {
-                                id: response.data.data.id,
-                            })
-                        );
-                    })
-                    .catch((e: AxiosError<ValidationBag>) => {
-                        if (e.response?.status === 422) {
-                            toast.dismiss();
-                            setErrors(e.response.data);
-                        }
-                    });
-            },
-        },
-        [design]
     );
+  };
 
-    return (
-        <Container>
-            <Card title={t('new_proposal_template')}>
-                <Element leftSide={t('name')}>
-                    <InputField
-                        value={design?.name}
-                        errorMessage={errors?.errors.name}
-                        onValueChange={(value) => handleChange('name', value)}
-                    />
-                </Element>
-            </Card>
-        </Container>
+  const handleResourceChange = (value: string, checked: boolean) => {
+    if (!design) {
+      return;
+    }
+
+    const entities =
+      design.entities.length > 1
+        ? design.entities.split(',') || ([] as string[])
+        : [];
+
+    const filtered = entities.filter((e) => e !== value);
+
+    if (checked) {
+      filtered.push(value);
+    }
+
+    setDesign(
+      (current) =>
+        current && {
+          ...current,
+          entities: filtered.join(','),
+        }
     );
+  };
+
+  useEffect(() => {
+    if (data) {
+      setDesign(data);
+    }
+
+    return () => setDesign(null);
+  }, [data]);
+
+  const navigate = useNavigate();
+
+  useSaveBtn(
+    {
+      onClick() {
+        toast.processing();
+        setErrors(null);
+
+        request('POST', endpoint('/api/v1/designs'), design)
+          .then((response: GenericSingleResourceResponse<Design>) => {
+            toast.success('design_created');
+
+            queryClient.invalidateQueries(['/api/v1/designs']);
+
+            navigate(
+              route('/settings/invoice_design/template_designs/:id/edit', {
+                id: response.data.data.id,
+              })
+            );
+          })
+          .catch((e: AxiosError<ValidationBag>) => {
+            if (e.response?.status === 422) {
+              toast.dismiss();
+              setErrors(e.response.data);
+            }
+          });
+      },
+    },
+    [design]
+  );
+
+  return (
+    <Container>
+      <Card title={t('new_proposal_template')}>
+        <Element leftSide={t('name')}>
+          <InputField
+            value={design?.name}
+            errorMessage={errors?.errors.name}
+            onValueChange={(value) => handleChange('name', value)}
+          />
+        </Element>
+
+        <Element leftSide={t('resource')}>
+          <Checkbox
+            label={t('invoice')}
+            value="invoice"
+            onValueChange={(value, checked) =>
+              handleResourceChange(value, Boolean(checked))
+            }
+            checked={design?.entities.includes('invoice')}
+          />
+        </Element>
+      </Card>
+    </Container>
+  );
 }
