1af58380b2b08dc123109efefd8b52c074a4df35
Civolilah <abedinhalilovic12345@gmail.com>
2023-10-25 23:02:48 +0200

    Changed the queryKey for comobox components

--- src/components/forms/Combobox.tsx
+++ src/components/forms/Combobox.tsx
@@ -755,7 +755,7 @@ export function ComboboxAsync<T = any>({
   const [url, setUrl] = useState(endpoint);
 
   const { data } = useQuery(
-    [url],
+    [new URL(url).pathname, new URL(url).searchParams.toString()],
     () => {
       const $url = new URL(url);
 
