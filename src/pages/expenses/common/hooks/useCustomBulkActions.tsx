005465fea68d87e7b113898f820514b19fc80f84
Benjamin Beganović <k1pstabug@gmail.com>
2023-10-27 18:52:41 +0200

    Bulk expense category change

--- src/pages/expenses/common/hooks/useCustomBulkActions.tsx
+++ src/pages/expenses/common/hooks/useCustomBulkActions.tsx
@@ -9,14 +9,78 @@
  */
 
 import { toast } from '$app/common/helpers/toast/toast';
+import { useTitle } from '$app/common/hooks/useTitle';
 import { Expense } from '$app/common/interfaces/expense';
 import { useDocumentsBulk } from '$app/common/queries/documents';
+import { useExpenseCategoriesQuery } from '$app/common/queries/expense-categories';
+import { useBulk } from '$app/common/queries/expenses';
 import { CustomBulkAction } from '$app/components/DataTable';
+import { Modal } from '$app/components/Modal';
+import { SearchableSelect } from '$app/components/SearchableSelect';
 import { DropdownElement } from '$app/components/dropdown/DropdownElement';
+import { Button, SelectField } from '$app/components/forms';
 import { Icon } from '$app/components/icons/Icon';
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { MdDownload } from 'react-icons/md';
+import { MdCategory, MdDownload } from 'react-icons/md';
+
+interface Props {
+  isVisible: boolean;
+  setIsVisible: Dispatch<SetStateAction<boolean>>;
+  selectedExpenses: Expense[];
+}
+
+function ChangeCategory({ isVisible, setIsVisible, selectedExpenses }: Props) {
+  const [t] = useTranslation();
+  const [category, setCategory] = useState('');
+
+  const { data: expenseCategories } = useExpenseCategoriesQuery({
+    status: ['active'],
+  });
+
+  useEffect(() => {
+    return () => {
+      setCategory('');
+    };
+  }, []);
+
+  const handleChange = () => {
+    toast.processing();
+
+    // 
+  };
+
+  return (
+    <Modal
+      title={`${t('change')} ${t('category')}`}
+      visible={isVisible}
+      onClose={setIsVisible}
+      overflowVisible
+    >
+      <p>{t('list_of_affected_expenses')}</p>
+
+      <ul>
+        {selectedExpenses.map(({ id, number }) => (
+          <li key={id}>{number}</li>
+        ))}
+      </ul>
+
+      <SelectField
+        value={category}
+        onValueChange={setCategory}
+        withBlank
+      >
+        {expenseCategories?.map(({ id, name }) => (
+          <option key={id} value={id}>
+            {name}
+          </option>
+        ))}
+      </SelectField>
+
+      <Button>{t('save')}</Button>
+    </Modal>
+  );
+}
 
 export const useCustomBulkActions = () => {
   const [t] = useTranslation();
@@ -41,6 +105,8 @@ export const useCustomBulkActions = () => {
     setSelected?.([]);
   };
 
+  const [isChangeCategoryVisible, setIsChangeCategoryVisible] = useState(false);
+
   const customBulkActions: CustomBulkAction<Expense>[] = [
     (_, selectedExpenses, setSelected) => (
       <DropdownElement
@@ -53,6 +119,28 @@ export const useCustomBulkActions = () => {
       >
         {t('documents')}
       </DropdownElement>
+    ),
+    (_, selectedExpenses, setSelected) => (
+      <>
+        {selectedExpenses ? (
+          <ChangeCategory
+            isVisible={isChangeCategoryVisible}
+            setIsVisible={setIsChangeCategoryVisible}
+            selectedExpenses={selectedExpenses}
+          />
+        ) : null}
+
+        <DropdownElement
+          onClick={() =>
+            selectedExpenses && selectedExpenses.length
+              ? setIsChangeCategoryVisible(true)
+              : toast.error('no_expenses_selected')
+          }
+          icon={<Icon element={MdCategory} />}
+        >
+          {t('change')} {t('category')}
+        </DropdownElement>
+      </>
     ),
   ];
 
