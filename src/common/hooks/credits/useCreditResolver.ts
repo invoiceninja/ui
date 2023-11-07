bf7ee3e6c11822d27e98671845cd6c2870b42a49
Civolilah <abedinhalilovic12345@gmail.com>
2023-11-07 17:08:38 +0100

    Implemented fixes for invoice and credit resolvers

--- src/common/hooks/credits/useCreditResolver.ts
+++ src/common/hooks/credits/useCreditResolver.ts
@@ -8,10 +8,25 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { CreditResolver } from '$app/common/helpers/credits/credit-resolver';
-
-const creditResolver = new CreditResolver();
+import { endpoint } from '$app/common/helpers';
+import { request } from '$app/common/helpers/request';
+import { Invoice } from '$app/common/interfaces/invoice';
+import { useQueryClient } from 'react-query';
 
 export function useCreditResolver() {
-  return creditResolver;
+  const queryClient = useQueryClient();
+
+  const find = (id: string) => {
+    return queryClient.fetchQuery<Invoice>(
+      ['/api/v1/credits', id],
+      () =>
+        request(
+          'GET',
+          endpoint('/api/v1/credits/:id?include=client', { id })
+        ).then((response) => response.data.data),
+      { staleTime: Infinity }
+    );
+  };
+
+  return { find };
 }
