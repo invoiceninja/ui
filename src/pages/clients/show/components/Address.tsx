e9a98e391467a26f69853677ea5ad423ff71a091
David Bomba <turbo124@gmail.com>
2023-11-09 17:14:15 +1100

    Fixes for address formatting

--- src/pages/clients/show/components/Address.tsx
+++ src/pages/clients/show/components/Address.tsx
@@ -40,8 +40,8 @@ export function Address(props: Props) {
 
                 <p>
                   {client.city.length > 0 && client.city} &nbsp;
-                  {client.postal_code.length > 0 && client.postal_code} &nbsp;
                   {client.state}
+                  {client.postal_code.length > 0 && client.postal_code} &nbsp;
                 </p>
 
                 <p>{resolveCountry(client.country_id)?.name}</p>
