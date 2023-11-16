c5a6d33ee4169ba0b1b12b5e7718332b8ebeaeb2
Civolilah <abedinhalilovic12345@gmail.com>
2023-11-16 19:01:56 +0100

    Changed order to asc for specific selectors

--- src/components/tax-rates/TaxRateSelector.tsx
+++ src/components/tax-rates/TaxRateSelector.tsx
@@ -49,6 +49,7 @@ export function TaxRateSelector(props: Props) {
             taxRate ? `${taxRate.name} ${taxRate.rate}%` : '',
           dropdownLabelFn: (taxRate) => `${taxRate.name} ${taxRate.rate}%`,
         }}
+        sortBy="name|asc"
         onDismiss={props.onClearButtonClick}
       />
 
