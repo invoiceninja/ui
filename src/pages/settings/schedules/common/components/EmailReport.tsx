be2acc09a58868a2a5220a3007f7490aac7f807d
David Bomba <turbo124@gmail.com>
2023-12-02 16:10:40 +1100

    Working on email reports

--- src/pages/settings/schedules/common/components/EmailReport.tsx
+++ src/pages/settings/schedules/common/components/EmailReport.tsx
@@ -8,19 +8,14 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { Element } from '$app/components/cards';
-import { SelectField } from '$app/components/forms';
 import { useAccentColor } from '$app/common/hooks/useAccentColor';
 import { Client } from '$app/common/interfaces/client';
 import { Parameters, Schedule } from '$app/common/interfaces/schedule';
 import { ValidationBag } from '$app/common/interfaces/validation-bag';
 import { useClientsQuery } from '$app/common/queries/clients';
-import { ClientSelector } from '$app/components/clients/ClientSelector';
-import Toggle from '$app/components/forms/Toggle';
 import { atom, useAtomValue } from 'jotai';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { MdClose } from 'react-icons/md';
 
 interface Props {
     schedule: Schedule;
