fa47a0ba018b6714687c89289524cefe8730aa92
David Bomba <turbo124@gmail.com>
2023-12-11 15:04:17 +1100

    Add tests

--- tests/e2e/schedule-reports.spec.ts
+++ tests/e2e/schedule-reports.spec.ts
@@ -48,3 +48,87 @@ test("Activity test", async ({ page }) => {
             .filter({ hasText: 'Edit Schedule' })).toBeVisible();
 
 });
+
+test("Client/Customer test", async ({ page }) => {
+
+    await login(page);
+
+    await page
+        .locator('[data-cy="navigationBar"]')
+        .getByRole('link', { name: 'Reports', exact: true })
+        .click();
+
+    await page.locator('section').filter({ hasText: 'ActivityCustomerContactCreditDocumentExpenseInvoiceInvoice ItemPurchase' }).getByRole('combobox').selectOption('client');
+    await page.getByRole('button', { name: 'More Actions' }).click();
+    await page.getByRole('button', { name: 'Schedule' }).click();
+    await page.getByRole('button', { name: 'Save' }).click();
+
+    await expect(page.locator('section')
+        .filter({ hasText: 'ActivityCustomerContactCreditDocumentExpenseInvoiceInvoice ItemPurchase' })
+        .getByRole('combobox'))
+        .toBeVisible();
+
+    await expect(page.locator('section')
+        .filter({ hasText: 'ActivityCustomerContactCreditDocumentExpenseInvoiceInvoice ItemPurchase' })
+        .getByRole('combobox'))
+        .toContainText('Customer');
+
+
+    const selectedValue = await page.locator('section')
+        .filter({ hasText: 'ActivityCustomerContactCreditDocumentExpenseInvoiceInvoice ItemPurchase' })
+        .getByRole('combobox').textContent();
+
+    expect(selectedValue === 'Customer');
+
+    await page
+        .locator('[data-cy="topNavbar"]')
+        .getByRole('button', { name: 'Save', exact: true })
+        .click();
+
+    await expect(
+        page.locator('h2')
+            .filter({ hasText: 'Edit Schedule' })).toBeVisible();
+
+});
+
+test("Contact test", async ({ page }) => {
+
+    await login(page);
+
+    await page
+        .locator('[data-cy="navigationBar"]')
+        .getByRole('link', { name: 'Reports', exact: true })
+        .click();
+
+    await page.locator('section').filter({ hasText: 'ActivityCustomerContactCreditDocumentExpenseInvoiceInvoice ItemPurchase' }).getByRole('combobox').selectOption('contact');
+    await page.getByRole('button', { name: 'More Actions' }).click();
+    await page.getByRole('button', { name: 'Schedule' }).click();
+    await page.getByRole('button', { name: 'Save' }).click();
+
+    await expect(page.locator('section')
+        .filter({ hasText: 'ActivityCustomerContactCreditDocumentExpenseInvoiceInvoice ItemPurchase' })
+        .getByRole('combobox'))
+        .toBeVisible();
+
+    await expect(page.locator('section')
+        .filter({ hasText: 'ActivityCustomerContactCreditDocumentExpenseInvoiceInvoice ItemPurchase' })
+        .getByRole('combobox'))
+        .toContainText('Customer');
+
+
+    const selectedValue = await page.locator('section')
+        .filter({ hasText: 'ActivityCustomerContactCreditDocumentExpenseInvoiceInvoice ItemPurchase' })
+        .getByRole('combobox').textContent();
+
+    expect(selectedValue === 'Contact');
+
+    await page
+        .locator('[data-cy="topNavbar"]')
+        .getByRole('button', { name: 'Save', exact: true })
+        .click();
+
+    await expect(
+        page.locator('h2')
+            .filter({ hasText: 'Edit Schedule' })).toBeVisible();
+
+});
