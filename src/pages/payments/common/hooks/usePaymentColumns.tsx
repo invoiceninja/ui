e18b30e9f76e5439b826c1d12586cf4ce78d3507
Civolilah <abedinhalilovic12345@gmail.com>
2023-12-13 17:30:09 +0100

    Adjusted invoice_number column to show all invoices

--- src/pages/payments/common/hooks/usePaymentColumns.tsx
+++ src/pages/payments/common/hooks/usePaymentColumns.tsx
@@ -158,12 +158,19 @@ export function usePaymentColumns() {
       id: 'id',
       label: t('invoice_number'),
       format: (value, payment) => (
-        <DynamicLink
-          to={route('/invoices/:id/edit', { id: payment.invoices?.[0]?.id })}
-          renderSpan={disableNavigation('invoice', payment.invoices?.[0])}
-        >
-          {payment.invoices?.[0]?.number}
-        </DynamicLink>
+        <div className="flex space-x-2">
+          {payment.invoices?.map((invoice) => (
+            <DynamicLink
+              key={invoice.id}
+              to={route('/invoices/:id/edit', {
+                id: invoice.id,
+              })}
+              renderSpan={disableNavigation('invoice', invoice)}
+            >
+              {invoice.number}
+            </DynamicLink>
+          ))}
+        </div>
       ),
     },
     {
