3916d68ab42714e277b74a8a30f77ebd77418ea5
Civolilah <abedinhalilovic12345@gmail.com>
2023-12-27 01:34:52 +0100

    Core of the solution for select field adjustment

--- src/components/forms/SelectField.tsx
+++ src/components/forms/SelectField.tsx
@@ -11,8 +11,11 @@
 import classNames from 'classnames';
 import { Alert } from '$app/components/Alert';
 import { InputLabel } from '.';
-import CommonProps from '../../common/interfaces/common-props.interface';
 import { useColorScheme } from '$app/common/colors';
+import CommonProps from '$app/common/interfaces/common-props.interface';
+import React, { isValidElement } from 'react';
+import Select, { StylesConfig } from 'react-select';
+import { SelectOption } from '../datatables/Actions';
 
 interface Props extends CommonProps {
   defaultValue?: any;
@@ -24,50 +27,127 @@ interface Props extends CommonProps {
   blankOptionValue?: string | number;
 }
 
-export function SelectField(props: Props) {
+export function SelectField({
+  id,
+  value,
+  innerRef,
+  disabled,
+  defaultValue,
+  label,
+  required,
+  onValueChange,
+  onChange,
+  className,
+  style,
+  withBlank,
+  errorMessage,
+  cypressRef,
+  blankOptionValue,
+  children,
+}: Props) {
   const colors = useColorScheme();
 
+  const $entries = React.Children.map(
+    children,
+    (child) =>
+      isValidElement(child) && {
+        label: Array.isArray(child.props.children)
+          ? child.props.children.join('')
+          : child.props.children,
+        value: child.props.value,
+      }
+  );
+
+  const customStyles: StylesConfig<SelectOption, false> = {
+    input: (styles) => ({
+      ...styles,
+      color: colors.$3,
+    }),
+    singleValue: (styles) => ({
+      ...styles,
+      color: colors.$3,
+    }),
+    menu: (base) => ({
+      ...base,
+      width: 'max-content',
+      minWidth: '100%',
+      backgroundColor: colors.$4,
+      borderColor: colors.$4,
+    }),
+    control: (base, { isDisabled }) => ({
+      ...base,
+      borderRadius: '3px',
+      backgroundColor: colors.$1,
+      color: colors.$3,
+      borderColor: colors.$5,
+      cursor: isDisabled ? 'not-allowed' : 'pointer',
+      pointerEvents: isDisabled ? 'auto' : 'unset',
+    }),
+    option: (base, { isSelected, isFocused }) => ({
+      ...base,
+      color: colors.$3,
+      backgroundColor: isSelected || isFocused ? colors.$7 : colors.$1,
+      ':hover': {
+        backgroundColor: colors.$7,
+      },
+    }),
+  };
+
   return (
-    <section>
-      {props.label && (
-        <InputLabel className="mb-2" for={props.id}>
-          {props.label}
-          {props.required && <span className="ml-1 text-red-600">*</span>}
+    <div className="space-y-2">
+      {label && (
+        <InputLabel className="mb-2" for={id}>
+          {label}
+          {required && <span className="ml-1 text-red-600">*</span>}
         </InputLabel>
       )}
 
+      <Select
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        options={$entries}
+        value={value}
+        onChange={(v) => {
+          if (v === null) {
+            return onValueChange?.('');
+          }
+
+          return onValueChange?.(v.value as string);
+        }}
+        isDisabled={disabled}
+        styles={customStyles}
+      />
+
       <select
         onChange={(event) => {
-          props.onValueChange && props.onValueChange(event.target.value);
-          props.onChange && props.onChange(event);
+          onValueChange && onValueChange(event.target.value);
+          onChange && onChange(event);
         }}
-        id={props.id}
+        id={id}
         className={classNames(
-          `w-full py-2 rounded text-sm border disabled:cursor-not-allowed ${props.className}`
+          `w-full py-2 rounded text-sm border disabled:cursor-not-allowed ${className}`
         )}
-        defaultValue={props.defaultValue}
-        value={props.value}
-        ref={props.innerRef}
-        disabled={props.disabled}
+        defaultValue={defaultValue}
+        value={value}
+        ref={innerRef}
+        disabled={disabled}
         style={{
           backgroundColor: colors.$1,
           borderColor: colors.$5,
           color: colors.$3,
-          ...props.style,
+          ...style,
         }}
-        data-cy={props.cypressRef}
+        data-cy={cypressRef}
       >
-        {props.withBlank && (
-          <option value={props.blankOptionValue ?? ''}></option>
-        )}
-        {props.children}
+        {withBlank && <option value={blankOptionValue ?? ''}></option>}
+        {children}
       </select>
 
-      {props.errorMessage && (
+      {errorMessage && (
         <Alert className="mt-2" type="danger">
-          {props.errorMessage}
+          {errorMessage}
         </Alert>
       )}
-    </section>
+    </div>
   );
 }
