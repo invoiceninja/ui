fedbc2788506dae2eb814c9442eb2ed1d319fed4
Civolilah <abedinhalilovic12345@gmail.com>
2023-12-29 19:17:07 +0100

    Finished export part of the feature

--- src/pages/settings/import-export/common/components/Export.tsx
+++ src/pages/settings/import-export/common/components/Export.tsx
@@ -8,9 +8,14 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { endpoint } from '$app/common/helpers';
+import { request } from '$app/common/helpers/request';
+import { toast } from '$app/common/helpers/toast/toast';
+import { ValidationBag } from '$app/common/interfaces/validation-bag';
 import { Card, Element } from '$app/components/cards';
-import { SelectField } from '$app/components/forms';
-import { useState } from 'react';
+import { InputField, SelectField } from '$app/components/forms';
+import { AxiosError } from 'axios';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 type Date =
@@ -43,6 +48,20 @@ type ExportType =
   | 'tax_summary_report'
   | 'user_sales_report';
 
+interface Range {
+  identifier: string;
+  label: string;
+}
+
+interface Export {
+  date_key: Date;
+  date_range: string;
+  end_date: string;
+  report_keys: string[];
+  send_email: boolean;
+  start_date: string;
+}
+
 const EXPORTS_DATES: Record<ExportType, Date[]> = {
   activities: [],
   clients: ['created_at'],
@@ -66,14 +85,113 @@ const EXPORTS_DATES: Record<ExportType, Date[]> = {
   tax_summary_report: [],
   user_sales_report: [],
 };
+
+const DATE_RANGES: Range[] = [
+  {
+    identifier: 'last7',
+    label: 'last_7_days',
+  },
+  {
+    identifier: 'last30',
+    label: 'last_30_days',
+  },
+  {
+    identifier: 'this_month',
+    label: 'this_month',
+  },
+  {
+    identifier: 'last_month',
+    label: 'last_month',
+  },
+  {
+    identifier: 'this_quarter',
+    label: 'this_quarter',
+  },
+  {
+    identifier: 'last_quarter',
+    label: 'last_quarter',
+  },
+  {
+    identifier: 'this_year',
+    label: 'this_year',
+  },
+  { identifier: 'custom', label: 'custom' },
+];
+
+const DEFAULT_EXPORT: Export = {
+  date_key: '' as Date,
+  date_range: '',
+  end_date: '',
+  report_keys: [],
+  send_email: true,
+  start_date: '',
+};
 export function Export() {
   const [t] = useTranslation();
 
+  const [errors, setErrors] = useState<ValidationBag>();
+  const [isFormBusy, setIsFormBusy] = useState<boolean>(false);
   const [exportType, setExportType] = useState<ExportType>('activities');
-  const [dateKey, setDateKey] = useState<Date>();
+  const [selectedExport, setSelectedExport] = useState<Export>(DEFAULT_EXPORT);
+
+  const handleChange = (
+    value: string | Date | boolean,
+    property: keyof Export
+  ) => {
+    if (property === 'date_key') {
+      setSelectedExport((current) => ({
+        ...current,
+        date_key: value as Date,
+        date_range: value ? 'last7' : '',
+      }));
+    } else if (property === 'date_range') {
+      setSelectedExport((current) => ({
+        ...current,
+        date_range: value as string,
+        start_date: value === 'custom' ? current.start_date : '',
+        end_date: value === 'custom' ? current.end_date : '',
+      }));
+    } else {
+      setSelectedExport((current) => ({
+        ...current,
+        [property]: value,
+      }));
+    }
+  };
+
+  const handleExport = () => {
+    if (!isFormBusy) {
+      toast.processing();
+      setIsFormBusy(true);
+
+      request('POST', endpoint(`/api/v1/reports/${exportType}`), selectedExport)
+        .then(() => toast.success('exported_data'))
+        .catch((error: AxiosError<ValidationBag>) => {
+          if (error.response?.status === 422) {
+            toast.dismiss();
+            setErrors(error.response.data);
+          }
+        })
+        .finally(() => setIsFormBusy(false));
+    }
+  };
+
+  useEffect(() => {
+    setSelectedExport(DEFAULT_EXPORT);
+  }, [exportType]);
 
   return (
-    <Card withSaveButton saveButtonLabel={t('export')} title={t('export')}>
+    <Card
+      title={t('export')}
+      saveButtonLabel={t('export')}
+      withSaveButton
+      onSaveClick={(event) => {
+        event.preventDefault();
+        handleExport();
+      }}
+      disableSubmitButton={isFormBusy}
+      disableWithoutIcon
+    >
       <Element leftSide={t('export')}>{t('csv')}</Element>
 
       <Element leftSide={t('export_type')}>
@@ -92,9 +210,10 @@ export function Export() {
       {Boolean(EXPORTS_DATES[exportType].length) && (
         <Element leftSide={t('date')}>
           <SelectField
-            value={dateKey || ''}
-            onValueChange={(value) => setDateKey(value as Date)}
+            value={selectedExport.date_key}
+            onValueChange={(value) => handleChange(value, 'date_key')}
             withBlank
+            errorMessage={errors?.errors.date_key}
           >
             {EXPORTS_DATES[exportType].map((date, index) => (
               <option key={index} value={date}>
@@ -103,6 +222,44 @@ export function Export() {
             ))}
           </SelectField>
         </Element>
+      )}
+
+      {selectedExport.date_key && (
+        <Element leftSide={t('date_range')}>
+          <SelectField
+            value={selectedExport.date_range}
+            onValueChange={(value) => handleChange(value, 'date_range')}
+            errorMessage={errors?.errors.date_range}
+          >
+            {DATE_RANGES.map(({ identifier, label }, index) => (
+              <option key={index} value={identifier}>
+                {t(label)}
+              </option>
+            ))}
+          </SelectField>
+        </Element>
+      )}
+
+      {selectedExport.date_range === 'custom' && (
+        <>
+          <Element leftSide={t('start_date')}>
+            <InputField
+              type="date"
+              value={selectedExport.start_date}
+              onValueChange={(value) => handleChange(value, 'start_date')}
+              errorMessage={errors?.errors.start_date}
+            />
+          </Element>
+
+          <Element leftSide={t('end_date')}>
+            <InputField
+              type="date"
+              value={selectedExport.end_date}
+              onValueChange={(value) => handleChange(value, 'end_date')}
+              errorMessage={errors?.errors.end_date}
+            />
+          </Element>
+        </>
       )}
     </Card>
   );
