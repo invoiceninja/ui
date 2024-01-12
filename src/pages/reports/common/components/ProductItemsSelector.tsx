dfa33ccd89b181d11f124fa6b4e1d19101f36dbf
Civolilah <abedinhalilovic12345@gmail.com>
2024-01-12 10:17:46 +0100

    Fixed bug with updating products list when query is invalidated

--- src/pages/reports/common/components/ProductItemsSelector.tsx
+++ src/pages/reports/common/components/ProductItemsSelector.tsx
@@ -34,7 +34,7 @@ export function ProductItemsSelector(props: Props) {
   const { data: products } = useProductsQuery({ status: ['active'] });
 
   useEffect(() => {
-    if (products && !productItems) {
+    if (products) {
       setProductItems(
         products.map((product) => ({
           value: product.product_key,
