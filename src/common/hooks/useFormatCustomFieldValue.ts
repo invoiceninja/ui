3c41efff91a7e85bd4e4f0e6c806f9d8e084363c
Civolilah <abedinhalilovic12345@gmail.com>
2024-01-17 20:40:07 +0100

    Created hook for formatting values for custom fields

---
+++ src/common/hooks/useFormatCustomFieldValue.ts
@@ -0,0 +1,32 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { useCurrentCompany } from './useCurrentCompany';
+import { useCurrentCompanyDateFormats } from './useCurrentCompanyDateFormats';
+import { date } from '$app/common/helpers';
+
+export function useFormatCustomFieldValue() {
+  const company = useCurrentCompany();
+  const { dateFormat } = useCurrentCompanyDateFormats();
+
+  const customFields = company?.custom_fields;
+
+  return (fieldKey: string, value: string) => {
+    if (customFields && customFields[fieldKey]) {
+      const currentField = customFields[fieldKey];
+
+      if (currentField.split('|')[1] === 'date') {
+        return date(value, dateFormat);
+      }
+    }
+
+    return value;
+  };
+}
