9c82a92db41a45f6e08e43363750d46b48a595a8
Civolilah <abedinhalilovic12345@gmail.com>
2024-01-18 05:12:16 +0100

    Formatted solution

--- src/common/hooks/useFormatCustomFieldValue.ts
+++ src/common/hooks/useFormatCustomFieldValue.ts
@@ -11,19 +11,26 @@
 import { useCurrentCompany } from './useCurrentCompany';
 import { useCurrentCompanyDateFormats } from './useCurrentCompanyDateFormats';
 import { date } from '$app/common/helpers';
+import { useTranslation } from 'react-i18next';
+import { AvailableTypes } from '$app/pages/settings/custom-fields/components';
 
 export function useFormatCustomFieldValue() {
+  const [t] = useTranslation();
   const company = useCurrentCompany();
   const { dateFormat } = useCurrentCompanyDateFormats();
 
   const customFields = company?.custom_fields;
 
   return (fieldKey: string, value: string) => {
-    if (customFields && customFields[fieldKey] && value) {
+    if (customFields && customFields[fieldKey]) {
       const currentField = customFields[fieldKey];
 
-      if (currentField.split('|')[1] === 'date') {
+      if (currentField?.split('|')[1] === AvailableTypes.Date && value) {
         return date(value, dateFormat);
+      }
+
+      if (currentField?.split('|')[1] === AvailableTypes.Switch) {
+        return value ? t('yes') : t('no');
       }
     }
 
