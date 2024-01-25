61861c9f239782f6df430e516cf9b3220999d470
Civolilah <abedinhalilovic12345@gmail.com>
2024-01-25 23:34:19 +0100

    Fixed vendor document uploading test

--- tests/e2e/vendors.spec.ts
+++ tests/e2e/vendors.spec.ts
@@ -487,13 +487,11 @@ test('vendor documents uploading with edit_vendor', async ({ page }) => {
 
   if (!doRecordsExist) {
     await createVendor({ page });
-
-    await checkShowPage(page, true);
   } else {
     await tableRow.getByRole('link').first().click();
-
-    await checkShowPage(page, false);
   }
+
+  await checkShowPage(page, true);
 
   await page
     .getByRole('link', {
