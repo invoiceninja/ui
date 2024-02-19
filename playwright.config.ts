0f6f729621f45cc177aef8b6c37f11a7c5e0dd38
Civolilah <abedinhalilovic12345@gmail.com>
2024-02-19 23:43:27 +0100

    Changed webServer configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -94,7 +94,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run preview',
-    port: 9323,
-    reuseExistingServer: true,
+    port: 4173,
+    reuseExistingServer: !process.env.REUSE_SERVER,
   },
 });
