53aeea6643f06868437a57fa7c23bec15d023c73
Civolilah <abedinhalilovic12345@gmail.com>
2024-02-19 23:12:27 +0100

    Deleted unnecessary console log

--- src/pages/tasks/index/Tasks.tsx
+++ src/pages/tasks/index/Tasks.tsx
@@ -52,8 +52,6 @@ export default function Tasks() {
 
   const showEditOption = useShowEditOption();
 
-  //testing actions
-
   return (
     <Default title={documentTitle} breadcrumbs={pages} withoutBackButton>
       <DataTable
