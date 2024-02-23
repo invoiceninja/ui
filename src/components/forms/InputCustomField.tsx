08a1b09c3faa4d390388bd52fd627ae9c07433ec
Civolilah <abedinhalilovic12345@gmail.com>
2024-02-23 13:11:23 +0100

    Changed select field custom prop

--- src/components/forms/InputCustomField.tsx
+++ src/components/forms/InputCustomField.tsx
@@ -80,7 +80,7 @@ export function InputCustomField(props: Props) {
       {!Object.values(AvailableTypes).includes(type as AvailableTypes) && (
         <SelectField
           style={{ color: colors.$3, colorScheme: colors.$0 }}
-          defaultValue={props.defaultValue || ''}
+          value={props.defaultValue || ''}
           onChange={(event: ChangeEvent<HTMLSelectElement>) =>
             props.onValueChange(event.target.value)
           }
