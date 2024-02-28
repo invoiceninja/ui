83ac8224d52f311b0b0247b4b4e9cee4a9d957cc
Civolilah <abedinhalilovic12345@gmail.com>
2024-02-28 02:02:17 +0100

    Returned changes

--- src/components/AboutModal.tsx
+++ src/components/AboutModal.tsx
@@ -62,7 +62,6 @@ interface SystemInfo {
   trailing_slash: boolean;
   file_permissions: string;
   exchange_rate_api_not_configured: boolean;
-  api_version: string;
 }
 
 interface Props {
@@ -331,9 +330,6 @@ export function AboutModal(props: Props) {
                 {t('cli')}: {systemInfo?.php_version.current_php_cli_version}
               </span>
               <span>Memory: {systemInfo?.php_version.memory_limit}</span>
-              <span>
-                {t('api_version')}: {systemInfo?.api_version}
-              </span>
             </div>
 
             <div>
