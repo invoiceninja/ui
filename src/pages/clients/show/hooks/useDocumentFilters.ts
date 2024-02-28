e0059c4be1639ba44db9f56935f2ce6a939c1ba2
Civolilah <abedinhalilovic12345@gmail.com>
2024-02-28 19:07:39 +0100

    Changed query key for custom filters

--- src/pages/clients/show/hooks/useDocumentFilters.ts
+++ src/pages/clients/show/hooks/useDocumentFilters.ts
@@ -20,36 +20,42 @@ export function useDocumentFilters() {
       value: 'all',
       color: 'black',
       backgroundColor: '#e4e4e4',
+      queryKey: 'type',
     },
     {
       label: t('public'),
       value: 'public',
       color: 'white',
       backgroundColor: '#6B7280',
+      queryKey: 'type',
     },
     {
       label: t('private'),
       value: 'private',
       color: 'white',
       backgroundColor: '#93C5FD',
+      queryKey: 'type',
     },
     {
       label: t('image'),
       value: 'image',
       color: 'white',
       backgroundColor: '#1D4ED8',
+      queryKey: 'type',
     },
     {
       label: t('pdf'),
       value: 'pdf',
       color: 'white',
       backgroundColor: '#22C55E',
+      queryKey: 'type',
     },
     {
       label: t('other'),
       value: 'other',
       color: 'white',
       backgroundColor: '#9CA3AF',
+      queryKey: 'type',
     },
   ];
 
