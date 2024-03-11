cb080efb29ec223e3531a75b2eb16b79c5d0021d
Civolilah <abedinhalilovic12345@gmail.com>
2024-03-11 19:00:16 +0100

    Implemented purchase_page columns for payment links

--- src/pages/settings/subscriptions/common/hooks/useSubscriptionColumns.tsx
+++ src/pages/settings/subscriptions/common/hooks/useSubscriptionColumns.tsx
@@ -11,7 +11,9 @@
 import { useFormatMoney } from '$app/common/hooks/money/useFormatMoney';
 import { useCurrentCompany } from '$app/common/hooks/useCurrentCompany';
 import { Subscription } from '$app/common/interfaces/subscription';
+import { CopyToClipboardIconOnly } from '$app/components/CopyToClipBoardIconOnly';
 import { DataTableColumns } from '$app/components/DataTable';
+import { Link } from '$app/components/forms';
 import { useTranslation } from 'react-i18next';
 
 export const useSubscriptionColumns = () => {
@@ -35,6 +37,22 @@ export const useSubscriptionColumns = () => {
           company?.settings.country_id,
           company?.settings.currency_id
         ),
+    },
+    {
+      id: 'purchase_page',
+      label: t('purchase_page'),
+      format: (value) => (
+        <div
+          className="flex space-x-2"
+          onClick={(event) => event.stopPropagation()}
+        >
+          <Link to={value as string} external>
+            {t('purchase_page')}
+          </Link>
+
+          <CopyToClipboardIconOnly text={value as string} />
+        </div>
+      ),
     },
   ];
 
