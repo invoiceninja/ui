409b1c29ee8d94de489b69e4cdb094a7fd16030b
Civolilah <abedinhalilovic12345@gmail.com>
2024-03-20 22:39:29 +0100

    Added palette color

--- src/components/forms/SelectField.tsx
+++ src/components/forms/SelectField.tsx
@@ -13,6 +13,9 @@ import { Alert } from '$app/components/Alert';
 import { InputLabel } from '.';
 import CommonProps from '../../common/interfaces/common-props.interface';
 import { useColorScheme } from '$app/common/colors';
+import React, { ReactNode, isValidElement } from 'react';
+import { SelectOption } from '../datatables/Actions';
+import Select, { StylesConfig } from 'react-select';
 
 export interface SelectProps extends CommonProps {
   defaultValue?: any;
@@ -22,13 +25,83 @@ export interface SelectProps extends CommonProps {
   onValueChange?: (value: string) => unknown;
   errorMessage?: string | string[];
   blankOptionValue?: string | number;
+  customSelector?: boolean;
 }
 
 export function SelectField(props: SelectProps) {
   const colors = useColorScheme();
 
+  const {
+    blankOptionValue,
+    withBlank,
+    children,
+    value,
+    defaultValue,
+    customSelector,
+    onValueChange,
+    className,
+    disabled,
+    cypressRef,
+  } = props;
+
+  const blankEntry: ReactNode = (
+    <option value={blankOptionValue ?? ''}></option>
+  );
+
+  const $entries = React.Children.map(
+    [withBlank ? blankEntry : [], children],
+    (child) =>
+      isValidElement(child) && {
+        label: Array.isArray(child.props.children)
+          ? child.props.children.join('')
+          : child.props.children,
+        value: child.props.value,
+      }
+  );
+
+  const selectedEntry = $entries?.find((entry) => entry.value === value);
+  const defaultEntry = $entries?.find((entry) => entry.value === defaultValue);
+
+  const customStyles: StylesConfig<SelectOption, false> = {
+    input: (styles) => ({
+      ...styles,
+      color: colors.$3,
+    }),
+    singleValue: (styles) => ({
+      ...styles,
+      color: colors.$3,
+    }),
+    menu: (base) => ({
+      ...base,
+      width: 'max-content',
+      minWidth: '100%',
+      backgroundColor: colors.$4,
+      borderColor: colors.$4,
+    }),
+    control: (base, { isDisabled }) => ({
+      ...base,
+      borderRadius: '3px',
+      backgroundColor: colors.$1,
+      color: colors.$3,
+      borderColor: colors.$5,
+      cursor: isDisabled ? 'not-allowed' : 'pointer',
+      pointerEvents: isDisabled ? 'auto' : 'unset',
+    }),
+    option: (base, { isSelected, isFocused }) => ({
+      ...base,
+      display: 'flex',
+      alignItems: 'center',
+      color: colors.$3,
+      backgroundColor: isSelected || isFocused ? colors.$7 : colors.$1,
+      ':hover': {
+        backgroundColor: colors.$7,
+      },
+      minHeight: '1.875rem',
+    }),
+  };
+
   return (
-    <section>
+    <div className="space-y-2">
       {props.label && (
         <InputLabel className="mb-2" for={props.id}>
           {props.label}
@@ -36,38 +109,63 @@ export function SelectField(props: SelectProps) {
         </InputLabel>
       )}
 
-      <select
-        onChange={(event) => {
-          props.onValueChange && props.onValueChange(event.target.value);
-          props.onChange && props.onChange(event);
-        }}
-        id={props.id}
-        className={classNames(
-          `w-full py-2 rounded text-sm border disabled:cursor-not-allowed ${props.className}`
-        )}
-        defaultValue={props.defaultValue}
-        value={props.value}
-        ref={props.innerRef}
-        disabled={props.disabled}
-        style={{
-          backgroundColor: colors.$1,
-          borderColor: colors.$5,
-          color: colors.$3,
-          ...props.style,
-        }}
-        data-cy={props.cypressRef}
-      >
-        {props.withBlank && (
-          <option value={props.blankOptionValue ?? ''}></option>
-        )}
-        {props.children}
-      </select>
+      {!customSelector ? (
+        <select
+          onChange={(event) => {
+            props.onValueChange && props.onValueChange(event.target.value);
+            props.onChange && props.onChange(event);
+          }}
+          id={props.id}
+          className={classNames(
+            `w-full py-2 rounded text-sm border disabled:cursor-not-allowed ${props.className}`
+          )}
+          defaultValue={props.defaultValue}
+          value={props.value}
+          ref={props.innerRef}
+          disabled={props.disabled}
+          style={{
+            backgroundColor: colors.$1,
+            borderColor: colors.$5,
+            color: colors.$3,
+            ...props.style,
+          }}
+          data-cy={props.cypressRef}
+        >
+          {props.withBlank && (
+            <option value={props.blankOptionValue ?? ''}></option>
+          )}
+          {props.children}
+        </select>
+      ) : (
+        <Select
+          className={className}
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          options={$entries}
+          defaultValue={defaultEntry}
+          value={selectedEntry}
+          onChange={(v) => {
+            if (v === null) {
+              return onValueChange?.((blankOptionValue as string) ?? '');
+            }
+
+            return onValueChange?.(v.value as string);
+          }}
+          isDisabled={disabled}
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          styles={customStyles}
+          isSearchable={false}
+          isClearable
+          data-cy={cypressRef}
+        />
+      )}
 
       {props.errorMessage && (
         <Alert className="mt-2" type="danger">
           {props.errorMessage}
         </Alert>
       )}
-    </section>
+    </div>
   );
 }
--- src/pages/settings/user/components/StatusColorTheme.tsx
+++ src/pages/settings/user/components/StatusColorTheme.tsx
@@ -10,34 +10,143 @@
 
 import { Element } from '$app/components/cards';
 import { SelectField } from '$app/components/forms';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-type ThemeKey = 'light' | 'dark' | 'cerulean';
+type ThemeKey =
+  | 'light'
+  | 'dark'
+  | 'cerulean'
+  | 'cosmo'
+  | 'cyborg'
+  | 'darkly'
+  | 'flatly'
+  | 'journal'
+  | 'litera'
+  | 'lumen'
+  | 'lux'
+  | 'materia'
+  | 'minty'
+  | 'pulse'
+  | 'sandstone'
+  | 'simplex'
+  | 'sketchy'
+  | 'slate'
+  | 'solar'
+  | 'spacelab'
+  | 'superhero'
+  | 'united'
+  | 'yeti';
 
 interface Theme {
   palette: string[];
 }
 const COLOR_THEMES: Record<ThemeKey, Theme> = {
   light: {
-    palette: [''],
+    palette: ['#58a6e4', '#324ea1', '#4c9a1d', '#cd8900', '#b83700'],
   },
   dark: {
-    palette: [''],
+    palette: ['#298aaa', '#0c45a3', '#0c45a3', '#a87001', '#8b3c40'],
   },
   cerulean: {
-    palette: [''],
+    palette: ['#043c73', '#2fa3e7', '#74a739', '#dd5601', '#c71b22'],
+  },
+  cosmo: {
+    palette: ['#9954bc', '#2680e3', '#3db616', '#ff7518', '#ff0039'],
+  },
+  cyborg: {
+    palette: ['#9933cc', '#299fd6', '#76b400', '#ff8802', '#cc0100'],
+  },
+  darkly: {
+    palette: ['#3498dc', '#375a7f', '#00bc8c', '#f29c13', '#e74b3c'],
+  },
+  flatly: {
+    palette: ['#3498dc', '#2c3f51', '#12bd9d', '#f29c13', '#e74b3c'],
+  },
+  journal: {
+    palette: ['#346599', '#eb6864', '#1fb34d', '#f6e524', '#f57900'],
+  },
+  litera: {
+    palette: ['#1aa1b8', '#4581eb', '#00b975', '#f0ad4e', '#d9534f'],
+  },
+  lumen: {
+    palette: ['#75caeb', '#158cba', '#29b72b', '#ff851b', '#ff4136'],
+  },
+  lux: {
+    palette: ['#209bcf', '#1a1a1a', '#4ac073', '#f0ad4e', '#d9534f'],
+  },
+  materia: {
+    palette: ['#9c27b0', '#2196f3', '#4dae51', '#ff9800', '#e61d23'],
+  },
+  minty: {
+    palette: ['#6cc3d6', '#78c2ad', '#55cc9d', '#ffce67', '#ff7852'],
+  },
+  pulse: {
+    palette: ['#009cdd', '#583196', '#0fba54', '#efa31b', '#fc3938'],
+  },
+  sandstone: {
+    palette: ['#2aaae0', '#335d87', '#94c44b', '#f37c3d', '#d9534f'],
+  },
+  simplex: {
+    palette: ['#009acf', '#d9230d', '#479505', '#d9831f', '#9c479f'],
+  },
+  sketchy: {
+    palette: ['#1aa1b8', '#333333', '#29a645', '#ffc008', '#db3546'],
+  },
+  slate: {
+    palette: ['#5ac0de', '#3a3f43', '#62c362', '#f89407', '#ee5f5a'],
+  },
+  solar: {
+    palette: ['#258bd2', '#b58802', '#2aa198', '#cb4a15', '#d33582'],
+  },
+  spacelab: {
+    palette: ['#3199f3', '#456e9c', '#3cb521', '#d47500', '#cd0300'],
+  },
+  superhero: {
+    palette: ['#cd0300', '#df6919', '#5bb85b', '#f0ad4e', '#d9534f'],
+  },
+  united: {
+    palette: ['#1aa1b8', '#e9551f', '#38b549', '#eeb83e', '#de382c'],
+  },
+  yeti: {
+    palette: ['#5ac0de', '#008cba', '#44ab6a', '#ea9005', '#f14125'],
   },
 };
 
 export function StatusColorTheme() {
   const [t] = useTranslation();
 
+  const [selectedTheme, setSelectedTheme] = useState<ThemeKey>('light');
+
   return (
     <Element leftSide={t('status_color_theme')}>
-      <SelectField>
+      <SelectField
+        value={selectedTheme}
+        onValueChange={(value) => setSelectedTheme(value as ThemeKey)}
+        customSelector
+      >
         {Object.keys(COLOR_THEMES).map((themeKey, index) => (
           <option key={index} value={themeKey}>
-            {t(themeKey)}
+            <div className="flex w-full space-x-2">
+              <span className="flex w-1/4 capitalize truncate">
+                {t(themeKey)}
+              </span>
+
+              <div className="flex">
+                {COLOR_THEMES[
+                  themeKey as keyof typeof COLOR_THEMES
+                ].palette.map((paletteColor) => (
+                  <div
+                    key={paletteColor}
+                    style={{
+                      backgroundColor: paletteColor,
+                      width: 50,
+                      height: 20,
+                    }}
+                  />
+                ))}
+              </div>
+            </div>
           </option>
         ))}
       </SelectField>
