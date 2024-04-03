3dfac73955ff770d119946a941f341c69f620a73
David Bomba <turbo124@gmail.com>
2024-04-03 14:05:52 +1100

    Stubs for e-invoicing

---
+++ src/common/interfaces/edoc/unit-of-measure-code.ts
@@ -0,0 +1,42 @@
+interface RootObject {
+    CodeList: CodeList;
+}
+interface CodeList {
+    Identification: Identification;
+    ColumnSet: ColumnSet;
+    SimpleCodeList: SimpleCodeList;
+}
+interface SimpleCodeList {
+    Row: Row[];
+}
+interface Row {
+    Value: Value[];
+}
+interface Value {
+    SimpleValue: number | string | string;
+}
+interface ColumnSet {
+    Column: Column[];
+    Key: Key;
+}
+interface Key {
+    ShortName: string;
+    ColumnRef: string;
+}
+interface Column {
+    ShortName: string;
+    Data: string;
+}
+interface Identification {
+    ShortName: string;
+    LongName: string[];
+    Version: string;
+    CanonicalUri: string;
+    CanonicalVersionUri: string;
+    LocationUri: string;
+    Agency: Agency;
+}
+interface Agency {
+    LongName: string;
+    Identifier: number;
+}
