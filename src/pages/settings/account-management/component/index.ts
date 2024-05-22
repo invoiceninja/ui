cd2b8178b3b04baabf4a3d0eac72d93e985d7323
Civolilah <abedinhalilovic12345@gmail.com>
2024-05-22 21:30:39 +0200

    Implementing referral program

--- src/pages/settings/account-management/component/index.ts
+++ src/pages/settings/account-management/component/index.ts
@@ -14,3 +14,4 @@ export { Overview as AccountManagementOverview } from './Overview';
 export * from './Plan';
 export * from './SecuritySettings';
 export * from './DangerZone';
+export * from './ReferralProgram';
