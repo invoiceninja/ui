9457574077703a6580f00c59e5b7b375a642f2fc
Benjamin Beganović <k1pstabug@gmail.com>
2024-06-14 19:17:06 +0200

    Added OAuth 2.0 support and test mode toggle for GoCardless gateway in settings page

--- src/pages/settings/gateways/create/components/gateways/GoCardlessOAuth2.tsx
+++ src/pages/settings/gateways/create/components/gateways/GoCardlessOAuth2.tsx
@@ -12,20 +12,38 @@ import { Element } from '$app/components/cards';
 import { useTranslation } from 'react-i18next';
 import { useResolveConfigValue } from '../../hooks/useResolveConfigValue';
 import { CompanyGateway } from '$app/common/interfaces/company-gateway';
-import { Check, X } from 'react-feather';
+import { Field } from '$app/pages/settings/gateways/create/hooks/useResolveInputField';
+import Toggle from '$app/components/forms/Toggle';
+import { Check } from 'react-feather';
+import { useHandleCredentialsChange } from '$app/pages/settings/gateways/create/hooks/useHandleCredentialsChange';
+import { isHosted } from '$app/common/helpers';
+import { Button } from '$app/components/forms';
+import { useHandleGoCardless } from '$app/pages/settings/gateways/create/hooks/useHandleGoCardless';
 
 interface Props {
   companyGateway: CompanyGateway;
+  setCompanyGateway: React.Dispatch<
+    React.SetStateAction<CompanyGateway | undefined>
+  >
 }
 
-export function GoCardlessOAuth2({ companyGateway }: Props) {
+export function GoCardlessOAuth2({ companyGateway, setCompanyGateway }: Props) {
   const { t } = useTranslation();
+
   const config = useResolveConfigValue(companyGateway);
+  const handleChange = useHandleCredentialsChange(setCompanyGateway);
 
   return (
     <>
+      <Element leftSide={t('OAuth 2.0')}>
+        <Check size={18} />
+      </Element>
+
       <Element leftSide={t('test_mode')}>
-        {config('livemode') ? <X size={18} /> : <Check size={18} />}
+        <Toggle
+          checked={config('testMode')}
+          onChange={(value) => handleChange('testMode' as keyof Field, value)}
+        />
       </Element>
     </>
   );
