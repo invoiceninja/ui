a2bdb9efc449b1ca15eed83e4240ffa534b03d52
David Bomba <turbo124@gmail.com>
2024-07-30 09:17:53 +1000

    POC websockets

--- src/index.tsx
+++ src/index.tsx
@@ -33,11 +33,7 @@ import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
 import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
 import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
 import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
-import { apiEndpoint } from '$app/common/helpers';
-import { defaultHeaders } from '$app/common/queries/common/headers';
-
-import Pusher from 'pusher-js';
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';2
 
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_URL as unknown as string,
