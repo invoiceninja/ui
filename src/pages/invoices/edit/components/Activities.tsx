a3424a66775becbe4c280f7152717311a089471d
Civolilah <abedinhalilovic12345@gmail.com>
2024-08-07 11:49:28 +0200

    Fixed way of displaying activities on invoice edit page

--- src/pages/invoices/edit/components/Activities.tsx
+++ src/pages/invoices/edit/components/Activities.tsx
@@ -8,78 +8,19 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { date, endpoint, trans } from '$app/common/helpers';
+import { date, endpoint } from '$app/common/helpers';
 import { request } from '$app/common/helpers/request';
-import { route } from '$app/common/helpers/route';
 import { GenericManyResponse } from '$app/common/interfaces/generic-many-response';
 import { InvoiceActivity } from '$app/common/interfaces/invoice-activity';
 import { NonClickableElement } from '$app/components/cards/NonClickableElement';
-import { Link } from '$app/components/forms';
 import { AxiosResponse } from 'axios';
 import { useTranslation } from 'react-i18next';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import { useCurrentCompanyDateFormats } from '$app/common/hooks/useCurrentCompanyDateFormats';
 import { Card } from '$app/components/cards';
-import reactStringReplace from 'react-string-replace';
 import { Spinner } from '$app/components/Spinner';
-
-export function useGenerateActivityElement() {
-  const [t] = useTranslation();
-
-  return (activity: InvoiceActivity) => {
-    let text = trans(`activity_${activity.activity_type_id}`, {});
-
-    const replacements = {
-      client: (
-        <Link to={route('/clients/:id', { id: activity.client?.hashed_id })}>
-          {activity.client?.label}
-        </Link>
-      ),
-
-      user: activity.user?.label ?? t('system'),
-      invoice:
-        (
-          <Link
-            to={route('/invoices/:id/edit', {
-              id: activity.invoice?.hashed_id,
-            })}
-          >
-            {activity?.invoice?.label}
-          </Link>
-        ) ?? '',
-
-      recurring_invoice:
-        (
-          <Link
-            to={route('/recurring_invoices/:id/edit', {
-              id: activity?.recurring_invoice?.hashed_id,
-            })}
-          >
-            {activity?.recurring_invoice?.label}
-          </Link>
-        ) ?? '',
-
-      contact:
-        (
-          <Link
-            to={route('/clients/:id/edit', {
-              id: activity?.contact?.hashed_id,
-            })}
-          >
-            {activity?.contact?.label}
-          </Link>
-        ) ?? '',
-    };
-    for (const [variable, value] of Object.entries(replacements)) {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      text = reactStringReplace(text, `:${variable}`, () => value);
-    }
-
-    return text;
-  };
-}
+import { useGenerateActivityElement } from '../../common/components/InvoiceSlider';
 
 export default function Activities() {
   const [t] = useTranslation();
