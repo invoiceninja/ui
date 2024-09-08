c21b933c690f664b8be55b63b853d07fb59eb513
Civolilah <abedinhalilovic12345@gmail.com>
2024-09-08 20:02:56 +0200

    Removed

--- src/index.tsx
+++ src/index.tsx
@@ -90,19 +90,17 @@ loader.init().then(/* ... */);
 const container = document.getElementById('root') as HTMLElement;
 
 createRoot(container).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <GoogleOAuth>
-          <Router>
-            <ScrollToTop>
-              <App />
-            </ScrollToTop>
-          </Router>
-        </GoogleOAuth>
-      </Provider>
-    </QueryClientProvider>
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <GoogleOAuth>
+        <Router>
+          <ScrollToTop>
+            <App />
+          </ScrollToTop>
+        </Router>
+      </GoogleOAuth>
+    </Provider>
+  </QueryClientProvider>
 );
 
 export const emitter = mitt<Events>();
