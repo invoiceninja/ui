4898d56ab3b61a8597c158047ac3837e0321e68b
Benjamin Beganović <k1pstabug@gmail.com>
2024-09-18 16:33:46 +0200

    useSocketEvent

--- src/common/queries/sockets.ts
+++ src/common/queries/sockets.ts
@@ -15,21 +15,65 @@ import { $refetch } from '../hooks/useRefetch';
 
 // This file defines global events system for query invalidation.
 
+type Callbacks = Record<string, (data: unknown) => unknown>;
+
 export function useGlobalSocketEvents() {
   const sockets = useSockets();
   const company = useCurrentCompany();
+
+  const callbacks: Callbacks = {
+    'client.archived': () => console.log('Client was archived'),
+    'invoice.paid': () => {
+      $refetch(['invoices']);
+    },
+  };
 
   useEffect(() => {
     if (!sockets || !company) {
       return;
     }
 
-    sockets.connection.bind('connected', () => {
-      const invoices = sockets.subscribe(`${company.company_key}.invoices`);
-      
-      invoices.bind('invoice.paid', () => $refetch(['invoices']));
+    const channel = sockets.subscribe(`private-company-${company.company_key}`);
+
+    channel.bind_global((eventName: string, data: unknown) => {
+      console.log(`channel: ${eventName}`);
+
+      const callback = callbacks[eventName];
+
+      if (callback) {
+        callback(data);
+
+        window.dispatchEvent(
+          new CustomEvent(`pusher::${eventName}`, {
+            detail: data,
+          })
+        );
+      }
     });
   }, [sockets, company]);
 
   return null;
+}
+
+export interface SocketEventProps<T> {
+  name: string;
+  callback: (data: T) => unknown;
+}
+
+export function useSocketEvent<T>({ name, callback }: SocketEventProps<T>) {
+  useEffect(() => {
+    const eventHandler = (event: CustomEvent<T>) => {
+      callback(event.detail);
+      console.log({ name });
+    };
+
+    window.addEventListener(`pusher::${name}`, eventHandler as EventListener);
+
+    return () => {
+      window.removeEventListener(
+        `pusher::${name}`,
+        eventHandler as EventListener
+      );
+    };
+  }, [name, callback]);
 }
