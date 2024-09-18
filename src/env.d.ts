0376a0375a86b1d5c5c2c8fbc5e167c341d5c08f
Benjamin Beganović <k1pstabug@gmail.com>
2024-09-18 16:33:29 +0200

    remove env variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -13,8 +13,6 @@ interface ImportMetaEnv extends Readonly<Record<string, string>> {
   readonly VITE_IS_PRODUCTION: string;
   readonly VITE_WHITELABEL_INVOICE_URL: string;
   readonly VITE_PUSHER_APP_KEY: string;
-  readonly VITE_PUSHER_URL: string;
-  readonly VITE_PUSHER_PORT: string;
 }
 
 interface ImportMeta {
