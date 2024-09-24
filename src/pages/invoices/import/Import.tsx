478a1e8ada4df300077c0d38213fc6b01d0f0997
Benjamin Beganović <k1pstabug@gmail.com>
2024-09-24 19:19:10 +0200

    add help widget

--- src/pages/invoices/import/Import.tsx
+++ src/pages/invoices/import/Import.tsx
@@ -8,10 +8,13 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { useAccentColor } from '$app/common/hooks/useAccentColor';
 import { useTitle } from '$app/common/hooks/useTitle';
 import { Page } from '$app/components/Breadcrumbs';
+import { $help, HelpWidget } from '$app/components/HelpWidget';
 import { UploadImport } from '$app/components/import/UploadImport';
 import { Default } from '$app/components/layouts/Default';
+import { HelpCircle } from 'react-feather';
 import { useTranslation } from 'react-i18next';
 
 export default function Import() {
@@ -23,11 +26,33 @@ export default function Import() {
     { name: t('import'), href: '/invoices/import' },
   ];
 
+  const accentColor = useAccentColor();
+
   return (
     <Default title={documentTitle} breadcrumbs={pages}>
       <div className="grid grid-cols-12">
-        <div className="col-span-12 xl:col-span-10">
-          <UploadImport entity="invoice" onSuccess={false} type="csv" />
+        <div className="col-span-12 xl:col-span-8">
+          <UploadImport
+            entity="invoice"
+            onSuccess={false}
+            type="csv"
+            postWidgetSlot={
+              <button
+                type="button"
+                style={{ color: accentColor }}
+                onClick={() => $help('import-and-export')}
+                className="inline-flex items-center space-x-1 mt-4"
+              >
+                <HelpCircle size={18} />
+                <span>How to import data</span>
+              </button>
+            }
+          />
+
+          <HelpWidget
+            id="import-and-export"
+            url="https://raw.githubusercontent.com/invoiceninja/invoiceninja.github.io/refs/heads/v5-rework/source/en/import-and-export.md"
+          />
         </div>
       </div>
     </Default>
