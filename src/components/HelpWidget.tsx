56920cf780e4813161575ea5ce38dc39932cb007
Benjamin Beganović <k1pstabug@gmail.com>
2024-09-25 14:27:44 +0200

    support for dark mode and side switching

--- src/components/HelpWidget.tsx
+++ src/components/HelpWidget.tsx
@@ -8,7 +8,8 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
-import { X } from 'react-feather';
+import { useColorScheme } from '$app/common/colors';
+import { Layers, X } from 'react-feather';
 import Markdown from 'react-markdown';
 import { useQuery } from 'react-query';
 import rehypeRaw from 'rehype-raw';
@@ -41,20 +42,57 @@ export function HelpWidget({ id, url }: Props) {
       ),
   });
 
+  // const [, slug] = url.split('v5-rework/source');
+
+  const colors = useColorScheme();
+
   return (
     <div
       id={`help-widget-${id}`}
-      className="hidden fixed top-0 right-0 w-full md:w-1/2 lg:w-1/3 xl:w-1/4 h-full bg-white shadow-xl border rounded-l-lg z-50 p-5 overflow-y-auto"
+      className="hidden fixed top-0 right-0 w-full md:w-1/2 lg:w-1/3 xl:w-1/4 h-full shadow-xl border rounded-l-lg z-50 overflow-y-auto"
+      style={{
+        backgroundColor: colors.$1,
+        color: colors.$3,
+        borderColor: colors.$4,
+      }}
     >
-      <div className="flex justify-between items-center">
+      <div
+        className="flex justify-between items-center sticky px-5 py-3 top-0"
+        style={{ backgroundColor: colors.$1 }}
+      >
         <div></div>
-        <button>
-          <X size={20} onClick={() => $help(id)} />
-        </button>
+
+        <div className="flex items-center space-x-2">
+          <button
+            className="hidden md:block"
+            type="button"
+            onClick={() => {
+              const e = document.getElementById(`help-widget-${id}`);
+
+              if (e) {
+                e.classList.toggle('right-0');
+                e.classList.toggle('left-0');
+              }
+            }}
+          >
+            <Layers size={18} />
+          </button>
+
+          <button>
+            <X size={20} onClick={() => $help(id)} />
+          </button>
+        </div>
       </div>
 
-      <div className="prose-sm">
+      <div className="prose-sm p-5">
         <Markdown rehypePlugins={[rehypeRaw]}>{data}</Markdown>
+
+        {/* <Link
+          to={`https://invoiceninja.github.io/${slug.replace('.md', '')}`}
+          external
+        >
+          {t('view_docs')}
+        </Link> */}
       </div>
     </div>
   );
