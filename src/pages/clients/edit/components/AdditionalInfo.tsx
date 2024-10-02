0d8fb0321d3b49bb9f831d1ca2d71cef4b005de8
Civolilah <abedinhalilovic12345@gmail.com>
2024-10-02 23:24:57 +0200

    Adjusted handle change logic for clients

--- src/pages/clients/edit/components/AdditionalInfo.tsx
+++ src/pages/clients/edit/components/AdditionalInfo.tsx
@@ -202,9 +202,6 @@ export function AdditionalInfo({ client, errors, setClient }: Props) {
                   ? 'disabled'
                   : ''
               }
-              className={
-                'appearance-none block px-3 py-1.5 text-base font-normal  text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
-              }
               onValueChange={(value) =>
                 handleSettingsChange(
                   'send_reminders',
--- src/pages/clients/edit/components/Details.tsx
+++ src/pages/clients/edit/components/Details.tsx
@@ -16,7 +16,7 @@ import { useUsersQuery } from '$app/common/queries/users';
 import { useTranslation } from 'react-i18next';
 import { Client } from '$app/common/interfaces/client';
 import { set } from 'lodash';
-import { ChangeEvent, Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction } from 'react';
 import { useCurrentCompany } from '$app/common/hooks/useCurrentCompany';
 import { CustomField } from '$app/components/CustomField';
 import { ValidationBag } from '$app/common/interfaces/validation-bag';
@@ -35,13 +35,10 @@ export function Details(props: Props) {
   const { data: users } = useUsersQuery();
   const { data: groupSettings } = useGroupSettingsQuery();
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (property: keyof Client, value: string | number) => {
     props.setErrors(undefined);
 
-    props.setClient(
-      (client) =>
-        client && set({ ...client }, event.target.id, event.target.value)
-    );
+    props.setClient((client) => client && set({ ...client }, property, value));
   };
 
   const handleCustomFieldChange = (
@@ -63,18 +60,16 @@ export function Details(props: Props) {
 
       <Element leftSide={t('name')}>
         <InputField
-          id="name"
           value={props.client?.name || ''}
-          onChange={handleChange}
+          onValueChange={(value) => handleChange('name', value)}
           errorMessage={props.errors?.errors.name}
         />
       </Element>
 
       <Element leftSide={t('number')}>
         <InputField
-          id="number"
           value={props.client?.number || ''}
-          onChange={handleChange}
+          onValueChange={(value) => handleChange('number', value)}
           errorMessage={props.errors?.errors.number}
         />
       </Element>
@@ -82,13 +77,12 @@ export function Details(props: Props) {
       {groupSettings && (
         <Element leftSide={t('group')}>
           <SelectField
-            id="group_settings_id"
             value={props.client?.group_settings_id}
-            onChange={handleChange}
+            onValueChange={(value) => handleChange('group_settings_id', value)}
             errorMessage={props.errors?.errors.group_settings_id}
+            withBlank
             customSelector
           >
-            <option value=""></option>
             {groupSettings.map((group, index: number) => (
               <option value={group.id} key={index}>
                 {group.name}
@@ -101,13 +95,12 @@ export function Details(props: Props) {
       {users && (
         <Element leftSide={t('user')}>
           <SelectField
-            id="assigned_user_id"
-            onChange={handleChange}
-            defaultValue={props.client?.assigned_user_id}
+            value={props.client?.assigned_user_id}
+            onValueChange={(value) => handleChange('assigned_user_id', value)}
             errorMessage={props.errors?.errors.assigned_user_id}
+            withBlank
             customSelector
           >
-            <option value=""></option>
             {users.data.data.map((user: User, index: number) => (
               <option value={user.id} key={index}>
                 {user.first_name} {user.last_name}
@@ -119,52 +112,46 @@ export function Details(props: Props) {
 
       <Element leftSide={t('id_number')}>
         <InputField
-          id="id_number"
           value={props.client?.id_number || ''}
-          onChange={handleChange}
+          onValueChange={(value) => handleChange('id_number', value)}
           errorMessage={props.errors?.errors.id_number}
         />
       </Element>
 
       <Element leftSide={t('vat_number')}>
         <InputField
-          id="vat_number"
           value={props.client?.vat_number || ''}
-          onChange={handleChange}
+          onValueChange={(value) => handleChange('vat_number', value)}
           errorMessage={props.errors?.errors.vat_number}
         />
       </Element>
 
       <Element leftSide={t('website')}>
         <InputField
-          id="website"
           value={props.client?.website || ''}
-          onChange={handleChange}
+          onValueChange={(value) => handleChange('website', value)}
           errorMessage={props.errors?.errors.website}
         />
       </Element>
 
       <Element leftSide={t('phone')}>
         <InputField
-          id="phone"
           value={props.client?.phone || ''}
-          onChange={handleChange}
+          onValueChange={(value) => handleChange('phone', value)}
           errorMessage={props.errors?.errors.phone}
         />
       </Element>
 
       <Element leftSide={t('routing_id')}>
         <InputField
-          id="routing_id"
           value={props.client?.routing_id || ''}
-          onChange={handleChange}
+          onValueChange={(value) => handleChange('routing_id', value)}
           errorMessage={props.errors?.errors.routing_id}
         />
       </Element>
 
       <Element leftSide={t('valid_vat_number')}>
         <Toggle
-          id="has_valid_vat_number"
           checked={Boolean(props.client?.has_valid_vat_number)}
           onValueChange={(value) =>
             handleCustomFieldChange('has_valid_vat_number', value)
@@ -174,7 +161,6 @@ export function Details(props: Props) {
 
       <Element leftSide={t('tax_exempt')}>
         <Toggle
-          id="is_tax_exempt"
           checked={Boolean(props.client?.is_tax_exempt)}
           onValueChange={(value) =>
             handleCustomFieldChange('is_tax_exempt', value)
@@ -184,9 +170,8 @@ export function Details(props: Props) {
 
       <Element leftSide={t('classification')}>
         <SelectField
-          id="classification"
-          defaultValue={props.client?.classification ?? ''}
-          onChange={handleChange}
+          value={props.client?.classification ?? ''}
+          onValueChange={(value) => handleChange('classification', value)}
           withBlank
           customSelector
         >
