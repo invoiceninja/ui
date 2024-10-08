b5540653ca6a62e5d3716455f0bb3a33eca41987
Civolilah <abedinhalilovic12345@gmail.com>
2024-10-08 13:43:31 +0200

    Added key for commonwealth_bank gateway

--- src/pages/settings/gateways/create/Create.tsx
+++ src/pages/settings/gateways/create/Create.tsx
@@ -69,7 +69,7 @@ export const gatewaysDetails = [
   { name: 'payfast', key: 'd6814fc83f45d2935e7777071e629ef9' },
   { name: 'eway', key: '944c20175bbe6b9972c05bcfe294c2c7' },
   { name: 'btcpay', key: 'vpyfbmdrkqcicpkjqdusgjfluebftuva' },
-  { name: 'commonwealth_bank', key: 'someKey' },
+  { name: 'commonwealth_bank', key: 'b67581d804dbad1743b61c57285142ad' },
 ];
 
 const hostedGatewayFilter = [
