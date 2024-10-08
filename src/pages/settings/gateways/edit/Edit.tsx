107451bdc8d6c01e62189b1e5d19903d10c1a658
Benjamin Beganović <k1pstabug@gmail.com>
2024-10-08 17:57:07 +0200

    include help widget on edit page

--- src/pages/settings/gateways/edit/Edit.tsx
+++ src/pages/settings/gateways/edit/Edit.tsx
@@ -32,6 +32,8 @@ import collect from 'collect.js';
 import { ResourceActions } from '$app/components/ResourceActions';
 import { useActions } from '../common/hooks/useActions';
 import { isEqual } from 'lodash';
+import { HelpWidget } from '$app/components/HelpWidget';
+import { createPortal } from 'react-dom';
 
 export function Edit() {
   const { documentTitle } = useTitle('edit_gateway');
@@ -111,6 +113,11 @@ export function Edit() {
         )
       }
     >
+      <HelpWidget
+        id="gateways"
+        url="https://raw.githubusercontent.com/invoiceninja/invoiceninja.github.io/refs/heads/v5-rework/source/en/gateways.md"
+      />
+      
       <TabGroup
         tabs={tabs}
         defaultTabIndex={Number(searchParams.get('tab')) ?? 0}
