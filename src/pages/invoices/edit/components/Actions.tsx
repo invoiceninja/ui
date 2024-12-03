162a9457d8a5bebb09db3c15237fd841b43c16d2
Civolilah <abedinhalilovic12345@gmail.com>
2024-12-03 01:13:05 +0100

    Added run_template action into common action section for invoices

--- src/pages/invoices/edit/components/Actions.tsx
+++ src/pages/invoices/edit/components/Actions.tsx
@@ -381,7 +381,14 @@ export function useActions(params?: Params) {
     //     </EntityActionElement>
     //   ),
     (invoice: Invoice) => (
-      <DropdownElement
+      <EntityActionElement
+        {...(!dropdown && {
+          key: 'run_template',
+        })}
+        entity="invoice"
+        actionKey="run_template"
+        isCommonActionSection={!dropdown}
+        tooltipText={t('run_template')}
         onClick={() => {
           setChangeTemplateVisible(true);
           setChangeTemplateResources([invoice]);
@@ -390,10 +397,10 @@ export function useActions(params?: Params) {
             entity: 'invoice',
           });
         }}
-        icon={<Icon element={MdDesignServices} />}
+        icon={MdDesignServices}
       >
         {t('run_template')}
-      </DropdownElement>
+      </EntityActionElement>
     ),
     () => dropdown && <Divider withoutPadding />,
     (invoice: Invoice) =>
