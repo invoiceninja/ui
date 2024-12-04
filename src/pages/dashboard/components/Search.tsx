081df99b87bb6668eb3a74378c28330f057b847d
Civolilah <abedinhalilovic12345@gmail.com>
2024-12-04 17:06:27 +0100

    Implementing popover

--- src/pages/dashboard/components/Search.tsx
+++ src/pages/dashboard/components/Search.tsx
@@ -8,7 +8,7 @@
  *
  * @license https://www.elastic.co/licensing/elastic-license
  */
-import { endpoint } from '$app/common/helpers';
+import { classNames, endpoint } from '$app/common/helpers';
 import { request } from '$app/common/helpers/request';
 import { useQuery } from 'react-query';
 import { useTranslation } from 'react-i18next';
@@ -17,15 +17,14 @@ import { Entry } from '$app/components/forms/Combobox';
 import { AxiosResponse } from 'axios';
 import { v4 } from 'uuid';
 import { useColorScheme } from '$app/common/colors';
-import { memo, useEffect, useRef, useState } from 'react';
+import { Fragment, memo, useEffect, useRef, useState } from 'react';
 import { styled } from 'styled-components';
-import { Combobox } from '@headlessui/react';
+import { Combobox, Popover, Transition } from '@headlessui/react';
 import { useClickAway } from 'react-use';
 import collect from 'collect.js';
 import { usePreventNavigation } from '$app/common/hooks/usePreventNavigation';
 import { debounce } from 'lodash';
 import { InputField } from '$app/components/forms';
-import { Modal } from '$app/components/Modal';
 
 const ComboboxOption = styled(Combobox.Option)`
   color: ${(props) => props.theme.color};
@@ -123,33 +122,61 @@ export function Search$() {
 
   return (
     <>
-      <InputField
-        className="border-transparent focus:border-transparent focus:ring-0 w-full"
-        value={query}
-        onValueChange={(value) => handleChange(value)}
-        onClick={() => setIsModalOpen(true)}
-        placeholder={`${t('search_placeholder')}. (Ctrl+K)`}
-        style={{ backgroundColor: colors.$1, color: colors.$3 }}
-      />
+      <Popover className="relative mt-2">
+        {() => (
+          <>
+            <Popover.Button
+              style={{ backgroundColor: colors.$1, color: colors.$3 }}
+              className={classNames(
+                'group inline-flex items-center rounded text-base font-medium  focus:outline-none'
+              )}
+            >
+              <InputField
+                className="border-transparent focus:border-transparent focus:ring-0 w-full"
+                value={query}
+                onValueChange={(value) => handleChange(value)}
+                onClick={() => setIsModalOpen(true)}
+                placeholder={`${t('search_placeholder')}. (Ctrl+K)`}
+                style={{ backgroundColor: colors.$1, color: colors.$3 }}
+              />
+            </Popover.Button>
 
-      <Modal visible={isModalOpen} onClose={() => setIsModalOpen(false)}>
-        {options?.map((entry) => (
-          <Div
-            key={entry.id}
-            theme={{ color: colors.$3, hoverColor: colors.$2 }}
-            className="cursor-pointer rounded px-4 py-2 active:font-semibold"
-          >
-            <span>
-              <div>
-                <p className="text-xs font-semibold">
-                  {entry.resource?.heading}
-                </p>
-                <p>{entry.label}</p>
-              </div>
-            </span>
-          </Div>
-        ))}
-      </Modal>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute m-auto left-0 right-0 z-10">
+                <div
+                  className="overflow-y-auto max-h-72"
+                  style={{ backgroundColor: colors.$1 }}
+                >
+                  {options?.map((entry) => (
+                    <Div
+                      key={entry.id}
+                      theme={{ color: colors.$3, hoverColor: colors.$2 }}
+                      className="cursor-pointer rounded px-4 py-2 active:font-semibold"
+                    >
+                      <span>
+                        <div>
+                          <p className="text-xs font-semibold">
+                            {entry.resource?.heading}
+                          </p>
+                          <p>{entry.label}</p>
+                        </div>
+                      </span>
+                    </Div>
+                  ))}
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </>
+        )}
+      </Popover>
 
       {/* <Combobox
         as="div"
