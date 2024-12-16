8581fa148aeffe758401488e5d918bec4fa7b05a
Civolilah <abedinhalilovic12345@gmail.com>
2024-12-16 09:52:15 +0100

    Adjusted solution

--- src/common/helpers/request.ts
+++ src/common/helpers/request.ts
@@ -13,8 +13,9 @@ import { defaultHeaders } from '$app/common/queries/common/headers';
 import { ValidationBag } from '../interfaces/validation-bag';
 import { toast } from './toast/toast';
 import { $refetch } from '../hooks/useRefetch';
-import { checkJsonObject } from '../helpers';
+import { checkJsonObject, trans } from '../helpers';
 import { clearLocalStorage } from './local-storage';
+import { toast as $toast } from 'react-hot-toast';
 
 const client = axios.create();
 
@@ -38,6 +39,23 @@ client.interceptors.response.use(
     return response;
   },
   (error: AxiosError<ValidationBag>) => {
+    const url = error.response?.config.url;
+
+    if (
+      url?.includes('einvoice') &&
+      (error.response?.status === 401 || error.response?.status === 403)
+    ) {
+      console.error(error);
+
+      if (!url.includes('quota')) {
+        $toast.error(trans('einvoice_something_went_wrong', {}), {
+          duration: 10_000,
+        });
+      }
+
+      return Promise.reject(error);
+    }
+
     if (error.response?.status === 429 || error.response?.status === 403) {
       window.location.reload();
       clearLocalStorage();
