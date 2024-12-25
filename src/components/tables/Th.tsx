229ab649711ad6a35644d07d87fe8c2d516f5106
Benjamin Beganović <k1pstabug@gmail.com>
2024-12-25 16:48:46 +0100

    Refactor resizing to custom hook

--- src/components/tables/Th.tsx
+++ src/components/tables/Th.tsx
@@ -11,11 +11,11 @@
 import React, { useState, useRef, useEffect } from 'react';
 import classNames from 'classnames';
 import CommonProps from '../../common/interfaces/common-props.interface';
-// import { ChevronDown, ChevronUp } from 'react-feather';
 import { useColorScheme } from '$app/common/colors';
 import { atomWithStorage } from 'jotai/utils';
 import { useAtom } from 'jotai';
 import { get } from 'lodash';
+import { ChevronDown, ChevronUp } from 'react-feather';
 
 export interface ColumnSortPayload {
   sort: string;
@@ -40,46 +40,35 @@ const currentWidthAtom = atomWithStorage<Record<string, number>>(
   {}
 );
 
-export function Th(props: Props) {
-  props = { ...defaultProps, ...props };
-
+export function useResizeColumn(resizable: string | undefined) {
   const thRef = useRef<HTMLTableCellElement>(null);
-
-  const [order, setOrder] = useState<'asc' | 'desc'>('asc');
   const [isResizing, setIsResizing] = useState(false);
   const [startX, setStartX] = useState<number>(0);
-
   const [widths, setWidths] = useAtom(currentWidthAtom);
 
   const currentWidth = () => {
-    if (props.resizable) {
-      return get(widths, props.resizable, 0);
+    if (resizable) {
+      return get(widths, resizable, 0);
     }
-
     return 0;
   };
 
   function setCurrentWidth(width: number) {
-    if (!props.resizable) {
-      return;
-    }
-
-    setWidths((prev) => ({ ...prev, [props.resizable!]: width }));
+    if (!resizable) return;
+    setWidths((prev) => ({ ...prev, [resizable]: width }));
   }
 
   const handleMouseDown = (e: React.MouseEvent) => {
-    if (!props.resizable || e.button !== 0) return;
+    if (!resizable || e.button !== 0) return;
 
     const thWidth = thRef.current?.offsetWidth;
     const clickPosition =
       e.clientX - thRef.current!.getBoundingClientRect().left;
-
     const borderThreshold = 60;
 
     if (thWidth && clickPosition > thWidth - borderThreshold) {
       setIsResizing(true);
       setStartX(e.clientX);
-
       const table = thRef.current?.closest('table');
       if (table) {
         table.style.userSelect = 'none';
@@ -91,8 +80,7 @@ export function Th(props: Props) {
     if (!isResizing || !thRef.current) return;
 
     const dx = e.clientX - startX;
-
-    const sensitivityFactor = 0.5;
+    const sensitivityFactor = 1.25;
     const newWidth = currentWidth() + dx * sensitivityFactor;
 
     if (newWidth >= 50 && newWidth <= 1000) {
@@ -129,16 +117,11 @@ export function Th(props: Props) {
 
   const handleDoubleClick = (e: React.MouseEvent) => {
     const borderThreshold = 40;
-
     const thWidth = thRef.current?.offsetWidth;
     const clickPosition =
       e.clientX - thRef.current!.getBoundingClientRect().left;
 
-    if (
-      props.resizable &&
-      thWidth &&
-      clickPosition > thWidth - borderThreshold
-    ) {
+    if (resizable && thWidth && clickPosition > thWidth - borderThreshold) {
       setCurrentWidth(0);
       if (thRef.current) {
         thRef.current.style.width = `0px`;
@@ -146,35 +129,11 @@ export function Th(props: Props) {
     }
   };
 
-  function handleClick(event: React.MouseEvent) {
-    if (isResizing) return;
-
-    const thWidth = (event.currentTarget as HTMLElement).offsetWidth;
-    const clickPosition =
-      event.clientX - event.currentTarget.getBoundingClientRect().left;
-
-    const borderThreshold = 30;
-
-    if (clickPosition > thWidth - borderThreshold) {
-      return;
-    } else {
-      if (props.onColumnClick) {
-        setOrder(order === 'desc' ? 'asc' : 'desc');
-
-        props.onColumnClick({
-          sort: `${props.id}|${order}`,
-          field: props.id,
-        });
-      }
-    }
-  }
-
-  function handleMouseEnter(event: React.MouseEvent) {
-    if (props.resizable) {
+  const handleMouseEnter = (event: React.MouseEvent) => {
+    if (resizable) {
       const thWidth = thRef.current?.offsetWidth;
       const clickPosition =
         event.clientX - thRef.current!.getBoundingClientRect().left;
-
       const borderThreshold = 40;
 
       if (thWidth && clickPosition > thWidth - borderThreshold) {
@@ -183,10 +142,47 @@ export function Th(props: Props) {
         thRef.current!.style.cursor = '';
       }
     }
-  }
+  };
 
-  function handleMouseLeave() {
+  const handleMouseLeave = () => {
     thRef.current!.style.cursor = '';
+  };
+
+  return {
+    thRef,
+    currentWidth,
+    handleMouseDown,
+    handleMouseMove,
+    handleMouseUp,
+    handleDoubleClick,
+    handleMouseEnter,
+    handleMouseLeave,
+    setCurrentWidth,
+  };
+}
+
+export function Th(props: Props) {
+  props = { ...defaultProps, ...props };
+
+  const {
+    thRef,
+    currentWidth,
+    handleMouseDown,
+    handleDoubleClick,
+    handleMouseEnter,
+    handleMouseLeave,
+  } = useResizeColumn(props.resizable);
+
+  const [order, setOrder] = useState<'asc' | 'desc'>('asc');
+
+  function handleClick() {
+    if (props.onColumnClick) {
+      setOrder(order === 'desc' ? 'asc' : 'desc');
+      props.onColumnClick({
+        sort: `${props.id}|${order}`,
+        field: props.id,
+      });
+    }
   }
 
   const colors = useColorScheme();
@@ -218,6 +214,22 @@ export function Th(props: Props) {
         {props.onColumnClick ? (
           <div className="flex items-center space-x-1" onClick={handleClick}>
             <span>{props.children}</span>
+
+            <div className="flex items-center">
+              <ChevronUp
+                className={classNames('opacity-25', {
+                  'opacity-100': order === 'asc' && props.isCurrentlyUsed,
+                })}
+                size={16}
+              />
+
+              <ChevronDown
+                className={classNames('opacity-25', {
+                  'opacity-100': order === 'desc' && props.isCurrentlyUsed,
+                })}
+                size={16}
+              />
+            </div>
           </div>
         ) : (
           <span>{props.children}</span>
