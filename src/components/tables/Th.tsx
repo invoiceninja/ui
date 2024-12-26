55d712820054f854a0af4bce24a39e55c1386460
Benjamin Beganović <k1pstabug@gmail.com>
2024-12-26 17:47:04 +0100

    Clean up

--- src/components/tables/Th.tsx
+++ src/components/tables/Th.tsx
@@ -215,7 +215,7 @@ export function Th$(props: Props) {
       onMouseEnter={(e) => {
         handleMouseEnter(e);
       }}
-      onMouseLeave={(e) => {
+      onMouseLeave={() => {
         handleMouseLeave();
       }}
       onMouseDown={handleMouseDown}
