5e31e98cc3cfcd584fb43d03408280b98001e48f
David Bomba <turbo124@gmail.com>
2025-02-03 17:32:50 +1100

    Remove test mode from oauth integration

--- src/pages/settings/gateways/create/components/gateways/GoCardlessOAuth2.tsx
+++ src/pages/settings/gateways/create/components/gateways/GoCardlessOAuth2.tsx
@@ -12,8 +12,6 @@ import { Element } from '$app/components/cards';
 import { useTranslation } from 'react-i18next';
 import { useResolveConfigValue } from '../../hooks/useResolveConfigValue';
 import { CompanyGateway } from '$app/common/interfaces/company-gateway';
-import { Field } from '$app/pages/settings/gateways/create/hooks/useResolveInputField';
-import Toggle from '$app/components/forms/Toggle';
 import { Check } from 'react-feather';
 import { useHandleCredentialsChange } from '$app/pages/settings/gateways/create/hooks/useHandleCredentialsChange';
 
@@ -27,21 +25,9 @@ interface Props {
 export function GoCardlessOAuth2({ companyGateway, setCompanyGateway }: Props) {
   const { t } = useTranslation();
 
-  const config = useResolveConfigValue(companyGateway);
-  const handleChange = useHandleCredentialsChange(setCompanyGateway);
-
   return (
-    <>
       <Element leftSide={t('OAuth 2.0')}>
         <Check size={18} />
       </Element>
-
-      <Element leftSide={t('test_mode')}>
-        <Toggle
-          checked={config('testMode')}
-          onChange={(value) => handleChange('testMode' as keyof Field, value)}
-        />
-      </Element>
-    </>
   );
 }
