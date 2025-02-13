adb255412748e7ae1d348689d9c4d28351d91693
Civolilah <abedinhalilovic12345@gmail.com>
2025-02-13 12:00:54 +0100

    Fixed scrolling issue

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,20 +1,27 @@
-/**
- * Invoice Ninja (https://invoiceninja.com).
- *
- * @link https://github.com/invoiceninja/invoiceninja source repository
- *
- * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
- *
- * @license https://www.elastic.co/licensing/elastic-license
- */
-
 import { useEffect } from 'react';
 import { useLocation } from 'react-router';
 
 export function ScrollToTop(props: any) {
   const location = useLocation();
 
+  const pathSegments = location.pathname.split('/');
+  const id = pathSegments?.[2] || '';
+
+  const isClientShowPage =
+    location.pathname.startsWith('/clients') &&
+    id &&
+    !location.pathname.endsWith('/edit');
+
+  const isVendorShowPage =
+    location.pathname.startsWith('/vendors') &&
+    id &&
+    !location.pathname.endsWith('/edit');
+
   useEffect(() => {
+    if (isClientShowPage || isVendorShowPage) {
+      return;
+    }
+
     window.scrollTo(0, 0);
   }, [location]);
 
