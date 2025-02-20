3372ac89d3364e21ecd8e6d5dd85029786aa8795
David Bomba <turbo124@gmail.com>
2025-02-20 18:26:12 +1100

    Unlock documents after payment

--- src/common/interfaces/company.interface.ts
+++ src/common/interfaces/company.interface.ts
@@ -338,6 +338,7 @@ export interface Settings {
   merge_e_invoice_to_pdf: boolean;
   payment_flow: string;
   enable_client_profile_update: boolean;
+  unlock_invoice_documents_after_payment: boolean;
 }
 
 export interface TaxData {
