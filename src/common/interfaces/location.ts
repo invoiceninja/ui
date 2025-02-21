aee36dd0be6c7a16212b3910238f09d563a26991
David Bomba <turbo124@gmail.com>
2025-02-21 13:35:29 +1100

    Add location interface and update entity props

---
+++ src/common/interfaces/location.ts
@@ -0,0 +1,32 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+interface Location {
+    id: string;
+    user_id: string;
+    vendor_id: string;
+    client_id: string;
+    name: string;
+    address1: string;
+    address2: string;
+    phone: string;
+    city: string;
+    state: string;
+    postal_code: string;
+    country_id: string;
+    custom_value1: string;
+    custom_value2: string;
+    custom_value3: string;
+    custom_value4: string;
+    is_deleted: boolean;
+    updated_at: number;
+    archived_at: number;
+    created_at: number;
+}
