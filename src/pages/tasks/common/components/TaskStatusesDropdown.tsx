859e5ff896eb592df0730a04534bc1b76480d83f
Civolilah <abedinhalilovic12345@gmail.com>
2025-03-23 17:11:08 +0100

    Redesigning status popover

--- src/pages/tasks/common/components/TaskStatusesDropdown.tsx
+++ src/pages/tasks/common/components/TaskStatusesDropdown.tsx
@@ -10,7 +10,6 @@
 
 import { useColorScheme } from '$app/common/colors';
 import { useTaskStatusesQuery } from '$app/common/queries/task-statuses';
-import { DropdownElement } from '$app/components/dropdown/DropdownElement';
 import Tippy from '@tippyjs/react/headless';
 import { Dispatch, SetStateAction, useState } from 'react';
 import { useUpdateTask } from '../hooks/useUpdateTask';
@@ -19,6 +18,16 @@ import { useTranslation } from 'react-i18next';
 import { CreateTaskStatusModal } from '$app/pages/settings/task-statuses/components/CreateTaskStatusModal';
 import { TaskStatus } from '$app/common/interfaces/task-status';
 import { useAdmin } from '$app/common/hooks/permissions/useHasPermission';
+import { Plus } from '$app/components/icons/Plus';
+import styled from 'styled-components';
+import { RadioChecked } from '$app/components/icons/RadioChecked';
+import { RadioUnchecked } from '$app/components/icons/RadioUnchecked';
+
+const OptionElement = styled.div`
+  &:hover {
+    background-color: ${(props) => props.theme.hoverColor};
+  }
+`;
 
 interface Props {
   visible: boolean;
@@ -53,7 +62,7 @@ export function TaskStatusesDropdown(props: Props) {
         interactive={true}
         render={() => (
           <div
-            className="border box rounded-md shadow-lg focus:outline-none"
+            className="border box rounded-md shadow-lg focus:outline-none p-1"
             style={{
               backgroundColor: colors.$1,
               borderColor: colors.$4,
@@ -61,37 +70,55 @@ export function TaskStatusesDropdown(props: Props) {
               maxWidth: '14.7rem',
             }}
           >
-            {(isAdmin || isOwner) && (
-              <DropdownElement
-                className="font-medium text-center py-3"
+            {taskStatuses?.data.map((taskStatus, index) => (
+              <OptionElement
+                key={index}
+                className="flex items-center p-2 space-x-2"
                 onClick={() => {
-                  setIsModalOpen(true);
                   setVisible(false);
+                  handleUpdateTask({ ...task, status_id: taskStatus.id });
+                }}
+                theme={{
+                  hoverColor: colors.$4,
                 }}
               >
-                {t('new_task_status')}
-              </DropdownElement>
-            )}
+                {taskStatus.id === task.status_id ? (
+                  <RadioChecked
+                    size="1.1rem"
+                    filledColor={colors.$1}
+                    borderColor={colors.$3}
+                  />
+                ) : (
+                  <RadioUnchecked size="1.1rem" color={colors.$5} />
+                )}
 
-            {taskStatuses?.data.map(
-              (taskStatus, index) =>
-                taskStatus.id !== task.status_id && (
-                  <DropdownElement
-                    key={index}
-                    onClick={() => {
-                      setVisible(false);
-                      handleUpdateTask({ ...task, status_id: taskStatus.id });
-                    }}
-                  >
-                    {taskStatus.name}
-                  </DropdownElement>
-                )
-            )}
+                <span>{taskStatus.name}</span>
+              </OptionElement>
+            ))}
 
             {Boolean(!taskStatuses?.data.length) && (
               <div className="font-medium text-center py-2 text-xs">
                 {t('no_records_found')}
               </div>
+            )}
+
+            {(isAdmin || isOwner) && (
+              <OptionElement
+                className="flex items-center font-medium text-center p-2 rounded-sm"
+                onClick={() => {
+                  setIsModalOpen(true);
+                  setVisible(false);
+                }}
+                theme={{
+                  hoverColor: colors.$4,
+                }}
+              >
+                <div className="flex items-center gap-2">
+                  <Plus color={colors.$5} size="1.1rem" />
+
+                  <span>{t('create_new')}</span>
+                </div>
+              </OptionElement>
             )}
           </div>
         )}
