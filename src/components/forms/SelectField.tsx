8b3b8985b33853618b42495b774b1d7cfee1e208
Civolilah <abedinhalilovic12345@gmail.com>
2025-04-02 19:29:57 +0200

    Fixed border color

--- src/components/forms/SelectField.tsx
+++ src/components/forms/SelectField.tsx
@@ -208,7 +208,7 @@ export function SelectField(props: SelectProps) {
                 style={{
                   height: '2.5rem',
                   backgroundColor: colors.$1,
-                  borderColor: isFocused ? '#2463eb' : colors.$24,
+                  borderColor: isFocused ? colors.$3 : colors.$24,
                   ...controlStyle,
                 }}
                 {...innerProps}
