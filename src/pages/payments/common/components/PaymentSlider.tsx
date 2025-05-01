b195f578a2c25940c42ce8ac4f3cc0221889b5cd
David Bomba <turbo124@gmail.com>
2025-05-01 16:00:35 +1000

    Add activities to payments

--- src/pages/payments/common/components/PaymentSlider.tsx
+++ src/pages/payments/common/components/PaymentSlider.tsx
@@ -39,66 +39,12 @@ import { PaymentActivity } from '$app/common/interfaces/payment-activity';
 import { CreditStatus } from '$app/pages/credits/common/components/CreditStatus';
 import paymentType from '$app/common/constants/payment-type';
 import { useCompanyTimeFormat } from '$app/common/hooks/useCompanyTimeFormat';
+import { useGenerateActivityElement } from '$app/pages/payments/common/hooks/useGenerateActivityElement';
 
 export const paymentSliderAtom = atom<Payment | null>(null);
 export const paymentSliderVisibilityAtom = atom(false);
 
 dayjs.extend(relativeTime);
-
-function useGenerateActivityElement() {
-  const [t] = useTranslation();
-
-  const formatMoney = useFormatMoney();
-
-  return (activity: PaymentActivity, payment: Payment | null) => {
-    let text = trans(`activity_${activity.activity_type_id}`, {});
-
-    const replacements = {
-      client: (
-        <Link to={route('/clients/:id', { id: activity.client?.hashed_id })}>
-          {activity.client?.label}
-        </Link>
-      ),
-      user: activity.user?.label ?? t('system'),
-      payment_amount: activity?.payment_amount?.label,
-      adjustment: activity?.adjustment?.label,
-      invoice: (
-        <Link
-          to={route('/invoices/:id/edit', {
-            id: activity.invoice?.hashed_id,
-          })}
-        >
-          {activity?.invoice?.label}
-        </Link>
-      ),
-      payment: (
-        <Link
-          to={route('/payments/:id/edit', {
-            id: activity.payment?.hashed_id,
-          })}
-        >
-          {activity?.payment?.label}
-        </Link>
-      ),
-      contact: (
-        <Link
-          to={route('/clients/:id/edit', {
-            id: activity?.contact?.hashed_id,
-          })}
-        >
-          {activity?.contact?.label}
-        </Link>
-      ),
-    };
-    for (const [variable, value] of Object.entries(replacements)) {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      text = reactStringReplace(text, `:${variable}`, () => value);
-    }
-
-    return text;
-  };
-}
 
 export function PaymentSlider() {
   const [t] = useTranslation();
