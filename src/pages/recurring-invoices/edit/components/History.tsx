aae3db0fe8b778456bedb8d2288a8e35268866a9
Civolilah <abedinhalilovic12345@gmail.com>
2025-05-06 18:30:10 +0200

    Redesigned all recurring invoice tabs

--- src/pages/recurring-invoices/edit/components/History.tsx
+++ src/pages/recurring-invoices/edit/components/History.tsx
@@ -13,9 +13,8 @@ import { request } from '$app/common/helpers/request';
 import { GenericSingleResourceResponse } from '$app/common/interfaces/generic-api-response';
 import { useTranslation } from 'react-i18next';
 import { useQuery } from 'react-query';
-import { useOutletContext } from 'react-router-dom';
-import { NonClickableElement } from '$app/components/cards/NonClickableElement';
-import { Card, ClickableElement } from '$app/components/cards';
+import { useOutletContext, useNavigate } from 'react-router-dom';
+import { Card } from '$app/components/cards';
 import { useFormatMoney } from '$app/common/hooks/money/useFormatMoney';
 import { DynamicLink } from '$app/components/DynamicLink';
 import { useDisableNavigation } from '$app/common/hooks/useDisableNavigation';
@@ -27,11 +26,27 @@ import { route } from '$app/common/helpers/route';
 import { RecurringInvoiceContext } from '../../create/Create';
 import { RecurringInvoice } from '$app/common/interfaces/recurring-invoice';
 import { useCompanyTimeFormat } from '$app/common/hooks/useCompanyTimeFormat';
+import { useColorScheme } from '$app/common/colors';
+import { History as HistoryIcon } from '$app/components/icons/History';
+import { ArrowRight } from 'react-feather';
+import styled from 'styled-components';
+import classNames from 'classnames';
 
 dayjs.extend(relativeTime);
 
+const HistoryBox = styled.div`
+  display: flex;
+  background-color: ${(props) => props.theme.backgroundColor};
+
+  &:hover {
+    background-color: ${(props) => props.theme.hoverBackgroundColor};
+  }
+`;
+
 export default function History() {
   const [t] = useTranslation();
+  const navigate = useNavigate();
+  const colors = useColorScheme();
 
   const context: RecurringInvoiceContext = useOutletContext();
   const { recurringInvoice } = context;
@@ -60,58 +75,105 @@ export default function History() {
   });
 
   return (
-    <Card title={t('history')} className="w-full xl:w-2/3">
-      {isLoading && (
-        <div className="flex justify-center">
-          <Spinner />
-        </div>
-      )}
+    <Card
+      title={t('history')}
+      className="shadow-sm"
+      style={{ borderColor: colors.$24 }}
+      headerStyle={{ borderColor: colors.$20 }}
+      withoutBodyPadding
+    >
+      <div
+        className={classNames('w-full px-2 pt-2', {
+          'pb-10': resource?.activities?.length,
+          'pb-6': resource?.activities && resource?.activities.length === 0,
+        })}
+      >
+        {isLoading && (
+          <div className="flex justify-center">
+            <Spinner />
+          </div>
+        )}
 
-      {!isLoading && resource?.activities && !resource.activities.length && (
-        <NonClickableElement>{t('api_404')}</NonClickableElement>
-      )}
+        {!isLoading && resource?.activities && !resource.activities.length && (
+          <div className="px-3 pt-3 text-sm">{t('api_404')}</div>
+        )}
 
-      {resource?.activities &&
-        resource.activities.map((activity) => (
-          <ClickableElement
-            key={activity.id}
-            to={route('/activities/:id', { id: activity.id })}
-            disableNavigation={Boolean(!activity.history.id)}
-          >
-            <div className="flex flex-col">
-              <div className="flex space-x-1">
-                <span>
-                  {recurringInvoice?.client
-                    ? formatMoney(
-                        activity.history.amount || 0,
-                        recurringInvoice?.client?.country_id,
-                        recurringInvoice?.client?.settings.currency_id
-                      )
-                    : null}
-                </span>
-
-                <span>&middot;</span>
-
-                <DynamicLink
-                  to={`/clients/${activity.client_id}`}
-                  renderSpan={disableNavigation(
-                    'client',
-                    recurringInvoice?.client
-                  )}
+        {Boolean(resource?.activities?.length) && (
+          <div className="flex flex-col w-full">
+            {resource?.activities &&
+              resource.activities.map((activity) => (
+                <HistoryBox
+                  className="flex items-center justify-start p-4 space-x-3 rounded-md cursor-pointer"
+                  key={activity.id}
+                  onClick={() =>
+                    navigate(
+                      route('/activities/:id', {
+                        id: activity.id,
+                      })
+                    )
+                  }
+                  theme={{
+                    backgroundColor: colors.$1,
+                    hoverBackgroundColor: colors.$25,
+                  }}
                 >
-                  {recurringInvoice?.client?.display_name}
-                </DynamicLink>
-              </div>
+                  <div
+                    className="p-2 rounded-full"
+                    style={{ backgroundColor: colors.$20 }}
+                  >
+                    <HistoryIcon
+                      size="1.3rem"
+                      color={colors.$16}
+                      filledColor={colors.$16}
+                    />
+                  </div>
 
-              <div className="inline-flex items-center space-x-1">
-                <p>
-                  {date(activity.created_at, `${dateFormat} ${timeFormat}`)}
-                </p>
-                <p>{dayjs.unix(activity.created_at).fromNow()}</p>
-              </div>
-            </div>
-          </ClickableElement>
-        ))}
+                  <div className="flex flex-col items-start space-y-0.5 justify-center">
+                    <div className="flex space-x-1 text-sm">
+                      <span style={{ color: colors.$3 }}>
+                        {recurringInvoice?.client
+                          ? formatMoney(
+                              activity.history.amount || 0,
+                              recurringInvoice?.client?.country_id,
+                              recurringInvoice?.client?.settings.currency_id
+                            )
+                          : null}
+                      </span>
+
+                      <div>
+                        <ArrowRight color={colors.$17} size="1.1rem" />
+                      </div>
+
+                      <DynamicLink
+                        to={`/clients/${activity.client_id}`}
+                        renderSpan={disableNavigation(
+                          'client',
+                          recurringInvoice?.client
+                        )}
+                      >
+                        {recurringInvoice?.client?.display_name}
+                      </DynamicLink>
+                    </div>
+
+                    <div
+                      className="flex items-center space-x-1 text-xs"
+                      style={{ color: colors.$17 }}
+                    >
+                      <span>
+                        {date(
+                          activity.created_at,
+                          `${dateFormat} ${timeFormat}`
+                        )}
+                      </span>
+
+                      <span>{dayjs.unix(activity.created_at).fromNow()}</span>
+                    </div>
+                  </div>
+                </HistoryBox>
+              ))}
+          </div>
+        )}
+      </div>
     </Card>
   );
 }
