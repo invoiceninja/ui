993263fc402439d7663bdb5412f57a212798a83f
Civolilah <abedinhalilovic12345@gmail.com>
2025-05-09 00:11:16 +0200

    Redesign of quote pages

--- src/pages/quotes/edit/components/Settings.tsx
+++ src/pages/quotes/edit/components/Settings.tsx
@@ -20,9 +20,12 @@ import { Card } from '$app/components/cards';
 import { QuoteContext } from '../../create/Create';
 import { Quote } from '$app/common/interfaces/quote';
 import { NumberInputField } from '$app/components/forms/NumberInputField';
+import { useColorScheme } from '$app/common/colors';
 
 export default function Settings() {
   const [t] = useTranslation();
+
+  const colors = useColorScheme();
 
   const hasPermission = useHasPermission();
 
@@ -39,56 +42,74 @@ export default function Settings() {
   };
 
   return (
-    <Card title={t('settings')} className="w-full xl:w-2/3">
-      <div className="grid grid-cols-12 gap-4 px-6">
-        <div className="col-span-12 lg:col-span-6 space-y-6">
-          <UserSelector
-            inputLabel={t('user')}
-            value={quote?.assigned_user_id}
-            onChange={(user) => handleChange('assigned_user_id', user.id)}
-            errorMessage={errors?.errors.assigned_user_id}
-            readonly={!hasPermission('edit_quote')}
-          />
+    <Card
+      title={t('settings')}
+      className="shadow-sm"
+      style={{ borderColor: colors.$24 }}
+      headerStyle={{ borderColor: colors.$20 }}
+    >
+      <div className="flex justify-center w-full pb-10 pt-2">
+        <div className="grid grid-cols-12 gap-4 px-6 w-full xl:w-2/3">
+          <div className="col-span-12 lg:col-span-6 space-y-6">
+            <div className="space-y-2">
+              <ProjectSelector
+                inputLabel={t('project')}
+                value={quote?.project_id}
+                onChange={(project) => handleChange('project_id', project.id)}
+                errorMessage={errors?.errors.project_id}
+                onClearButtonClick={() => handleChange('project_id', '')}
+              />
+            </div>
 
-          <div className="lg:pt-1">
-            <VendorSelector
-              inputLabel={t('vendor')}
-              value={quote?.vendor_id}
-              onChange={(vendor) => handleChange('vendor_id', vendor.id)}
-              onClearButtonClick={() => handleChange('vendor_id', '')}
-              errorMessage={errors?.errors.vendor_id}
+            <NumberInputField
+              label={t('exchange_rate')}
+              value={quote?.exchange_rate || 1.0}
+              onValueChange={(value) =>
+                handleChange('exchange_rate', parseFloat(value) || 1.0)
+              }
+              errorMessage={errors?.errors.exchange_rate}
+              disablePrecision
             />
+
+            <Toggle
+              label={t('auto_bill_enabled')}
+              checked={quote?.auto_bill_enabled || false}
+              onChange={(value) => handleChange('auto_bill_enabled', value)}
+            />
+
+            <div className="space-y-2">
+              <DesignSelector
+                inputLabel={t('design')}
+                value={quote?.design_id}
+                onChange={(design) => handleChange('design_id', design.id)}
+                onClearButtonClick={() => handleChange('design_id', '')}
+                disableWithQueryParameter
+                errorMessage={errors?.errors.design_id}
+              />
+            </div>
           </div>
 
-          <DesignSelector
-            inputLabel={t('design')}
-            value={quote?.design_id}
-            onChange={(design) => handleChange('design_id', design.id)}
-            onClearButtonClick={() => handleChange('design_id', '')}
-            disableWithQueryParameter
-            errorMessage={errors?.errors.design_id}
-          />
-        </div>
+          <div className="col-span-12 lg:col-span-6 space-y-6">
+            <div className="space-y-2">
+              <UserSelector
+                inputLabel={t('user')}
+                value={quote?.assigned_user_id}
+                onChange={(user) => handleChange('assigned_user_id', user.id)}
+                errorMessage={errors?.errors.assigned_user_id}
+                readonly={!hasPermission('edit_quote')}
+              />
+            </div>
 
-        <div className="col-span-12 lg:col-span-6 space-y-6">
-          <ProjectSelector
-            inputLabel={t('project')}
-            value={quote?.project_id}
-            onChange={(project) => handleChange('project_id', project.id)}
-            errorMessage={errors?.errors.project_id}
-          />
+            <div className="space-y-2">
+              <VendorSelector
+                inputLabel={t('vendor')}
+                value={quote?.vendor_id}
+                onChange={(vendor) => handleChange('vendor_id', vendor.id)}
+                onClearButtonClick={() => handleChange('vendor_id', '')}
+                errorMessage={errors?.errors.vendor_id}
+              />
+            </div>
 
-          <NumberInputField
-            label={t('exchange_rate')}
-            value={quote?.exchange_rate || 1.0}
-            onValueChange={(value) =>
-              handleChange('exchange_rate', parseFloat(value))
-            }
-            errorMessage={errors?.errors.exchange_rate}
-            disablePrecision
-          />
-
-          <div className="lg:pt-7">
             <Toggle
               label={t('inclusive_taxes')}
               checked={quote?.uses_inclusive_taxes || false}
