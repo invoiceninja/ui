1800853be5f494f92d43c01f223f7d604e6263fc
David Bomba <turbo124@gmail.com>
2025-06-02 09:43:26 +1000

    Clean up

--- src/pages/settings/account-management/component/upgrade/PlanUpgrade.tsx
+++ src/pages/settings/account-management/component/upgrade/PlanUpgrade.tsx
@@ -1,25 +1,10 @@
-import { endpoint } from "$app/common/helpers";
-import { useCurrentAccount } from "$app/common/hooks/useCurrentAccount";
-import { useCurrentCompany } from "$app/common/hooks/useCurrentCompany";
-import { Company } from "$app/common/interfaces/docuninja/api";
-import { Card } from "$app/components/cards";
+
 import { useTranslation } from "react-i18next";
-import { useQueryClient } from "react-query";
-import { request } from "$app/common/helpers/request";
 import { Button } from "$app/components/forms";
-import { useState } from "react";
-import { Check } from "react-feather";
-import { useAccentColor } from "$app/common/hooks/useAccentColor";
-import { useColorScheme } from "$app/common/colors";
-import { useLogin } from "$app/common/queries/docuninja";
-import { Alert } from "$app/components/Alert";
 
 export function PlanUpgrade() {
 
     const { t } = useTranslation();
-    const account = useCurrentAccount();
-    const [startTrialFlag, setStartTrialFlag] = useState(false);
-
 
     return (
     <div className="space-y-2">
