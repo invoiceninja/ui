f13d69173e07d1b0c3aa32b0699d5a4f07779e47
David Bomba <turbo124@gmail.com>
2025-08-14 07:33:26 +1000

    Add user,assigned_user includes for project tasks

--- src/pages/projects/show/Show.tsx
+++ src/pages/projects/show/Show.tsx
@@ -261,7 +261,7 @@ export default function Show() {
             resource="task"
             columns={columns}
             customActions={taskActions}
-            endpoint={`/api/v1/tasks?include=status,client,project&sort=id|desc&project_tasks=${project.id}&without_deleted_clients=true`}
+            endpoint={`/api/v1/tasks?include=status,client,project,user,assigned_user&sort=id|desc&project_tasks=${project.id}&without_deleted_clients=true`}
             bulkRoute="/api/v1/tasks/bulk"
             linkToCreate={`/tasks/create?project=${id}&rate=${project.task_rate}`}
             linkToEdit="/tasks/:id/edit"
