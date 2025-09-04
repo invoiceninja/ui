8bd73d3132c709e4aef281bba033ee8be61ee506
Benjamin Beganović <k1pstabug@gmail.com>
2025-09-04 12:13:51 +0200

    Challenge wrapper

---
+++ src/components/CloudflareChallenge.tsx
@@ -0,0 +1,46 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2024. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { request } from '$app/common/helpers/request';
+import { useQuery } from 'react-query';
+import { Spinner } from './Spinner';
+import { endpoint } from '$app/common/helpers';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export function CloudflareChallenge({ children }: Props) {
+  const { isLoading, isError } = useQuery({
+    queryKey: ['/api/v1/signup/protect'],
+    queryFn: () =>
+      request('GET', endpoint('/api/v1/signup/protect'), null, {
+        withCredentials: true,
+      }),
+  });
+
+  if (isLoading) {
+    return (
+      <div className="m-3">
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <p className="m-3">
+        There was an issue loading the page. Please refresh page and try again.
+      </p>
+    );
+  }
+
+  return <>{children}</>;
+}
