118a89371916f4da5c57256c13916a8618d4e77b
Civolilah <abedinhalilovic12345@gmail.com>
2025-11-26 19:05:52 +0100

    Removed unnecessary function

--- src/pages/recurring-invoices/common/components/RecurringInvoiceSlider.tsx
+++ src/pages/recurring-invoices/common/components/RecurringInvoiceSlider.tsx
@@ -157,23 +157,6 @@ const useCalculateNextDueDate = (recurringInvoice: RecurringInvoice | null) => {
   return null;
 };
 
-const getDaySuffix = (day: number): string => {
-  if (day >= 11 && day <= 13) {
-    return 'th';
-  }
-
-  switch (day % 10) {
-    case 1:
-      return 'st';
-    case 2:
-      return 'nd';
-    case 3:
-      return 'rd';
-    default:
-      return 'th';
-  }
-};
-
 export const RecurringInvoiceSlider = () => {
   const [isVisible, setIsSliderVisible] = useAtom(
     recurringInvoiceSliderVisibilityAtom
