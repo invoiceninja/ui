ed4ec27a457eaca1824a26d4600a5b0dd95bf6f8
Benjamin Beganović <k1pstabug@gmail.com>
2025-12-10 07:57:49 +0100

    #2844

---
+++ src/common/hooks/useCompanyUsers.ts
@@ -0,0 +1,17 @@
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+import { CompanyUser } from '$app/common/interfaces/company-user';
+import { RootState } from '$app/common/stores/store';
+import { useSelector } from 'react-redux';
+
+export function useCompanyUsers(): CompanyUser[] {
+  return useSelector((state: RootState) => state.companyUsers.api || []);
+}
