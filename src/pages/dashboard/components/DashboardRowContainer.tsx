f548c20ea7615c6154c96f02996c9a031e339ffb
David Bomba <turbo124@gmail.com>
2025-12-11 23:57:39 +0000

    Add back

---
+++ src/pages/dashboard/components/DashboardRowContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Stub component to fix build error
+export function DashboardRowContainer(props: any) {
+  return (
+    <div>
+      <p>DashboardRowContainer - This is a placeholder component</p>
+      {props.children}
+    </div>
+  );
+}
