c016928bcfa26169b203fb3d1d53f64cf734bf5e
David Bomba <turbo124@gmail.com>
2025-12-11 22:42:23 +0000

    Fxies

--- src/pages/dashboard/components/PreferenceCardsGrid.tsx
+++ src/pages/dashboard/components/PreferenceCardsGrid.tsx
@@ -268,7 +268,7 @@ export function PreferenceCardsGrid(props: Props) {
         xxs: 24,
       }}
       draggableHandle=".preference-card-drag-handle"
-      margin={[5, 5]}
+      margin={[10, 0]}
       rowHeight={60}
       isDraggable={isEditMode}
       isDroppable={true}
