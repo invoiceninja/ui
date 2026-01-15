9a77218bbc4b474b1ef9441de2189b0a95603f0a
Civolilah <abedinhalilovic12345@gmail.com>
2026-01-15 19:24:01 +0100

    Adding date range filters on tables

--- src/pages/payments/index/Payments.tsx
+++ src/pages/payments/index/Payments.tsx
@@ -139,6 +139,9 @@ export default function Payments() {
           )
         }
         enableSavingFilterPreference
+        dateRangeColumns={[
+          { column: 'created_at', queryParameterKey: 'created_between' },
+        ]}
       />
 
       {!disableNavigation('payment', paymentSlider) && <PaymentSlider />}
