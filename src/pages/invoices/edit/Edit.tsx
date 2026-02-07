f374943394467f35e38774ad8d89631b9009a02a
Benjamin Beganović <k1pstabug@gmail.com>
2026-02-07 07:43:25 +0100

    Fixes for compiler

--- src/pages/invoices/edit/Edit.tsx
+++ src/pages/invoices/edit/Edit.tsx
@@ -103,7 +103,7 @@ export default function Edit() {
   const { changeTemplateVisible, setChangeTemplateVisible } =
     useChangeTemplate();
 
-    const statusThemeColors = useStatusThemeColorScheme();
+  const statusThemeColors = useStatusThemeColorScheme();
 
   return (
     <>
@@ -123,19 +123,25 @@ export default function Edit() {
                     {t('status')}
                   </span>
 
-                <div className="flex items-center space-x-2">
-                  <InvoiceStatusBadge entity={invoice} />
+                  <div className="flex items-center space-x-2">
+                    <InvoiceStatusBadge entity={invoice} />
 
-                  {invoice && invoice.sync?.dn_completed && invoice.sync?.invitations[0]?.dn_id && (
-                    <Badge variant="green" style={{ backgroundColor: statusThemeColors.$3 }}>
-                      <RouterLink
-                        className="font-medium"
-                        to={`/docuninja/${invoice.sync?.invitations[0]?.dn_id}`}
-                      >
-                        {t('signed_document')}
-                      </RouterLink>
-                    </Badge>
-                  )}
+                    {invoice &&
+                      invoice.sync?.dn_completed &&
+                      invoice.sync?.invitations[0]?.dn_id && (
+                        <Badge
+                          variant="green"
+                          style={{ backgroundColor: statusThemeColors.$3 }}
+                        >
+                          <RouterLink
+                            className="font-medium"
+                            to={`/docuninja/${invoice.sync?.invitations[0]?.dn_id}`}
+                          >
+                            {t('signed_document')}
+                          </RouterLink>
+                        </Badge>
+                      )}
+                  </div>
                 </div>
 
                 <TaxExemptBadge
@@ -144,20 +150,24 @@ export default function Edit() {
               </div>
             )}
 
-            {invoice && invoice.sync?.dn_completed && invoice.sync?.invitations[0]?.dn_id && (
-              <div className="flex items-center space-x-9">
-                <span
-                  className="text-sm font-medium"
-                  style={{ color: colors.$22 }}
-                >
-                  {t('signed_document')}
-                </span>
+            {invoice &&
+              invoice.sync?.dn_completed &&
+              invoice.sync?.invitations[0]?.dn_id && (
+                <div className="flex items-center space-x-9">
+                  <span
+                    className="text-sm font-medium"
+                    style={{ color: colors.$22 }}
+                  >
+                    {t('signed_document')}
+                  </span>
 
-                <Link to={`/docuninja/${invoice.sync?.invitations[0]?.dn_id}`}>
-                  {t('link')}
-                </Link>
-              </div>
-            )}
+                  <Link
+                    to={`/docuninja/${invoice.sync?.invitations[0]?.dn_id}`}
+                  >
+                    {t('link')}
+                  </Link>
+                </div>
+              )}
 
             <Assigned
               entityId={invoice?.project_id}
