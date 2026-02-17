d7829747c13848b0ef08825a1a4af84ce4fbe5d8
Benjamin Beganović <k1pstabug@gmail.com>
2026-02-17 16:46:41 +0100

    Hide template selection from wizard

--- src/pages/documents/pages/blueprints/create/components/BlueprintWizard.tsx
+++ src/pages/documents/pages/blueprints/create/components/BlueprintWizard.tsx
@@ -130,13 +130,13 @@ function SelectionStep({
       icon: <Icon element={Tool} size={32} />,
       onClick: onSelectCustom,
     },
-    {
-      id: 'template',
-      title: t('templates'),
-      description: t('new_template_description'),
-      icon: <Icon element={Clipboard} size={32} />,
-      onClick: onSelectTemplate,
-    },
+    // {
+    //   id: 'template',
+    //   title: t('templates'),
+    //   description: t('new_template_description'),
+    //   icon: <Icon element={Clipboard} size={32} />,
+    //   onClick: onSelectTemplate,
+    // },
   ];
 
   return (
@@ -147,7 +147,7 @@ function SelectionStep({
         </h2>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-2 gap-4">
         {options.map((option) => (
           <button
             type="button"
