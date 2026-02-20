b2718d4925f577477942a6f2c0156182cb032856
Benjamin Beganović <k1pstabug@gmail.com>
2026-02-20 12:26:47 +0100

    Navigate away if document not completed

--- src/pages/documents/pdf/Pdf.tsx
+++ src/pages/documents/pdf/Pdf.tsx
@@ -13,15 +13,16 @@ import { Default } from '$app/components/layouts/Default';
 import { Spinner } from '$app/components/Spinner';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { InvoiceViewer } from '$app/pages/invoices/common/components/InvoiceViewer';
 import { Actions } from './components/Actions';
 import { Page } from '$app/components/Breadcrumbs';
 import { route } from '$app/common/helpers/route';
-import { Document } from '$app/common/interfaces/docuninja/api';
+import { Document, DocumentStatus } from '$app/common/interfaces/docuninja/api';
 
 export default function Pdf() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const { data } = useDocumentQuery({ id, enabled: Boolean(id) });
 
   const [t] = useTranslation();
@@ -33,11 +34,16 @@ export default function Pdf() {
     if (data) {
       setDocument(data);
 
+      if (data.status_id !== DocumentStatus.Completed) {
+        navigate(route('/docuninja/:id', { id }));
+        return;
+      }
+
       if (data.files && data.files.length > 0 && data.files[0].url) {
         setPdfUrl(data.files[0].url);
       }
     }
-  }, [data]);
+  }, [data, navigate, id]);
 
   const onLink = (url: string) => setBlobUrl(url);
 
