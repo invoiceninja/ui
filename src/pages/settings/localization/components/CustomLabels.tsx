32ecee22bbe26f6b7b2417a4514c90580264e328
Civolilah <abedinhalilovic12345@gmail.com>
2026-02-24 20:49:42 +0100

    Increased timeout value

--- src/pages/settings/localization/components/CustomLabels.tsx
+++ src/pages/settings/localization/components/CustomLabels.tsx
@@ -188,7 +188,7 @@ export function CustomLabels() {
         <Button
           behavior="button"
           type="secondary"
-          onClick={() => setTimeout(() => setIsCustomModalOpen(true), 100)}
+          onClick={() => setTimeout(() => setIsCustomModalOpen(true), 250)}
         >
           {t('add_custom')}
         </Button>
