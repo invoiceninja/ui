9738f7214263fff3493673225714e08f96ada162
Benjamin Beganović <k1pstabug@gmail.com>
2026-03-12 17:41:28 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [react()],
   server: {
     port: 3000,
   },
