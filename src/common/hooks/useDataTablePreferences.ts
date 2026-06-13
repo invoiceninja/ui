09e1ff907f2aa9f33f0a33eb2f9dddb1ef103e44
Civolilah <abedinhalilovic12345@gmail.com>
2026-06-13 16:05:14 +0200

    Fixed triggering preference endpoint

--- src/common/hooks/useDataTablePreferences.ts
+++ src/common/hooks/useDataTablePreferences.ts
@@ -95,7 +95,6 @@ export function useDataTablePreferences(params: Params) {
       sort: apiEndpoint.searchParams.get('sort') || 'id|asc',
       status: ['active'],
       ...(!withoutStoringPerPage && { perPage: '10' }),
-      ...(!withoutStoringPage && { currentPage: 1 }),
     };
 
     const cleanedUpFilters = {
@@ -104,7 +103,6 @@ export function useDataTablePreferences(params: Params) {
       sort,
       status,
       ...(!withoutStoringPerPage && { perPage }),
-      ...(!withoutStoringPage && { currentPage }),
     };
 
     if (isEqual(defaultFilters, cleanedUpFilters) && !currentTableFilters) {
