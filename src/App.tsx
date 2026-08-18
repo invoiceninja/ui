ec45e27507d48bf4d217be3aaef8e4591e473eb5
David Bomba <turbo124@gmail.com>
2026-08-18 13:38:29 +1000

    Audit used packages

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,6 @@ import {
 import { routes } from './common/routes';
 import { RootState } from './common/stores/store';
 import { antdLocaleAtom } from './components/DropdownDateRangePicker';
-import { dayJSLocaleAtom } from './components/forms';
 import { PreventNavigationModal } from './components/PreventNavigationModal';
 import { CompanyEdit } from './pages/settings/company/edit/CompanyEdit';
 
@@ -99,7 +98,6 @@ export function App() {
   const setRefreshEntityDataBanner = useSetAtom(refreshEntityDataBannerAtom);
 
   const updateAntdLocale = useSetAtom(antdLocaleAtom);
-  const updateDayJSLocale = useSetAtom(dayJSLocaleAtom);
 
   const { isCompanySettingsActive, isGroupSettingsActive } =
     useCurrentSettingsLevel();
@@ -145,7 +143,6 @@ export function App() {
   useEffect(() => {
     if (resolvedLanguage?.locale) {
       resolveDayJSLocale(resolvedLanguage.locale).then((resolvedLocale) => {
-        updateDayJSLocale(resolvedLocale);
         dayjs.locale(resolvedLocale);
       });
 
