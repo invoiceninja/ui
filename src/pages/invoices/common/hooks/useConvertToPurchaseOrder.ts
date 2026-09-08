09ff17ed664031c5fd2ac9912af9c93b5305123d
Civolilah <abedinhalilovic12345@gmail.com>
2026-09-08 17:19:12 +0200

    Cleanup

--- src/pages/invoices/common/hooks/useConvertToPurchaseOrder.ts
+++ src/pages/invoices/common/hooks/useConvertToPurchaseOrder.ts
@@ -8,13 +8,14 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { AxiosResponse } from 'axios';
 import { useSetAtom } from 'jotai';
 import { useNavigate } from 'react-router-dom';
 import { v4 } from 'uuid';
 import { endpoint } from '$app/common/helpers';
 import { request } from '$app/common/helpers/request';
 import { toast } from '$app/common/helpers/toast/toast';
-import { GenericSingleResourceResponse } from '$app/common/interfaces/generic-api-response';
+import { GenericManyResponse } from '$app/common/interfaces/generic-many-response';
 import { PurchaseOrder } from '$app/common/interfaces/purchase-order';
 import { purchaseOrderAtom } from '$app/pages/purchase-orders/common/atoms';
 
@@ -29,8 +30,8 @@ export function useConvertToPurchaseOrder() {
     request('POST', endpoint('/api/v1/invoices/bulk'), {
       action: 'convert_to_purchase_order',
       ids,
-    }).then((response: GenericSingleResourceResponse<PurchaseOrder>) => {
-      const purchaseOrder = response.data.data;
+    }).then((response: AxiosResponse<GenericManyResponse<PurchaseOrder>>) => {
+      const purchaseOrder = response.data.data[0];
 
       purchaseOrder.line_items.forEach((lineItem) => (lineItem._id = v4()));
 
