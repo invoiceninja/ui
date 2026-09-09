f05d97f2581eb525e504d60601a5ff28a58aa4c3
Civolilah <abedinhalilovic12345@gmail.com>
2026-09-09 21:05:30 +0200

    Cleanup

--- src/pages/purchase-orders/create/Create.tsx
+++ src/pages/purchase-orders/create/Create.tsx
@@ -128,7 +128,8 @@ export default function Create() {
       if (
         typeof data !== 'undefined' &&
         typeof value === 'undefined' &&
-        searchParams.get('action') !== 'clone'
+        searchParams.get('action') !== 'clone' &&
+        searchParams.get('action') !== 'convert_to_purchase_order'
       ) {
         const po = cloneDeep(data);
 
@@ -157,7 +158,10 @@ export default function Create() {
     });
 
     return () => {
-      if (searchParams.get('action') !== 'clone') {
+      if (
+        searchParams.get('action') !== 'clone' &&
+        searchParams.get('action') !== 'convert_to_purchase_order'
+      ) {
         setPurchaseOrder(undefined);
       }
     };
